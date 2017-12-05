6344a3ba6d0beaaa5cccc0074c421ecfb3cd25d7
Aviv Eyal <aviveyal07@gmail.com>
2017-12-05 12:23:35 +0200

    Add app test

--- app/tests/app_test.go
+++ app/tests/app_test.go
@@ -8,14 +8,13 @@ import (
 )
 
 func TestApp(t *testing.T) {
+
 	go app.Main("", "master","")
 
 	assert.NotNil(t,app.App)
 
 	// let node warmup
 	time.Sleep(3 * time.Second)
-
 	assert.NotNil(t,app.App.Node)
-
 	app.ExitApp <- true
 }
