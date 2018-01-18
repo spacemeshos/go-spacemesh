c7b5cea583b1b7c898625abeac55c1d84516af43
Aviv Eyal <aviveyal07@gmail.com>
2018-01-18 23:36:38 +0200

    Try longer timeout

--- api/api_test.go
+++ api/api_test.go
@@ -83,7 +83,7 @@ func TestJsonApi(t *testing.T) {
 		return
 	}
 
-	// Without this running this on Travis CI may generate a connection refused error
+	// Without this running this on Travis CI might generate a connection refused error
 	time.Sleep(10 * time.Second)
 
 	url := fmt.Sprintf("http://127.0.0.1:%d/v1/example/echo", config.ConfigValues.JsonServerPort)
