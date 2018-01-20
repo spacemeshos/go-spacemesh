0ca3644a89c3d15431d7ccfedae5a3744a1f4911
Aviv Eyal <aviveyal07@gmail.com>
2018-01-20 18:12:05 +0200

    Basic post table

--- post/table.go
+++ post/table.go
@@ -1,14 +1,16 @@
 package post
 
 const (
-	S = 65536 // 137438953472 // number of entries (factor 1) - must be 2^n for some n
-	E = 8     // bits - entry size
+	s = 65536 // 137438953472 // number of entries (factor 1) - must be 2^n for some n
+	e = 8     // bits - entry size
 )
 
 type Table interface {
-	read(off int64, out []byte) error   // read len(out) bytes at offset off from the table
-	write(off int64, data []byte) error // write data at offset off
-	deleteAllData() error               // delete all existing data from the table
+	read(off int64, out []byte) error // read len(out) bytes at offset off from the table
+	seek(off int64) error
+	write(data []byte) error // write data at current offset
+	sync() error
+	deleteAllData() error // delete all existing data from the table
 }
 
 type tableImpl struct {
@@ -24,8 +26,8 @@ type tableImpl struct {
 // dir: node data dir directory
 // mul: table size factor, e.g. 2 for x2 of min table size of S*E
 func NewTable(mul int, id string, dir string) (Table, error) {
-	size := S * int64(mul)
-	entries := size / E
+	size := s * int64(mul)
+	entries := size / e
 	dataFile := newDataFile(dir, id)
 
 	// for now always delete the file if it exists and create a new one
@@ -55,10 +57,18 @@ func (d *tableImpl) deleteAllData() error {
 	return d.dataFile.create()
 }
 
-func (d *tableImpl) write(off int64, data []byte) error {
-	return d.write(off, data)
+func (d *tableImpl) write(data []byte) error {
+	return d.dataFile.write(data)
+}
+
+func (d *tableImpl) seek(off int64) error {
+	return d.dataFile.seek(off)
+}
+
+func (d *tableImpl) sync() error {
+	return d.dataFile.sync()
 }
 
 func (d *tableImpl) read(off int64, out []byte) error {
-	return d.read(off, out)
+	return d.dataFile.read(off, out)
 }
--- post/table_test.go
+++ post/table_test.go
@@ -1,1 +1,32 @@
 package post
+
+import (
+	"github.com/spacemeshos/go-spacemesh/assert"
+	"github.com/spacemeshos/go-spacemesh/crypto"
+	"github.com/spacemeshos/go-spacemesh/p2p"
+	"testing"
+)
+
+func TestPingProtocol(t *testing.T) {
+
+	node, _ := p2p.GenerateTestNode(t)
+
+	dir, err := node.EnsureNodeDataDirectory()
+	assert.NoErr(t, err, "expected node data dir")
+
+	table, err := NewTable(1, node.String(), dir)
+	assert.NoErr(t, err, "expected no error")
+
+	err = table.deleteAllData()
+	assert.NoErr(t, err, "failed to delete table data")
+
+	//data := make([]byte, 1)
+
+	for i := 0; i < 65536; i++ {
+		data, _ := crypto.GetRandomBytes(1)
+		table.write(data)
+	}
+
+	table.sync()
+
+}
