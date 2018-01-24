9b1273aaf41b7b61fb1178a714ee4cf7a2fa52ea
Aviv Eyal <aviveyal07@gmail.com>
2018-01-24 20:14:28 +0200

    fixed typo

--- assert/assert_test.go
+++ assert/assert_test.go
@@ -10,12 +10,12 @@ func TestAsserts(t *testing.T) {
 	NotNil(t, "foo'", "expected not nil")
 	True(t, true, "expected true")
 	False(t, false, "expected true")
-	Equal(t, 1,1, "exected equal")
-	Equal(t, "foo","foo", "exected equal")
+	Equal(t, 1, 1, "expected equal")
+	Equal(t, "foo", "foo", "expected equal")
 
 	var err error
 	NoErr(t, err, "expected no error")
 
 	err = errors.New("An error")
-	Err (t, err, "exected an error")
+	Err(t, err, "exected an error")
 }
