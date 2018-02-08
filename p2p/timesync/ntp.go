695ba038cfc13c1101a5172c5afc3ee12cf4bb2f
y0sher <lyosher@gmail.com>
2018-02-08 15:39:58 +0200

    fixed some concurrency issues added different ntp servers

--- p2p/timesync/ntp.go
+++ p2p/timesync/ntp.go
@@ -3,7 +3,9 @@ package timesync
 import (
 	"encoding/binary"
 	"errors"
+	"fmt"
 	"github.com/spacemeshos/go-spacemesh/log"
+	"math/rand"
 	"net"
 	"time"
 )
@@ -18,12 +20,18 @@ const (
 )
 
 // Relay on more than one server
-var DEFAULT_SERVERS = []string{
-	"0.asia.pool.ntp.org",
-	"1.asia.pool.ntp.org",
-	"2.asia.pool.ntp.org",
-	"3.asia.pool.ntp.org",
-}
+var (
+	DEFAULT_SERVERS = []string{
+		"0.pool.ntp.org",
+		"1.pool.ntp.org",
+		"time1.google.com",
+		"time2.google.com",
+		"time.asia.apple.com",
+		"time.americas.apple.com",
+	}
+	zeroDuration = time.Duration(0)
+	zeroTime     = time.Time{}
+)
 
 // 48 bytes packet.
 type NtpPacket struct {
@@ -42,49 +50,51 @@ type NtpPacket struct {
 	RxTimeFrac     uint32 // receive time frac3
 	TxTimeSec      uint32 // transmit time secs
 	TxTimeFrac     uint32 // transmit time frac
+
 }
 
+// Time makes a Time struct from NtpPacket data
 func (n *NtpPacket) Time() time.Time {
 	secs := float64(n.TxTimeSec) - NTP_OFFSET
 	nanos := (int64(n.TxTimeFrac) * 1e9) >> 32
 	return time.Unix(int64(secs), nanos)
 }
 
-// ntpRequest requests a Ntp packet from a server and returns a NtpPacket struct
-func ntpRequest(server string, rq *NtpPacket) (*NtpPacket, error) {
-
+// ntpRequest requests a Ntp packet from a server and  request time, latency and a NtpPacket struct
+func ntpRequest(server string, rq *NtpPacket) (time.Time, time.Duration, *NtpPacket, error) {
 	addr, err := net.ResolveUDPAddr("udp", net.JoinHostPort(server, DEFAULT_NTP_PORT))
 	if err != nil {
-		return nil, err
+		return zeroTime, zeroDuration, nil, err
 	}
 
 	conn, err := net.DialUDP("udp", nil, addr)
 	if err != nil {
 		log.Error("failed to connect:", err)
-		return nil, err
+		return zeroTime, zeroDuration, nil, err
 	}
 	defer conn.Close()
 
 	if err := conn.SetDeadline(
 		time.Now().Add(DEFAULT_TIMEOUT_LATENCY)); err != nil {
 		log.Error("failed to set deadline: ", err)
-		return nil, err
+		return zeroTime, zeroDuration, nil, err
 	}
-
+	before := time.Now()
 	if err := binary.Write(conn, binary.BigEndian, rq); err != nil {
 		log.Error("failed to send request: %v", err)
-		return nil, err
+		return zeroTime, zeroDuration, nil, err
 	}
-
+	latency := time.Since(before)
 	rsp := &NtpPacket{}
 	if err := binary.Read(conn, binary.BigEndian, rsp); err != nil {
 		log.Error("failed to read server response: %v", err)
-		return nil, err
+		return zeroTime, zeroDuration, nil, err
 	}
 
-	return rsp, nil
+	return before, latency, rsp, nil
 }
 
+// ntpTimeDrift queries random servers from our list to calculate a drift average
 func ntpTimeDrift() (time.Duration, error) {
 
 	// 00 011 011 = 0x1B
@@ -93,56 +103,57 @@ func ntpTimeDrift() (time.Duration, error) {
 	// Version = 3
 	resultsChan := make(chan time.Duration)
 	errorChan := make(chan error)
-	results := []time.Duration{}
 	req := &NtpPacket{Settings: 0x1B}
 
-	// Make 3 concurrent calls to different ntp servers and calculate an average drift
-	serverIndex := 0
+	// Make 3 concurrent calls to different ntp servers
+	queriedServers := make(map[int]bool)
+	serverSeed := len(DEFAULT_SERVERS) - 1
 	for i := 0; i < NTP_QUERIES; i++ {
 		go func() {
-			if serverIndex >= len(DEFAULT_SERVERS) || serverIndex < 0 {
-				serverIndex = 0
+			rndsrv := rand.Intn(serverSeed)
+			for queriedServers[rndsrv] {
+				rndsrv = rand.Intn(serverSeed)
 			}
-			server := DEFAULT_SERVERS[serverIndex]
-			serverIndex++
+			queriedServers[rndsrv] = true
 
-			before := time.Now()
-			rsp, err := ntpRequest(server, req)
+			rt, lat, rsp, err := ntpRequest(DEFAULT_SERVERS[rndsrv], req)
 			if err != nil {
-				log.Error("Failed to query NTP ", err)
 				errorChan <- err
+				return
 			}
-			latency := time.Since(before)
-			drift := before.UTC().Sub(rsp.Time().UTC().Add(latency / 2))
+			// Calculate drift with latency
+			drift := rt.UTC().Sub(rsp.Time().UTC().Add(lat / 2))
 			resultsChan <- drift
 		}()
 	}
 
 	all := time.Duration(0)
-	select {
-	case err := <-errorChan:
-		close(errorChan)
-		return all, err
-	case result := <-resultsChan:
-		all += result
-		if len(results) == NTP_QUERIES {
-			close(resultsChan)
+	for i := 0; i < NTP_QUERIES; i++ {
+		select {
+		case err := <-errorChan:
+			close(errorChan)
+			return all, err
+		case result := <-resultsChan:
+			all += result
 		}
 	}
-
-	return time.Duration(int(all) / NTP_QUERIES), nil
+	// return an average of all collected drifts
+	// TODO: ignore extreme edge results
+	return time.Duration(all / NTP_QUERIES), nil
 }
 
-func CheckSystemClockDrift() error {
+// CheckSystemClockDrift is comparing our clock to the collected ntp data
+// return the drift and an error when drift reading failed or exceeds our preset MAX_ALLOWED_DRIFT
+func CheckSystemClockDrift() (time.Duration, error) {
+	// Read average drift form ntpTimeDrift
 	drift, err := ntpTimeDrift()
 	if err != nil {
-		return err
+		return drift, err
 	}
-
+	// Check if drift exceeds our max allowed drift
 	if drift < -MAX_ALLOWED_DRIFT || drift > MAX_ALLOWED_DRIFT {
-		return errors.New("System clock is too far from NTP servers. please synchronize your OS")
+		return drift, errors.New(fmt.Sprintf("System clock is %s away from NTP servers. please synchronize your OS", drift))
 	}
 
-	log.Info("System clock is accurate with ntp allowed drift: %v", drift)
-	return nil
+	return drift, nil
 }
