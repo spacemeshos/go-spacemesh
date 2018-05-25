ffc19dccae9b334e69cb14d7ea69e4a2eb53a46d
y0sher <lyosher@gmail.com>
2018-05-26 01:19:45 +0300

    fixed test to be async

--- p2p/swarm_test.go
+++ p2p/swarm_test.go
@@ -6,12 +6,12 @@ import (
 	"testing"
 	"time"
 
-	"sync/atomic"
-
 	"github.com/spacemeshos/go-spacemesh/assert"
 	"github.com/spacemeshos/go-spacemesh/crypto"
 	"github.com/spacemeshos/go-spacemesh/filesystem"
 	"github.com/spacemeshos/go-spacemesh/p2p/nodeconfig"
+	"sync"
+	"sync/atomic"
 )
 
 // Basic session test
@@ -25,7 +25,7 @@ func TestSessionCreation(t *testing.T) {
 	node2Local, _ := GenerateTestNode(t)
 	node1Local.GetSwarm().getHandshakeProtocol().RegisterNewSessionCallback(callback)
 	node2Local.GetSwarm().getHandshakeProtocol().RegisterNewSessionCallback(callback2)
-	node1Local.GetSwarm().ConnectTo(node2Local.GetRemoteNodeData())
+	node1Local.GetSwarm().ConnectTo(node2Local.GetRemoteNodeData(), nil)
 
 	sessions := uint32(0)
 Loop:
@@ -57,6 +57,7 @@ Loop:
 func TestMultipleSessions(t *testing.T) {
 
 	filesystem.SetupTestSpacemeshDataFolders(t, "swarm_test")
+	defer filesystem.DeleteSpacemeshDataFolders(t)
 
 	finishChan := make(chan struct{})
 	const timeout = time.Second * 10
@@ -85,7 +86,7 @@ func TestMultipleSessions(t *testing.T) {
 	for i := 0; i < count; i++ {
 		n, _ := GenerateTestNode(t) // create a node
 		nodes[i] = n
-		n.GetSwarm().ConnectTo(rn.GetRemoteNodeData()) // connect to first node
+		n.GetSwarm().ConnectTo(rn.GetRemoteNodeData(), nil) // connect to first node
 		//nodes = append(nodes, n)
 	}
 
@@ -101,8 +102,6 @@ func TestMultipleSessions(t *testing.T) {
 	for _, node := range nodes {
 		node.Shutdown()
 	}
-
-	filesystem.DeleteSpacemeshDataFolders(t)
 }
 
 func TestSimpleBootstrap(t *testing.T) {
@@ -158,7 +157,7 @@ func TestSmallBootstrap(t *testing.T) {
 	const timeout = 20 * time.Second
 	const tick = 1 * time.Second
 	const randomConnections = 5
-	const totalNodes = 10
+	const totalNodes = 30
 
 	// setup:
 	//
@@ -178,21 +177,32 @@ func TestSmallBootstrap(t *testing.T) {
 	c.SwarmConfig.RandomConnections = randomConnections
 	c.SwarmConfig.BootstrapNodes = []string{bs}
 
+	nmutex := sync.Mutex{}
 	nodes := make([]LocalNode, 0)
 
 	for i := 0; i < totalNodes; i++ {
-		n, _ := GenerateTestNodeWithConfig(t, c)
-		nodes = append(nodes, n)
+		nodechan := make(chan LocalNode)
+		go func() { nmutex.Lock(); nodes = append(nodes, <-nodechan); nmutex.Unlock() }()
+		go func() { ln, _ := GenerateTestNodeWithConfig(t, c); nodechan <- ln }()
 	}
 
 	healthCheckNodes := func() bool {
+		nmutex.Lock()
+		defer nmutex.Unlock()
+		if len(nodes) < totalNodes {
+			return false
+		}
+
 		for _, n := range nodes {
 			size := make(chan int)
 			n.GetSwarm().getRoutingTable().Size(size)
-			if <-size < randomConnections {
+			s := <-size
+			if s < randomConnections {
+				t.Logf("############ ROUTING TANLE HAS ONLY %v NODES\n\r\n\r\n\r\n", s)
 				return false
 			}
 		}
+
 		return true
 	}
 
@@ -218,7 +228,8 @@ HEALTH:
 
 // todo: fix me - this test is broken
 func TestBasicBootstrap(t *testing.T) {
-
+	filesystem.SetupTestSpacemeshDataFolders(t, "swarm_test")
+	defer filesystem.DeleteSpacemeshDataFolders(t)
 	// setup:
 	// node1 - bootstrap node
 	// node2 - connects to node1 on startup (bootstrap)
@@ -234,19 +245,24 @@ func TestBasicBootstrap(t *testing.T) {
 	c.SwarmConfig.Bootstrap = true
 	c.SwarmConfig.RandomConnections = 2
 	c.SwarmConfig.BootstrapNodes = []string{bs}
+	nchan := make(chan LocalNode)
+	nchan2 := make(chan LocalNode)
+	go func() {
+		n, _ := GenerateTestNodeWithConfig(t, c)
+		nchan <- n
+	}()
+	go func() {
+		n, _ := GenerateTestNodeWithConfig(t, c)
+		nchan2 <- n
+	}()
 
-	node2Local, _ := GenerateTestNodeWithConfig(t, c)
-	_, node3Remote := GenerateTestNodeWithConfig(t, c)
+	node2Local := <-nchan
+	node3Local := <-nchan2
 
-
-//	if !node2Local.GetSwarm().getRoutingTable().IsHealthy() && !node3l.GetSwarm().getRoutingTable().IsHealthy() {
-//		t.Error("DHTs are not formed")
-//	}
-	// ping node2 -> node 3
 	reqID := crypto.UUID()
 	callback := make(chan SendPingResp)
 	node2Local.GetPing().Register(callback)
-	node2Local.GetPing().Send("hello spacemesh", reqID, node3Remote.String())
+	node2Local.GetPing().Send("hello Spacemesh", reqID, node3Local.GetRemoteNodeData().ID())
 
 Loop:
 	for {
@@ -260,4 +276,8 @@ Loop:
 			t.Fatal("Timeout error - expected callback")
 		}
 	}
+
+	node1Local.Shutdown()
+	node2Local.Shutdown()
+	node3Local.Shutdown()
 }
