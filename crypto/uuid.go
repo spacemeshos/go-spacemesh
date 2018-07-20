42d5140395c5a7000e93b975c5b5860d2ed511b9
y0sher <lyosher@gmail.com>
2018-07-20 16:26:57 +0300

    let UUID be 16 byte array

--- crypto/uuid.go
+++ crypto/uuid.go
@@ -2,12 +2,14 @@ package crypto
 
 import "github.com/google/uuid"
 
+type UUID [16]byte
+
 // UUIDString returns a new random type-4 UUID string.
 func UUIDString() string {
 	return uuid.New().String()
 }
 
-// UUID returns a new random type-4 UUID raw bytes.
-func UUID() []byte {
-	return []byte(uuid.New().String())
+// NewUUID returns a new random type-4 UUID raw bytes.
+func NewUUID() [16]byte {
+	return uuid.New()
 }
