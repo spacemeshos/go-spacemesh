2840b069355986f74969638cf6de643add0db61d
almog <almogdepaz@gmail.com>
2018-11-19 16:28:37 +0200

    test proto sudo apt get

--- ci/install-protobuf.sh
+++ ci/install-protobuf.sh
@@ -1,5 +1,16 @@
 #!/bin/sh
-set -ex
-wget https://github.com/google/protobuf/releases/download/v3.6.0/protobuf-all-3.6.0.tar.gz
-tar -xzvf protobuf-all-3.6.0.tar.gz
-cd protobuf-3.6.0 && ./configure --prefix=/usr && make && sudo make install
+# Make sure you grab the latest version
+curl -OL https://github.com/protocolbuffers/protobuf/releases/download/v3.6.1/protoc-3.6.1-linux-x86_64.zip
+
+# Unzip
+unzip protoc-3.6.1-linux-x86_64.zip -d protoc3
+
+# Move protoc to /usr/local/bin/
+sudo mv protoc3/bin/* /usr/local/bin/
+
+# Move protoc3/include to /usr/local/include/
+sudo mv protoc3/include/* /usr/local/include/
+
+# Optional: change owner
+sudo chown [user] /usr/local/bin/protoc
+sudo chown -R [user] /usr/local/include/google
