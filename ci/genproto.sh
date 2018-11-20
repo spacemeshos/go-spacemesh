189c798fcfadfd341eca468d9806356155e9c9f5
almog <almogdepaz@gmail.com>
2018-11-20 10:08:46 +0200

    boom remove autogen file test

--- ci/genproto.sh
+++ ci/genproto.sh
@@ -5,3 +5,23 @@ while read -r p; do
   echo "Generating protobuf for $p"
   protoc -I/usr/local/include -I. -I$GOPATH/src -I$GOPATH/src/github.com/grpc-ecosystem/grpc-gateway/third_party/googleapis --go_out=plugins=grpc:. $p/*.proto
 done <<< "$res"
+
+cd api/pb/
+
+protoc -I/usr/local/include -I. \
+-I$GOPATH/src \
+-I$GOPATH/src/github.com/grpc-ecosystem/grpc-gateway/third_party/googleapis \
+--go_out=plugins=grpc:. \
+api.proto
+
+protoc -I/usr/local/include -I. \
+  -I$GOPATH/src \
+  -I$GOPATH/src/github.com/grpc-ecosystem/grpc-gateway/third_party/googleapis \
+  --grpc-gateway_out=logtostderr=true:. \
+api.proto
+
+protoc -I/usr/local/include -I. \
+  -I$GOPATH/src \
+  -I$GOPATH/src/github.com/grpc-ecosystem/grpc-gateway/third_party/googleapis \
+  --swagger_out=logtostderr=true:. \
+api.proto
