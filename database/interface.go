2f3eb44858b0d437f450f89827a1d69e76982315
anton <anton.lerner@gmail.com>
2018-11-27 14:16:24 +0200

    moved to spacemesh leveldb

--- database/interface.go
+++ database/interface.go
@@ -14,7 +14,7 @@
 // You should have received a copy of the GNU Lesser General Public License
 // along with the go-ethereum library. If not, see <http://www.gnu.org/licenses/>.
 
-package ethdb
+package database
 
 // Code using batches should try to add this much data to the batch.
 // The value was determined empirically.
