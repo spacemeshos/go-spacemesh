f4bd7e901954f0992b3bee69a86c8614cbc4574a
almog <almogdepaz@gmail.com>
2018-12-13 17:40:38 +0200

    fix test

--- sync/sync_test.go
+++ sync/sync_test.go
@@ -25,7 +25,8 @@ func (BlockValidatorMock) ValidateBlock(block *mesh.Block) bool {
 func getMesh(newBlockCh chan *mesh.Block, id string) mesh.Mesh {
 	bdb := database.NewLevelDbStore("blocks_test_"+id, nil, nil)
 	ldb := database.NewLevelDbStore("layers_test_"+id, nil, nil)
-	layers := mesh.NewMesh(ldb, bdb)
+	cv := database.NewLevelDbStore("contextualy_valid_test_"+id, nil, nil)
+	layers := mesh.NewMesh(ldb, bdb, cv)
 	return layers
 }
 
