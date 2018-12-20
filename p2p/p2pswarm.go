25e750ea63201423c4af8408815efd7ff7cee787
y0sher <lyosher@gmail.com>
2018-12-20 17:04:47 +0200

    make it that outer pkgs can also use before/after

--- p2p/p2pswarm.go
+++ p2p/p2pswarm.go
@@ -5,6 +5,7 @@ import (
 	"fmt"
 	"github.com/spacemeshos/go-spacemesh/p2p/config"
 	"github.com/spacemeshos/go-spacemesh/p2p/node"
+	"github.com/spacemeshos/go-spacemesh/p2p/service"
 	"github.com/stretchr/testify/assert"
 	"golang.org/x/sync/errgroup"
 	"sync"
@@ -23,8 +24,8 @@ func createP2pInstance(t testing.TB, config config.Config) *swarm {
 }
 
 type P2PSwarm struct {
-	before func(s *swarm)
-	after  func(s *swarm)
+	before func(s service.Service)
+	after  func(s service.Service)
 
 	boot  []*swarm
 	Swarm []*swarm
@@ -37,7 +38,7 @@ func testLog(text string, args ...interface{}) {
 	fmt.Println("################################################################################################")
 }
 
-func NewP2PSwarm(before func(s *swarm), after func(s *swarm)) *P2PSwarm {
+func NewP2PSwarm(before func(s service.Service), after func(s service.Service)) *P2PSwarm {
 	p2ps := new(P2PSwarm)
 	p2ps.before = before
 	p2ps.after = after
