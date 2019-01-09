695ca2f0607027e9b39a1e584dba8d921ad93c89
Ivan Kuznetsov <me@jeiwan.ru>
2019-01-09 15:14:46 +0700

    Move peers.go from 'sync' pacage to 'p2p'

--- p2p/peers.go
+++ p2p/peers.go
@@ -1,10 +1,11 @@
-package sync
+package p2p
 
 import (
+	"sync/atomic"
+
 	"github.com/spacemeshos/go-spacemesh/crypto"
 	"github.com/spacemeshos/go-spacemesh/log"
 	"github.com/spacemeshos/go-spacemesh/p2p/service"
-	"sync/atomic"
 )
 
 type Peer crypto.PublicKey
@@ -15,32 +16,32 @@ type Peers interface {
 }
 
 type PeersImpl struct {
-	snapshot *atomic.Value
-	exit     chan struct{}
+	Snapshot *atomic.Value
+	Exit     chan struct{}
 }
 
 func NewPeers(s service.Service) Peers {
 	value := atomic.Value{}
 	value.Store(make([]Peer, 0, 20))
-	pi := &PeersImpl{snapshot: &value, exit: make(chan struct{})}
+	pi := &PeersImpl{Snapshot: &value, Exit: make(chan struct{})}
 	newPeerC, expiredPeerC := s.SubscribePeerEvents()
 	go pi.listenToPeers(newPeerC, expiredPeerC)
 	return pi
 }
 
 func (pi PeersImpl) Close() {
-	close(pi.exit)
+	close(pi.Exit)
 }
 
 func (pi PeersImpl) GetPeers() []Peer {
-	return pi.snapshot.Load().([]Peer)
+	return pi.Snapshot.Load().([]Peer)
 }
 
 func (pi *PeersImpl) listenToPeers(newPeerC chan crypto.PublicKey, expiredPeerC chan crypto.PublicKey) {
 	peerSet := make(map[Peer]bool) //set of uniq peers
 	for {
 		select {
-		case <-pi.exit:
+		case <-pi.Exit:
 			log.Debug("run stoped")
 			return
 		case peer := <-newPeerC:
@@ -52,6 +53,6 @@ func (pi *PeersImpl) listenToPeers(newPeerC chan crypto.PublicKey, expiredPeerC 
 		for k := range peerSet {
 			keys = append(keys, k)
 		}
-		pi.snapshot.Store(keys) //swap snapshot
+		pi.Snapshot.Store(keys) //swap snapshot
 	}
 }
