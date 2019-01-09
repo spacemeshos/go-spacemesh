daa423554bfea450f101c86c0c7e9a53c7ad85f1
Yongfeng LI <wliyongfeng@gmail.com>
2019-01-09 22:23:45 +0800

    add more test cases for proposaltracker_test.go

--- hare/proposaltracker_test.go
+++ hare/proposaltracker_test.go
@@ -15,6 +15,13 @@ func BuildProposalMsg(pubKey crypto.PublicKey, s *Set) *pb.HareMessage {
 	return builder.Build()
 }
 
+func BuildProposalMsgWithRoleProof(pubKey crypto.PublicKey, s *Set, signature Signature) *pb.HareMessage {
+	msg := BuildProposalMsg(pubKey, s)
+	msg.Message.RoleProof = signature
+
+	return msg
+}
+
 func TestProposalTracker_OnProposalConflict(t *testing.T) {
 	s := NewEmptySet(lowDefaultSize)
 	s.Add(value1)
@@ -31,6 +38,56 @@ func TestProposalTracker_OnProposalConflict(t *testing.T) {
 	assert.True(t, tracker.IsConflicting())
 }
 
+func TestProposalTracker_OnProposal_IgnoreMsgWithLowerRankedRoleProof(t *testing.T) {
+	s := NewEmptySet(lowDefaultSize)
+	s.Add(value1)
+
+	pubKey := generatePubKey(t)
+	m1 := BuildProposalMsgWithRoleProof(pubKey, s, []byte{1})
+
+	tracker := NewProposalTracker(lowThresh10)
+	tracker.OnProposal(m1)
+
+	s.Add(value2)
+	newPubKey := generatePubKey(t)
+	m2 := BuildProposalMsgWithRoleProof(newPubKey, s, []byte{2})
+	tracker.OnProposal(m2)
+	assert.NotEqual(t, tracker.proposal, m2)
+}
+
+func TestProposalTracker_OnLateProposal(t *testing.T) {
+	s := NewEmptySet(lowDefaultSize)
+	s.Add(value1)
+
+	pubKey := generatePubKey(t)
+	m1 := BuildProposalMsg(pubKey, s)
+
+	tracker := NewProposalTracker(lowThresh10)
+	tracker.OnProposal(m1)
+
+	s.Add(value2)
+	m2 := BuildProposalMsg(pubKey, s)
+	tracker.OnLateProposal(m2)
+	assert.True(t, tracker.IsConflicting())
+}
+
+func TestProposalTracker_TestProposalTracker_OnLateProposal_IgnoreMsgWithHigherRankedRoleProof(t *testing.T) {
+	s := NewEmptySet(lowDefaultSize)
+	s.Add(value1)
+
+	pubKey := generatePubKey(t)
+	m1 := BuildProposalMsgWithRoleProof(pubKey, s, []byte{2})
+
+	tracker := NewProposalTracker(lowThresh10)
+	tracker.OnProposal(m1)
+
+	s.Add(value2)
+	newPubKey := generatePubKey(t)
+	m2 := BuildProposalMsgWithRoleProof(newPubKey, s, []byte{1})
+	tracker.OnLateProposal(m2)
+	assert.NotEqual(t, tracker.proposal, m2)
+}
+
 func TestProposalTracker_IsConflicting(t *testing.T) {
 	s := NewEmptySet(lowDefaultSize)
 	s.Add(value1)
@@ -40,4 +97,25 @@ func TestProposalTracker_IsConflicting(t *testing.T) {
 		tracker.OnProposal(BuildProposalMsg(generatePubKey(t), s))
 		assert.False(t, tracker.IsConflicting())
 	}
+}
+
+func TestProposalTracker_ProposedSet(t *testing.T) {
+	tracker := NewProposalTracker(lowThresh10)
+	expectNil := tracker.ProposedSet()
+	assert.Nil(t, expectNil)
+
+	s := NewEmptySet(lowDefaultSize)
+	s.Add(value1)
+	s.Add(value2)
+	pubKey := generatePubKey(t)
+
+	m1 := BuildProposalMsg(pubKey, s)
+	tracker.OnProposal(m1)
+	proposedSet := tracker.ProposedSet()
+	assert.True(t, proposedSet.Equals(s));
+	s.Add(value3)
+	m2 := BuildProposalMsg(pubKey, s)
+	tracker.OnProposal(m2) // isConflicting will be true
+	proposedSetWhenConflicting := tracker.ProposedSet()
+	assert.Nil(t, proposedSetWhenConflicting)
 }
