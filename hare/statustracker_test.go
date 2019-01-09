f474112e6c1189eb869e8d8bb8bb235471e04828
Yongfeng LI <wliyongfeng@gmail.com>
2019-01-10 00:29:15 +0800

    add mote test cases for statustracker_test.go

--- hare/statustracker_test.go
+++ hare/statustracker_test.go
@@ -30,6 +30,72 @@ func TestStatusTracker_RecordStatus(t *testing.T) {
 
 	tracker.RecordStatus(BuildPreRoundMsg(generatePubKey(t), s))
 	assert.True(t, tracker.IsSVPReady())
+
+	preStatusLen := len(tracker.statuses)
+	tracker.RecordStatus(BuildPreRoundMsg(generatePubKey(t), s))
+	assert.Equal(t, preStatusLen, len(tracker.statuses))
+}
+
+func TestStatusTracker_RecordStatus_NoSamePubKey(t *testing.T) {
+	s := NewEmptySet(lowDefaultSize)
+	s.Add(value1)
+
+	tracker := NewStatusTracker(lowThresh10, lowThresh10)
+	msg := BuildPreRoundMsg(generatePubKey(t), s)
+
+	tracker.RecordStatus(msg)
+	preStatusLen := len(tracker.statuses)
+	tracker.RecordStatus(msg)
+	assert.Equal(t, preStatusLen, len(tracker.statuses))
+}
+
+func TestStatusTracker_RecordStatus_PubkeyError(t *testing.T) {
+	s := NewEmptySet(lowDefaultSize)
+	s.Add(value1)
+	msg := BuildPreRoundMsg(generatePubKey(t), s)
+	msg.PubKey = []byte("wrong pub key")
+
+	tracker := NewStatusTracker(lowThresh10, lowThresh10)
+	tracker.RecordStatus(msg)
+
+	assert.Equal(t, len(tracker.statuses), 0)
+}
+
+func TestStatusTracker_ProposalSet(t *testing.T) {
+	s := NewEmptySet(lowDefaultSize)
+	s.Add(value1)
+
+	tracker := NewStatusTracker(lowThresh10, lowThresh10)
+	tracker.RecordStatus(BuildPreRoundMsg(generatePubKey(t), s))
+
+	proposalSet := tracker.ProposalSet(lowThresh10)
+	assert.Equal(t, proposalSet, tracker.buildUnionSet(lowThresh10))
+
+	tracker.maxKi = 1
+	assert.NotNil(t, tracker.ProposalSet(lowThresh10))
+
+	tracker.maxRawSet = nil
+	assert.Panics(t, func() {
+		tracker.ProposalSet(lowThresh10)
+	})
+}
+
+func TestStatusTracker_BuildSVP(t *testing.T) {
+	s := NewEmptySet(lowDefaultSize)
+	s.Add(value1)
+
+	tracker := NewStatusTracker(lowThresh10, lowThresh10)
+	tracker.RecordStatus(BuildStatusMsg(generatePubKey(t), s))
+
+	expectNil := tracker.BuildSVP()
+	assert.Nil(t, expectNil)
+
+	for i := 0; i < lowThresh10-1; i++ {
+		tracker.RecordStatus(BuildPreRoundMsg(generatePubKey(t), s))
+	}
+
+	expectNotNil := tracker.BuildSVP()
+	assert.NotNil(t, expectNotNil)
 }
 
 func TestStatusTracker_BuildUnionSet(t *testing.T) {
