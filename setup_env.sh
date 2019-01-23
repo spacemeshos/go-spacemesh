c745abd9804a78a8bab93210d88c8880412669a9
Noam Nelke <noam@spacemesh.io>
2019-01-23 14:28:22 +0200

    check go ver and install deps ahead of relying on them

--- setup_env.sh
+++ setup_env.sh
@@ -1,6 +1,14 @@
 #!/bin/bash -e
+# Ensure we use Go version 1.11+
+read major minor patch <<< $(go version | sed 's/go version go\([0-9]*\)\.\([0-9]*\)\.\(.*\) .*/\1 \2/')
+if [[ ${major} -ne 1 || ${minor} -lt 11 ]]; then
+    echo "Go 1.11+ is required (v$major.$minor.$patch is installed at `which go`)"
+    exit 1;
+fi
+
 ./scripts/install-protobuf.sh
 
+go mod download
 protobuf_path=$(go list -m -f '{{.Dir}}' github.com/golang/protobuf)
 echo "installing protoc-gen-go..."
 go install $protobuf_path/protoc-gen-go
