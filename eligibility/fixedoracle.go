c7aab302807869ded06ee96576ed843f6f5c584c
Noam Nelke <NoamNelke@gmail.com>
2019-02-10 11:54:41 +0200

    Test fmt (#528)
    This commit adds a test during Travis builds that ensures that `go fmt ./...` doesn't change anything.
    
    **Starting now, PRs that don't conform to `go fmt` will not be mergeable.**

--- eligibility/fixedoracle.go
+++ eligibility/fixedoracle.go
@@ -13,7 +13,6 @@ type FixedRolacle struct {
 	mapRW  sync.RWMutex
 }
 
-
 func New() *FixedRolacle {
 	rolacle := &FixedRolacle{}
 	rolacle.honest = make(map[string]struct{})
@@ -130,7 +129,7 @@ func (fo *FixedRolacle) generateEligibility(expCom int) map[string]struct{} {
 		pickUnique(expFaulty, fau, emap)
 	}
 
-	rem := expCom/2-1 - len(fo.faulty)
+	rem := expCom/2 - 1 - len(fo.faulty)
 	if rem > 0 { // need to pickUnique the remaining from honest
 		pickUnique(rem, hon, emap)
 	}
