99431d0b50b80d811eb1c974b1b6b15ff3171b7c
Yuval Zalmenson <zalmenson.yuval@gmail.com>
2019-02-13 18:28:09 +0200

    CR comments

--- app/cmd/root.go
+++ app/cmd/root.go
@@ -33,7 +33,7 @@ func init() {
 		config.OracleServer, "The oracle server url. (temporary) ")
 	RootCmd.PersistentFlags().Uint64Var(&config.OracleServerWorldId, "oracle_server_worldid",
 		config.OracleServerWorldId, "The worldid to use with the oracle server (temporary) ")
-	RootCmd.PersistentFlags().StringVar(&config.GenesisTime, "genesis_time",
+	RootCmd.PersistentFlags().StringVar(&config.GenesisTime, "genesis-time",
 		config.GenesisTime, "Time of the genesis layer in 2019-13-02T17:02:00+00:00 format")
 	RootCmd.PersistentFlags().Uint32Var(&config.LayerDurationSec, "layer-duration-sec",
 		config.LayerDurationSec, "Duration between layers in seconds")
