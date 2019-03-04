6a91bd4cda2d494915229ccbd5c0b392418e8541
almog <almogdepaz@gmail.com>
2019-03-04 12:35:48 +0200

    fix param names

--- consensus/ninja_tortoise_test.go
+++ consensus/ninja_tortoise_test.go
@@ -108,22 +108,25 @@ func PrintMemUsage() {
 
 var badblocks = 0.1
 
-func TestNinjaTortoise_S10P9(t *testing.T)    { sanity(100, 10, 10, badblocks) }
-func TestNinjaTortoise_S50P49(b *testing.T)   { sanity(100, 50, 50, badblocks) }
-func TestNinjaTortoise_S100P99(b *testing.T)  { sanity(100, 100, 100, badblocks) }
-func TestNinjaTortoise_S200P199(b *testing.T) { sanity(100, 200, 200, badblocks) }
+func TestNinjaTortoise_S10P9(t *testing.T)   { sanity(100, 10, 10, badblocks) }
+func TestNinjaTortoise_S50P49(t *testing.T)  { sanity(100, 50, 50, badblocks) }
+func TestNinjaTortoise_S100P99(t *testing.T) { sanity(100, 100, 100, badblocks) }
+func TestNinjaTortoise_S10P7(b *testing.T)   { sanity(100, 10, 7, badblocks) }
+func TestNinjaTortoise_S50P35(b *testing.T)  { sanity(100, 50, 35, badblocks) }
+func TestNinjaTortoise_S100P70(b *testing.T) { sanity(100, 100, 70, badblocks) }
 
-func TestNinjaTortoise_S10P7(b *testing.T)    { sanity(100, 10, 7, badblocks) }
-func TestNinjaTortoise_S50P35(b *testing.T)   { sanity(100, 50, 35, badblocks) }
-func TestNinjaTortoise_S100P70(b *testing.T)  { sanity(100, 100, 70, badblocks) }
-func TestNinjaTortoise_S200P140(b *testing.T) { sanity(100, 200, 140, badblocks) }
-
-func benchmarkTortoise(layers int, layerSize int, patternSize int, b *testing.B) {
-	b.ReportAllocs()
-	for i := 0; i < b.N; i++ {
-		sanity(layers, layerSize, patternSize, 0)
+func TestNinjaTortoise_S200P199(t *testing.T) {
+	if testing.Short() {
+		t.Skip()
 	}
-	PrintMemUsage()
+	sanity(100, 200, 200, badblocks)
+}
+
+func TestNinjaTortoise_S200P140(t *testing.T) {
+	if testing.Short() {
+		t.Skip()
+	}
+	sanity(100, 200, 140, badblocks)
 }
 
 //vote explicitly only for previous layer
