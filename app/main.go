efcea4fa5a304b1099b360738aabd308e26660aa
Elad Gavra <gavrad@gmail.com>
2019-03-10 18:46:11 +0200

    Removed app main

--- app/main.go
+++
@@ -1,7 +0,0 @@
-package app
-
-// Main is the entry point for the Spacemesh console app - responsible for parsing and routing cli flags and commands.
-// This is the root of all evil, called from Main.main().
-func Main() {
-
-}
