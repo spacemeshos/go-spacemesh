28c21ad3cff6f98df38ff3ce3ddb4b6b01b6b91b
Alon Peretz <alonp@rewire.to>
2019-03-16 17:21:55 +0200

    pb files new

--- sync/pb/sync.pb.go
+++ sync/pb/sync.pb.go
@@ -19,7 +19,7 @@ var _ = math.Inf
 const _ = proto.ProtoPackageIsVersion2 // please upgrade the proto package
 
 type FetchBlockReq struct {
-	Id                   uint32   `protobuf:"varint,1,opt,name=Id,proto3" json:"Id,omitempty"`
+	Id                   uint64   `protobuf:"varint,1,opt,name=Id,proto3" json:"Id,omitempty"`
 	XXX_NoUnkeyedLiteral struct{} `json:"-"`
 	XXX_unrecognized     []byte   `json:"-"`
 	XXX_sizecache        int32    `json:"-"`
@@ -29,7 +29,7 @@ func (m *FetchBlockReq) Reset()         { *m = FetchBlockReq{} }
 func (m *FetchBlockReq) String() string { return proto.CompactTextString(m) }
 func (*FetchBlockReq) ProtoMessage()    {}
 func (*FetchBlockReq) Descriptor() ([]byte, []int) {
-	return fileDescriptor_sync_b89eda05eda53572, []int{0}
+	return fileDescriptor_sync_37767203ac85219d, []int{0}
 }
 func (m *FetchBlockReq) XXX_Unmarshal(b []byte) error {
 	return xxx_messageInfo_FetchBlockReq.Unmarshal(m, b)
@@ -49,7 +49,7 @@ func (m *FetchBlockReq) XXX_DiscardUnknown() {
 
 var xxx_messageInfo_FetchBlockReq proto.InternalMessageInfo
 
-func (m *FetchBlockReq) GetId() uint32 {
+func (m *FetchBlockReq) GetId() uint64 {
 	if m != nil {
 		return m.Id
 	}
@@ -67,7 +67,7 @@ func (m *FetchBlockResp) Reset()         { *m = FetchBlockResp{} }
 func (m *FetchBlockResp) String() string { return proto.CompactTextString(m) }
 func (*FetchBlockResp) ProtoMessage()    {}
 func (*FetchBlockResp) Descriptor() ([]byte, []int) {
-	return fileDescriptor_sync_b89eda05eda53572, []int{1}
+	return fileDescriptor_sync_37767203ac85219d, []int{1}
 }
 func (m *FetchBlockResp) XXX_Unmarshal(b []byte) error {
 	return xxx_messageInfo_FetchBlockResp.Unmarshal(m, b)
@@ -95,7 +95,7 @@ func (m *FetchBlockResp) GetBlock() *Block {
 }
 
 type LayerHashReq struct {
-	Layer                uint32   `protobuf:"varint,1,opt,name=layer,proto3" json:"layer,omitempty"`
+	Layer                uint64   `protobuf:"varint,1,opt,name=layer,proto3" json:"layer,omitempty"`
 	XXX_NoUnkeyedLiteral struct{} `json:"-"`
 	XXX_unrecognized     []byte   `json:"-"`
 	XXX_sizecache        int32    `json:"-"`
@@ -105,7 +105,7 @@ func (m *LayerHashReq) Reset()         { *m = LayerHashReq{} }
 func (m *LayerHashReq) String() string { return proto.CompactTextString(m) }
 func (*LayerHashReq) ProtoMessage()    {}
 func (*LayerHashReq) Descriptor() ([]byte, []int) {
-	return fileDescriptor_sync_b89eda05eda53572, []int{2}
+	return fileDescriptor_sync_37767203ac85219d, []int{2}
 }
 func (m *LayerHashReq) XXX_Unmarshal(b []byte) error {
 	return xxx_messageInfo_LayerHashReq.Unmarshal(m, b)
@@ -125,7 +125,7 @@ func (m *LayerHashReq) XXX_DiscardUnknown() {
 
 var xxx_messageInfo_LayerHashReq proto.InternalMessageInfo
 
-func (m *LayerHashReq) GetLayer() uint32 {
+func (m *LayerHashReq) GetLayer() uint64 {
 	if m != nil {
 		return m.Layer
 	}
@@ -143,7 +143,7 @@ func (m *LayerHashResp) Reset()         { *m = LayerHashResp{} }
 func (m *LayerHashResp) String() string { return proto.CompactTextString(m) }
 func (*LayerHashResp) ProtoMessage()    {}
 func (*LayerHashResp) Descriptor() ([]byte, []int) {
-	return fileDescriptor_sync_b89eda05eda53572, []int{3}
+	return fileDescriptor_sync_37767203ac85219d, []int{3}
 }
 func (m *LayerHashResp) XXX_Unmarshal(b []byte) error {
 	return xxx_messageInfo_LayerHashResp.Unmarshal(m, b)
@@ -171,7 +171,7 @@ func (m *LayerHashResp) GetHash() []byte {
 }
 
 type LayerIdsReq struct {
-	Layer                uint32   `protobuf:"varint,1,opt,name=layer,proto3" json:"layer,omitempty"`
+	Layer                uint64   `protobuf:"varint,1,opt,name=layer,proto3" json:"layer,omitempty"`
 	XXX_NoUnkeyedLiteral struct{} `json:"-"`
 	XXX_unrecognized     []byte   `json:"-"`
 	XXX_sizecache        int32    `json:"-"`
@@ -181,7 +181,7 @@ func (m *LayerIdsReq) Reset()         { *m = LayerIdsReq{} }
 func (m *LayerIdsReq) String() string { return proto.CompactTextString(m) }
 func (*LayerIdsReq) ProtoMessage()    {}
 func (*LayerIdsReq) Descriptor() ([]byte, []int) {
-	return fileDescriptor_sync_b89eda05eda53572, []int{4}
+	return fileDescriptor_sync_37767203ac85219d, []int{4}
 }
 func (m *LayerIdsReq) XXX_Unmarshal(b []byte) error {
 	return xxx_messageInfo_LayerIdsReq.Unmarshal(m, b)
@@ -201,7 +201,7 @@ func (m *LayerIdsReq) XXX_DiscardUnknown() {
 
 var xxx_messageInfo_LayerIdsReq proto.InternalMessageInfo
 
-func (m *LayerIdsReq) GetLayer() uint32 {
+func (m *LayerIdsReq) GetLayer() uint64 {
 	if m != nil {
 		return m.Layer
 	}
@@ -209,7 +209,7 @@ func (m *LayerIdsReq) GetLayer() uint32 {
 }
 
 type LayerIdsResp struct {
-	Ids                  []uint32 `protobuf:"varint,1,rep,packed,name=ids,proto3" json:"ids,omitempty"`
+	Ids                  []uint64 `protobuf:"varint,1,rep,packed,name=ids,proto3" json:"ids,omitempty"`
 	XXX_NoUnkeyedLiteral struct{} `json:"-"`
 	XXX_unrecognized     []byte   `json:"-"`
 	XXX_sizecache        int32    `json:"-"`
@@ -219,7 +219,7 @@ func (m *LayerIdsResp) Reset()         { *m = LayerIdsResp{} }
 func (m *LayerIdsResp) String() string { return proto.CompactTextString(m) }
 func (*LayerIdsResp) ProtoMessage()    {}
 func (*LayerIdsResp) Descriptor() ([]byte, []int) {
-	return fileDescriptor_sync_b89eda05eda53572, []int{5}
+	return fileDescriptor_sync_37767203ac85219d, []int{5}
 }
 func (m *LayerIdsResp) XXX_Unmarshal(b []byte) error {
 	return xxx_messageInfo_LayerIdsResp.Unmarshal(m, b)
@@ -239,7 +239,7 @@ func (m *LayerIdsResp) XXX_DiscardUnknown() {
 
 var xxx_messageInfo_LayerIdsResp proto.InternalMessageInfo
 
-func (m *LayerIdsResp) GetIds() []uint32 {
+func (m *LayerIdsResp) GetIds() []uint64 {
 	if m != nil {
 		return m.Ids
 	}
@@ -247,9 +247,9 @@ func (m *LayerIdsResp) GetIds() []uint32 {
 }
 
 type Block struct {
-	Id                   uint32   `protobuf:"varint,1,opt,name=Id,proto3" json:"Id,omitempty"`
-	Layer                uint32   `protobuf:"varint,2,opt,name=layer,proto3" json:"layer,omitempty"`
-	VisibleMesh          []uint32 `protobuf:"varint,3,rep,packed,name=VisibleMesh,proto3" json:"VisibleMesh,omitempty"`
+	Id                   uint64   `protobuf:"varint,1,opt,name=Id,proto3" json:"Id,omitempty"`
+	Layer                uint64   `protobuf:"varint,2,opt,name=layer,proto3" json:"layer,omitempty"`
+	VisibleMesh          []uint64 `protobuf:"varint,3,rep,packed,name=VisibleMesh,proto3" json:"VisibleMesh,omitempty"`
 	XXX_NoUnkeyedLiteral struct{} `json:"-"`
 	XXX_unrecognized     []byte   `json:"-"`
 	XXX_sizecache        int32    `json:"-"`
@@ -259,7 +259,7 @@ func (m *Block) Reset()         { *m = Block{} }
 func (m *Block) String() string { return proto.CompactTextString(m) }
 func (*Block) ProtoMessage()    {}
 func (*Block) Descriptor() ([]byte, []int) {
-	return fileDescriptor_sync_b89eda05eda53572, []int{6}
+	return fileDescriptor_sync_37767203ac85219d, []int{6}
 }
 func (m *Block) XXX_Unmarshal(b []byte) error {
 	return xxx_messageInfo_Block.Unmarshal(m, b)
@@ -279,21 +279,21 @@ func (m *Block) XXX_DiscardUnknown() {
 
 var xxx_messageInfo_Block proto.InternalMessageInfo
 
-func (m *Block) GetId() uint32 {
+func (m *Block) GetId() uint64 {
 	if m != nil {
 		return m.Id
 	}
 	return 0
 }
 
-func (m *Block) GetLayer() uint32 {
+func (m *Block) GetLayer() uint64 {
 	if m != nil {
 		return m.Layer
 	}
 	return 0
 }
 
-func (m *Block) GetVisibleMesh() []uint32 {
+func (m *Block) GetVisibleMesh() []uint64 {
 	if m != nil {
 		return m.VisibleMesh
 	}
@@ -310,23 +310,23 @@ func init() {
 	proto.RegisterType((*Block)(nil), "sync.Block")
 }
 
-func init() { proto.RegisterFile("sync/pb/sync.proto", fileDescriptor_sync_b89eda05eda53572) }
+func init() { proto.RegisterFile("sync/pb/sync.proto", fileDescriptor_sync_37767203ac85219d) }
 
-var fileDescriptor_sync_b89eda05eda53572 = []byte{
-	// 231 bytes of a gzipped FileDescriptorProto
+var fileDescriptor_sync_37767203ac85219d = []byte{
+	// 232 bytes of a gzipped FileDescriptorProto
 	0x1f, 0x8b, 0x08, 0x00, 0x00, 0x00, 0x00, 0x00, 0x02, 0xff, 0x74, 0x90, 0x3d, 0x6b, 0xc3, 0x30,
-	0x10, 0x86, 0xf1, 0x57, 0x87, 0x73, 0x1c, 0xca, 0xd1, 0x41, 0x5b, 0x55, 0xa5, 0x83, 0xa7, 0x04,
-	0x9a, 0x7f, 0x90, 0xa1, 0xd4, 0xd0, 0x52, 0xd0, 0xd0, 0xa1, 0x9b, 0x65, 0x09, 0x64, 0x6a, 0x6a,
-	0x35, 0x97, 0x25, 0xff, 0xbe, 0xe8, 0x1c, 0xa8, 0x28, 0x64, 0xd2, 0xfb, 0xea, 0x1e, 0x1e, 0x7d,
-	0x00, 0xd2, 0xf9, 0x7b, 0xd8, 0x05, 0xb3, 0x8b, 0xeb, 0x36, 0x1c, 0xe7, 0xd3, 0x8c, 0x65, 0xcc,
-	0xea, 0x1e, 0x9a, 0x67, 0x77, 0x1a, 0xfc, 0x61, 0x9a, 0x87, 0x2f, 0xed, 0x7e, 0x70, 0x0d, 0x79,
-	0x67, 0x45, 0x26, 0xb3, 0xb6, 0xd1, 0x79, 0x67, 0xd5, 0x1e, 0xd6, 0x29, 0x40, 0x01, 0x1f, 0xa0,
-	0x32, 0xb1, 0x88, 0x42, 0x66, 0x6d, 0xfd, 0x54, 0x6f, 0x59, 0xba, 0xcc, 0x97, 0x89, 0x7a, 0x84,
-	0xd5, 0x6b, 0x7f, 0x76, 0xc7, 0x97, 0x9e, 0x7c, 0x94, 0xde, 0x41, 0x35, 0xc5, 0x7e, 0xf1, 0x2e,
-	0x45, 0x6d, 0xa0, 0x49, 0x28, 0x0a, 0x88, 0x50, 0xfa, 0x9e, 0x3c, 0x53, 0x2b, 0xcd, 0x59, 0x6d,
-	0xa0, 0x66, 0xa8, 0xb3, 0x74, 0xdd, 0x24, 0x2f, 0xe7, 0x31, 0x44, 0x01, 0x6f, 0xa1, 0x18, 0x2d,
-	0x89, 0x4c, 0x16, 0x6d, 0xa3, 0x63, 0x54, 0xef, 0x50, 0xf1, 0x0d, 0xff, 0xbf, 0xef, 0x4f, 0x98,
-	0x27, 0x42, 0x94, 0x50, 0x7f, 0x8c, 0x34, 0x9a, 0xc9, 0xbd, 0x39, 0xf2, 0xa2, 0x60, 0x51, 0xba,
-	0x75, 0x28, 0x3f, 0xf3, 0x60, 0xcc, 0x0d, 0xff, 0xe5, 0xfe, 0x37, 0x00, 0x00, 0xff, 0xff, 0x8d,
-	0x27, 0x48, 0x8b, 0x61, 0x01, 0x00, 0x00,
+	0x10, 0x86, 0xb1, 0x2d, 0x77, 0x38, 0x27, 0xa1, 0x1c, 0x1d, 0xbc, 0x55, 0x55, 0x3a, 0x78, 0x4a,
+	0xa0, 0xf9, 0x07, 0x19, 0x4a, 0x0d, 0x2d, 0x05, 0x0d, 0x1d, 0xba, 0x59, 0x96, 0x40, 0xa6, 0xa6,
+	0x56, 0x73, 0x59, 0xf2, 0xef, 0x8b, 0xce, 0x86, 0x8a, 0x42, 0x26, 0xbd, 0xaf, 0xee, 0xe1, 0xd1,
+	0x07, 0x20, 0x5d, 0xbe, 0xfb, 0x7d, 0x30, 0xfb, 0xb8, 0xee, 0xc2, 0x69, 0x3a, 0x4f, 0x28, 0x62,
+	0x56, 0xf7, 0xb0, 0x7e, 0x76, 0xe7, 0xde, 0x1f, 0xc7, 0xa9, 0xff, 0xd2, 0xee, 0x07, 0x37, 0x90,
+	0xb7, 0xb6, 0xce, 0x64, 0xd6, 0x08, 0x9d, 0xb7, 0x56, 0x1d, 0x60, 0x93, 0x02, 0x14, 0xf0, 0x01,
+	0x4a, 0x13, 0x4b, 0x5d, 0xc8, 0xac, 0xa9, 0x9e, 0xaa, 0x1d, 0x4b, 0xe7, 0xf9, 0x3c, 0x51, 0x8f,
+	0xb0, 0x7a, 0xed, 0x2e, 0xee, 0xf4, 0xd2, 0x91, 0x8f, 0xd2, 0x3b, 0x28, 0xc7, 0xd8, 0x17, 0xef,
+	0x5c, 0xd4, 0x16, 0xd6, 0x09, 0x45, 0x01, 0x11, 0x84, 0xef, 0xc8, 0x33, 0xb5, 0xd2, 0x9c, 0xd5,
+	0x16, 0x2a, 0x86, 0x5a, 0x4b, 0xd7, 0x4d, 0x72, 0x39, 0x8f, 0x21, 0x0a, 0x78, 0x0b, 0xc5, 0x60,
+	0xa9, 0xce, 0x64, 0xd1, 0x08, 0x1d, 0xa3, 0x7a, 0x87, 0x92, 0x6f, 0xf8, 0xff, 0x7d, 0x7f, 0xc2,
+	0x3c, 0x11, 0xa2, 0x84, 0xea, 0x63, 0xa0, 0xc1, 0x8c, 0xee, 0xcd, 0x91, 0xaf, 0x0b, 0x16, 0xa5,
+	0x5b, 0x47, 0xf1, 0x99, 0x07, 0x63, 0x6e, 0xf8, 0x2f, 0x0f, 0xbf, 0x01, 0x00, 0x00, 0xff, 0xff,
+	0x1f, 0x51, 0x46, 0x59, 0x61, 0x01, 0x00, 0x00,
 }
