ed5b0eb12ad541917210d4bfd006899a5d61c468
beckmani <beckmani@gmail.com>
2019-03-18 14:25:34 +0200

    Set entrypoint in config

--- tests/k8s/log-client-pods.sh
+++ tests/k8s/log-client-pods.sh
@@ -1,11 +1,13 @@
 #!/bin/bash
 set -xe
 
-pods=`kubectl get pods -n sm | grep client | awk '{print $1}'`
-deployment_id=`kubectl get pods -n sm | grep client | awk '{print $1}' | head -n1 | cut -d'-' -f2`
+NAMESPACE=$1
+
+pods=`kubectl get pods -n $NAMESPACE | grep client | awk '{print $1}'`
+deployment_id=`kubectl get pods -n $NAMESPACE | grep client | awk '{print $1}' | head -n1 | cut -d'-' -f2`
 deployment='deployment_'$deployment_id
 mkdir -p /tmp/$deployment
 for p in $pods; do
-  kubectl logs $p -n sm > /tmp/$deployment/$p.log
+  kubectl logs $p -n $NAMESPACE > /tmp/$deployment/$p.log
 done
    
