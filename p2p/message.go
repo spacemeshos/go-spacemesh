6a50382ec8842db4eef4930fa616460bacf3504d
rehs0y <lyosher@gmail.com>
2019-03-18 16:37:50 +0200

    P2p ping address  (#676)
    * change p2p cmd to debug and json log for automation reading.
    * update ping callback, add small test
    * add metadata to message
    * adapt message server and protocols to changes
    * code review fixes

--- p2p/message.go
+++ p2p/message.go
@@ -4,6 +4,7 @@ import (
 	"github.com/spacemeshos/go-spacemesh/p2p/config"
 	"github.com/spacemeshos/go-spacemesh/p2p/p2pcrypto"
 	"github.com/spacemeshos/go-spacemesh/p2p/pb"
+	"github.com/spacemeshos/go-spacemesh/p2p/service"
 	"time"
 )
 
@@ -14,5 +15,69 @@ func NewProtocolMessageMetadata(author p2pcrypto.PublicKey, protocol string) *pb
 		ClientVersion: config.ClientVersion,
 		Timestamp:     time.Now().Unix(),
 		AuthPubkey:    author.Bytes(),
+	}
+}
+
+//type p2pMetadata struct {
+//	data map[string]string // connection address
+//	// todo: anything more ?
+//}
+//
+//func (m p2pMetadata) set(k, v string) {
+//	m.data[k] = v
+//}
+//
+//// Address is the address on which a connection
+//func (m p2pMetadata) Get(k string) (string, bool) {
+//	v, ok := m.data[k]
+//	return v, ok
+//}
+//
+//func newP2PMetadata() p2pMetadata {
+//	return p2pMetadata{make(map[string]string)}
+//}
+
+type directProtocolMessage struct {
+	metadata service.P2PMetadata
+	sender   p2pcrypto.PublicKey
+	data     service.Data
+}
+
+func (pm directProtocolMessage) Metadata() service.P2PMetadata {
+	return pm.metadata
+}
+
+func (pm directProtocolMessage) Sender() p2pcrypto.PublicKey {
+	return pm.sender
+}
+
+func (pm directProtocolMessage) Data() service.Data {
+	return pm.data
+}
+
+func (pm directProtocolMessage) Bytes() []byte {
+	return pm.data.Bytes()
+}
+
+type gossipProtocolMessage struct {
+	data           service.Data
+	validationChan chan service.MessageValidation
+}
+
+func (pm gossipProtocolMessage) Data() service.Data {
+	return pm.data
+}
+
+func (pm gossipProtocolMessage) Bytes() []byte {
+	return pm.data.Bytes()
+}
+
+func (pm gossipProtocolMessage) ValidationCompletedChan() chan service.MessageValidation {
+	return pm.validationChan
+}
+
+func (pm gossipProtocolMessage) ReportValidation(protocol string, isValid bool) {
+	if pm.validationChan != nil {
+		pm.validationChan <- service.NewMessageValidation(pm.Bytes(), protocol, isValid)
 	}
 }
