3244b3d88f3a92bda1b68c22869dc3645f1e5f4c
beckmani <beckmani@gmail.com>
2019-03-26 10:17:35 +0200

    Read args from config.yml

--- tests/misc.py
+++ tests/misc.py
@@ -22,11 +22,14 @@ class ContainerSpec():
 
     REPLACEABLE_ARGS = ['randcon', 'oracle_server', 'bootnodes', 'genesis_time']
 
-    def __init__(self, cname, cimage, centry, **kwargs):
+    def __init__(self, cname, cimage, centry):
         self.name = cname
         self.image = cimage
         self.entrypoint = centry
-        self.args = kwargs
+        self.args = {}
+
+    def append_args(self, **kwargs):
+        self.args.update(kwargs)
 
     def update_deployment(self, dep):
         containers = dep['spec']['template']['spec']['containers']
@@ -37,21 +40,20 @@ class ContainerSpec():
                     c['image'] = self.image
                 if self.entrypoint:
                     c['command'] = self.entrypoint
-                c['args'] = self.update_args(c['args'], **(self.args))
+                c['args'] = self._update_args(c['args'], **(self.args))
                 break
         return dep
 
-    def update_args(self, args_input_yaml, **kwargs):
+    def _update_args(self, args_input_yaml, **kwargs):
         for k in kwargs:
-            if k not in ContainerSpec.REPLACEABLE_ARGS:
-                raise Exception("Unknown arg: {0} - parsing error")
             replaced = False
-            for i, arg in enumerate(args_input_yaml):
-                if arg[2:].replace('-', '_') == k:
-                    # replace the value
-                    args_input_yaml[i + 1] = kwargs[k]
-                    replaced = True
-                    break
+            if k in ContainerSpec.REPLACEABLE_ARGS:
+                for i, arg in enumerate(args_input_yaml):
+                    if arg[2:].replace('-', '_') == k:
+                        # replace the value
+                        args_input_yaml[i + 1] = kwargs[k]
+                        replaced = True
+                        break
             if not replaced:
                 args_input_yaml += (['--{0}'.format(k.replace('_', '-')), kwargs[k]])
         return args_input_yaml
