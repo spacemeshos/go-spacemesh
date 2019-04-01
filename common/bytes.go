01c9954c049de86dc63bc0f0c88179d875e5d692
almog <almogdepaz@gmail.com>
2019-04-01 12:01:42 +0300

    change address to 32 bytes

--- common/bytes.go
+++ common/bytes.go
@@ -14,7 +14,7 @@ const (
 	// HashLength is the expected length of the hash
 	HashLength = 32
 	// AddressLength is the expected length of the address
-	AddressLength = 20
+	AddressLength = 32
 )
 
 func BytesToUint32(i []byte) uint32 { return binary.LittleEndian.Uint32(i) }
