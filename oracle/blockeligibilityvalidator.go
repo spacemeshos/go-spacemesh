78f3207189936784fe426f903cd96ffa5029a9ca
Noam Nelke <noam@spacemesh.io>
2019-04-05 08:11:25 +0300

    extract and explain calcEligibleLayer

--- oracle/blockeligibilityvalidator.go
+++ oracle/blockeligibilityvalidator.go
@@ -1,7 +1,6 @@
 package oracle
 
 import (
-	"encoding/binary"
 	"fmt"
 	"github.com/spacemeshos/go-spacemesh/log"
 	"github.com/spacemeshos/go-spacemesh/mesh"
@@ -53,15 +52,14 @@ func (v MinerBlockEligibilityValidator) BlockEligible(layerID mesh.LayerID, node
 
 	epochBeacon := v.beaconProvider.GetBeacon(epochNumber)
 	message := serializeVRFMessage(epochBeacon, epochNumber, counter)
-	sig := proof.Sig
-	err = v.validateVRF(message, sig, []byte(nodeID.Vrf))
+	vrfSig := proof.Sig
+	err = v.validateVRF(message, vrfSig, []byte(nodeID.Vrf))
 	if err != nil {
 		log.Error("eligibility VRF validation failed: %v", err)
 		return false, err
 	}
-	hash := sha256.Sum256(sig)
-	epochOffset := epochNumber * uint64(v.layersPerEpoch)
-	eligibleLayer := mesh.LayerID(binary.LittleEndian.Uint64(hash[:])%uint64(v.layersPerEpoch) + epochOffset)
+	vrfHash := sha256.Sum256(vrfSig)
+	eligibleLayer := calcEligibleLayer(epochNumber, v.layersPerEpoch, vrfHash)
 
 	return layerID == eligibleLayer, nil
 }
