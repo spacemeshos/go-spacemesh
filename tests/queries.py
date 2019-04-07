bccbb5641c5b07c914144b6b46a09e439a40ddd0
y0sher <lyosher@gmail.com>
2019-04-07 18:17:19 +0300

    some nice automation queries changes

---
+++ tests/queries.py
@@ -0,0 +1,52 @@
+import os
+from datetime import datetime
+
+from elasticsearch import Elasticsearch
+from elasticsearch_dsl import Search, Q
+
+dt = datetime.now()
+todaydate = dt.strftime("%Y.%m.%d")
+current_index = 'kubernetes_cluster-'+todaydate
+
+def get_elastic_search_api():
+    ES_PASSWD = os.getenv("ES_PASSWD")
+    if not ES_PASSWD:
+        raise Exception("Unknown Elasticsearch password. Please check 'ES_PASSWD' environment variable")
+    es = Elasticsearch("http://es.spacemesh.io",
+                       http_auth=("spacemesh", ES_PASSWD), port=80)
+    return es
+
+
+def get_podlist(namespace, depname):
+    api = get_elastic_search_api()
+    fltr = Q("match_phrase", kubernetes__pod_name=depname) & Q("match_phrase", kubernetes__namespace_name=namespace)
+    s = Search(index=current_index, using=api).query('bool').filter(fltr)
+    hits = list(s.scan())
+    podnames = set([hit.kubernetes.pod_name for hit in hits])
+    return podnames
+
+
+def get_pod_logs(namespace, pod_name):
+    api = get_elastic_search_api()
+    fltr = Q("match_phrase", kubernetes__pod_name=pod_name) & Q("match_phrase", kubernetes__namespace_name=namespace)
+    s = Search(index=current_index, using=api).query('bool').filter(fltr).sort("time")
+    res = s.execute()
+    full = Search(index=current_index, using=api).query('bool').filter(fltr).sort("time").extra(size=res.hits.total)
+    res = full.execute()
+    hits = list(res.hits)
+    print("Writing ${0} log lines for pod {1} ".format(len(hits), pod_name))
+    f = open('./logs/' + pod_name + '.txt', 'w')
+    for i in hits:
+        f.write(i.log)
+    f.close()
+
+
+def get_podlist_logs(namespace, podlist):
+    for i in podlist:
+        get_pod_logs(namespace, i)
+
+
+def get_deployment_logs(namespace, depname):
+    lst = get_podlist(namespace, depname)
+    print("Getting pod list ", lst)
+    get_podlist_logs(namespace, lst)
