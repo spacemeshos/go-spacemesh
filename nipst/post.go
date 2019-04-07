637146595000f01bb366df1e28f3383b37f9f590
Moshe Shababo <dicentralized@gmail.com>
2019-04-07 21:29:21 +0300

    Simple PoST proving client and verifier

--- nipst/post.go
+++ nipst/post.go
@@ -1,13 +1,44 @@
 package nipst
 
-type postProof []byte
+import (
+	"github.com/spacemeshos/go-spacemesh/common"
+	"github.com/spacemeshos/post/initialization"
+	"github.com/spacemeshos/post/proving"
+	"github.com/spacemeshos/post/validation"
 
-func (p *postProof) valid() bool {
-	// TODO(moshababo): implement
-	return true
-}
+	"time"
+)
+
+type postProof proving.Proof
 
 func (p *postProof) serialize() []byte {
 	// TODO(moshababo): implement
-	return *p
+	return []byte("")
+}
+
+func verifyPost(proof *postProof, leafCount uint64, numberOfProvenLabels uint8, difficulty proving.Difficulty) (bool, error) {
+	err := validation.Validate(proving.Proof(*proof), leafCount, numberOfProvenLabels, difficulty)
+	if err != nil {
+		return false, err
+	}
+
+	return true, nil
+}
+
+type PostClient struct{}
+
+func newPostClient() *PostClient {
+	return &PostClient{}
+}
+
+func (c *PostClient) initialize(id []byte, space uint64, numberOfProvenLabels uint8, difficulty proving.Difficulty, timeout time.Duration) (commitment *postProof, err error) {
+	// TODO(moshababo): implement timeout
+	proof, err := initialization.Initialize(id, space, numberOfProvenLabels, difficulty)
+	return (*postProof)(&proof), err
+}
+
+func (c *PostClient) execute(id []byte, challenge common.Hash, numberOfProvenLabels uint8, difficulty proving.Difficulty, timeout time.Duration) (*postProof, error) {
+	// TODO(moshababo): implement timeout
+	proof, err := proving.GenerateProof(id, challenge[:], numberOfProvenLabels, difficulty)
+	return (*postProof)(&proof), err
 }
