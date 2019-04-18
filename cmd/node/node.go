cb5e9e1d701950315fbbf61d0d20140017f93ca4
Elad Gavra <gavrad@gmail.com>
2019-04-18 17:29:29 +0300

    rebase fix

--- cmd/node/node.go
+++ cmd/node/node.go
@@ -293,13 +293,13 @@ func (app *SpacemeshApp) initServices(nodeID types.NodeId, swarm service.Service
 	}
 
 	//todo: put in config
-	atxdb := activation.NewActivationDb(atxdbstore, mdb, 1000)
-	trtl := tortoise.NewAlgorithm(layerSize, mdb, lg.WithName("trtl"))
 	iddbstore, err := database.NewLDBDatabase(dbStorepath+"ids", 0, 0)
 	if err != nil {
 		return err
 	}
 	idStore := activation.NewIdentityStore(iddbstore)
+	atxdb := activation.NewActivationDb(atxdbstore, idStore, mdb, 1000)
+	trtl := tortoise.NewAlgorithm(layerSize, mdb, lg.WithName("trtl"))
 	msh := mesh.NewMesh(mdb, atxdb, app.Config.REWARD, trtl, processor, lg.WithName("mesh")) //todo: what to do with the logger?
 
 	conf := sync.Configuration{SyncInterval: 1 * time.Second, Concurrency: 4, LayerSize: int(layerSize), RequestTimeout: 100 * time.Millisecond}
