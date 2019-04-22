30400e96c4895d98fa4d7ca29bb8814487658379
y0sher <lyosher@gmail.com>
2019-04-22 13:49:30 +0300

    remove bootnodes but add if needed

--- p2p/dht/bootstrap.go
+++ p2p/dht/bootstrap.go
@@ -44,38 +44,35 @@ func (d *KadDHT) Bootstrap(ctx context.Context) error {
 		return ErrZeroConnections
 	}
 	// register bootstrap nodes
-	bn := 0
+	bn := make([]discNode, 0, len(d.config.BootstrapNodes))
 	for _, n := range d.config.BootstrapNodes {
 		nd, err := node.NewNodeFromString(n)
 		if err != nil {
 			// TODO : handle errors
 			continue
 		}
-
-		d.rt.Update(discNode{nd, nd.Address()})
-		defer d.rt.Remove(discNode{nd, nd.Address()})
-
-		bn++
+		bn = append(bn, discNode{nd, nd.Address()})
 		d.local.Info("added new bootstrap node %v", nd)
 	}
 
-	if bn == 0 {
+	if len(bn) == 0 {
 		return ErrConnectToBootNode
 	}
 
-	d.local.Debug("netLookup using %d preloaded bootnodes ", bn)
+	d.local.Debug("lookup using %d preloaded bootnodes ", bn)
 
 	err := d.tryBoot(ctx, bn, c)
-
 	return err
 }
 
-func (d *KadDHT) tryBoot(ctx context.Context, bootnodecount int, minPeers int) error {
+func (d *KadDHT) tryBoot(ctx context.Context, bootnodes []discNode, minPeers int) error {
 
 	searchFor := d.local.PublicKey()
+	servers := make([]discNode, len(bootnodes))
+	copy(servers, bootnodes)
+
 	tries := 0
-	var size int
-	d.local.Debug("BOOTSTRAP: Running kademlia lookup for ourselves")
+	size := 0
 
 loop:
 	for {
@@ -86,9 +83,13 @@ loop:
 				//TODO: consider choosing a random key that is close to the local id
 				//or TODO: implement real kademlia refreshes - #241
 				searchFor = p2pcrypto.NewRandomPubkey()
-				d.local.Debug("BOOTSTRAP: Running kademlia lookup for random peer")
+				servers = d.internalLookup(searchFor)
+				if len(servers) == 0 {
+					servers = append(servers, bootnodes...)
+				}
 			}
-			_, err := d.netLookup(searchFor)
+			d.local.Info("BOOTSTRAP: peer lookup w/ %v servers", len(servers))
+			_, err := d.kadLookup(searchFor, servers)
 			reschan <- err
 		}()
 
@@ -98,16 +99,16 @@ loop:
 		case err := <-reschan:
 			tries++
 			if err == nil {
+				d.local.Log.Warning("Found node in bootstrap lookup (%v)", searchFor.String())
 				// if we got the peer we were looking for (us or random)
 				// the best thing we can do is just try again or try another random peer.
 				// hence we continue here.
-				continue
 			}
 			req := make(chan int)
 			d.rt.Size(req)
 			size = <-req
 
-			if size+bootnodecount >= minPeers {
+			if size > 0 && size-len(bootnodes) >= minPeers {
 				break loop
 			}
 		}
@@ -122,6 +123,11 @@ loop:
 			continue loop
 		}
 
+	}
+
+	// we don't need bootstrap node anymore
+	for i := 0; i < len(bootnodes); i++ {
+		d.rt.Remove(bootnodes[i])
 	}
 
 	return nil
--- p2p/dht/dht.go
+++ p2p/dht/dht.go
@@ -133,7 +133,7 @@ func (d *KadDHT) netLookup(pubkey p2pcrypto.PublicKey) (node.Node, error) {
 func (d *KadDHT) internalLookup(key p2pcrypto.PublicKey) []discNode {
 	poc := make(PeersOpChannel)
 	dhtid := node.NewDhtID(key.Bytes())
-	d.rt.NearestPeers(NearestPeersReq{dhtid, d.config.RoutingTableAlpha, poc})
+	d.rt.NearestPeers(NearestPeersReq{dhtid, d.config.RoutingTableBucketSize, poc})
 	res := (<-poc).Peers
 	if len(res) == 0 {
 		return nil
@@ -148,7 +148,7 @@ func (d *KadDHT) internalLookup(key p2pcrypto.PublicKey) []discNode {
 // Also used as a bootstrap function to populate the routing table with the results.
 func (d *KadDHT) kadLookup(id p2pcrypto.PublicKey, searchList []discNode) (discNode, error) {
 	// save queried node ids for the operation
-	queried := make(map[string]bool)
+	queried := make(map[discNode]bool)
 
 	// iterative lookups for nodeId using searchList
 
@@ -200,7 +200,7 @@ func (d *KadDHT) kadLookup(id p2pcrypto.PublicKey, searchList []discNode) (discN
 }
 
 // filterFindNodeServers picks up to count server who haven't been queried recently.
-func filterFindNodeServers(nodes []discNode, queried map[string]bool, alpha int) []discNode {
+func filterFindNodeServers(nodes []discNode, queried map[discNode]bool, alpha int) []discNode {
 
 	// If no server have been queried already, just make sure the list len is alpha
 	if len(queried) == 0 {
@@ -215,7 +215,7 @@ func filterFindNodeServers(nodes []discNode, queried map[string]bool, alpha int)
 	// filter out queried servers.
 	i := 0
 	for _, v := range nodes {
-		if _, exist := queried[v.PublicKey().String()]; exist {
+		if _, exist := queried[v]; exist {
 			continue
 		}
 
@@ -238,7 +238,7 @@ type findNodeOpRes struct {
 // findNodeOp a target node on one or more servers
 // returns closest nodes which are closers than closestNode to targetId
 // if node found it will be in top of results list
-func (d *KadDHT) findNodeOp(servers []discNode, queried map[string]bool, id p2pcrypto.PublicKey) []discNode {
+func (d *KadDHT) findNodeOp(servers []discNode, queried map[discNode]bool, id p2pcrypto.PublicKey) []discNode {
 
 	var out []discNode
 	startTime := time.Now()
@@ -301,7 +301,7 @@ Loop:
 		case qres := <-results:
 
 			// we mark active nodes
-			queried[qres.server.String()] = qres.res != nil
+			queried[qres.server] = qres.res != nil
 
 			res := qres.res
 			for _, n := range res {
@@ -311,7 +311,7 @@ Loop:
 				}
 				idSet[n.PublicKey().String()] = struct{}{}
 
-				if _, ok := queried[n.PublicKey().String()]; ok {
+				if _, ok := queried[n]; ok {
 					continue
 				}
 				out = append(out, n)
