fe2efca12508b0b7098be4f580e01cc8adc144e8
Isaac Beckman <beckmani@gmail.com>
2019-05-27 09:52:44 +0300

    setup network by 1 fixture (#917)
    * setup network by 1 fixture
    
    * Add oracle and poet deployment info

--- tests/fixtures.py
+++ tests/fixtures.py
@@ -20,6 +20,16 @@ class DeploymentInfo():
         self.pods = []
 
 
+class NetworkDeploymentInfo():
+    def __init__(self, dep_id, oracle_deployment_info, poet_deployment_info, bs_deployment_info, cl_deployment_info):
+        self.deployment_name = ''
+        self.deployment_id = dep_id
+        self.oracle = oracle_deployment_info
+        self.poet = poet_deployment_info
+        self.bootstrap = bs_deployment_info
+        self.clients = cl_deployment_info
+
+
 @pytest.fixture(scope='session')
 def load_config():
     kube_config_var = os.getenv('KUBECONFIG', '~/.kube/config')
