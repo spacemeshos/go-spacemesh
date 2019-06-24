5d1bb3d8726153f2751b0a0745db6fbc2bd61978
almog <almogdepaz@gmail.com>
2019-06-24 15:19:44 +0300

    increase poet grpc timeout

--- cmd/node/node.go
+++ cmd/node/node.go
@@ -516,7 +516,7 @@ func (app *SpacemeshApp) Start(cmd *cobra.Command, args []string) {
 	}
 
 	log.Info("connecting to POET on IP %v", app.Config.PoETServer)
-	poet, err := nipst.NewRemoteRPCPoetClient(app.Config.PoETServer, 2*time.Second)
+	poet, err := nipst.NewRemoteRPCPoetClient(app.Config.PoETServer, 30*time.Second)
 	if err != nil {
 		log.Error("poet server not found on addr %v, err: %v", app.Config.PoETServer, err)
 		return
