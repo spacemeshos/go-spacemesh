033bbf26501884bf01b57e4ef12d600978bad5b3
almog <almogdepaz@gmail.com>
2019-06-30 18:01:57 +0300

    refactor delete validate.go

--- sync/syncer.go
+++ sync/syncer.go
@@ -30,6 +30,28 @@ type BlockValidator interface {
 	ValidateAtx(atx *types.ActivationTx) error
 }
 
+type EligibilityValidator interface {
+	BlockEligible(block *types.BlockHeader) (bool, error)
+}
+
+type TxValidator interface {
+	ValidateTx(tx *types.SerializableTransaction) (bool, error)
+}
+
+type AtxValidator interface {
+	ValidateAtx(atx *types.ActivationTx) error
+}
+
+type blockValidator struct {
+	EligibilityValidator
+	TxValidator
+	AtxValidator
+}
+
+func NewBlockValidator(bev EligibilityValidator, txv TxValidator, atxv AtxValidator) BlockValidator {
+	return &blockValidator{bev, txv, atxv}
+}
+
 type Configuration struct {
 	Concurrency    int //number of workers for sync method
 	LayerSize      int
