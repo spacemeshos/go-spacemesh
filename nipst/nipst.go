481693d05240cc934d79684ed12c5432e5072794
Noam Nelke <noam@spacemesh.io>
2019-07-03 14:37:10 +0300

    go fmt

--- nipst/nipst.go
+++ nipst/nipst.go
@@ -210,7 +210,7 @@ func (nb *NIPSTBuilder) BuildNIPST(challenge *common.Hash) (*types.NIPST, error)
 
 		membership, err := nb.poetDb.GetMembershipMap(poetProofRef)
 		if err != nil {
-			log.Panic("failed to fetch membership for PoET proof") // TODO: handle inconsistent state
+			log.Panic("failed to fetch membership for PoET proof")              // TODO: handle inconsistent state
 			return nil, fmt.Errorf("failed to fetch membership for PoET proof") // inconsistent state
 		}
 		if !membership[*nipst.NipstChallenge] {
