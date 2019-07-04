b6666d1180178169c8b99eaa43322563db9045e5
Isaac Beckman <isaac@spacemesh.io>
2019-07-04 15:11:29 +0300

    Oracle remove (#1028)
    * Print deployment size to stdout
    
    * Clean oracle
    
    * Update test_bs
    
    * Finish cleaning oracle and poet except for hare tests
    
    * Handle PR comments
    
    * Rebase with develop
    
    * Improve pytest verbosity

--- tests/misc.py
+++ tests/misc.py
@@ -18,7 +18,7 @@ class CoreV1ApiClient(client.CoreV1Api):
 
 class ContainerSpec:
 
-    REPLACEABLE_ARGS = ['randcon', 'oracle_server', 'bootnodes', 'genesis_time']
+    REPLACEABLE_ARGS = ['randcon', 'oracle_server', 'bootnodes', 'genesis_time', 'poet_server']
 
     def __init__(self, cname, cimage, centry):
         self.name = cname
