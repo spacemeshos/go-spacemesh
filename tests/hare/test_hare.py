bf7f981b047663b1a970ff8f4ca10d160330a555
Elad Gavra <gavrad@gmail.com>
2019-07-11 18:11:03 +0300

    mem usage and fix test bs

--- tests/hare/test_hare.py
+++ tests/hare/test_hare.py
@@ -4,13 +4,22 @@ import pytest
 from pytest_testconfig import config as testconfig
 
 from tests.fixtures import DeploymentInfo
-from tests.hare.assert_hare import assert_all
-from tests.test_bs import current_index, setup_bootstrap_in_namespace, setup_clients_in_namespace
+from tests.fixtures import init_session, load_config, set_namespace, session_id, set_docker_images
+from tests.hare.assert_hare import expect_hare, assert_all, get_max_mem_usage
+from tests.test_bs import current_index, setup_bootstrap_in_namespace, setup_clients_in_namespace, setup_server, create_configmap, wait_genesis
+
+ORACLE_DEPLOYMENT_FILE = './k8s/oracle.yml'
 
 
 # ==============================================================================
 #    Fixtures
 # ==============================================================================
+@pytest.fixture(scope='module')
+def setup_oracle(request):
+    oracle_deployment_name = 'oracle'
+    return setup_server(oracle_deployment_name, ORACLE_DEPLOYMENT_FILE, testconfig['namespace'])
+
+
 @pytest.fixture(scope='module')
 def setup_bootstrap_for_hare(request, init_session, setup_oracle, create_configmap):
     bootstrap_deployment_info = DeploymentInfo(dep_id=init_session)
@@ -37,17 +46,41 @@ def setup_clients_for_hare(request, init_session, setup_oracle, setup_bootstrap_
 # ==============================================================================
 
 
-NUM_OF_EXPECTED_ROUNDS = 5
-EFK_LOG_PROPAGATION_DELAY = 10
+EFK_LOG_PROPAGATION_DELAY = 20
 
 
+# simple sanity test run for one layer
 def test_hare_sanity(init_session, setup_bootstrap_for_hare, setup_clients_for_hare, wait_genesis):
     # Need to wait for 1 full iteration + the time it takes the logs to propagate to ES
     round_duration = int(testconfig['client']['args']['hare-round-duration-sec'])
-    layers_count = int(testconfig['client']['args']['cmd-hare-layers-count'])
-    delay = round_duration * NUM_OF_EXPECTED_ROUNDS * layers_count + \
-            EFK_LOG_PROPAGATION_DELAY + int(testconfig['client']['args']['hare-wakeup-delta'])
+    wakeup_delta = int(testconfig['client']['args']['hare-wakeup-delta'])
+    layer_duration = int(testconfig['client']['args']['layer-duration-sec'])
+    layers_count = int(1)
+    print("Number of layers is ", layers_count)
+    delay = layer_duration * layers_count + EFK_LOG_PROPAGATION_DELAY + wakeup_delta * (layers_count - 1) + round_duration
     print("Going to sleep for {0}".format(delay))
     time.sleep(delay)
 
     assert_all(current_index, testconfig['namespace'])
+
+
+EXPECTED_MAX_MEM = 50*1024*1024  # MB
+# scale test run for 3 layers
+def test_hare_scale(init_session, setup_bootstrap_for_hare, setup_clients_for_hare, wait_genesis):
+    total = int(testconfig['client']['replicas']) + int(testconfig['bootstrap']['replicas'])
+
+    # Need to wait for 1 full iteration + the time it takes the logs to propagate to ES
+    round_duration = int(testconfig['client']['args']['hare-round-duration-sec'])
+    wakeup_delta = int(testconfig['client']['args']['hare-wakeup-delta'])
+    layer_duration = int(testconfig['client']['args']['layer-duration-sec'])
+    layers_count = int(5)
+    print("Number of layers is ", layers_count)
+    delay = layer_duration * layers_count + EFK_LOG_PROPAGATION_DELAY + wakeup_delta * (layers_count - 1) + round_duration
+    print("Going to sleep for {0}".format(delay))
+    time.sleep(delay)
+
+    ns = testconfig['namespace']
+    expect_hare(current_index, ns, 1, layers_count, total)
+    max_mem = get_max_mem_usage(current_index, ns)
+    print('Mem usage is {0} expected max is {1}'.format(max_mem, EXPECTED_MAX_MEM))
+    assert max_mem < EXPECTED_MAX_MEM
