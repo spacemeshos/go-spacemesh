033a907151abd7ada83067ccdf8cfed50f2d3a58
Noam Nelke <noam@spacemesh.io>
2019-07-22 09:20:18 +0300

    format test_p2p.py

--- tests/p2p/test_p2p.py
+++ tests/p2p/test_p2p.py
@@ -1,18 +1,20 @@
-import time
 import random
 import re
+import time
+from datetime import datetime
 from random import choice
 from string import ascii_lowercase
 
-from datetime import datetime, timedelta
-from pytest_testconfig import config as testconfig
 from elasticsearch_dsl import Search, Q
+from pytest_testconfig import config as testconfig
 
-from tests.queries import ES, query_message
+# noinspection PyUnresolvedReferences
 from tests.fixtures import init_session, load_config, set_namespace, session_id, set_docker_images
-from tests.test_bs import setup_bootstrap, create_configmap, setup_clients
-from tests.test_bs import save_log_on_exit, add_client, api_call, add_curl
+from tests.queries import ES, query_message
 from tests.test_bs import add_single_client, add_multi_clients, get_conf
+from tests.test_bs import api_call
+# noinspection PyUnresolvedReferences
+from tests.test_bs import setup_bootstrap, create_configmap, setup_clients, save_log_on_exit, add_client, add_curl
 
 # ==============================================================================
 #    TESTS
@@ -47,7 +49,7 @@ def test_bootstrap(init_session, setup_bootstrap):
 
 
 def test_client(init_session, setup_clients, add_curl, save_log_on_exit):
-    fields = {'M':'discovery_bootstrap'}
+    fields = {'M': 'discovery_bootstrap'}
     timetowait = len(setup_clients.pods) * timeout_factor
     print("Sleeping " + str(timetowait) + " before checking out bootstrap results")
     time.sleep(timetowait)
@@ -56,7 +58,6 @@ def test_client(init_session, setup_clients, add_curl, save_log_on_exit):
 
 
 def test_add_client(add_client):
-
     # Sleep a while before checking the node is bootstarped
     time.sleep(20 * timeout_factor)
     fields = {'M': 'discovery_bootstrap'}
@@ -65,7 +66,6 @@ def test_add_client(add_client):
 
 
 def test_add_many_clients(init_session, setup_bootstrap, setup_clients):
-
     bs_info = setup_bootstrap.pods[0]
     cspec = get_conf(bs_info, testconfig['client'])
 
@@ -78,7 +78,7 @@ def test_add_many_clients(init_session, setup_bootstrap, setup_clients):
 
 
 def test_gossip(init_session, setup_clients, add_curl):
-    fields = {'M':'new_gossip_message', 'protocol': 'api_test_gossip'}
+    fields = {'M': 'new_gossip_message', 'protocol': 'api_test_gossip'}
     initial = len(query_message(current_index, testconfig['namespace'], setup_clients.deployment_name, fields))
     # *note*: this already waits for bootstrap so we can send the msg right away.
     # send message to client via rpc
@@ -95,23 +95,24 @@ def test_gossip(init_session, setup_clients, add_curl):
 
     # Need to sleep for a while in order to enable the propagation of the gossip message - 0.5 sec for each node
     # TODO: check frequently before timeout so we might be able to finish earlier.
-    gossip_propagation_sleep = len(setup_clients.pods) * timeout_factor / 2 # currently we expect short propagation times.
+    gossip_propagation_sleep = len(
+        setup_clients.pods) * timeout_factor / 2  # currently we expect short propagation times.
     print('sleep for {0} sec to enable gossip propagation'.format(gossip_propagation_sleep))
     time.sleep(gossip_propagation_sleep)
 
     after = query_message(current_index, testconfig['namespace'], setup_clients.deployment_name, fields, False)
-    assert initial+len(setup_clients.pods) == len(after)
+    assert initial + len(setup_clients.pods) == len(after)
 
 
 def test_many_gossip_messages(setup_clients, add_curl):
-    fields = {'M':'new_gossip_message', 'protocol': 'api_test_gossip'}
+    fields = {'M': 'new_gossip_message', 'protocol': 'api_test_gossip'}
     initial = len(query_message(current_index, testconfig['namespace'], setup_clients.deployment_name, fields))
 
     # *note*: this already waits for bootstrap so we can send the msg right away.
     # send message to client via rpc
     TEST_MESSAGES = 10
     for i in range(TEST_MESSAGES):
-        rnd = random.randint(0, len(setup_clients.pods)-1)
+        rnd = random.randint(0, len(setup_clients.pods) - 1)
         client_ip = setup_clients.pods[rnd]['pod_ip']
         podname = setup_clients.pods[rnd]['name']
         print("Sending gossip from client ip: {0}/{1}".format(podname, client_ip))
@@ -124,12 +125,12 @@ def test_many_gossip_messages(setup_clients, add_curl):
 
         # Need to sleep for a while in order to enable the propagation of the gossip message - 0.5 sec for each node
         # TODO: check frequently before timeout so we might be able to finish earlier.
-        gossip_propagation_sleep = 10 * timeout_factor # currently we expect short propagation times.
+        gossip_propagation_sleep = 10 * timeout_factor  # currently we expect short propagation times.
         print('sleep for {0} sec to enable gossip propagation'.format(gossip_propagation_sleep))
         time.sleep(gossip_propagation_sleep)
 
         after = query_message(current_index, testconfig['namespace'], setup_clients.deployment_name, fields, False)
-        assert initial + len(setup_clients.pods)*(i+1) == len(after)
+        assert initial + len(setup_clients.pods) * (i + 1) == len(after)
 
 
 def test_many_gossip_sim(setup_clients, add_curl):
@@ -140,7 +141,7 @@ def test_many_gossip_sim(setup_clients, add_curl):
     initial = len(query_message(current_index, testconfig['namespace'], setup_clients.deployment_name, fields))
 
     for i in range(TEST_MESSAGES):
-        rnd = random.randint(0, len(setup_clients.pods)-1)
+        rnd = random.randint(0, len(setup_clients.pods) - 1)
         client_ip = setup_clients.pods[rnd]['pod_ip']
         podname = setup_clients.pods[rnd]['name']
         print("Sending gossip from client ip: {0}/{1}".format(podname, client_ip))
@@ -158,7 +159,7 @@ def test_many_gossip_sim(setup_clients, add_curl):
     time.sleep(gossip_propagation_sleep)
 
     after = query_message(current_index, testconfig['namespace'], setup_clients.deployment_name, fields, False)
-    assert initial+len(setup_clients.pods)*TEST_MESSAGES == len(after)
+    assert initial + len(setup_clients.pods) * TEST_MESSAGES == len(after)
 
 
 # NOTE : this test is ran in the end because it affects the network structure,
@@ -170,7 +171,8 @@ def test_late_bootstraps(init_session, setup_bootstrap, setup_clients):
     testnames = []
 
     for i in range(TEST_NUM):
-        client = add_single_client(setup_bootstrap.deployment_id, get_conf(setup_bootstrap.pods[0], testconfig['client']))
+        client = add_single_client(setup_bootstrap.deployment_id,
+                                   get_conf(setup_bootstrap.pods[0], testconfig['client']))
         testnames.append((client, datetime.now()))
 
     time.sleep(TEST_NUM * timeout_factor)
