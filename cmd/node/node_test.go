b92b927dff483a13fe7555a6361492f378f289f7
Noam Nelke <noam@spacemesh.io>
2019-07-23 11:39:38 +0300

    Load identity from (and save to) PoST datadir (#1177)

---
+++ cmd/node/node_test.go
@@ -0,0 +1,55 @@
+package node
+
+import (
+	"fmt"
+	"github.com/spacemeshos/go-spacemesh/log"
+	"github.com/stretchr/testify/require"
+	"io/ioutil"
+	"os"
+	"path/filepath"
+	"testing"
+)
+
+func TestSpacemeshApp_getEdIdentity(t *testing.T) {
+	r := require.New(t)
+
+	defer func() {
+		// cleanup
+		err := os.RemoveAll("tmp")
+		r.NoError(err)
+	}()
+
+	// setup spacemesh app
+	app := NewSpacemeshApp()
+	app.Config.POST.DataDir = "tmp"
+	app.log = log.NewDefault("logger")
+
+	// get new identity
+	sgn, err := app.LoadOrCreateEdSigner()
+	r.NoError(err)
+
+	// ensure we have a single subdirectory under tmp
+	infos, err := ioutil.ReadDir("tmp")
+	r.NoError(err)
+	r.Len(infos, 1)
+
+	// run the method again
+	sgn2, err := app.LoadOrCreateEdSigner()
+	r.NoError(err)
+
+	// ensure that we didn't create another identity
+	infos, err = ioutil.ReadDir("tmp")
+	r.NoError(err)
+	r.Len(infos, 1)
+
+	// ensure both signers are identical
+	r.Equal(sgn.PublicKey(), sgn2.PublicKey())
+
+	// mess with the directory name
+	err = os.Rename(filepath.Join("tmp", infos[0].Name()), filepath.Join("tmp", "wrong name"))
+	r.NoError(err)
+
+	// run the method again
+	_, err = app.LoadOrCreateEdSigner()
+	r.EqualError(err, fmt.Sprintf("identity file path ('tmp/wrong name') does not match public key (%v)", sgn.PublicKey().String()))
+}
