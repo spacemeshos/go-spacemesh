da842408885d3f82a6759d78a4bcbb2890dc813a
almog <almogdepaz@gmail.com>
2019-09-12 12:55:17 +0300

    fix test

--- sync/block_listener_test.go
+++ sync/block_listener_test.go
@@ -255,7 +255,7 @@ func TestBlockListener_ValidateVotesGoodFlow(t *testing.T) {
 	bl1.MeshDB.AddBlock(block6)
 	bl1.MeshDB.AddBlock(block7)
 
-	assert.True(t, validateVotes(block1, bl1.ForBlockInView, bl1.Hdist))
+	assert.True(t, validateVotes(block1, bl1.ForBlockInView, bl1.Hdist, log.New("", "", "")))
 }
 
 func TestBlockListener_ValidateVotesBadFlow(t *testing.T) {
@@ -310,7 +310,7 @@ func TestBlockListener_ValidateVotesBadFlow(t *testing.T) {
 	bl1.MeshDB.AddBlock(block6)
 	bl1.MeshDB.AddBlock(block7)
 
-	assert.False(t, validateVotes(block1, bl1.ForBlockInView, bl1.Hdist))
+	assert.False(t, validateVotes(block1, bl1.ForBlockInView, bl1.Hdist, log.New("", "", "")))
 }
 
 func TestBlockListenerViewTraversal(t *testing.T) {
