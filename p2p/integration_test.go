072b2b4457b3d435379f6210dc983a079a8a0189
Noam Nelke <noam@spacemesh.io>
2019-11-14 13:01:14 +0200

    skip slow test

--- p2p/integration_test.go
+++ p2p/integration_test.go
@@ -82,9 +82,7 @@ func (its *IntegrationTestSuite) Test_Gossiping() {
 }
 
 func Test_ReallySmallP2PIntegrationSuite(t *testing.T) {
-	if testing.Short() {
-		t.Skip()
-	}
+	t.Skip("very long test") // TODO
 
 	s := new(IntegrationTestSuite)
 
