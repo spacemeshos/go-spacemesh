ea9c5a1ff8f57f14e16c6da4a14b6a07f03b36e3
AmitShaul <amit1shaul@gmail.com>
2020-01-05 12:57:02 +0200

    test face lift

--- tests/stress/sync_stress/test_sync.py
+++ tests/stress/sync_stress/test_sync.py
@@ -15,13 +15,13 @@ START_SYNC = "start synchronize"
 
 
 def test_sync_stress(init_session, setup_bootstrap, save_log_on_exit):
+    clients_num = 10
     bs_info = setup_bootstrap.pods[0]
     cspec = get_conf(bs_info, testconfig['client'])
-
-    clients = add_multi_clients(init_session, cspec, 10)
+    _ = add_multi_clients(init_session, cspec, clients_num)
 
     hits = []
-    number_of_pods = len(clients) + 1  # add 1 for bootstrap pod
+    number_of_pods = clients_num + 1  # add 1 for bootstrap pod
     tts = 70
     while len(hits) != number_of_pods:
         print(f"waiting for all clients to finish downloading all files, sleeping for {tts} seconds")
@@ -31,12 +31,12 @@ def test_sync_stress(init_session, setup_bootstrap, save_log_on_exit):
     del cspec.args['remote-data']
     cspec.args['data-folder'] = ""
 
-    # Adding a new client
+    # Adding a single new client
     res_lst = add_multi_clients(init_session, cspec, 1, 'client')
     new_client = res_lst[0]
 
     curr_try = 0
-    max_retries = 20
+    max_retries = 13
     interval_time = 60
     print("waiting for new pod to be synced")
     while True:
@@ -47,9 +47,7 @@ def test_sync_stress(init_session, setup_bootstrap, save_log_on_exit):
         print(f"not synced after {curr_try} tries of {interval_time} secs each", end="\r")
         time.sleep(interval_time)
 
-        if curr_try >= max_retries:
-            assert 0, f"node failed syncing after waiting for {max_retries} minutes"
-
+        assert curr_try < max_retries, f"node failed syncing after waiting for {max_retries} minutes"
         curr_try += 1
 
     # parsing sync start time
