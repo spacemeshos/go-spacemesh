87a51e4cb4e3e37827317ce7469f38c23b195303
Elad Gavra <gavrad@gmail.com>
2020-01-13 13:53:35 +0200

    Added UT for convert

--- timesync/converter.go
+++ timesync/converter.go
@@ -10,10 +10,15 @@ type LayerConv struct {
 	genesis  time.Time     // the genesis time
 }
 
+// TimeToLayer returns the layer of the provided time
 func (lc LayerConv) TimeToLayer(t time.Time) types.LayerID {
-	return types.LayerID((t.Sub(lc.genesis) / lc.duration * time.Second) + 2)
+	return types.LayerID((t.Sub(lc.genesis) / lc.duration) + 1)
 }
 
+// LayerToTime returns the time of the provided layer
 func (lc LayerConv) LayerToTime(id types.LayerID) time.Time {
-	return lc.genesis.Add(time.Duration(id) * lc.duration)
+	if id == 0 { // layer 1 is genesis, consider 0 also as genesis
+		return lc.genesis
+	}
+	return lc.genesis.Add(time.Duration(id-1) * lc.duration)
 }
--- timesync/converter_test.go
+++ timesync/converter_test.go
@@ -1,11 +1,40 @@
 package timesync
 
-import "testing"
+import (
+	"github.com/spacemeshos/go-spacemesh/common/types"
+	"github.com/stretchr/testify/require"
+	"testing"
+	"time"
+)
+
+func getTime() time.Time {
+	layout := "2006-01-02T15:04:05.000Z"
+	curr := "2018-11-12T11:45:26.000Z"
+	tm, _ := time.Parse(layout, curr)
+
+	return tm
+}
 
 func TestLayerConv_LayerToTime(t *testing.T) {
-
+	r := require.New(t)
+	tm := getTime()
+	lc := LayerConv{5 * time.Second, tm}
+	r.Equal(tm.Add(10*time.Second), lc.LayerToTime(3))
 }
 
 func TestLayerConv_TimeToLayer(t *testing.T) {
+	r := require.New(t)
+	tm := getTime()
+	lc := LayerConv{5 * time.Second, tm}
+	r.Equal(types.LayerID(2), lc.TimeToLayer(tm.Add(9*time.Second)))
+	r.Equal(types.LayerID(3), lc.TimeToLayer(tm.Add(10*time.Second)))
+	r.Equal(types.LayerID(3), lc.TimeToLayer(tm.Add(12*time.Second)))
+}
 
+func TestTicker_pingPong(t *testing.T) {
+	r := require.New(t)
+	tm := getTime()
+	lc := LayerConv{5 * time.Second, tm}
+	ttl := lc.TimeToLayer(tm.Add(9 * time.Second))
+	r.Equal(types.LayerID(2), lc.TimeToLayer(lc.LayerToTime(ttl)))
 }
