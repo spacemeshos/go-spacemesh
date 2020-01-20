29407623e751923b4d3a14cde182f15b3609141e
Moshe Shababo <dicentralized@gmail.com>
2020-01-20 16:05:07 +0200

    Windows scripts fix

--- setup_env.bat
+++ setup_env.bat
@@ -4,8 +4,19 @@ if %ERRORLEVEL% GTR 0 exit /B 1
 
 call ./scripts/win/install-protobuf.bat
 
-go get github.com/golang/protobuf/protoc-gen-go
-set GO111MODULE=off
-go get github.com/grpc-ecosystem/grpc-gateway/protoc-gen-grpc-gateway
-go get github.com/grpc-ecosystem/grpc-gateway/protoc-gen-swagger
-set GO111MODULE=on
+SET GO111MODULE=on
+
+FOR /F "tokens=* USEBACKQ" %%F IN (`go list -m -f {{.Dir}} github.com/golang/protobuf`) DO (
+SET protobuf_path=%%F
+)
+ECHO installing protoc-gen-go...
+go install %protobuf_path%/protoc-gen-go
+
+FOR /F "tokens=* USEBACKQ" %%F IN (`go list -m -f {{.Dir}} github.com/grpc-ecosystem/grpc-gateway`) DO (
+SET grpc_gateway_path=%%F
+)
+ECHO installing protoc-gen-go...
+go install %grpc_gateway_path%/protoc-gen-grpc-gateway
+ECHO installing protoc-gen-swagger...
+go install %grpc_gateway_path%/protoc-gen-swagger
+
