spacemeshos/go-spacemesh

commit de6f51a13cad830d9dae15d3e6ea706c9d504de6

Index all of a node's atxs (#1710)
## Motivation
Closes #1682 

## Changes
Index all of a node's ATXs based on their target epoch. Use the desired target epoch to retrieve the relevant ATX.

## Test Plan
- [x] Add unit tests for new functionality.
- [ ] Add an automation test for restarting a node after publishing an ATX and ensure it calculated the correct eligible blocks within the same epoch. [?]

activation/atxdb.go
@@ -17,8 +17,12 @@ import (
 
 const topAtxKey = "topAtxKey"
 
-func getNodeIdKey(id types.NodeId) []byte {
-	return []byte(fmt.Sprintf("n_%v", id.Key))
+func getNodeAtxKey(nodeId types.NodeId, targetEpoch types.EpochId) []byte {
+	return append(getNodeAtxPrefix(nodeId), util.Uint64ToBytesBigEndian(uint64(targetEpoch))...)
+}
+
+func getNodeAtxPrefix(nodeId types.NodeId) []byte {
+	return []byte(fmt.Sprintf("n_%v_", nodeId.Key))
 }
 
 func getAtxHeaderKey(atxId types.AtxId) []byte {
@@ -569,7 +573,7 @@ func (db ActivationDb) getTopAtx() (atxIdAndLayer, error) {
 
 // addAtxToNodeId inserts activation atx id by node
 func (db *ActivationDb) addAtxToNodeId(nodeId types.NodeId, atx *types.ActivationTx) error {
-	err := db.atxs.Put(getNodeIdKey(nodeId), atx.Id().Bytes())
+	err := db.atxs.Put(getNodeAtxKey(nodeId, atx.TargetEpoch(db.LayersPerEpoch)), atx.Id().Bytes())
 	if err != nil {
 		return fmt.Errorf("failed to store ATX ID for node: %v", err)
 	}
@@ -578,11 +582,25 @@ func (db *ActivationDb) addAtxToNodeId(nodeId types.NodeId, atx *types.Activatio
 
 // GetNodeLastAtxId returns the last atx id that was received for node nodeId
 func (db *ActivationDb) GetNodeLastAtxId(nodeId types.NodeId) (types.AtxId, error) {
-	db.log.Debug("fetching atxIDs for node %v", nodeId.ShortString())
+	nodeAtxsIterator := db.atxs.Find(getNodeAtxPrefix(nodeId))
+	// ATX syntactic validation ensures that each ATX is at least one epoch after a referenced previous ATX.
+	// Contextual validation ensures that the previous ATX referenced matches what this method returns, so the next ATX
+	// added will always be the next ATX returned by this method.
+	// leveldb_iterator.Last() returns the last entry in lexicographical order of the keys:
+	//   https://github.com/google/leveldb/blob/master/doc/index.md#comparators
+	// For the lexicographical order to match the epoch order we must encode the epoch id using big endian encoding when
+	// composing the key.
+	if exists := nodeAtxsIterator.Last(); !exists {
+		return *types.EmptyAtxId, fmt.Errorf("atx for node %v does not exist", nodeId.ShortString())
+	}
+	return types.AtxId(types.BytesToHash(nodeAtxsIterator.Value())), nil
+}
 
-	id, err := db.atxs.Get(getNodeIdKey(nodeId))
+func (db *ActivationDb) GetNodeAtxIdForEpoch(nodeId types.NodeId, targetEpoch types.EpochId) (types.AtxId, error) {
+	id, err := db.atxs.Get(getNodeAtxKey(nodeId, targetEpoch))
 	if err != nil {
-		return *types.EmptyAtxId, err
+		return *types.EmptyAtxId, fmt.Errorf("atx for node %v targeting epoch %v: %v",
+			nodeId.ShortString(), targetEpoch, err)
 	}
 	return types.AtxId(types.BytesToHash(id)), nil
 }
@@ -643,56 +661,6 @@ func (db *ActivationDb) GetFullAtx(id types.AtxId) (*types.ActivationTx, error) 
 	}
 	atx.ActivationTxHeader = header
 	return atx, nil
-}
-
-// IsIdentityActive returns whether edId is active for the epoch of layer layer.
-// it returns error if no associated atx is found in db
-func (db *ActivationDb) IsIdentityActive(edId string, layer types.LayerID) (*types.NodeId, bool, types.AtxId, error) {
-	// TODO: genesis flow should decide what we want to do here
-	if layer.GetEpoch(db.LayersPerEpoch) == 0 {
-		return nil, true, *types.EmptyAtxId, nil
-	}
-
-	epoch := layer.GetEpoch(db.LayersPerEpoch)
-	nodeId, err := db.GetIdentity(edId)
-	if err != nil { // means there is no such identity
-		db.log.Error("IsIdentityActive erred while getting identity err=%v", err)
-		return nil, false, *types.EmptyAtxId, nil
-	}
-	atxId, err := db.GetNodeLastAtxId(nodeId)
-	if err != nil {
-		db.log.Error("IsIdentityActive erred while getting last node atx id err=%v", err)
-		return nil, false, *types.EmptyAtxId, err
-	}
-	atx, err := db.GetAtxHeader(atxId)
-	if err != nil {
-		db.log.With().Error("IsIdentityActive erred while getting atx", log.AtxId(atxId.ShortString()), log.Err(err))
-		return nil, false, *types.EmptyAtxId, nil
-	}
-
-	lastAtxTargetEpoch := atx.TargetEpoch(db.LayersPerEpoch)
-	if lastAtxTargetEpoch < epoch {
-		db.log.With().Info("IsIdentityActive latest atx is too old", log.Uint64("expected", uint64(epoch)),
-			log.Uint64("actual", uint64(lastAtxTargetEpoch)), log.AtxId(atx.ShortString()))
-		return nil, false, *types.EmptyAtxId, nil
-	}
-
-	if lastAtxTargetEpoch > epoch {
-		// This could happen if we already published the ATX for the next epoch, so we check the previous one as well
-		if atx.PrevATXId == *types.EmptyAtxId {
-			db.log.With().Info("IsIdentityActive latest atx is too new but no previous atx", log.AtxId(atxId.ShortString()))
-			return nil, false, *types.EmptyAtxId, nil
-		}
-		prevAtxId := atx.PrevATXId
-		atx, err = db.GetAtxHeader(prevAtxId)
-		if err != nil {
-			db.log.With().Error("IsIdentityActive erred while getting atx for second newest", log.AtxId(prevAtxId.ShortString()), log.Err(err))
-			return nil, false, *types.EmptyAtxId, nil
-		}
-	}
-
-	// lastAtxTargetEpoch = epoch
-	return &nodeId, atx.TargetEpoch(db.LayersPerEpoch) == epoch, atx.Id(), nil
 }
 
 // ValidateSignedAtx extracts public key from message and verifies public key exists in IdStore, this is how we validate

activation/activationdb_test.go
@@ -402,6 +402,27 @@ func Test_CalcActiveSetFromView(t *testing.T) {
 	assert.Equal(t, 3, int(num))
 }
 
+func TestActivationDb_GetNodeLastAtxId(t *testing.T) {
+	r := require.New(t)
+
+	atxdb, _, _ := getAtxDb("t6")
+	id1 := types.NodeId{Key: uuid.New().String()}
+	coinbase1 := types.HexToAddress("aaaa")
+	epoch1 := types.EpochId(2)
+	atx1 := types.NewActivationTx(newChallenge(id1, 0, *types.EmptyAtxId, *types.EmptyAtxId, epoch1.FirstLayer(atxdb.LayersPerEpoch)), coinbase1, 3, []types.BlockID{}, &types.NIPST{}, nil)
+	r.NoError(atxdb.StoreAtx(epoch1, atx1))
+
+	epoch2 := types.EpochId(1) + (1 << 8)
+	// This will fail if we convert the epoch id to bytes using LittleEndian, since LevelDB's lexicographic sorting will
+	// then sort by LSB instead of MSB, first.
+	atx2 := types.NewActivationTx(newChallenge(id1, 1, atx1.Id(), atx1.Id(), epoch2.FirstLayer(atxdb.LayersPerEpoch)), coinbase1, 3, []types.BlockID{}, &types.NIPST{}, nil)
+	r.NoError(atxdb.StoreAtx(epoch2, atx2))
+
+	id, err := atxdb.GetNodeLastAtxId(id1)
+	r.NoError(err)
+	r.Equal(atx2.ShortString(), id.ShortString(), "atx1.ShortString(): %v", atx1.ShortString())
+}
+
 func Test_DBSanity(t *testing.T) {
 	atxdb, _, _ := getAtxDb("t6")
 
@@ -413,8 +434,8 @@ func Test_DBSanity(t *testing.T) {
 	coinbase3 := types.HexToAddress("cccc")
 
 	atx1 := types.NewActivationTxForTests(id1, 0, *types.EmptyAtxId, 1, 0, *types.EmptyAtxId, coinbase1, 3, []types.BlockID{}, &types.NIPST{})
-	atx2 := types.NewActivationTxForTests(id1, 0, *types.EmptyAtxId, 1, 0, *types.EmptyAtxId, coinbase2, 3, []types.BlockID{}, &types.NIPST{})
-	atx3 := types.NewActivationTxForTests(id1, 0, *types.EmptyAtxId, 1, 0, *types.EmptyAtxId, coinbase3, 3, []types.BlockID{}, &types.NIPST{})
+	atx2 := types.NewActivationTxForTests(id1, 0, *types.EmptyAtxId, 1001, 0, *types.EmptyAtxId, coinbase2, 3, []types.BlockID{}, &types.NIPST{})
+	atx3 := types.NewActivationTxForTests(id1, 0, *types.EmptyAtxId, 2001, 0, *types.EmptyAtxId, coinbase3, 3, []types.BlockID{}, &types.NIPST{})
 
 	err := atxdb.storeAtxUnlocked(atx1)
 	assert.NoError(t, err)
@@ -428,6 +449,10 @@ func Test_DBSanity(t *testing.T) {
 	id, err := atxdb.GetNodeLastAtxId(id1)
 	assert.NoError(t, err)
 	assert.Equal(t, atx1.Id(), id)
+	assert.Equal(t, types.EpochId(1), atx1.TargetEpoch(atxdb.LayersPerEpoch))
+	id, err = atxdb.GetNodeAtxIdForEpoch(id1, atx1.TargetEpoch(atxdb.LayersPerEpoch))
+	assert.NoError(t, err)
+	assert.Equal(t, atx1.Id(), id)
 
 	err = atxdb.addAtxToNodeId(id2, atx2)
 	assert.NoError(t, err)
@@ -438,13 +463,21 @@ func Test_DBSanity(t *testing.T) {
 	id, err = atxdb.GetNodeLastAtxId(id2)
 	assert.NoError(t, err)
 	assert.Equal(t, atx2.Id(), id)
+	assert.Equal(t, types.EpochId(2), atx2.TargetEpoch(atxdb.LayersPerEpoch))
+	id, err = atxdb.GetNodeAtxIdForEpoch(id2, atx2.TargetEpoch(atxdb.LayersPerEpoch))
+	assert.NoError(t, err)
+	assert.Equal(t, atx2.Id(), id)
 
 	id, err = atxdb.GetNodeLastAtxId(id1)
 	assert.NoError(t, err)
 	assert.Equal(t, atx3.Id(), id)
+	assert.Equal(t, types.EpochId(3), atx3.TargetEpoch(atxdb.LayersPerEpoch))
+	id, err = atxdb.GetNodeAtxIdForEpoch(id1, atx3.TargetEpoch(atxdb.LayersPerEpoch))
+	assert.NoError(t, err)
+	assert.Equal(t, atx3.Id(), id)
 
 	id, err = atxdb.GetNodeLastAtxId(id3)
-	assert.Error(t, err)
+	assert.EqualError(t, err, fmt.Sprintf("atx for node %v does not exist", id3.ShortString()))
 	assert.Equal(t, *types.EmptyAtxId, id)
 }
 
@@ -655,12 +688,16 @@ func TestActivationDB_ValidateAtxErrors(t *testing.T) {
 	err = atxdb.StoreAtx(1, atx)
 	assert.NoError(t, err)
 	atx = types.NewActivationTxForTests(idx1, 1, prevAtx.Id(), 12, 0, posAtx.Id(), coinbase, 3, []types.BlockID{}, &types.NIPST{})
-	err = atxdb.atxs.Delete(getNodeIdKey(atx.NodeId))
-	assert.NoError(t, err)
+	iter := atxdb.atxs.Find(getNodeAtxPrefix(atx.NodeId))
+	for iter.Next() {
+		err = atxdb.atxs.Delete(iter.Key())
+		assert.NoError(t, err)
+	}
 	err = SignAtx(signer, atx)
 	assert.NoError(t, err)
 	err = atxdb.ContextuallyValidateAtx(atx.ActivationTxHeader)
-	assert.EqualError(t, err, "could not fetch node last ATX: leveldb: not found")
+	assert.EqualError(t, err,
+		fmt.Sprintf("could not fetch node last ATX: atx for node %v does not exist", atx.NodeId.ShortString()))
 
 	// Prev atx not declared but commitment not included.
 	atx = types.NewActivationTxForTests(idx1, 0, *types.EmptyAtxId, 1012, 0, posAtx.Id(), coinbase, 3, []types.BlockID{}, &types.NIPST{})

common/util/bytes.go
@@ -20,6 +20,12 @@ func Uint64ToBytes(i uint64) []byte {
 	return a
 }
 
+func Uint64ToBytesBigEndian(i uint64) []byte {
+	a := make([]byte, 8)
+	binary.BigEndian.PutUint64(a, i)
+	return a
+}
+
 // CopyBytes returns an exact copy of the provided bytes.
 func CopyBytes(b []byte) (copiedBytes []byte) {
 	if b == nil {

mesh/atxdb_mock.go
@@ -18,14 +18,6 @@ func NewAtxDbMock() *AtxDbMock {
 	}
 }
 
-func (*AtxDbMock) IsIdentityActive(edId string, layer types.LayerID) (*types.NodeId, bool, types.AtxId, error) {
-	return nil, true, *types.EmptyAtxId, nil
-}
-
-func (t *AtxDbMock) GetPosAtxId(id types.EpochId) (types.AtxId, error) {
-	return types.AtxId{}, nil /*todo: mock if needed */
-}
-
 func (t *AtxDbMock) GetAtxHeader(id types.AtxId) (*types.ActivationTxHeader, error) {
 	if id == *types.EmptyAtxId {
 		return nil, fmt.Errorf("trying to fetch empty atx id")
@@ -44,10 +36,6 @@ func (t *AtxDbMock) GetFullAtx(id types.AtxId) (*types.ActivationTx, error) {
 func (t *AtxDbMock) AddAtx(id types.AtxId, atx *types.ActivationTx) {
 	t.db[id] = atx
 	t.nipsts[id] = atx.Nipst
-}
-
-func (t *AtxDbMock) GetNipst(id types.AtxId) (*types.NIPST, error) {
-	return t.nipsts[id], nil
 }
 
 func (t *AtxDbMock) ProcessAtxs(atxs []*types.ActivationTx) error {

oracle/blockeligibilityvalidator.go
@@ -64,7 +64,7 @@ func (v BlockEligibilityValidator) BlockSignedAndEligible(block *types.Block) (b
 		activeSetSize = v.genesisActiveSetSize
 	}
 
-	numberOfEligibleBlocks, err := getNumberOfEligibleBlocks(activeSetSize, v.committeeSize, v.layersPerEpoch, v.log)
+	numberOfEligibleBlocks, err := getNumberOfEligibleBlocks(activeSetSize, v.committeeSize, v.layersPerEpoch)
 	if err != nil {
 		return false, fmt.Errorf("failed to get number of eligible blocks: %v", err)
 	}

oracle/blockeligibilityvalidator_test.go
@@ -15,15 +15,11 @@ type mockAtxDB struct {
 	err  error
 }
 
-func (m *mockAtxDB) IsIdentityActive(edId string, layer types.LayerID) (*types.NodeId, bool, types.AtxId, error) {
-	return &types.NodeId{}, false, types.AtxId{}, m.err
-}
-
 func (m mockAtxDB) GetIdentity(edId string) (types.NodeId, error) {
 	return types.NodeId{Key: edId, VRFPublicKey: vrfPubkey}, nil
 }
 
-func (m mockAtxDB) GetNodeLastAtxId(node types.NodeId) (types.AtxId, error) {
+func (m mockAtxDB) GetNodeAtxIdForEpoch(nodeId types.NodeId, targetEpoch types.EpochId) (types.AtxId, error) {
 	return types.AtxId{}, m.err
 }
 

oracle/blockoracle.go
@@ -11,10 +11,9 @@ import (
 )
 
 type ActivationDb interface {
-	GetNodeLastAtxId(node types.NodeId) (types.AtxId, error)
+	GetNodeAtxIdForEpoch(nodeId types.NodeId, targetEpoch types.EpochId) (types.AtxId, error)
 	GetAtxHeader(id types.AtxId) (*types.ActivationTxHeader, error)
 	GetIdentity(edId string) (types.NodeId, error)
-	IsIdentityActive(edId string, layer types.LayerID) (*types.NodeId, bool, types.AtxId, error)
 }
 
 type Signer interface {
@@ -79,7 +78,7 @@ func (bo *MinerBlockOracle) calcEligibilityProofs(epochNumber types.EpochId) err
 	epochBeacon := bo.beaconProvider.GetBeacon(epochNumber)
 
 	var activeSetSize uint32
-	atx, err := bo.getValidLatestATX(epochNumber)
+	atx, err := bo.getValidAtxForEpoch(epochNumber)
 	if err != nil {
 		if !epochNumber.IsGenesis() {
 			return fmt.Errorf("failed to get latest ATX: %v", err)
@@ -94,7 +93,7 @@ func (bo *MinerBlockOracle) calcEligibilityProofs(epochNumber types.EpochId) err
 		bo.log.Info("genesis epoch detected, using GenesisActiveSetSize (%v)", activeSetSize)
 	}
 
-	numberOfEligibleBlocks, err := getNumberOfEligibleBlocks(activeSetSize, bo.committeeSize, bo.layersPerEpoch, bo.log)
+	numberOfEligibleBlocks, err := getNumberOfEligibleBlocks(activeSetSize, bo.committeeSize, bo.layersPerEpoch)
 	if err != nil {
 		bo.log.Error("failed to get number of eligible blocks: %v", err)
 		return err
@@ -127,22 +126,15 @@ func (bo *MinerBlockOracle) calcEligibilityProofs(epochNumber types.EpochId) err
 	return nil
 }
 
-func (bo *MinerBlockOracle) getValidLatestATX(validForEpoch types.EpochId) (*types.ActivationTxHeader, error) {
-	latestATXID, err := bo.getLatestATXID()
+func (bo *MinerBlockOracle) getValidAtxForEpoch(validForEpoch types.EpochId) (*types.ActivationTxHeader, error) {
+	atxId, err := bo.getAtxIdForEpoch(validForEpoch)
 	if err != nil {
-		return nil, fmt.Errorf("not in genesis (epoch %v) yet failed to get atx: %v", validForEpoch, err)
+		return nil, fmt.Errorf("failed to get ATX ID for target epoch %v: %v", validForEpoch, err)
 	}
-	atx, err := bo.activationDb.GetAtxHeader(latestATXID)
+	atx, err := bo.activationDb.GetAtxHeader(atxId)
 	if err != nil {
 		bo.log.Error("getting ATX failed: %v", err)
 		return nil, err
-	}
-	atxTargetEpoch := atx.PubLayerIdx.GetEpoch(bo.layersPerEpoch) + 1
-	if validForEpoch != atxTargetEpoch {
-		bo.log.Warning("latest ATX (target epoch %d) not valid in eligibility epoch (%d), miner not eligible",
-			atxTargetEpoch, validForEpoch)
-		return nil, fmt.Errorf("latest ATX (target epoch %v) not valid in eligibility epoch (%v)",
-			atxTargetEpoch, validForEpoch)
 	}
 	return atx, nil
 }
@@ -153,19 +145,19 @@ func calcEligibleLayer(epochNumber types.EpochId, layersPerEpoch uint16, vrfHash
 	return epochNumber.FirstLayer(layersPerEpoch).Add(uint16(eligibleLayerOffset))
 }
 
-func getNumberOfEligibleBlocks(activeSetSize uint32, committeeSize uint32, layersPerEpoch uint16, lg log.Log) (uint32, error) {
+func getNumberOfEligibleBlocks(activeSetSize, committeeSize uint32, layersPerEpoch uint16) (uint32, error) {
 	if activeSetSize == 0 {
 		return 0, errors.New("empty active set not allowed")
 	}
-	numberOfEligibleBlocks := uint32(committeeSize) * uint32(layersPerEpoch) / activeSetSize
+	numberOfEligibleBlocks := committeeSize * uint32(layersPerEpoch) / activeSetSize
 	if numberOfEligibleBlocks == 0 {
 		numberOfEligibleBlocks = 1
 	}
 	return numberOfEligibleBlocks, nil
 }
 
-func (bo *MinerBlockOracle) getLatestATXID() (types.AtxId, error) {
-	latestATXID, err := bo.activationDb.GetNodeLastAtxId(bo.nodeID)
+func (bo *MinerBlockOracle) getAtxIdForEpoch(targetEpoch types.EpochId) (types.AtxId, error) {
+	latestATXID, err := bo.activationDb.GetNodeAtxIdForEpoch(bo.nodeID, targetEpoch)
 	if err != nil {
 		bo.log.With().Info("did not find ATX IDs for node", log.String("atx_node_id", bo.nodeID.ShortString()), log.Err(err))
 		return types.AtxId{}, err

oracle/blockoracle_test.go
@@ -30,21 +30,12 @@ type mockActivationDB struct {
 	atxs                map[string]map[types.LayerID]types.AtxId
 }
 
-func (a *mockActivationDB) IsIdentityActive(edId string, layer types.LayerID) (*types.NodeId, bool, types.AtxId, error) {
-	if idmap, ok := a.atxs[edId]; ok {
-		if atxid, ok := idmap[layer]; ok {
-			return &types.NodeId{Key: edId, VRFPublicKey: vrfPubkey}, true, atxid, nil
-		}
-	}
-	return &types.NodeId{Key: edId, VRFPublicKey: vrfPubkey}, true, *types.EmptyAtxId, nil
-}
-
 func (a mockActivationDB) GetIdentity(edId string) (types.NodeId, error) {
 	return types.NodeId{Key: edId, VRFPublicKey: vrfPubkey}, nil
 }
 
-func (a mockActivationDB) GetNodeLastAtxId(node types.NodeId) (types.AtxId, error) {
-	if node.Key != nodeID.Key {
+func (a mockActivationDB) GetNodeAtxIdForEpoch(nodeId types.NodeId, targetEpoch types.EpochId) (types.AtxId, error) {
+	if nodeId.Key != nodeID.Key || targetEpoch.IsGenesis() {
 		return *types.EmptyAtxId, errors.New("not found")
 	}
 	return atxID, nil
@@ -188,7 +179,7 @@ func TestBlockOracleNoActivationsForNode(t *testing.T) {
 	blockOracle := NewMinerBlockOracle(committeeSize, activeSetSize, layersPerEpoch, activationDB, beaconProvider, vrfSigner, nodeID, func() bool { return true }, lg.WithName("blockOracle"))
 
 	_, proofs, err := blockOracle.BlockEligible(types.LayerID(layersPerEpoch * 2))
-	r.EqualError(err, "failed to get latest ATX: not in genesis (epoch 2) yet failed to get atx: not found")
+	r.EqualError(err, "failed to get latest ATX: failed to get ATX ID for target epoch 2: not found")
 	r.Nil(proofs)
 }
 

commit 8eb5ecb517826627e4dc90be1264e9d28210fe36

Fix atx contextual validation (#1749)
## Motivation
Contextual validation was broken since #1710 due to a change in the error type returned from `ActivationDb.GetNodeLastAtxId()`.

## Changes
Return and detect a specific error type when ATXs for a given node ID do not exist in the DB.

## Test Plan
- [x] Add unit test
- [x] Add an assertion that the error that occurs when a contextually invalid ATX is detected doesn't occur in cloud tests.

activation/atxdb.go
@@ -441,7 +441,7 @@ func (db *ActivationDb) ContextuallyValidateAtx(atx *types.ActivationTxHeader) e
 		}
 	} else {
 		lastAtx, err := db.GetNodeLastAtxId(atx.NodeId)
-		if err != nil && err != database.ErrNotFound {
+		if _, ok := err.(ErrAtxNotFound); err != nil && !ok {
 			db.log.Error("fetching ATX ids failed: %v", err)
 			return err
 		}
@@ -580,6 +580,8 @@ func (db *ActivationDb) addAtxToNodeId(nodeId types.NodeId, atx *types.Activatio
 	return nil
 }
 
+type ErrAtxNotFound error
+
 // GetNodeLastAtxId returns the last atx id that was received for node nodeId
 func (db *ActivationDb) GetNodeLastAtxId(nodeId types.NodeId) (types.AtxId, error) {
 	nodeAtxsIterator := db.atxs.Find(getNodeAtxPrefix(nodeId))
@@ -591,7 +593,7 @@ func (db *ActivationDb) GetNodeLastAtxId(nodeId types.NodeId) (types.AtxId, erro
 	// For the lexicographical order to match the epoch order we must encode the epoch id using big endian encoding when
 	// composing the key.
 	if exists := nodeAtxsIterator.Last(); !exists {
-		return *types.EmptyAtxId, fmt.Errorf("atx for node %v does not exist", nodeId.ShortString())
+		return *types.EmptyAtxId, ErrAtxNotFound(fmt.Errorf("atx for node %v does not exist", nodeId.ShortString()))
 	}
 	return types.AtxId(types.BytesToHash(nodeAtxsIterator.Value())), nil
 }

activation/activationdb_test.go
@@ -1132,3 +1132,16 @@ func TestActivationDb_AwaitAtx(t *testing.T) {
 	atxdb.UnsubscribeAtx(otherId)
 	r.Len(atxdb.atxChannels, 0) // last unsubscribe clears the channel
 }
+
+func TestActivationDb_ContextuallyValidateAtx(t *testing.T) {
+	r := require.New(t)
+
+	lg := log.NewDefault("sigValidation")
+	idStore := NewIdentityStore(database.NewMemDatabase())
+	memesh := mesh.NewMemMeshDB(lg.WithName("meshDB"))
+	atxdb := NewActivationDb(database.NewMemDatabase(), idStore, memesh, layersPerEpochBig, &ValidatorMock{}, lg.WithName("atxDB"))
+
+	atx := types.NewActivationTx(newChallenge(nodeId, 0, *types.EmptyAtxId, *types.EmptyAtxId, 0), [20]byte{}, 5, nil, nil, nil)
+	err := atxdb.ContextuallyValidateAtx(atx.ActivationTxHeader)
+	r.NoError(err)
+}

tests/queries.py
@@ -1,9 +1,9 @@
 import collections
 import random
 import re
+import time
 from datetime import datetime
 
-import time
 from elasticsearch_dsl import Search, Q
 
 from tests import convenience
@@ -516,3 +516,13 @@ def assert_equal_state_roots(indx, ns):
             break
         compare_state_roots(hits)
         layer += 1
+
+
+# =====================================================================================
+# Assert No ATX Validation Errors
+# =====================================================================================
+
+
+def assert_no_contextually_invalid_atxs(indx, ns):
+    hits = query_message(indx, ns, ns, {'M': 'ATX failed contextual validation'})
+    assert len(hits) == 0

tests/test_bs.py
@@ -514,4 +514,5 @@ def test_mining(init_session, setup_network):
 
     queries.assert_equal_layer_hashes(current_index, ns)
     queries.assert_equal_state_roots(current_index, ns)
+    queries.assert_no_contextually_invalid_atxs(current_index, ns)
     validate_hare(current_index, ns)  # validate hare