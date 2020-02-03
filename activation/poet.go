0ae0823e04bee84f26c89b300f3e73b4d66ce515
Moshe Shababo <dicentralized@gmail.com>
2020-02-03 14:37:14 +0200

    HTTP Poet client

--- activation/poet.go
+++ activation/poet.go
@@ -1,12 +1,14 @@
 package activation
 
 import (
+	"bytes"
 	"context"
+	"encoding/json"
 	"fmt"
 	"github.com/spacemeshos/go-spacemesh/common/types"
 	"github.com/spacemeshos/poet/rpc/api"
-	"google.golang.org/grpc"
-	"google.golang.org/grpc/keepalive"
+	"io/ioutil"
+	"net/http"
 	"time"
 )
 
@@ -58,40 +60,106 @@ func NewRPCPoetClient(client api.PoetClient, cleanUp func(cleanup bool) error) *
 	}
 }
 
-// NewRemoteRPCPoetClient returns a new instance of
-// RPCPoetClient for the specified target.
-func NewRemoteRPCPoetClient(target string, ctx context.Context) (*RPCPoetClient, error) {
-	conn, err := newClientConn(target, ctx)
-	if err != nil {
+// HTTPPoetClient implements PoetProvingServiceClient interface.
+type HTTPPoetClient struct {
+	baseUrl    string
+	ctxFactory func() (context.Context, context.CancelFunc)
+}
+
+// A compile time check to ensure that HTTPPoetClient fully implements PoetProvingServiceClient.
+var _ PoetProvingServiceClient = (*HTTPPoetClient)(nil)
+
+// NewHTTPPoetClient returns new instance of HTTPPoetClient for the specified target.
+func NewHTTPPoetClient(target string, ctx context.Context) *HTTPPoetClient {
+	return &HTTPPoetClient{
+		baseUrl: fmt.Sprintf("http://%s/v1", target),
+		ctxFactory: func() (context.Context, context.CancelFunc) {
+			return context.WithTimeout(ctx, 10*time.Second)
+		},
+	}
+}
+
+func (c *HTTPPoetClient) Start(gatewayAddresses []string) error {
+	reqBody := StartRequest{GatewayAddresses: gatewayAddresses}
+	if err := c.req("POST", "/start", reqBody, nil); err != nil {
+		return err
+	}
+
+	return nil
+}
+
+func (c *HTTPPoetClient) submit(challenge types.Hash32) (*types.PoetRound, error) {
+	reqBody := SubmitRequest{Challenge: challenge[:]}
+	resBody := &SubmitResponse{}
+	if err := c.req("POST", "/submit", reqBody, resBody); err != nil {
 		return nil, err
 	}
 
-	client := api.NewPoetClient(conn)
-	cleanUp := func(cleanup bool) error {
-		return conn.Close()
-	}
-
-	return NewRPCPoetClient(client, cleanUp), nil
+	return &types.PoetRound{Id: resBody.RoundId}, nil
 }
 
-// newClientConn returns a new gRPC client
-// connection to the specified target.
-func newClientConn(target string, ctx context.Context) (*grpc.ClientConn, error) {
-	opts := []grpc.DialOption{
-		grpc.WithInsecure(),
-		grpc.WithBlock(),
-		// XXX: this is done to prevent routers from cleaning up our connections (e.g aws load balances..)
-		// TODO: these parameters work for now but we might need to revisit or add them as configuration
-		grpc.WithKeepaliveParams(keepalive.ClientParameters{
-			time.Minute,
-			time.Minute * 3,
-			true,
-		})}
-
-	conn, err := grpc.DialContext(ctx, target, opts...)
-	if err != nil {
-		return nil, fmt.Errorf("failed to connect to rpc server: %v", err)
+func (c *HTTPPoetClient) getPoetServiceId() ([]byte, error) {
+	resBody := &GetInfoResponse{}
+	if err := c.req("GET", "/info", nil, resBody); err != nil {
+		return nil, err
 	}
 
-	return conn, nil
+	return resBody.ServicePubKey, nil
+}
+
+func (c *HTTPPoetClient) req(method string, endUrl string, reqBody interface{}, resBody interface{}) error {
+	jsonReqBody, err := json.Marshal(reqBody)
+	if err != nil {
+		return fmt.Errorf("request json marshal failure: %v", err)
+	}
+
+	url := fmt.Sprintf("%s%s", c.baseUrl, endUrl)
+	req, err := http.NewRequest(method, url, bytes.NewBuffer(jsonReqBody))
+	if err != nil {
+		return err
+	}
+
+	req.Header.Set("Content-Type", "application/json")
+
+	ctx, cancel := c.ctxFactory()
+	defer cancel()
+	req.WithContext(ctx)
+
+	res, err := http.DefaultClient.Do(req)
+	if err != nil {
+		return err
+	}
+	defer res.Body.Close()
+
+	if res.StatusCode != http.StatusOK {
+		data, _ := ioutil.ReadAll(res.Body)
+		return fmt.Errorf("response status code: %d, body: %s", res.StatusCode, string(data))
+	}
+
+	if err := json.NewDecoder(res.Body).Decode(resBody); err != nil {
+		return fmt.Errorf("response json decode failure: %v", err)
+	}
+
+	return nil
+}
+
+type SubmitRequest struct {
+	Challenge []byte `json:"challenge,omitempty"`
+}
+
+type StartRequest struct {
+	GatewayAddresses       []string `json:"gatewayAddresses,omitempty"`
+	DisableBroadcast       bool     `json:"disableBroadcast,omitempty"`
+	ConnAcksThreshold      int      `json:"connAcksThreshold,omitempty"`
+	BroadcastAcksThreshold int      `json:"broadcastAcksThreshold,omitempty"`
+}
+
+type SubmitResponse struct {
+	RoundId string
+}
+
+type GetInfoResponse struct {
+	OpenRoundId        string
+	ExecutingRoundsIds []string
+	ServicePubKey      []byte
 }
