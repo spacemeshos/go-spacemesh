e8f70ba321587791d001a69a21e6b92ce4c3c55e
Elad Gavra <gavrad@gmail.com>
2020-03-16 17:53:54 +0200

    fixes for beacon and oracle

--- hare/refcounttracker_test.go
+++ hare/refcounttracker_test.go
@@ -9,26 +9,26 @@ type MyInt struct {
 	val uint32
 }
 
-func (m MyInt) Id() uint32 {
+func (m MyInt) ID() uint32 {
 	return m.val
 }
 
 func TestRefCountTracker_Track(t *testing.T) {
 	tracker := NewRefCountTracker()
 	mi1 := MyInt{1}
-	tracker.Track(mi1.Id())
+	tracker.Track(mi1.ID())
 	assert.Equal(t, 1, len(tracker.table))
 	mi2 := MyInt{2}
-	tracker.Track(mi2.Id())
+	tracker.Track(mi2.ID())
 	assert.Equal(t, 2, len(tracker.table))
 }
 
 func TestRefCountTracker_CountStatus(t *testing.T) {
 	tracker := NewRefCountTracker()
 	myInt := MyInt{1}
-	assert.Equal(t, uint32(0), tracker.CountStatus(myInt.Id()))
-	tracker.Track(myInt.Id())
-	assert.Equal(t, uint32(1), tracker.CountStatus(myInt.Id()))
-	tracker.Track(myInt.Id())
-	assert.Equal(t, uint32(2), tracker.CountStatus(myInt.Id()))
+	assert.Equal(t, uint32(0), tracker.CountStatus(myInt.ID()))
+	tracker.Track(myInt.ID())
+	assert.Equal(t, uint32(1), tracker.CountStatus(myInt.ID()))
+	tracker.Track(myInt.ID())
+	assert.Equal(t, uint32(2), tracker.CountStatus(myInt.ID()))
 }
