a8c4369e36ccd1ed1d0006d1af676da1e673bcfc
antonlerner <anton.lerner@gmail.com>
2020-03-17 13:33:45 +0000

    fixed wrong order between persist votes and pushLayersToState on lateblock (#1898)
    fixed wrong order between persist votes and pushLayersToState on lateblock

--- mesh/mesh.go
+++ mesh/mesh.go
@@ -235,10 +235,10 @@ func (m *validator) SetProcessedLayer(lyr types.LayerID) {
 func (v *validator) HandleLateBlock(b *types.Block) {
 	v.Info("Validate late block %s", b.Id())
 	oldPbase, newPbase := v.trtl.HandleLateBlock(b)
-	v.pushLayersToState(oldPbase, newPbase)
 	if err := v.trtl.PersistTortoise(); err != nil {
 		v.Error("could not persist Tortoise on late block %s from layer index %d", b.Id(), b.Layer())
 	}
+	v.pushLayersToState(oldPbase, newPbase)
 }
 
 func (v *validator) ValidateLayer(lyr *types.Layer) {
