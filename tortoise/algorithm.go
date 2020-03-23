3286fd3b97d0aedf12d7abe715827f8063da50e2
almogdepaz <almogdepaz@gmail.com>
2020-03-23 14:37:49 +0000

    Tortoise code cleanup (#1896)
    ## Motivation
    code cleanup
    
    ## Changes
    cosmetic
    
    ## Test Plan
    same

--- tortoise/algorithm.go
+++ tortoise/algorithm.go
@@ -4,69 +4,80 @@ import (
 	"github.com/spacemeshos/go-spacemesh/common/types"
 	"github.com/spacemeshos/go-spacemesh/log"
 	"github.com/spacemeshos/go-spacemesh/mesh"
-	"sync"
 )
 
-type Algorithm struct {
-	Tortoise
-	sync.Mutex
-}
-
+//Tortoise represents an instance of a vote counting algorithm
 type Tortoise interface {
-	handleIncomingLayer(ll *types.Layer)
-	PersistTortoise() error
+	HandleLateBlock(b *types.Block) (types.LayerID, types.LayerID)
+	HandleIncomingLayer(ll *types.Layer) (types.LayerID, types.LayerID)
 	LatestComplete() types.LayerID
+	Persist() error
 }
 
-func NewAlgorithm(layerSize int, mdb *mesh.MeshDB, hdist int, lg log.Log) *Algorithm {
-	alg := &Algorithm{Tortoise: NewNinjaTortoise(layerSize, mdb, hdist, lg)}
+type tortoise struct {
+	*ninjaTortoise
+}
+
+//NewTortoise returns a new Tortoise instance
+func NewTortoise(layerSize int, mdb *mesh.MeshDB, hdist int, lg log.Log) Tortoise {
+	alg := &tortoise{ninjaTortoise: NewNinjaTortoise(layerSize, mdb, hdist, lg)}
 	alg.HandleIncomingLayer(mesh.GenesisLayer())
 	return alg
 }
 
-func NewRecoveredAlgorithm(mdb *mesh.MeshDB, lg log.Log) *Algorithm {
-	trtl := &NinjaTortoise{Log: lg, Mesh: mdb}
-
-	ni, err := trtl.RecoverTortoise()
+//NewRecoveredTortoise recovers a previously persisted tortoise copy from mesh.MeshDB
+func NewRecoveredTortoise(mdb *mesh.MeshDB, lg log.Log) Tortoise {
+	tmp, err := RecoverTortoise(mdb)
 	if err != nil {
 		lg.Panic("could not recover tortoise state from disc ", err)
 	}
 
+	trtl := tmp.(*ninjaTortoise)
+
 	lg.Info("recovered tortoise from disc")
-	trtl.ninjaTortoise = ni.(*ninjaTortoise)
+	trtl.db = mdb
+	trtl.logger = lg
 
-	alg := &Algorithm{Tortoise: trtl}
-
-	return alg
+	return &tortoise{ninjaTortoise: trtl}
 }
 
-func (alg *Algorithm) HandleLateBlock(b *types.Block) (types.LayerID, types.LayerID) {
+//HandleLateBlock processes a late blocks votes (for late block definition see white paper)
+//returns the old pbase and new pbase after taking into account the blocks votes
+func (trtl *tortoise) HandleLateBlock(b *types.Block) (types.LayerID, types.LayerID) {
 	//todo feed all layers from b's layer to tortoise
 	l := types.NewLayer(b.Layer())
 	l.AddBlock(b)
-	oldPbase, newPbase := alg.HandleIncomingLayer(l)
+	oldPbase, newPbase := trtl.HandleIncomingLayer(l)
 	log.With().Info("late block ", log.LayerId(uint64(b.Layer())), log.BlockId(b.Id().String()))
 	return oldPbase, newPbase
 }
 
-func (alg *Algorithm) PersistTortoise() error {
-	alg.Lock()
-	defer alg.Unlock()
+//Persist saves a copy of the current tortoise state to the database
+func (trtl *tortoise) Persist() error {
+	trtl.mutex.Lock()
+	defer trtl.mutex.Unlock()
 	log.Info("persist tortoise ")
-	return alg.Tortoise.PersistTortoise()
+	return trtl.ninjaTortoise.persist()
 }
 
-func (alg *Algorithm) HandleIncomingLayer(ll *types.Layer) (types.LayerID, types.LayerID) {
-	alg.Lock()
-	defer alg.Unlock()
-	oldPbase := alg.LatestComplete()
-	alg.Tortoise.handleIncomingLayer(ll)
-	newPbase := alg.LatestComplete()
-	updateMetrics(alg, ll)
+//HandleIncomingLayer processes all layer block votes
+//returns the old pbase and new pbase after taking into account the blocks votes
+func (trtl *tortoise) HandleIncomingLayer(ll *types.Layer) (types.LayerID, types.LayerID) {
+	trtl.mutex.Lock()
+	defer trtl.mutex.Unlock()
+	oldPbase := trtl.latestComplete()
+	trtl.ninjaTortoise.handleIncomingLayer(ll)
+	newPbase := trtl.latestComplete()
+	updateMetrics(trtl, ll)
 	return oldPbase, newPbase
 }
 
-func updateMetrics(alg *Algorithm, ll *types.Layer) {
-	pbaseCount.Set(float64(alg.LatestComplete()))
+//LatestComplete returns the latest complete (a.k.a irreversible) layer
+func (trtl *tortoise) LatestComplete() types.LayerID {
+	return trtl.latestComplete()
+}
+
+func updateMetrics(alg *tortoise, ll *types.Layer) {
+	pbaseCount.Set(float64(alg.latestComplete()))
 	processedCount.Set(float64(ll.Index()))
 }
