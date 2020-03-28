bbf5e1d97c7a81a61745b4d4b1f38092248bbad0
Lane Rettig <lanerettig@gmail.com>
2020-03-28 16:32:36 -0400

    Harmonize commands

--- scripts/win/genproto.bat
+++ scripts/win/genproto.bat
@@ -3,7 +3,7 @@ PowerShell.exe -NoProfile -ExecutionPolicy Bypass -Command "& './scripts/win/ver
 if %ERRORLEVEL% GTR 0 exit /B 1
 
 ECHO Generating protobuf for api/pb
-%USERPROFILE%\protoc-3.6.1\bin\protoc -I%~dp1 -I %GOPATH%\src\github.com\grpc-ecosystem\grpc-gateway\third_party\googleapis --go_out=plugins=grpc:%~dp1 api\pb
+%USERPROFILE%\protoc-3.6.1\bin\protoc -I%CD%\api\pb -I %GOPATH%\src\github.com\grpc-ecosystem\grpc-gateway\third_party\googleapis --go_out=plugins=grpc:%CD%\api\pb %CD%\api\pb\api.proto
 %USERPROFILE%\protoc-3.6.1\bin\protoc -I%CD%\api\pb -I %GOPATH%\src\github.com\grpc-ecosystem\grpc-gateway\third_party\googleapis --grpc-gateway_out=logtostderr=true:%CD%\api\pb %CD%\api\pb\api.proto
 %USERPROFILE%\protoc-3.6.1\bin\protoc -I%CD%\api\pb -I %GOPATH%\src\github.com\grpc-ecosystem\grpc-gateway\third_party\googleapis --swagger_out=logtostderr=true:%CD%\api\pb %CD%\api\pb\api.proto
 
