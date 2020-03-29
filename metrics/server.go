4da2ca77cbf2e5f7adeca3389d61e90a618821c3
Noam Nelke <noam@spacemesh.io>
2020-03-29 14:56:19 +0300

    added docs and fixed lint errors for metrics package. (#1929)

--- metrics/server.go
+++ metrics/server.go
@@ -1,12 +1,18 @@
+// Package metrics defines telemetry primitives to be used across components. it uses the prometheus format.
 package metrics
 
 import (
 	"fmt"
 	"github.com/prometheus/client_golang/prometheus/promhttp"
+	"github.com/spacemeshos/go-spacemesh/log"
 	"net/http"
 )
 
+// StartCollectingMetrics begins listening and supplying metrics on localhost:`metricsPort`/metrics
 func StartCollectingMetrics(metricsPort int) {
 	http.Handle("/metrics", promhttp.Handler())
-	go http.ListenAndServe(fmt.Sprintf(":%v", metricsPort), nil)
+	go func() {
+		err := http.ListenAndServe(fmt.Sprintf(":%v", metricsPort), nil)
+		log.With().Warning("Metrics server stopped: %v", log.Err(err))
+	}()
 }
