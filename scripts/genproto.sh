685c99d0d8e224ab38fd9dc955f6d92b3cca9040
rehs0y <lyosher@gmail.com>
2020-04-30 12:18:23 +0000

    protoc is now saved locally and used from locally if possible. (#2001)
    ## Motivation
    
    easy building.
    
    Closes #1678
    Closes #2000
    <!-- `Closes #XXXX, closes #XXXX, ...` links mentioned issues to this PR and automatically closes them when this it's merged -->
    
    ## Changes
    
    protoc is installed to `devtools` folder and used from there if possible. 
    
    ## Test Plan
    not testing scripts
    
    
    Co-authored-by: Lane Rettig <lanerettig@gmail.com>

--- scripts/genproto.sh
+++ scripts/genproto.sh
@@ -1,14 +1,41 @@
 #!/bin/bash -e
 ./scripts/verify-protoc-gen-go.sh
 
+protoc=./devtools/bin/protoc
+if [[ -v PROTOCPATH ]]; then
+	protoc=${PROTOCPATH};
+fi
+
+compile() {
+        eval $protoc "$@"
+}
+
+errcho() {
+    RED='\033[0;31m'
+    NC='\033[0m' # no color
+    echo -e "${RED}$1${NC}"
+}
+
+
+if [ ! -f $protoc ] ; then
+    errcho "Could not find protoc in $protoc. Trying to use protoc in PATH."
+    protoc=protoc # try loading from PATH
+    if !(hash protoc 2>/dev/null) ; then
+        errcho "Could not find protoc. Try running 'make install' or setting PROTOCPATH to your protoc bin file."
+        exit 1;
+    fi
+fi
+
+echo "using protoc from $protoc"
+
 protobuf_directories=$(find . -not -path ./.git/ -not -path ./.idea/ -type d -name "pb")
 grpc_gateway_path=$(go list -m -f '{{.Dir}}' github.com/grpc-ecosystem/grpc-gateway)
 googleapis_path="$grpc_gateway_path/third_party/googleapis"
 
 while read -r p; do
   echo "Generating protobuf for $p"
-  protoc -I. -I$googleapis_path --go_out=plugins=grpc:. $p/*.proto
+  compile -I. -I$googleapis_path --go_out=plugins=grpc:. $p/*.proto
 done <<< "$protobuf_directories"
 
-protoc -I. -I$googleapis_path --grpc-gateway_out=logtostderr=true:. api/pb/api.proto
-protoc -I. -I$googleapis_path --swagger_out=logtostderr=true:. api/pb/api.proto
+compile -I. -I$googleapis_path --grpc-gateway_out=logtostderr=true:. api/pb/api.proto
+compile -I. -I$googleapis_path --swagger_out=logtostderr=true:. api/pb/api.proto
