0742eafe35d0bbd971360aab04412d67d883e080
Noam Nelke <noam@spacemesh.io>
2020-05-07 11:11:20 +0300

    gcp profiler poc

--- main.go
+++ main.go
@@ -3,6 +3,7 @@
 package main
 
 import (
+	"cloud.google.com/go/profiler"
 	"fmt"
 	"os"
 
@@ -17,6 +18,14 @@ var (
 )
 
 func main() { // run the app
+	if err := profiler.Start(profiler.Config{
+		Service:        "go-spacemesh",
+		ServiceVersion: fmt.Sprintf("%s+%s+%s", version, commit, branch),
+		MutexProfiling: true,
+	}); err != nil {
+		_, _ = fmt.Fprintln(os.Stderr, "failed to start profiler:", err)
+	}
+
 	cmd.Version = version
 	cmd.Commit = commit
 	cmd.Branch = branch
