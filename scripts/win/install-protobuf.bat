c0e33d3848f6c5726699115e3557456103d0fd38
Lane Rettig <lanerettig@gmail.com>
2020-05-22 10:50:34 -0400

    Add local go bin dir to path
    for protoc-gen-go

--- scripts/win/install-protobuf.bat
+++ scripts/win/install-protobuf.bat
@@ -9,6 +9,6 @@ if "%protoc_version%" == "libprotoc 3.6.1" (
     ECHO Extracting protoc...
     powershell.exe -nologo -noprofile -command "& { Add-Type -A 'System.IO.Compression.FileSystem'; [IO.Compression.ZipFile]::ExtractToDirectory('%TEMP%\protoc-3.6.1-win32.zip', '%USERPROFILE%\protoc-3.6.1'); }"
     for /F "tokens=2* delims= " %%f IN ('reg query HKCU\Environment /v PATH ^| findstr /i path') do set OLD_PATH=%%g
-    setx PATH "%USERPROFILE%\protoc-3.6.1\bin;%OLD_PATH%"
+    setx PATH "%USERPROFILE%\protoc-3.6.1\bin;%GOPATH%\bin;%OLD_PATH%"
 )
 
