303bc356ff0113ee59ad763578f69be7e0b40032
Lane Rettig <lanerettig@gmail.com>
2020-05-23 15:12:47 -0400

    Make error clearer

--- setup_env.bat
+++ setup_env.bat
@@ -7,12 +7,22 @@ call ./scripts/win/install-protobuf.bat
 FOR /F "tokens=* USEBACKQ" %%F IN (`go list -m -f {{.Dir}} github.com/golang/protobuf`) DO (
 SET protobuf_path=%%F
 )
+IF NOT DEFINED %protobuf_path% DO (
+  ECHO Failed to find path for github.com/golang/protobuf
+  exit /B 9
+)
+
 ECHO installing protoc-gen-go...
 go install %protobuf_path%/protoc-gen-go
 
 FOR /F "tokens=* USEBACKQ" %%F IN (`go list -m -f {{.Dir}} github.com/grpc-ecosystem/grpc-gateway`) DO (
 SET grpc_gateway_path=%%F
 )
+IF NOT DEFINED %grpc_gateway_path% DO (
+  ECHO Failed to find path for github.com/grpc-ecosystem/grpc-gateway
+  exit /B 1
+)
+
 ECHO installing protoc-gen-grpc-gateway...
 go install %grpc_gateway_path%/protoc-gen-grpc-gateway
 ECHO installing protoc-gen-swagger...
