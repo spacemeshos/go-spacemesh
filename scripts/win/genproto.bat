627223f106f5c574555c624bb4cc61cab3e39cf8
Lane Rettig <lanerettig@gmail.com>
2020-05-25 12:23:51 +0000

    Fix windows setup (#2028)
    Closes #1954
    Requires #1955 
    
    ## Motivation
    Fix some issues that are happening with building on windows
    
    ## Changes
    Fix go and protobuf related issues in windows batch scripts

--- scripts/win/genproto.bat
+++ scripts/win/genproto.bat
@@ -2,8 +2,12 @@
 PowerShell.exe -NoProfile -ExecutionPolicy Bypass -Command "& './scripts/win/verify-protoc-gen-go.ps1'"
 if %ERRORLEVEL% GTR 0 exit /B 1
 
+FOR /F "tokens=* USEBACKQ" %%F IN (`go list -m -f {{.Dir}} github.com/grpc-ecosystem/grpc-gateway`) DO (
+  SET grpc_gateway_path=%%F
+)
+
 ECHO Generating protobuf for api/pb
-%USERPROFILE%\protoc-3.6.1\bin\protoc -I%CD%\api\pb -I %GOPATH%\src\github.com\grpc-ecosystem\grpc-gateway\third_party\googleapis --go_out=plugins=grpc:%CD%\api\pb %CD%\api\pb\api.proto
-%USERPROFILE%\protoc-3.6.1\bin\protoc -I%CD%\api\pb -I %GOPATH%\src\github.com\grpc-ecosystem\grpc-gateway\third_party\googleapis --grpc-gateway_out=logtostderr=true:%CD%\api\pb %CD%\api\pb\api.proto
-%USERPROFILE%\protoc-3.6.1\bin\protoc -I%CD%\api\pb -I %GOPATH%\src\github.com\grpc-ecosystem\grpc-gateway\third_party\googleapis --swagger_out=logtostderr=true:%CD%\api\pb %CD%\api\pb\api.proto
+%USERPROFILE%\protoc-3.6.1\bin\protoc -I%CD%\api\pb -I %grpc_gateway_path%\third_party\googleapis --go_out=plugins=grpc:%CD%\api\pb %CD%\api\pb\api.proto
+%USERPROFILE%\protoc-3.6.1\bin\protoc -I%CD%\api\pb -I %grpc_gateway_path%\third_party\googleapis --grpc-gateway_out=logtostderr=true:%CD%\api\pb %CD%\api\pb\api.proto
+%USERPROFILE%\protoc-3.6.1\bin\protoc -I%CD%\api\pb -I %grpc_gateway_path%\third_party\googleapis --swagger_out=logtostderr=true:%CD%\api\pb %CD%\api\pb\api.proto
 
