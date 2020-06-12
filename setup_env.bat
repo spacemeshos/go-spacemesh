1600a34c6dc17e2b300012035290db0cbd80d3bb
Lane Rettig <lanerettig@gmail.com>
2020-06-12 15:34:49 -0400

    Remove all protobuf-related scripts
    Will be nice if this actually works!

--- setup_env.bat
+++ setup_env.bat
@@ -2,27 +2,3 @@
 PowerShell.exe -NoProfile -ExecutionPolicy Bypass -Command "& './scripts/win/check-go-version.ps1'"
 if %ERRORLEVEL% GTR 0 exit /B 1
 
-call ./scripts/win/install-protobuf.bat
-
-FOR /F "tokens=* USEBACKQ" %%F IN (`go list -m -f {{.Path}} github.com/golang/protobuf`) DO (
-SET protobuf_path=%%F
-)
-IF [%protobuf_path%]==[] (
-  ECHO Failed to find path for github.com/golang/protobuf
-  exit /B 1
-)
-
-ECHO installing protoc-gen-go...
-go install %protobuf_path%/protoc-gen-go
-
-FOR /F "tokens=* USEBACKQ" %%F IN (`go list -m -f {{.Path}} github.com/grpc-ecosystem/grpc-gateway`) DO (
-SET grpc_gateway_path=%%F
-)
-IF [%grpc_gateway_path%]==[] (
-  ECHO Failed to find path for github.com/grpc-ecosystem/grpc-gateway
-  exit /B 1
-)
-
-ECHO installing protoc-gen-grpc-gateway...
-go install %grpc_gateway_path%/protoc-gen-grpc-gateway
-
--- setup_env.sh
+++ setup_env.sh
@@ -1,18 +1,10 @@
 #!/bin/bash -e
 ./scripts/check-go-version.sh
-./scripts/install-protobuf.sh
 
 go mod download
-protobuf_path=$(go list -m -f '{{.Dir}}' github.com/golang/protobuf)
-echo "installing protoc-gen-go..."
-go install $protobuf_path/protoc-gen-go
 
 # Current version of grpc_gateway does not support go modules, so we install it to the gopath
 # TODO: Follow this issue: https://github.com/grpc-ecosystem/grpc-gateway/issues/755
-
-grpc_gateway_path=$(go list -m -f '{{.Dir}}' github.com/grpc-ecosystem/grpc-gateway)
-echo "installing protoc-gen-grpc-gateway"
-go install $grpc_gateway_path/protoc-gen-grpc-gateway
 
 GO111MODULE=off go get golang.org/x/lint/golint # TODO: also install on Windows
 
