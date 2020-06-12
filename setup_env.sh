2c0fd86e970518946389e210d642e66dbe02ed5c
Lane Rettig <lanerettig@gmail.com>
2020-06-12 18:23:25 -0400

    Remove a protobuf reference I missed

--- setup_env.sh
+++ setup_env.sh
@@ -3,9 +3,6 @@
 
 go mod download
 
-# Current version of grpc_gateway does not support go modules, so we install it to the gopath
-# TODO: Follow this issue: https://github.com/grpc-ecosystem/grpc-gateway/issues/755
-
 GO111MODULE=off go get golang.org/x/lint/golint # TODO: also install on Windows
 
 echo "setup complete 🎉"
