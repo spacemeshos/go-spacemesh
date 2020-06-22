70f845d3a8c6da465061779a14ec38a06a8c26b3
Lane Rettig <lanerettig@gmail.com>
2020-06-22 14:17:02 -0400

    Prevent null pointer exception
    This can happen when the system is shut down before the server has had a
    chance to start up

--- api/http_server.go
+++ api/http_server.go
@@ -28,8 +28,10 @@ func NewJSONHTTPServer(port int, grpcPort int) *JSONHTTPServer {
 // Close stops the server.
 func (s *JSONHTTPServer) Close() error {
 	log.Debug("Stopping json-http service...")
-	if err := s.server.Shutdown(context.TODO()); err != nil {
-		return err
+	if s.server != nil {
+		if err := s.server.Shutdown(context.TODO()); err != nil {
+			return err
+		}
 	}
 	return nil
 }
