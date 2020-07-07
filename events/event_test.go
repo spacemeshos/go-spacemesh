423939e692b1e8b293d264f335ba7952682d03b7
Lane Rettig <lanerettig@gmail.com>
2020-07-07 14:41:20 -0400

    Rename streamer -> reporter
    The name is a bit clearer and reflects its general purpose better
    Add a non-API related test for the new reporting interface
    Make event reporting non-blocking

--- events/event_test.go
+++ events/event_test.go
@@ -2,7 +2,11 @@ package events
 
 import (
 	"github.com/spacemeshos/go-spacemesh/common/types"
+	"github.com/spacemeshos/go-spacemesh/log"
+	"github.com/spacemeshos/go-spacemesh/signing"
 	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
+	"sync"
 	"testing"
 	"time"
 )
@@ -43,5 +47,63 @@ func TestNewBlockEvent(t *testing.T) {
 
 		assert.Equal(t, orig, e)
 	}
+}
 
+const (
+	defaultGasLimit = 10
+	defaultFee      = 1
+)
+
+var (
+	addr1    = types.HexToAddress("33333")
+	globalTx = MakeTx(1, addr1, signing.NewEdSigner())
+)
+
+func MakeTx(nonce uint64, recipient types.Address, signer *signing.EdSigner) *types.Transaction {
+	tx, err := types.NewSignedTx(nonce, recipient, 1, defaultGasLimit, defaultFee, signer)
+	if err != nil {
+		log.Error("error creating new signed tx: %v", err)
+		return nil
+	}
+	return tx
+}
+
+func TestEventReporter(t *testing.T) {
+	// There should be no error reporting an event before initializing the reporter
+	ReportNewTx(globalTx)
+
+	// Stream is nil before we initialize it
+	txStream := GetNewTxStream()
+	require.Nil(t, txStream, "expected tx stream not to be initialized")
+
+	InitializeEventReporter()
+	txStream = GetNewTxStream()
+	require.NotNil(t, txStream, "expected tx stream to be initialized")
+
+	// This will not be received as no one is listening
+	// This also makes sure that this call is nonblocking.
+	ReportNewTx(globalTx)
+
+	// listen on the channel
+	wgListening := sync.WaitGroup{}
+	wgListening.Add(1)
+	wgDone := sync.WaitGroup{}
+	wgDone.Add(1)
+	go func() {
+		defer wgDone.Done()
+		// report that we're listening
+		wgListening.Done()
+		require.Equal(t, globalTx, <-txStream, "expected same input and output tx")
+	}()
+
+	// Wait until goroutine is listening
+	wgListening.Wait()
+	ReportNewTx(globalTx)
+
+	// Wait for goroutine to finish
+	wgDone.Wait()
+
+	// This should also not cause an error
+	CloseEventReporter()
+	ReportNewTx(globalTx)
 }
