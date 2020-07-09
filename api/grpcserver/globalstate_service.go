a14063245ca70f243383e33ca3bd182fd0647b8a
Lane Rettig <lanerettig@gmail.com>
2020-07-09 17:07:37 -0400

    WIP: partially implemented AccountDataQuery
    There is currently no way to get smesher for each reward

--- api/grpcserver/globalstate_service.go
+++ api/grpcserver/globalstate_service.go
@@ -7,6 +7,8 @@ import (
 	"github.com/spacemeshos/go-spacemesh/log"
 	"github.com/spacemeshos/go-spacemesh/p2p/peers"
 	"golang.org/x/net/context"
+	"google.golang.org/grpc/codes"
+	"google.golang.org/grpc/status"
 )
 
 // GlobalStateService exposes global state data, output from the STF
@@ -64,6 +66,56 @@ func (s GlobalStateService) Account(ctx context.Context, in *pb.AccountRequest) 
 // AccountDataQuery returns historical account data such as rewards and receipts
 func (s GlobalStateService) AccountDataQuery(ctx context.Context, in *pb.AccountDataQueryRequest) (*pb.AccountDataQueryResponse, error) {
 	log.Info("GRPC GlobalStateService.AccountDataQuery")
+
+	if in.Filter == nil {
+		return nil, status.Errorf(codes.InvalidArgument, "`Filter` must be provided")
+	}
+	if in.Filter.AccountId == nil {
+		return nil, status.Errorf(codes.InvalidArgument, "`Filter.AccountId` must be provided")
+	}
+	if in.Filter.AccountDataFlags == uint32(pb.AccountDataFlag_ACCOUNT_DATA_FLAG_UNSPECIFIED) {
+		return nil, status.Errorf(codes.InvalidArgument, "`Filter.AccountMeshDataFlags` must set at least one bitfield")
+	}
+
+	// Read the filter flags
+	filterTxReceipt := in.Filter.AccountDataFlags&uint32(pb.AccountDataFlag_ACCOUNT_DATA_FLAG_TRANSACTION_RECEIPT) != 0
+	filterReward := in.Filter.AccountDataFlags&uint32(pb.AccountDataFlag_ACCOUNT_DATA_FLAG_REWARD) != 0
+	filterAccount := in.Filter.AccountDataFlags&uint32(pb.AccountDataFlag_ACCOUNT_DATA_FLAG_ACCOUNT) != 0
+
+	addr := types.BytesToAddress(in.Filter.AccountId.Address)
+	res := &pb.AccountDataQueryResponse{}
+
+	if filterTxReceipt {
+		// TODO: Implement this. The node does not implement tx receipts yet.
+	}
+
+	if filterReward {
+		dbRewards, err := s.Mesh.GetRewards(addr)
+		if err != nil {
+			return nil, status.Errorf(codes.Internal, "error getting rewards data")
+		}
+		for _, r := range dbRewards {
+			res.AccountItem = append(res.AccountItem, &pb.AccountData{Item: &pb.AccountData_Reward{
+				Reward: &pb.Reward{
+					Layer:       r.Layer.Uint64(),
+					Total:       &pb.Amount{Value: r.TotalReward},
+					LayerReward: &pb.Amount{Value: r.LayerRewardEstimate},
+					// Leave this out for now as this is changing
+					//LayerComputed: 0,
+					Coinbase: &pb.AccountId{Address: addr.Bytes()},
+					Smesher:  nil,
+				},
+			}})
+		}
+
+	}
+
+	if filterAccount {
+
+	}
+
+	// Adjust for max results, offset
+
 	return nil, nil
 }
 
