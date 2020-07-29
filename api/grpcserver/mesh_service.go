1fa7a2bf845c1fe29e29ae69fdefacfc4af13fe8
Lane Rettig <lanerettig@gmail.com>
2020-07-29 17:52:31 -0400

    Fix error about missing layer state root
    This is expected. Update log message accordingly.

--- api/grpcserver/mesh_service.go
+++ api/grpcserver/mesh_service.go
@@ -323,8 +323,10 @@ func (s MeshService) readLayer(layer *types.Layer, layerStatus pb.Layer_LayerSta
 
 	stateRoot, err := s.Mesh.GetLayerStateRoot(layer.Index())
 	if err != nil {
-		// This could happen with, e.g., a zero-block layer. It's not a fatal error.
-		log.With().Error("error getting state root for layer", layer, log.Err(err))
+		// This is expected. We can only retrieve state root for a layer that was applied to state,
+		// which only happens after it's approved/confirmed.
+		log.With().Debug("error getting state root for layer",
+			layer, log.String("status", layerStatus.String()), log.Err(err))
 	}
 	return &pb.Layer{
 		Number:        layer.Index().Uint64(),
--- mesh/mesh.go
+++ mesh/mesh.go
@@ -213,10 +213,10 @@ func (msh *Mesh) SetLatestLayer(idx types.LayerID) {
 			Status: events.LayerStatusTypeUnknown,
 		})
 	}
-	events.ReportNodeStatusUpdate()
 	defer msh.lkMutex.Unlock()
 	msh.lkMutex.Lock()
 	if idx > msh.latestLayer {
+		events.ReportNodeStatusUpdate()
 		msh.Info("set latest known layer to %v", idx)
 		msh.latestLayer = idx
 		if err := msh.general.Put(constLATEST, idx.Bytes()); err != nil {
@@ -354,6 +354,8 @@ func (msh *Mesh) HandleValidatedLayer(validatedLayer types.LayerID, layer []type
 	}
 	lyr := types.NewExistingLayer(validatedLayer, blocks)
 	invalidBlocks := msh.getInvalidBlocksByHare(lyr)
+	// Reporting of the validated layer happens deep inside this call stack, below
+	// updateStateWithLayer, inside applyState. No need to report here.
 	msh.updateStateWithLayer(validatedLayer, lyr)
 	msh.reInsertTxsToPool(blocks, invalidBlocks, lyr.Index())
 }
@@ -628,7 +630,7 @@ func (msh *Mesh) handleOrphanBlocks(blk *types.Block) {
 	for _, b := range blk.ViewEdges {
 		for layerID, layermap := range msh.orphanBlocks {
 			if _, has := layermap[b]; has {
-				msh.Log.Debug("delete block ", b, "from orphans")
+				msh.Log.With().Debug("delete block from orphans", b)
 				delete(layermap, b)
 				if len(layermap) == 0 {
 					delete(msh.orphanBlocks, layerID)
