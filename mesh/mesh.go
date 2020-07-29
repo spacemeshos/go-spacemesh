6901fd23b0547b638c2501e7862bab74fc59af4f
Lane Rettig <lanerettig@gmail.com>
2020-07-29 16:55:16 -0400

    WIP troubleshoot LayerStream issue
    See #2084

--- mesh/mesh.go
+++ mesh/mesh.go
@@ -280,10 +280,6 @@ func (vl *validator) ValidateLayer(lyr *types.Layer) {
 
 	oldPbase, newPbase := vl.trtl.HandleIncomingLayer(lyr)
 	vl.SetProcessedLayer(lyr.Index())
-	events.ReportNewLayer(events.NewLayer{
-		Layer:  lyr,
-		Status: events.LayerStatusTypeConfirmed,
-	})
 
 	if err := vl.trtl.Persist(); err != nil {
 		vl.Error("could not persist tortoise layer index %d", lyr.Index())
@@ -292,6 +288,10 @@ func (vl *validator) ValidateLayer(lyr *types.Layer) {
 		vl.Error("could not persist validated layer index %d", lyr.Index())
 	}
 	vl.pushLayersToState(oldPbase, newPbase)
+	events.ReportNewLayer(events.NewLayer{
+		Layer:  lyr,
+		Status: events.LayerStatusTypeConfirmed,
+	})
 	vl.Info("done validating layer %v", lyr.Index())
 }
 
