0f1aedfcf75be117883720b96a724e285f8ac3cb
Ivan Danyliuk <ivan.daniluk@gmail.com>
2020-08-01 20:08:40 +0200

    Fix go version check script for MacOS.

--- scripts/check-go-version.sh
+++ scripts/check-go-version.sh
@@ -7,14 +7,34 @@ errcho() {
     echo -e "${RED}$1${NC}"
 }
 
+# Minimial Go version supported
+min_major=1
+min_minor=14
+
 if ! (hash go 2>/dev/null) ; then
     errcho "Could not find a Go installation, please install Go and try again."
     exit 1;
 fi
 
-# Ensure we use Go version 1.14+
-read major minor patch <<< $(go version | sed 's/go version go\([0-9]*\)\.\([0-9]*\)\.\([0-9]*\).*/\1 \2 \3/')
-if [[ ${major} -ne 1 || ${minor} -lt 14 ]]; then
-    errcho "Go 1.14+ is required (v$major.$minor.$patch is installed at `which go`)"
+parse_go_version() {
+	# read version out of 'go version go1.X.Y arch' output into 'v'
+	read _ _ v _ <<< $(go version)
+
+	# slash out 'go'
+	v=${v#go}
+
+	# split into array by '.'
+	vv=(${v//./ })
+
+	# assign values to global vars
+	major=${vv[0]};
+	minor=${vv[1]};
+	patch=${vv[2]};
+}
+parse_go_version
+
+# Ensure Go version satisfies requirements
+if [[ ${major} -ne $min_major || ${minor} -lt $min_minor ]]; then
+    errcho "Go $min_major.$min_minor+ is required ($v is installed at `which go`)"
     exit 1;
 fi
