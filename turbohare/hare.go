812d6db5cceae94373be748b1a0fdfafb94bdc1f
rehs0y <lyosher@gmail.com>
2020-08-30 04:44:51 +0300

    turbohare now save results on tick

--- turbohare/hare.go
+++ turbohare/hare.go
@@ -5,31 +5,63 @@ import (
 	"bytes"
 	"github.com/spacemeshos/go-spacemesh/common/types"
 	"github.com/spacemeshos/go-spacemesh/log"
+	"github.com/spacemeshos/go-spacemesh/timesync"
 	"sort"
+	"time"
 )
 
 type blockProvider interface {
 	LayerBlockIds(layerID types.LayerID) ([]types.BlockID, error)
+	SaveLayerInputVector(lyrid types.LayerID, vector []types.BlockID) error
 }
 
 // SuperHare is a method to provide fast hare results without consensus based on received blocks from gossip
 type SuperHare struct {
-	blocks blockProvider
+	ticker    timesync.LayerTimer
+	blocks    blockProvider
+	closeChan chan struct{}
 }
 
 // New creates a new instance of SuperHare
-func New(blocks blockProvider) *SuperHare {
-	return &SuperHare{blocks}
+func New(blocks blockProvider, ticker timesync.LayerTimer) *SuperHare {
+	return &SuperHare{ticker, blocks, make(chan struct{}, 1)}
 }
 
 // Start is a stub to support service API
 func (h *SuperHare) Start() error {
+	go h.dbSaveRoutine()
 	return nil
 }
 
 // Close is a stup to support service API
 func (h *SuperHare) Close() {
+	close(h.closeChan)
+}
 
+func (h *SuperHare) dbSaveRoutine() {
+	for {
+		select {
+		case l := <-h.ticker:
+			if l < types.GetEffectiveGenesis() {
+				continue
+			}
+
+			time.Sleep(time.Second * 3)
+			// this cover ups the fact that hare doesn't run in tests
+			//  but tortoise relays on hare finishing
+			blks, err := h.blocks.LayerBlockIds(l)
+			if err != nil {
+				log.Error("WTF SUPERHARE?? %v err: %v", l, err)
+				continue
+			}
+			if err := h.blocks.SaveLayerInputVector(l, blks); err != nil {
+				log.Error("WTF SUPERHARE?? %v err: %v", l, err)
+				continue
+			}
+		case <-h.closeChan:
+			return
+		}
+	}
 }
 
 // GetResult is the implementation for receiving consensus process result
