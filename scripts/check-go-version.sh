71b46311a22b3fb0aac5d5e0ce2462d7746a679a
Lane Rettig <lanerettig@gmail.com>
2020-09-01 15:14:59 -0400

    Simplify and cleanup

--- scripts/check-go-version.sh
+++ scripts/check-go-version.sh
@@ -1,40 +1,24 @@
 #!/usr/bin/env bash
 set -e
-# Ensure we use Go installed
+
 errcho() {
     RED='\033[0;31m'
     NC='\033[0m' # no color
     echo -e "${RED}$1${NC}"
 }
 
-# Minimial Go version supported
+# Minimal Go version supported
 min_major=1
 min_minor=14
 
+# Ensure Go is installed
 if ! (hash go 2>/dev/null) ; then
     errcho "Could not find a Go installation, please install Go and try again."
     exit 1;
 fi
 
-parse_go_version() {
-	# read version out of 'go version go1.X.Y arch' output into 'v'
-	read _ _ v _ <<< $(go version)
-
-	# slash out 'go'
-	v=${v#go}
-
-	# split into array by '.'
-	vv=(${v//./ })
-
-	# assign values to global vars
-	major=${vv[0]};
-	minor=${vv[1]};
-	patch=${vv[2]};
-}
-parse_go_version
-
-# Ensure Go version satisfies requirements
+read major minor <<< $(go version | sed 's/go version go\([0-9]*\)\.\([0-9]*\).*/\1 \2/')
 if [[ ${major} -ne $min_major || ${minor} -lt $min_minor ]]; then
-    errcho "Go $min_major.$min_minor+ is required ($v is installed at `which go`)"
+    errcho "Go ${min_major}.${min_minor}+ is required (v$major.$minor is installed at `which go`)"
     exit 1;
 fi
