d13b6626b2a7978d8c14787ed6364b98c092001c
Anton Lerner <anton.lerner@gmail.com>
2020-11-08 14:47:22 +0000

    Sync causes blocks to be considered invalid fix (#2181)
    ## Motivation
    Blocks are invalid because sync ATXs does not end before node is marked synced- causing blocks to be invalid because ATXs are not yet found
    
    ## Changes
    Re ordered syncing of ATXs do be done before layers are validated. 
    
    ## Test Plan
    Standart tests
    
    ## TODO
    <!-- This section should be removed when all items are complete -->
    - [ ] Explain motivation or link existing issue(s)
    - [ ] Test changes and document test plan
    - [ ] Update documentation as needed

--- sync/syncer.go
+++ sync/syncer.go
@@ -335,11 +335,13 @@ func (s *Syncer) synchronise() {
 	// we have all the data of the prev layers so we can simply validate
 	if s.weaklySynced(curr) {
 		s.handleWeaklySynced()
+		err := s.syncEpochActivations(curr.GetEpoch())
+		if err != nil {
+			s.With().Error("cannot fetch epoch atxs ", curr, log.Err(err))
+		}
 	} else {
 		s.handleNotSynced(s.ProcessedLayer() + 1)
 	}
-
-	s.syncAtxs(curr)
 }
 
 func (s *Syncer) handleWeaklySynced() {
@@ -446,7 +448,7 @@ func (s *Syncer) handleNotSynced(currentSyncLayer types.LayerID) {
 				return
 			}
 		}
-
+		s.syncAtxs(currentSyncLayer)
 		s.ValidateLayer(lyr) // wait for layer validation
 	}
 
