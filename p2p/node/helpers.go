cc1f376e3d975e222964296324149aa6e749e37b
rehs0y <lyosher@gmail.com>
2021-02-10 16:43:37 +0000

    p2p roundtrip unit test fix (#2274)
    ## Motivation
    The test was failing occasionally and was pointed to me by @valar999   
    ## Changes
    the ffunction  is now able to check a udp port and request specific port

--- p2p/node/helpers.go
+++ p2p/node/helpers.go
@@ -2,11 +2,13 @@ package node
 
 import (
 	"errors"
-	"github.com/spacemeshos/go-spacemesh/p2p/p2pcrypto"
-	"github.com/spacemeshos/go-spacemesh/rand"
+	"fmt"
 	"net"
 	"testing"
 	"time"
+
+	"github.com/spacemeshos/go-spacemesh/p2p/p2pcrypto"
+	"github.com/spacemeshos/go-spacemesh/rand"
 )
 
 var localhost = net.IP{127, 0, 0, 1}
@@ -22,12 +24,16 @@ func GenerateTestNode(t *testing.T) (LocalNode, *Info) {
 // GenerateTestNodeWithConfig creates a local test node without persisting data to local store.
 func GenerateTestNodeWithConfig(t *testing.T) (LocalNode, *Info) {
 
-	port, err := GetUnboundedPort()
+	tcpPort, err := GetUnboundedPort("tcp", 0)
+	if err != nil {
+		t.Error("failed to get a port to bind", err)
+	}
+	udpPort, err := GetUnboundedPort("udp", tcpPort)
 	if err != nil {
 		t.Error("failed to get a port to bind", err)
 	}
 
-	addr := net.TCPAddr{IP: net.ParseIP("0.0.0.0"), Port: port}
+	addr := net.TCPAddr{IP: net.ParseIP("0.0.0.0"), Port: tcpPort}
 
 	var localNode LocalNode
 
@@ -36,7 +42,7 @@ func GenerateTestNodeWithConfig(t *testing.T) (LocalNode, *Info) {
 		t.Error(ErrFailedToCreate)
 		return emptyNode, nil
 	}
-	return localNode, &Info{localNode.PublicKey().Array(), addr.IP, uint16(port), uint16(port)}
+	return localNode, &Info{localNode.PublicKey().Array(), addr.IP, uint16(tcpPort), uint16(udpPort)}
 }
 
 // GenerateRandomNodeData generates a remote random node data for testing.
@@ -56,12 +62,31 @@ func GenerateRandomNodesData(n int) []*Info {
 	return res
 }
 
-// GetUnboundedPort returns a port that is for sure unbounded or an error.
-func GetUnboundedPort() (int, error) {
-	l, e := net.Listen("tcp", ":0")
-	if e != nil {
-		return 0, e
+// GetUnboundedPort returns a port that is for sure unbounded or an error
+// 	optionalPort can be 0 to return a random port.
+func GetUnboundedPort(protocol string, optionalPort int) (int, error) {
+	if protocol == "tcp" {
+		l, e := net.Listen("tcp", fmt.Sprintf(":%v", optionalPort))
+		if e != nil {
+			l, e = net.Listen("tcp", ":0")
+			if e != nil {
+				return 0, e
+			}
+		}
+		defer l.Close()
+		return l.Addr().(*net.TCPAddr).Port, nil
 	}
-	defer l.Close()
-	return l.Addr().(*net.TCPAddr).Port, nil
+	if protocol == "udp" {
+		l, e := net.ListenUDP("udp", &net.UDPAddr{Port: optionalPort})
+		if e != nil {
+			l, e = net.ListenUDP("udp", &net.UDPAddr{Port: 0})
+			if e != nil {
+				return 0, e
+			}
+		}
+		defer l.Close()
+		return l.LocalAddr().(*net.UDPAddr).Port, nil
+	}
+
+	return 0, errors.New("unknown protocol")
 }
