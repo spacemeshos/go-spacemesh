51f1aaf7792897efbe7c6a478d7cec3c7f4b4196
Lane Rettig <lanerettig@gmail.com>
2021-02-25 12:44:08 -0500

    Lower tortoise thresholds for testnet

--- tortoise/ninja_tortoise.go
+++ tortoise/ninja_tortoise.go
@@ -16,8 +16,9 @@ type vec [2]int
 type patternID uint32 // this hash does not include the layer id
 
 const ( // Threshold
-	window          = 10
-	globalThreshold = 0.6
+	window             = 10
+	globalThreshold    = 0.5
+	newlyGoodThreshold = 0.4
 )
 
 var ( // correction vectors type
@@ -382,10 +383,10 @@ func (ni *ninjaTortoise) findMinimalNewlyGoodLayer(lyr *types.Layer) types.Layer
 		// todo do this as part of previous for if possible
 		// for each p that was updated and not the good layer of j check if it is the good layer
 		for p := range sUpdated {
-			// if a majority supports p (p is good)
+			// if a minimum threshold supports p (p is good)
 			// according to tal we dont have to know the exact amount, we can multiply layer size by number of layers
 			jGood, found := ni.TGood[j]
-			threshold := 0.5 * float64(types.LayerID(ni.AvgLayerSize)*(ni.Last-p.Layer()))
+			threshold := newlyGoodThreshold * float64(types.LayerID(ni.AvgLayerSize)*(ni.Last-p.Layer()))
 			if (jGood != p || !found) && float64(ni.TSupport[p]) > threshold {
 				ni.TGood[p.Layer()] = p
 				// if p is the new minimal good layer
