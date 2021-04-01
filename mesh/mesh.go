31fcc9df563a195fa48715865efdb542013af065
Sidharth Sankhe <sankhe@seas.upenn.edu>
2021-04-01 17:40:58 -0400

    Added debugging info

--- mesh/mesh.go
+++ mesh/mesh.go
@@ -828,7 +828,7 @@ func (msh *Mesh) accumulateRewards(l *types.Layer, params Config) {
 		for smesherString, cnt := range smesherAccountEntry {
 			smesherEntry, err := types.StringToNodeID(smesherString)
 			if err != nil {
-				log.With().Error("unable to convert bytes to nodeid", log.Err(err))
+				log.With().Error("unable to convert string to nodeid", log.Err(err), log.String("smesherString", smesherString))
 				return
 			}
 			events.ReportRewardReceived(events.Reward{
