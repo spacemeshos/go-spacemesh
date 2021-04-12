a8efe8b0a72c1e542ddb7037c125d9c3fdc8a727
Nikita Kryuchkov <nkryuchkov10@gmail.com>
2021-04-12 21:56:43 +0300

    Extend a comment

--- tortoisebeacon/config.go
+++ tortoisebeacon/config.go
@@ -14,7 +14,7 @@ type Config struct { // TODO(nkryuchkov): use unused fields
 func DefaultConfig() Config {
 	return Config{
 		ATXThreshold: 50, // TODO(nkryuchkov): change
-		RoundsNumber: 6,  // TODO(nkryuchkov): arbitrary for now, find a better number
+		RoundsNumber: 6,  // TODO(nkryuchkov): arbitrary for now, find a better number, r = (κ+log(m)) / log(1/(1−p)), where r = rounds number, p = weak coin probability, m = number of identities
 		WakeupDelta:  30, // TODO(nkryuchkov): arbitrary for now, find a better number
 		Theta:        1,  // TODO(nkryuchkov): change
 		HDist:        20, // TODO(nkryuchkov): arbitrary for now, find a better number
