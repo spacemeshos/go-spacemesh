spacemeshos/go-spacemesh

commit efa7538ba4fbb4ea90be43c827233a580c736e19

Refactored block builder + begin of active set size (#2011)
## Motivation
part of #1977

mesh/mesh.go
@@ -22,9 +22,6 @@ import (
 
 const (
 	layerSize = 200
-
-	// Genesis layer index
-	Genesis = types.LayerID(0)
 )
 
 var constTrue = []byte{1}
@@ -62,6 +59,7 @@ type txProcessor interface {
 	GetLayerApplied(txID types.TransactionID) *types.LayerID
 	GetStateRoot() types.Hash32
 	LoadState(layer types.LayerID) error
+	ValidateAndAddTxToPool(tx *types.Transaction) error
 }
 
 type txMemPoolInValidator interface {
@@ -80,10 +78,6 @@ type AtxDB interface {
 	SyntacticallyValidateAtx(atx *types.ActivationTx) error
 }
 
-type blockBuilder interface {
-	ValidateAndAddTxToPool(tx *types.Transaction) error
-}
-
 // Mesh is the logic layer above our mesh.DB database
 type Mesh struct {
 	log.Log
@@ -92,9 +86,7 @@ type Mesh struct {
 	txProcessor
 	Validator
 	trtl               tortoise
-	blockBuilder       blockBuilder
 	txInvalidator      txMemPoolInValidator
-	atxInvalidator     atxMemPoolInValidator
 	config             Config
 	latestLayer        types.LayerID
 	latestLayerInState types.LayerID
@@ -112,18 +104,19 @@ type Mesh struct {
 }
 
 // NewMesh creates a new instant of a mesh
-func NewMesh(db *DB, atxDb AtxDB, rewardConfig Config, mesh tortoise, txInvalidator txMemPoolInValidator, atxInvalidator atxMemPoolInValidator, pr txProcessor, logger log.Log) *Mesh {
+func NewMesh(db *DB, atxDb AtxDB, rewardConfig Config, mesh tortoise, txInvalidator txMemPoolInValidator, pr txProcessor, logger log.Log) *Mesh {
 	ll := &Mesh{
-		Log:             logger,
-		trtl:            mesh,
-		txInvalidator:   txInvalidator,
-		atxInvalidator:  atxInvalidator,
-		txProcessor:     pr,
-		done:            make(chan struct{}),
-		DB:              db,
-		config:          rewardConfig,
-		AtxDB:           atxDb,
-		nextValidLayers: make(map[types.LayerID]*types.Layer),
+		Log:                logger,
+		trtl:               mesh,
+		txInvalidator:      txInvalidator,
+		txProcessor:        pr,
+		done:               make(chan struct{}),
+		DB:                 db,
+		config:             rewardConfig,
+		AtxDB:              atxDb,
+		nextValidLayers:    make(map[types.LayerID]*types.Layer),
+		latestLayer:        types.GetEffectiveGenesis(),
+		latestLayerInState: types.GetEffectiveGenesis(),
 	}
 
 	ll.Validator = &validator{ll, 0}
@@ -132,8 +125,8 @@ func NewMesh(db *DB, atxDb AtxDB, rewardConfig Config, mesh tortoise, txInvalida
 }
 
 // NewRecoveredMesh creates new instance of mesh with recovered mesh data fom database
-func NewRecoveredMesh(db *DB, atxDb AtxDB, rewardConfig Config, mesh tortoise, txInvalidator txMemPoolInValidator, atxInvalidator atxMemPoolInValidator, pr txProcessor, logger log.Log) *Mesh {
-	msh := NewMesh(db, atxDb, rewardConfig, mesh, txInvalidator, atxInvalidator, pr, logger)
+func NewRecoveredMesh(db *DB, atxDb AtxDB, rewardConfig Config, mesh tortoise, txInvalidator txMemPoolInValidator, pr txProcessor, logger log.Log) *Mesh {
+	msh := NewMesh(db, atxDb, rewardConfig, mesh, txInvalidator, pr, logger)
 
 	latest, err := db.general.Get(constLATEST)
 	if err != nil {
@@ -190,11 +183,6 @@ func (msh *Mesh) CacheWarmUp(layerSize int) {
 	}
 
 	msh.Info("cache warm up done")
-}
-
-// SetBlockBuilder sets the block builder in use by mesh
-func (msh *Mesh) SetBlockBuilder(blockBuilder blockBuilder) {
-	msh.blockBuilder = blockBuilder
 }
 
 // LatestLayerInState returns the latest layer we applied to state
@@ -311,7 +299,7 @@ func (msh *Mesh) reInsertTxsToPool(validBlocks, invalidBlocks []*types.Block, l 
 		msh.removeRejectedFromAccountTxs(account, grouped, l)
 	}
 	for _, tx := range returnedTxs {
-		err := msh.blockBuilder.ValidateAndAddTxToPool(tx)
+		err := msh.ValidateAndAddTxToPool(tx)
 		// We ignore errors here, since they mean that the tx is no longer valid and we shouldn't re-add it
 		if err == nil {
 			msh.With().Info("transaction from contextually invalid block re-added to mempool", tx.ID())
@@ -375,7 +363,7 @@ func (msh *Mesh) updateStateWithLayer(validatedLayer types.LayerID, layer *types
 		msh.maxValidatedLayer = validatedLayer
 	}
 	if validatedLayer > latest+1 {
-		log.Info("early layer result was received for layer %v, max validated so far %v", validatedLayer, msh.maxValidatedLayer)
+		log.Info("early layer result was received for layer %v, max validated so far %v latest %v", validatedLayer, msh.maxValidatedLayer, latest)
 		msh.nextValidLayers[validatedLayer] = layer
 		return
 	}
@@ -522,7 +510,7 @@ func (msh *Mesh) AddBlock(blk *types.Block) error {
 
 // SetZeroBlockLayer tags lyr as a layer without blocks
 func (msh *Mesh) SetZeroBlockLayer(lyr types.LayerID) error {
-
+	msh.Info("setting zero block layer %v", lyr)
 	// check database for layer
 	_, err := msh.GetLayer(lyr)
 
@@ -543,13 +531,7 @@ func (msh *Mesh) SetZeroBlockLayer(lyr types.LayerID) error {
 	lm.m.Lock()
 	defer lm.m.Unlock()
 	// layer doesnt exist, need to insert new layer
-	blockIds := make([]types.BlockID, 0, 1)
-	w, err := types.BlockIdsToBytes(blockIds)
-	if err != nil {
-		return errors.New("could not encode layer blk ids")
-	}
-	err = msh.layers.Put(lyr.Bytes(), w)
-	return err
+	return msh.AddZeroBlockLayer(lyr)
 }
 
 // AddBlockWithTxs adds a block to the database
@@ -571,7 +553,12 @@ func (msh *Mesh) AddBlockWithTxs(blk *types.Block, txs []*types.Transaction, atx
 	}
 
 	// Store block (delete if storing ATXs fails)
-	if err := msh.DB.AddBlock(blk); err != nil && err != ErrAlreadyExist {
+	err := msh.DB.AddBlock(blk)
+	if err != nil && err == ErrAlreadyExist {
+		return nil
+	}
+
+	if err != nil {
 		msh.With().Error("failed to add block", blk.ID(), log.Err(err))
 		return err
 	}
@@ -600,10 +587,6 @@ func (msh *Mesh) AddBlockWithTxs(blk *types.Block, txs []*types.Transaction, atx
 func (msh *Mesh) invalidateFromPools(blk *types.MiniBlock) {
 	for _, id := range blk.TxIDs {
 		msh.txInvalidator.Invalidate(id)
-	}
-	msh.atxInvalidator.Invalidate(blk.ATXID)
-	for _, id := range blk.ATXIDs {
-		msh.atxInvalidator.Invalidate(id)
 	}
 }
 
@@ -645,7 +628,7 @@ func (msh *Mesh) GetOrphanBlocksBefore(l types.LayerID) ([]types.BlockID, error)
 
 	blocks, err := msh.LayerBlockIds(l - 1)
 	if err != nil {
-		return nil, errors.New(fmt.Sprint("failed getting latest layer ", err))
+		return nil, fmt.Errorf("failed getting latest layer %v err %v", l-1, err)
 	}
 
 	// add last layer blocks
@@ -720,13 +703,15 @@ func (msh *Mesh) accumulateRewards(l *types.Layer, params Config) {
 
 }
 
-// GenesisBlock is a mock genesis block used until genesis flow is implemented
-var GenesisBlock = types.NewExistingBlock(0, []byte("genesis"))
+// GenesisBlock is a is the first static block that xists at the beginning of each network. it exist one layer before actual blocks could be created
+func GenesisBlock() *types.Block {
+	return types.NewExistingBlock(types.GetEffectiveGenesis(), []byte("genesis"))
+}
 
 // GenesisLayer generates layer 0 should be removed after the genesis flow is implemented
 func GenesisLayer() *types.Layer {
-	l := types.NewLayer(Genesis)
-	l.AddBlock(GenesisBlock)
+	l := types.NewLayer(types.GetEffectiveGenesis())
+	l.AddBlock(GenesisBlock())
 	return l
 }
 

mesh/meshdb.go
@@ -32,6 +32,7 @@ type DB struct {
 	general            database.Database
 	unappliedTxs       database.Database
 	unappliedTxsMutex  sync.Mutex
+	blockMutex         sync.RWMutex
 	orphanBlocks       map[types.LayerID]map[types.BlockID]struct{}
 	layerMutex         map[types.LayerID]*layerMutex
 	lhMutex            sync.Mutex
@@ -78,6 +79,8 @@ func NewPersistentMeshDB(path string, blockCacheSize int, log log.Log) (*DB, err
 		layerMutex:         make(map[types.LayerID]*layerMutex),
 		exit:               make(chan struct{}),
 	}
+	ll.AddBlock(GenesisBlock())
+	ll.SaveContextualValidity(GenesisBlock().ID(), true)
 	return ll, nil
 }
 
@@ -106,6 +109,8 @@ func NewMemMeshDB(log log.Log) *DB {
 		layerMutex:         make(map[types.LayerID]*layerMutex),
 		exit:               make(chan struct{}),
 	}
+	ll.AddBlock(GenesisBlock())
+	ll.SaveContextualValidity(GenesisBlock().ID(), true)
 	return ll
 }
 
@@ -125,6 +130,8 @@ var ErrAlreadyExist = errors.New("block already exist in database")
 
 // AddBlock adds a block to the database
 func (m *DB) AddBlock(bl *types.Block) error {
+	m.blockMutex.Lock()
+	defer m.blockMutex.Unlock()
 	if _, err := m.getBlockBytes(bl.ID()); err == nil {
 		m.With().Warning(ErrAlreadyExist.Error(), bl.ID())
 		return ErrAlreadyExist
@@ -137,11 +144,6 @@ func (m *DB) AddBlock(bl *types.Block) error {
 
 // GetBlock gets a block from the database by id
 func (m *DB) GetBlock(id types.BlockID) (*types.Block, error) {
-	if id == GenesisBlock.ID() {
-		// todo fit real genesis here
-		return GenesisBlock, nil
-	}
-
 	if blkh := m.blockCache.Get(id); blkh != nil {
 		return blkh, nil
 	}
@@ -231,7 +233,8 @@ func (m *DB) LayerBlockIds(index types.LayerID) ([]types.BlockID, error) {
 	}
 
 	if len(idsBytes) == 0 {
-		return nil, fmt.Errorf("no ids for layer %v in database ", index)
+		//zero block layer
+		return []types.BlockID{}, nil
 	}
 
 	blockIds, err := types.BytesToBlockIds(idsBytes)
@@ -240,6 +243,16 @@ func (m *DB) LayerBlockIds(index types.LayerID) ([]types.BlockID, error) {
 	}
 
 	return blockIds, nil
+}
+
+// AddZeroBlockLayer tags lyr as a layer without blocks
+func (m *DB) AddZeroBlockLayer(index types.LayerID) error {
+	blockIds := make([]types.BlockID, 0, 1)
+	w, err := types.BlockIdsToBytes(blockIds)
+	if err != nil {
+		return errors.New("could not encode layer blk ids")
+	}
+	return m.layers.Put(index.Bytes(), w)
 }
 
 func (m *DB) getBlockBytes(id types.BlockID) ([]byte, error) {

sync/requests.go
@@ -41,6 +41,28 @@ func layerIdsReqFactory(lyr types.LayerID) requestFactory {
 	}
 }
 
+func getEpochAtxIds(epoch types.EpochID, s networker, peer p2ppeers.Peer) (chan interface{}, error) {
+	ch := make(chan interface{}, 1)
+	foo := func(msg []byte) {
+		defer close(ch)
+		if len(msg) == 0 || msg == nil {
+			s.Warning("peer %v responded with nil to atx %v request", peer, epoch)
+			return
+		}
+		var atxIDs []types.ATXID
+		err := types.BytesToInterface(msg, &atxIDs)
+		if err != nil {
+			s.Error("could not unmarshal mesh.LayerIDs response ", err)
+			return
+		}
+		ch <- atxIDs
+	}
+	if err := s.SendRequest(atxIdsMsg, epoch.ToBytes(), peer, foo); err != nil {
+		return nil, err
+	}
+	return ch, nil
+}
+
 func hashReqFactory(lyr types.LayerID) requestFactory {
 	return func(s networker, peer p2ppeers.Peer) (chan interface{}, error) {
 		ch := make(chan interface{}, 1)
@@ -59,6 +81,32 @@ func hashReqFactory(lyr types.LayerID) requestFactory {
 			ch <- &peerHashPair{peer: peer, hash: h}
 		}
 		if err := s.SendRequest(layerHashMsg, lyr.Bytes(), peer, foo); err != nil {
+			return nil, err
+		}
+
+		return ch, nil
+	}
+
+}
+
+func atxHashReqFactory(ep types.EpochID) requestFactory {
+	return func(s networker, peer p2ppeers.Peer) (chan interface{}, error) {
+		ch := make(chan interface{}, 1)
+		foo := func(msg []byte) {
+			defer close(ch)
+			if len(msg) == 0 || msg == nil {
+				s.Warning("peer %v responded with nil to hash request layer %v", peer, ep)
+				return
+			}
+			if len(msg) != types.Hash32Length {
+				s.Error("received layer hash in wrong length, len %v", len(msg))
+				return
+			}
+			var h types.Hash32
+			h.SetBytes(msg)
+			ch <- &peerHashPair{peer: peer, hash: h}
+		}
+		if err := s.SendRequest(atxIdrHashMsg, ep.ToBytes(), peer, foo); err != nil {
 			return nil, err
 		}
 

commit fd66aa1c3569a6fdc328f92b8ed45592be7c1730

Fix SetZeroBlockLayer logic (#2374)
## Motivation
See https://github.com/spacemeshos/bug-reports/issues/1

## Changes
Fixes a bug in SetZeroBlockLayer to make it idempotent: the same layer can be marked as a zero block layer any number of times (not only once)

Print the name of each database file as it's being closed for debug purposes

Looks like the bug was introduced in #2011

## Test Plan
Includes two new tests

## TODO
<!-- This section should be removed when all items are complete -->
- [x] Explain motivation or link existing issue(s)
- [x] Test changes and document test plan
- [ ] Update documentation as needed

## DevOps Notes
<!-- Please uncheck these items as applicable to make DevOps aware of changes that may affect releases -->
- [x] This PR does not require configuration changes (e.g., environment variables, GitHub secrets, VM resources)
- [x] This PR does not affect public APIs
- [x] This PR does not rely on a new version of external services (PoET, elasticsearch, etc.)
- [ ] This PR does not make changes to log messages (which monitoring infrastructure may rely on)

database/database.go
@@ -152,9 +152,9 @@ func (db *LDBDatabase) Close() {
 	}
 	err := db.db.Close()
 	if err == nil {
-		db.log.Info("Database closed")
+		db.log.With().Info("database closed", log.String("file", db.fn))
 	} else {
-		db.log.Error("Failed to close database", "err", err)
+		db.log.With().Error("failed to close database", log.String("file", db.fn), log.Err(err))
 	}
 }
 

mesh/mesh.go
@@ -164,7 +164,7 @@ func NewRecoveredMesh(db *DB, atxDb AtxDB, rewardConfig Config, mesh tortoise, t
 		msh.pushLayersToState(msh.LatestLayerInState()+1, msh.trtl.LatestComplete())
 	}
 
-	msh.With().Info("recovered mesh from disc",
+	msh.With().Info("recovered mesh from disk",
 		log.FieldNamed("latest_layer", msh.latestLayer),
 		log.FieldNamed("validated_layer", msh.ProcessedLayer()),
 		log.String("layer_hash", util.Bytes2Hex(msh.layerHash)),
@@ -206,7 +206,7 @@ func (msh *Mesh) SetLatestLayer(idx types.LayerID) {
 	// Report the status update, as well as the layer itself.
 	layer, err := msh.GetLayer(idx)
 	if err != nil {
-		msh.Error("error reading layer data for layer", layer, log.Err(err))
+		msh.With().Error("error reading layer data", idx, log.Err(err))
 	} else {
 		events.ReportNewLayer(events.NewLayer{
 			Layer:  layer,
@@ -220,7 +220,7 @@ func (msh *Mesh) SetLatestLayer(idx types.LayerID) {
 		msh.With().Info("set latest known layer", idx)
 		msh.latestLayer = idx
 		if err := msh.general.Put(constLATEST, idx.Bytes()); err != nil {
-			msh.Error("could not persist Latest layer index")
+			msh.Error("could not persist latest layer index")
 		}
 	}
 }
@@ -611,17 +611,16 @@ func (msh *Mesh) AddBlock(blk *types.Block) error {
 func (msh *Mesh) SetZeroBlockLayer(lyr types.LayerID) error {
 	msh.Info("setting zero block layer %v", lyr)
 	// check database for layer
-	_, err := msh.GetLayer(lyr)
-
-	if err == nil {
-		// layer exists
-		msh.Info("layer has blocks, dont set layer to 0 ")
-		return fmt.Errorf("layer exists")
-	}
-
-	if err != database.ErrNotFound {
-		// database error
-		return fmt.Errorf("could not fetch layer from database %s", err)
+	if l, err := msh.GetLayer(lyr); err != nil {
+		if err != database.ErrNotFound {
+			msh.With().Error("error trying to fetch layer from database", lyr, log.Err(err))
+			return err
+		}
+	} else if len(l.Blocks()) != 0 {
+		msh.With().Error("layer has blocks, cannot tag as zero block layer",
+			l,
+			log.Int("num_blocks", len(l.Blocks())))
+		return fmt.Errorf("layer has blocks")
 	}
 
 	msh.SetLatestLayer(lyr)

mesh/meshdb.go
@@ -103,10 +103,10 @@ func NewPersistentMeshDB(path string, blockCacheSize int, log log.Log) (*DB, err
 // PersistentData checks to see if db is empty
 func (m *DB) PersistentData() bool {
 	if _, err := m.general.Get(constLATEST); err == nil {
-		m.Info("found data to recover on disc")
+		m.Info("found data to recover on disk")
 		return true
 	}
-	m.Info("did not find data to recover on disc")
+	m.Info("did not find data to recover on disk")
 	return false
 }
 

sync/requests.go
@@ -245,7 +245,7 @@ func inputVectorReqFactory(lyreq []byte) requestFactory {
 			s.Info("handle inputvec response")
 			defer close(ch)
 			if len(msg) == 0 || msg == nil {
-				s.Warning("peer responded with nil to inputvec request %v", peer, lyreq)
+				s.With().Warning("peer responded with nil to inputvec request", peer)
 				return
 			}
 

sync/syncer_test.go
@@ -182,7 +182,6 @@ func createBlock(activationTx types.ActivationTx, signer *signing.EdSigner) *typ
 }
 
 func TestSyncer_Close(t *testing.T) {
-
 	syncs, _, _ := SyncMockFactory(2, conf, t.Name(), memoryDB, newMockPoetDb)
 
 	sync := syncs[0]
@@ -200,7 +199,6 @@ func TestSyncer_Close(t *testing.T) {
 	assert.True(t, !ok, "channel 'forceSync' still open")
 	_, ok = <-sync.exit
 	assert.True(t, !ok, "channel 'exit' still open")
-
 }
 
 func TestSyncProtocol_BlockRequest(t *testing.T) {
@@ -210,8 +208,9 @@ func TestSyncProtocol_BlockRequest(t *testing.T) {
 	assert.NoError(t, err)
 	syncs, nodes, _ := SyncMockFactory(2, conf, t.Name(), memoryDB, newMockPoetDb)
 	syncObj := syncs[0]
-	syncObj2 := syncs[1]
 	defer syncObj.Close()
+	syncObj2 := syncs[1]
+	defer syncObj2.Close()
 	block := types.NewExistingBlock(1, []byte(rand.String(8)), nil)
 	addTxsToPool(syncObj.txpool, []*types.Transaction{tx1})
 
@@ -232,7 +231,6 @@ func TestSyncProtocol_BlockRequest(t *testing.T) {
 	case <-timeout.C:
 		assert.Fail(t, "no message received on channel")
 	}
-
 }
 
 func TestSyncProtocol_LayerHashRequest(t *testing.T) {
@@ -573,7 +571,6 @@ loop:
 			time.Sleep(1 * time.Second)
 		}
 	}
-
 }
 
 func getPeersMock(peers []p2ppeers.Peer) *p2ppeers.Peers {
@@ -947,7 +944,6 @@ end:
 	log.Debug("sync 3 ", syncObj3.ProcessedLayer())
 	log.Debug("sync 4 ", syncObj4.ProcessedLayer())
 	log.Debug("sync 5 ", syncObj5.ProcessedLayer())
-	return
 }
 
 func tx() *types.Transaction {
@@ -1048,7 +1044,6 @@ func TestFetchLayerBlockIds(t *testing.T) {
 	l, err := syncObj3.GetLayer(2)
 	assert.NoError(t, err)
 	assert.True(t, len(l.Blocks()) == 0)
-
 }
 
 func TestFetchLayerBlockIdsNoResponse(t *testing.T) {
@@ -1165,7 +1160,6 @@ func TestFetchLayerBlockIdsNoResponse(t *testing.T) {
 	//check that we got layer 5
 	err = syncObj5.getAndValidateLayer(5)
 	assert.NoError(t, err)
-
 }
 
 type mockLayerValidator struct {
@@ -1234,6 +1228,7 @@ func TestSyncer_Synchronise2(t *testing.T) {
 	types.SetLayersPerEpoch(1)
 	syncs, _, _ := SyncMockFactory(1, conf, t.Name(), memoryDB, newMockPoetDb)
 	sync := syncs[0]
+	defer sync.Close()
 	gen := types.GetEffectiveGenesis()
 	sync.AddBlockWithTxs(types.NewExistingBlock(1+gen, []byte(rand.String(8)), nil))
 	sync.AddBlockWithTxs(types.NewExistingBlock(2+gen, []byte(rand.String(8)), nil))
@@ -1283,6 +1278,7 @@ func TestSyncer_ListenToGossip(t *testing.T) {
 	r := require.New(t)
 	syncs, _, _ := SyncMockFactory(1, conf, t.Name(), memoryDB, newMockPoetDb)
 	sync := syncs[0]
+	defer sync.Close()
 	sync.AddBlockWithTxs(types.NewExistingBlock(1, []byte(rand.String(8)), nil))
 	lv := &mockLayerValidator{0, 0, 0, nil}
 	sync.Mesh.Validator = lv
@@ -1304,12 +1300,51 @@ func TestSyncer_handleNotSyncedFlow(t *testing.T) {
 	atxpool := activation.NewAtxMemPool()
 	ts := &mockClock{Layer: 10}
 	sync := NewSync(service.NewSimulator().NewNode(), getMesh(memoryDB, Path+t.Name()+"_"+time.Now().String()), txpool, atxpool, blockEligibilityValidatorMock{}, newMockPoetDb(), conf, ts, log.NewDefault(t.Name()))
+	defer sync.Close()
 	lv := &mockLayerValidator{0, 0, 0, nil}
 	sync.Mesh.Validator = lv
 	sync.SetLatestLayer(20)
 	go sync.handleNotSynced(10)
 	time.Sleep(100 * time.Millisecond)
 	r.Equal(1, ts.countSub)
+}
+
+// Make sure this can be called successfully several times for the same layer
+func TestSyncer_handleNotSyncedZeroBlocksLayer(t *testing.T) {
+	r := require.New(t)
+	// layers per epoch must be > 1 so layer 0 has no genesis block
+	types.SetLayersPerEpoch(100)
+	ts := &mockClock{Layer: 2}
+	syncs, nodes := SyncMockFactoryManClock(1, conf, t.Name(), memoryDB, newMockPoetDb, ts)
+	sync := syncs[0]
+	defer sync.Close()
+	sync.peers = getPeersMock([]p2ppeers.Peer{nodes[0].PublicKey()})
+	lv := &mockLayerValidator{0, 0, 0, nil}
+	sync.Mesh.Validator = lv
+	sync.SetLatestLayer(1)
+	r.NoError(sync.SetZeroBlockLayer(1))
+	r.Equal(0, lv.countValidated)
+	r.Equal(types.LayerID(0), lv.processedLayer)
+	go sync.handleNotSynced(1)
+	time.Sleep(100 * time.Millisecond)
+	r.Equal(1, lv.countValidate)
+	r.Equal(types.LayerID(1), lv.processedLayer)
+}
+
+func TestSyncer_SetZeroBlockLayer(t *testing.T) {
+	r := require.New(t)
+	// layers per epoch must be > 1 so layer 0 has no genesis block
+	types.SetLayersPerEpoch(100)
+	txpool := state.NewTxMemPool()
+	atxpool := activation.NewAtxMemPool()
+	ts := &mockClock{Layer: 10}
+	sync := NewSync(service.NewSimulator().NewNode(), getMesh(memoryDB, Path+t.Name()+"_"+time.Now().String()), txpool, atxpool, blockEligibilityValidatorMock{}, newMockPoetDb(), conf, ts, log.NewDefault(t.Name()))
+	defer sync.Close()
+	sync.SetLatestLayer(1)
+
+	// We should be able to perform this successfully multiple times for the same layer
+	r.NoError(sync.SetZeroBlockLayer(1))
+	r.NoError(sync.SetZeroBlockLayer(1))
 }
 
 func TestSyncer_p2pSyncForTwoLayers(t *testing.T) {
@@ -1822,6 +1857,7 @@ func TestSyncer_Await(t *testing.T) {
 
 	syncs, _, _ := SyncMockFactory(1, conf, t.Name(), memoryDB, newMockPoetDb)
 	syncer := syncs[0]
+	defer syncer.Close()
 	err := syncer.AddBlockWithTxs(types.NewExistingBlock(1, []byte(rand.String(8)), nil))
 	r.NoError(err)
 	lv := &mockLayerValidator{0, 0, 0, nil}