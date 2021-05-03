6f4c69a0a4c9f129cf362b31d96503aab2890765
Lane Rettig <lanerettig@gmail.com>
2021-05-03 15:05:19 -0400

    Properly handle errors in meshdb

--- mesh/meshdb.go
+++ mesh/meshdb.go
@@ -43,38 +43,38 @@ type DB struct {
 }
 
 // NewPersistentMeshDB creates an instance of a mesh database
-func NewPersistentMeshDB(path string, blockCacheSize int, log log.Log) (*DB, error) {
-	bdb, err := database.NewLDBDatabase(filepath.Join(path, "blocks"), 0, 0, log)
+func NewPersistentMeshDB(path string, blockCacheSize int, logger log.Log) (*DB, error) {
+	bdb, err := database.NewLDBDatabase(filepath.Join(path, "blocks"), 0, 0, logger)
 	if err != nil {
 		return nil, fmt.Errorf("failed to initialize blocks db: %v", err)
 	}
-	ldb, err := database.NewLDBDatabase(filepath.Join(path, "layers"), 0, 0, log)
+	ldb, err := database.NewLDBDatabase(filepath.Join(path, "layers"), 0, 0, logger)
 	if err != nil {
 		return nil, fmt.Errorf("failed to initialize layers db: %v", err)
 	}
-	vdb, err := database.NewLDBDatabase(filepath.Join(path, "validity"), 0, 0, log)
+	vdb, err := database.NewLDBDatabase(filepath.Join(path, "validity"), 0, 0, logger)
 	if err != nil {
 		return nil, fmt.Errorf("failed to initialize validity db: %v", err)
 	}
-	tdb, err := database.NewLDBDatabase(filepath.Join(path, "transactions"), 0, 0, log)
+	tdb, err := database.NewLDBDatabase(filepath.Join(path, "transactions"), 0, 0, logger)
 	if err != nil {
 		return nil, fmt.Errorf("failed to initialize transactions db: %v", err)
 	}
-	gdb, err := database.NewLDBDatabase(filepath.Join(path, "general"), 0, 0, log)
+	gdb, err := database.NewLDBDatabase(filepath.Join(path, "general"), 0, 0, logger)
 	if err != nil {
 		return nil, fmt.Errorf("failed to initialize general db: %v", err)
 	}
-	utx, err := database.NewLDBDatabase(filepath.Join(path, "unappliedTxs"), 0, 0, log)
+	utx, err := database.NewLDBDatabase(filepath.Join(path, "unappliedTxs"), 0, 0, logger)
 	if err != nil {
 		return nil, fmt.Errorf("failed to initialize mesh unappliedTxs db: %v", err)
 	}
-	iv, err := database.NewLDBDatabase(filepath.Join(path, "inputvector"), 0, 0, log)
+	iv, err := database.NewLDBDatabase(filepath.Join(path, "inputvector"), 0, 0, logger)
 	if err != nil {
 		return nil, fmt.Errorf("failed to initialize mesh unappliedTxs db: %v", err)
 	}
 
 	ll := &DB{
-		Log:                log,
+		Log:                logger,
 		blockCache:         newBlockCache(blockCacheSize * layerSize),
 		blocks:             bdb,
 		layers:             ldb,
@@ -87,7 +87,6 @@ func NewPersistentMeshDB(path string, blockCacheSize int, log log.Log) (*DB, err
 		layerMutex:         make(map[types.LayerID]*layerMutex),
 		exit:               make(chan struct{}),
 	}
-
 	for _, blk := range GenesisLayer().Blocks() {
 		ll.Log.With().Info("Adding genesis block ", blk.ID(), blk.LayerIndex)
 		if err := ll.AddBlock(blk); err != nil {
@@ -377,19 +376,15 @@ func (m *DB) SaveLayerHashInputVector(h types.Hash32, data []byte) error {
 }
 
 func (m *DB) writeBlock(bl *types.Block) error {
-	bytes, err := types.InterfaceToBytes(bl)
-	if err != nil {
-		return fmt.Errorf("could not encode bl")
-	}
-
-	if err := m.blocks.Put(bl.ID().AsHash32().Bytes(), bytes); err != nil {
+	if bytes, err := types.InterfaceToBytes(bl); err != nil {
+		return fmt.Errorf("could not encode block: %w", err)
+	} else if err := m.blocks.Put(bl.ID().Bytes(), bytes); err != nil {
+		return fmt.Errorf("could not add block %v to database: %w", bl.ID(), err)
+	} else if err := m.blocks.Put(bl.ID().AsHash32().Bytes(), bytes); err != nil {
 		return fmt.Errorf("could not add bl %v to database %v", bl.ID(), err)
 	}
 
-	m.updateLayerWithBlock(bl)
-
 	m.blockCache.put(bl)
-
 	return nil
 }
 
@@ -401,12 +396,12 @@ func (m *DB) updateLayerWithBlock(blk *types.Block) error {
 	ids, err := m.layers.Get(blk.LayerIndex.Bytes())
 	var blockIds []types.BlockID
 	if err != nil {
-		// layer doesnt exist, need to insert new layer
+		// layer doesn't exist, need to insert new layer
 		blockIds = make([]types.BlockID, 0, 1)
 	} else {
 		blockIds, err = types.BytesToBlockIds(ids)
 		if err != nil {
-			return errors.New("could not get all blocks from database ")
+			return errors.New("could not get all blocks from database")
 		}
 	}
 	m.Debug("added block %v to layer %v", blk.ID(), blk.LayerIndex)
