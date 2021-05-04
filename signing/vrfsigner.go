c7ea63aa06619c736c50e9a55c6cbda5a302773d
Noam Nelke <noam@spacemesh.io>
2021-05-04 18:18:40 +0300

    lint fix

--- signing/vrfsigner.go
+++ signing/vrfsigner.go
@@ -6,14 +6,17 @@ import (
 	"github.com/spacemeshos/ed25519"
 )
 
+// VRFSigner is a signer for VRF purposes
 type VRFSigner struct {
 	privKey []byte
 }
 
+// Sign signs a message for VRF purposes
 func (s VRFSigner) Sign(m []byte) ([]byte, error) {
 	return ed25519.Sign(s.privKey, m), nil
 }
 
+// NewVRFSigner creates a new VRFSigner from a 32-byte seed
 func NewVRFSigner(seed []byte) (*VRFSigner, []byte, error) {
 	if len(seed) < ed25519.SeedSize {
 		return nil, nil, fmt.Errorf("seed must be >=%d bytes (len(seed)=%d)", ed25519.SeedSize, len(seed))
@@ -25,6 +28,7 @@ func NewVRFSigner(seed []byte) (*VRFSigner, []byte, error) {
 	return &VRFSigner{privKey: vrfPriv}, vrfPub, nil
 }
 
+// VRFVerify verifies a message and signature, given a public key
 func VRFVerify(msg, sig, pub []byte) (bool, error) {
 	return ed25519.Verify(pub, msg, sig), nil
 }
