85497044df26c6b446dfcee9c5bd460ec57295e3
Nikita Kryuchkov <nkryuchkov10@gmail.com>
2021-06-22 17:38:19 +0300

    Revert "Temporarily mock tortoise beacon results"
    This reverts commit 14e92d7ed0e86ee1163c50fc21abe3f286db0fea.

--- tortoisebeacon/tortoise_beacon.go
+++ tortoisebeacon/tortoise_beacon.go
@@ -241,9 +241,6 @@ func (tb *TortoiseBeacon) Close() error {
 
 // GetBeacon returns a Tortoise Beacon value as []byte for a certain epoch or an error if it doesn't exist.
 func (tb *TortoiseBeacon) GetBeacon(epochID types.EpochID) ([]byte, error) {
-	// TODO: remove
-	return genesisBeacon.Bytes(), nil
-
 	if tb.tortoiseBeaconDB != nil {
 		if val, ok := tb.tortoiseBeaconDB.GetTortoiseBeacon(epochID); ok {
 			return val.Bytes(), nil
