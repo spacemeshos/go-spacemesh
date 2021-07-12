744c5d60b3da5303ad8eafd9937af51f62f1045f
Benny Daon <benny@tuzig.com>
2021-07-12 15:03:23 +0300

    Improve readability with a better name
    GetSeq() was replaced by WhoAmI()

--- plans/mining/main.go
+++ plans/mining/main.go
@@ -9,16 +9,16 @@ var testcases = map[string]interface{}{
 	"new_account": InitTestCase(testNewAccount),
 }
 
-// test_transaction transers 100 coins between the two tests accounts waits
-//					for next epoch and asserts the accounts' balance
+// test_transaction the first node transers 100 coins between the two tests
+//					accounts, all nodes wait for the next epoch and asserts
+//					the accounts' balance
 func testTransaction(t *T) {
-	seq := t.GetSeq()
 	t.Log("Starting transaction test, %d is up", seq)
 	// TODO: ready should be replaced with starting the node and waiting for
 	// genesis
 	t.SetState("ready")
 	t.WaitAll("ready")
-	if seq == 1 {
+	if t.WhoAmI() == 1 {
 		t.SendCoins(Account1, Account2, 100)
 	}
 	t.WaitForNextEpoch()
@@ -29,13 +29,12 @@ func testTransaction(t *T) {
 // test_new_account uses the firest node to creates a new account and
 // send it 100 coins from a test account to it and asserts the accounts' balance
 func testNewAccount(t *T) {
-	seq := t.GetSeq()
 	t.Log("Starting new account test, %d is up", seq)
 	// TODO: ready should be replaced with starting the node and waiting for
 	// genesis
 	t.SetState("ready")
 	t.WaitAll("ready")
-	if seq == 1 {
+	if t.WhoAmI() == 1 {
 		account := t.NewAccount()
 		t.SendCoins(Account1, account, 100)
 	}
