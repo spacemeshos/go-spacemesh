1d873c0a791ab4983de5c470bc90de72d362e1b7
Benny Daon <benny@tuzig.com>
2021-07-13 12:49:52 +0300

    Improve tests stability
    all the testcases run on the same network and the test cases can't make
    any assumptions as to the balance of accounts. In this change I've
    solved this issue by first reading the starting balance and using it
    instead of a constant.

--- plans/transactions/main.go
+++ plans/transactions/main.go
@@ -9,38 +9,44 @@ var testcases = map[string]interface{}{
 	"new_account": InitTestCase(testNewAccount),
 }
 
-// test_transaction the first node transers 100 coins between the two tests
-// accounts, all nodes wait for the next epoch and asserts
-// the accounts' balance
+// test_transaction in this test each node starts with getting the ballance
+// of the test account. Than the first node transers 100 coins between the
+// tests accounts.
+// Then all the nodes wait for the next epoch and assert the accounts' balance.
 func testTransaction(t *T) {
 	t.Log("Starting transaction test, %d is up", t.WhoAmI())
 	// TODO: ready should be replaced with starting the node and waiting for
 	// genesis
 	t.SetState("ready")
 	t.WaitAll("ready")
+	start1 := t.GetBalance(Account1)
+	start2 := t.GetBalance(Account2)
 	if t.WhoAmI() == 1 {
 		t.SendCoins(Account1, Account2, 100)
 	}
 	t.WaitTillEpoch()
-	t.RequireBalance(Account1, 100000000000000000-100)
-	t.RequireBalance(Account2, 100000000000000000+100)
+	t.RequireBalance(Account1, start1-100)
+	t.RequireBalance(Account2, start2+100)
 }
 
-// test_new_account uses the firest node to creates a new account and
-// send it 100 coins from a test account to it and asserts the
-// accounts' balance
+// test_new_account in this test each node starts with getting the ballance
+// of the test account. Than the first node creates a new account and
+// transers 100 coins between inb from a test account.
+// Then all the nodes wait for the next epoch and assert the accounts' balance.
+
 func testNewAccount(t *T) {
 	t.Log("Starting new account test, %d is up", t.WhoAmI())
 	// TODO: ready should be replaced with starting the node and waiting for
 	// genesis
 	t.SetState("ready")
 	t.WaitAll("ready")
+	start1 := t.GetBalance(Account1)
 	if t.WhoAmI() == 1 {
 		account := t.NewAccount()
 		t.SendCoins(Account1, account, 100)
 	}
 	t.WaitTillEpoch()
-	t.RequireBalance(Account1, 100000000000000000-100)
+	t.RequireBalance(Account1, start1-100)
 	t.RequireBalance(account, 100)
 }
 
