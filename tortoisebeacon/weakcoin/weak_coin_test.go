d90cf42fe877fa02d912efbb66a22293fcdd24a0
Nikita Kryuchkov <nkryuchkov10@gmail.com>
2021-07-27 01:08:13 +0300

    Fix weak coin unit tests

--- tortoisebeacon/weakcoin/weak_coin_test.go
+++ tortoisebeacon/weakcoin/weak_coin_test.go
@@ -1,28 +1,28 @@
 package weakcoin
 
 import (
+	"encoding/hex"
 	"math/big"
 	"testing"
 
+	"github.com/spacemeshos/go-spacemesh/common/types"
 	"github.com/stretchr/testify/require"
 )
 
-/*
 func Test_weakCoin_generateProposal(t *testing.T) {
 	t.Parallel()
 
 	r := require.New(t)
 
-	logger := log.NewDefault("WeakCoin")
-	wcg := NewWeakCoin(DefaultPrefix, DefaultThreshold, nil, logger)
+	var wc weakCoin
 	epoch := types.EpochID(3)
 	round := types.RoundID(1)
-	expected := "0xb150cef2e30473bf3b1d24e253cce39f995c15ebc37bb019d7ab618e873509fc"
+	expected := "5765616b436f696e03000000000000000100000000000000"
 
-	p, err := wcg.(*weakCoin).generateProposal(epoch, round)
+	p, err := wc.generateProposal(epoch, round)
 	r.NoError(err)
 
-	r.EqualValues(expected, p.String())
+	r.EqualValues(expected, hex.EncodeToString(p))
 }
 
 func Test_weakCoin_proposalExceedsThreshold(t *testing.T) {
@@ -30,24 +30,31 @@ func Test_weakCoin_proposalExceedsThreshold(t *testing.T) {
 
 	r := require.New(t)
 
-	logger := log.NewDefault("WeakCoin")
+	proposal1, ok := new(big.Int).SetString("0xb150cef2e30473bf3b1d24e253cce39f995c15ebc37bb019d7ab618e873509fc", 0)
+	require.True(t, ok)
+
+	proposal2, ok := new(big.Int).SetString("0x01", 0)
+	require.True(t, ok)
+
+	threshold, ok := new(big.Int).SetString("0x8000000000000000000000000000000000000000000000000000000000000000", 0)
+	require.True(t, ok)
 
 	tt := []struct {
 		name      string
-		proposal  types.Hash32
-		threshold types.Hash32
+		proposal  []byte
+		threshold []byte
 		expected  bool
 	}{
 		{
 			name:      "Case 1",
-			proposal:  types.HexToHash32("0xb150cef2e30473bf3b1d24e253cce39f995c15ebc37bb019d7ab618e873509fc"),
-			threshold: types.HexToHash32("0x80" + strings.Repeat("00", 31)),
+			proposal:  proposal1.Bytes(),
+			threshold: threshold.Bytes(),
 			expected:  true,
 		},
 		{
 			name:      "Case 2",
-			proposal:  types.HexToHash32("0x01"),
-			threshold: types.HexToHash32("0x80" + strings.Repeat("00", 31)),
+			proposal:  proposal2.Bytes(),
+			threshold: threshold.Bytes(),
 			expected:  false,
 		},
 	}
@@ -57,14 +64,13 @@ func Test_weakCoin_proposalExceedsThreshold(t *testing.T) {
 		t.Run(tc.name, func(t *testing.T) {
 			t.Parallel()
 
-			wcg := NewWeakCoin(DefaultPrefix, DefaultThreshold, nil, logger)
-			got := wcg.(*weakCoin).proposalExceedsThreshold(tc.proposal)
+			var wc weakCoin
+			wc.threshold = DefaultThreshold()
+			got := wc.proposalExceedsThreshold(tc.proposal)
 			r.Equal(tc.expected, got)
 		})
 	}
 }
-
-*/
 
 func Test_weakCoin_coinBitToBool(t *testing.T) {
 	t.Parallel()
