c1766a4ae277a455266c9148656dc3a40733f7ea
dmitry <yashulyak@gmail.com>
2021-07-28 14:02:53 +0300

    Use noop if level is not provided but enable DEBUG on CI

--- log/logtest/log.go
+++ log/logtest/log.go
@@ -1,16 +1,24 @@
 package logtest
 
 import (
+	"flag"
 	"testing"
 
 	"github.com/spacemeshos/go-spacemesh/log"
-	"go.uber.org/zap"
+	"go.uber.org/zap/zapcore"
 	"go.uber.org/zap/zaptest"
 )
 
-var logLevel = zap.LevelFlag("log", zap.DebugLevel, "log level to use in tests")
+var logLevel = flag.String("log", "", "log level to use in tests")
 
 // New creates log.Log instance that will use testing.TB.Log internally.
 func New(t testing.TB) log.Log {
-	return log.NewFromLog(zaptest.NewLogger(t, zaptest.Level(logLevel)))
+	if len(*logLevel) == 0 {
+		return log.NewNop()
+	}
+	level := zapcore.Level(0)
+	if err := level.Set(*logLevel); err != nil {
+		panic(err)
+	}
+	return log.NewFromLog(zaptest.NewLogger(t, zaptest.Level(level)))
 }
