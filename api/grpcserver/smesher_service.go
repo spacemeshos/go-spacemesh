ff20961f1a8a0fcf2a62538f04f375168103009b
dmitry <yashulyak@gmail.com>
2021-08-26 17:30:39 +0300

    api: start smeshing using background context

--- api/grpcserver/smesher_service.go
+++ api/grpcserver/smesher_service.go
@@ -2,6 +2,7 @@ package grpcserver
 
 import (
 	"fmt"
+
 	"github.com/golang/protobuf/ptypes/empty"
 	pb "github.com/spacemeshos/api/release/go/spacemesh/v1"
 	"github.com/spacemeshos/go-spacemesh/activation"
@@ -42,7 +43,6 @@ func (s SmesherService) IsSmeshing(context.Context, *empty.Empty) (*pb.IsSmeshin
 // StartSmeshing requests that the node begin smeshing
 func (s SmesherService) StartSmeshing(ctx context.Context, in *pb.StartSmeshingRequest) (*pb.StartSmeshingResponse, error) {
 	log.Info("GRPC SmesherService.StartSmeshing")
-
 	if in.Coinbase == nil {
 		return nil, status.Errorf(codes.InvalidArgument, "`Coinbase` must be provided")
 	}
@@ -72,7 +72,8 @@ func (s SmesherService) StartSmeshing(ctx context.Context, in *pb.StartSmeshingR
 		Throttle:          in.Opts.Throttle,
 	}
 
-	if err := s.smeshingProvider.StartSmeshing(ctx, coinbaseAddr, opts); err != nil {
+	// NOTE(dshulyak) don't use grpc context it will be canceled on method return or when client closes connection
+	if err := s.smeshingProvider.StartSmeshing(context.Background(), coinbaseAddr, opts); err != nil {
 		err := fmt.Sprintf("failed to start smeshing: %v", err)
 		log.Error(err)
 		return nil, status.Error(codes.Internal, err)
