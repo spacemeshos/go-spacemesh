ed1a5534ff2555ae74a6220a5e6ffad2405a659a
dmitry <yashulyak@gmail.com>
2021-09-03 14:25:08 +0300

    Replace xdr in transaction tests

--- tests/tx_generator/models/tx_generator.py
+++ tests/tx_generator/models/tx_generator.py
@@ -1,12 +1,12 @@
-import xdrlib
+import struct
 
 import tests.tx_generator.config as conf
 from tests.ed25519.eddsa import signature2
 
 
 ADDRESS_SIZE = 20
-SIGNATURE_SIZE = 64
-
+INT_SIZE = INT_SIZE
+ENDIAN = "little"
 
 class TxGenerator:
     """
@@ -19,19 +19,12 @@ class TxGenerator:
         self.privateK = bytes.fromhex(pri)
 
     def generate(self, dst, nonce, gas_limit, fee, amount):
-        p = xdrlib.Packer()
-        p.pack_hyper(nonce)
-        dst_bytes = bytes.fromhex(dst)
-        # get the LAST 20 bytes of the dst address
-        addr = dst_bytes[-ADDRESS_SIZE:]
-        p.pack_fstring(ADDRESS_SIZE, addr)
-        p.pack_hyper(gas_limit)
-        p.pack_hyper(fee)
-        p.pack_hyper(amount)
+        inner = nonce.to_bytes(INT_SIZE, ENDIAN)
+        inner += bytes.fromhex(dst)[-ADDRESS_SIZE:]
+        inner += gas_limit.to_bytes(INT_SIZE, ENDIAN)
+        inner += fee.to_bytes(INT_SIZE, ENDIAN)
+        inner += amount.to_bytes(INT_SIZE, ENDIAN)
 
-        data = p.get_buffer()
-        sign = signature2(data, self.privateK)
-        p.pack_fstring(SIGNATURE_SIZE, sign)
-
-        return p.get_buffer()
+        sign = signature2(inner, self.privateK)
+        return inner + sign
 
