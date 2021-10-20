9b5873430ab38bc995dbc000017fcbeb0209dd02
Noam Nelke <noam@spacemesh.io>
2021-10-20 08:44:39 +0300

    bad merge

--- hare/hare_test.go
+++ hare/hare_test.go
@@ -7,8 +7,6 @@ import (
 	"testing"
 	"time"
 
-	"github.com/spacemeshos/go-spacemesh/eligibility"
-
 	"github.com/golang/mock/gomock"
 	"github.com/stretchr/testify/assert"
 	"github.com/stretchr/testify/require"
