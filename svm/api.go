b4d70ff5680fd7b9be844c0e064e0734cce06d8c
Filippo Costa <filippocosta.italy@gmail.com>
2021-10-21 15:22:25 +0200

    Rename txProcessor -> state

--- svm/api.go
+++ svm/api.go
@@ -12,13 +12,13 @@ import (
 
 // SVM is an entry point for all SVM operations.
 type SVM struct {
-	txProcessor *state.TransactionProcessor
-	log         log.Logger
+	state *state.TransactionProcessor
+	log   log.Logger
 }
 
 // New creates a new `SVM` instance from the given `state` and `logger`.
-func New(txProcessor *state.TransactionProcessor, logger log.Log) *SVM {
-	return &SVM{txProcessor, log.NewDefault("svm")}
+func New(state *state.TransactionProcessor, logger log.Log) *SVM {
+	return &SVM{state, log.NewDefault("svm")}
 }
 
 // SetupGenesis creates new accounts and adds balances as dictated by `conf`.
@@ -36,14 +36,14 @@ func (svm *SVM) SetupGenesis(conf *config.GenesisConfig) error {
 			return fmt.Errorf("%s must be an address of size %d", id, types.AddressLength)
 		}
 		addr := types.BytesToAddress(bytes)
-		svm.txProcessor.CreateAccount(addr)
-		svm.txProcessor.AddBalance(addr, balance)
+		svm.state.CreateAccount(addr)
+		svm.state.AddBalance(addr, balance)
 		svm.log.With().Info("genesis account created",
 			log.String("address", addr.Hex()),
 			log.Uint64("balance", balance))
 	}
 
-	_, err := svm.txProcessor.Commit()
+	_, err := svm.state.Commit()
 	if err != nil {
 		return fmt.Errorf("cannot commit genesis state: %w", err)
 	}
@@ -54,8 +54,8 @@ func (svm *SVM) SetupGenesis(conf *config.GenesisConfig) error {
 // transactions for the given layer. to miners vector for layer. It returns an
 // error on failure, as well as a vector of failed transactions.
 func (svm *SVM) ApplyLayer(layerID types.LayerID, transactions []*types.Transaction, rewards map[types.Address]uint64) ([]*types.Transaction, error) {
-	svm.txProcessor.ApplyRewards(layerID, rewards)
-	failedTxs, err := svm.txProcessor.ApplyTransactions(layerID, transactions)
+	svm.state.ApplyRewards(layerID, rewards)
+	failedTxs, err := svm.state.ApplyTransactions(layerID, transactions)
 	if err != nil {
 		return failedTxs, fmt.Errorf("SVM couldn't apply layer %d: %w", layerID.Uint32(), err)
 	}
@@ -65,17 +65,17 @@ func (svm *SVM) ApplyLayer(layerID types.LayerID, transactions []*types.Transact
 
 // AddressExists checks if an account address exists in this node's global state.
 func (svm *SVM) AddressExists(addr types.Address) bool {
-	return svm.txProcessor.AddressExists(addr)
+	return svm.state.AddressExists(addr)
 }
 
 // GetLayerApplied gets the layer id at which this tx was applied.
 func (svm *SVM) GetLayerApplied(txID types.TransactionID) *types.LayerID {
-	return svm.txProcessor.GetLayerApplied(txID)
+	return svm.state.GetLayerApplied(txID)
 }
 
 // GetLayerStateRoot returns the state root at a given layer.
 func (svm *SVM) GetLayerStateRoot(layer types.LayerID) (types.Hash32, error) {
-	hash, err := svm.txProcessor.GetLayerStateRoot(layer)
+	hash, err := svm.state.GetLayerStateRoot(layer)
 	if err != nil {
 		err = fmt.Errorf("SVM couldn't get the root hash of layer %d: %w", layer.Uint32(), err)
 	}
@@ -84,12 +84,12 @@ func (svm *SVM) GetLayerStateRoot(layer types.LayerID) (types.Hash32, error) {
 
 // GetStateRoot gets the current state root hash.
 func (svm *SVM) GetStateRoot() types.Hash32 {
-	return svm.txProcessor.GetStateRoot()
+	return svm.state.GetStateRoot()
 }
 
 // LoadState loads the last state from persistent storage.
 func (svm *SVM) LoadState(layer types.LayerID) error {
-	if err := svm.txProcessor.LoadState(layer); err != nil {
+	if err := svm.state.LoadState(layer); err != nil {
 		return fmt.Errorf("SVM couldn't recover the state at layer %d: %w", layer.Uint32(), err)
 	}
 	return nil
@@ -97,18 +97,18 @@ func (svm *SVM) LoadState(layer types.LayerID) error {
 
 // GetBalance Retrieve the balance from the given address or 0 if object not found.
 func (svm *SVM) GetBalance(addr types.Address) uint64 {
-	return svm.txProcessor.GetBalance(addr)
+	return svm.state.GetBalance(addr)
 }
 
 // GetNonce gets the current nonce of the given addr, if the address is not
 // found it returns 0.
 func (svm *SVM) GetNonce(addr types.Address) uint64 {
-	return svm.txProcessor.GetNonce(addr)
+	return svm.state.GetNonce(addr)
 }
 
 // GetAllAccounts returns a dump of all accounts in global state.
 func (svm *SVM) GetAllAccounts() (*types.MultipleAccountsState, error) {
-	accounts, err := svm.txProcessor.GetAllAccounts()
+	accounts, err := svm.state.GetAllAccounts()
 	if err != nil {
 		err = fmt.Errorf("SVM couldn't get all accounts: %w", err)
 	}
@@ -118,7 +118,7 @@ func (svm *SVM) GetAllAccounts() (*types.MultipleAccountsState, error) {
 // ValidateNonceAndBalance validates that the tx origin account has enough balance to apply the tx,
 // also, it checks that nonce in tx is correct, returns error otherwise.
 func (svm *SVM) ValidateNonceAndBalance(transaction *types.Transaction) error {
-	if err := svm.txProcessor.ValidateNonceAndBalance(transaction); err != nil {
+	if err := svm.state.ValidateNonceAndBalance(transaction); err != nil {
 		return fmt.Errorf("SVM couldn't validate nonce and balance: %w", err)
 	}
 	return nil
@@ -129,7 +129,7 @@ func (svm *SVM) ValidateNonceAndBalance(transaction *types.Transaction) error {
 //
 // TODO: Remove this and use a whole separate API for mempool management.
 func (svm *SVM) ValidateAndAddTxToPool(tx *types.Transaction) error {
-	if err := svm.txProcessor.ValidateAndAddTxToPool(tx); err != nil {
+	if err := svm.state.ValidateAndAddTxToPool(tx); err != nil {
 		return fmt.Errorf("SVM couldn't validate and/or add transaction to mempool: %w", err)
 	}
 	return nil
