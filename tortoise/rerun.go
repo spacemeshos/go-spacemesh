fde6e8d96fae1073cb6959e2252db23387a2f87d
Dmitry Shulyak <yashulyak@gmail.com>
2021-11-02 06:10:33 +0000

    tortoise: move rerun to background goroutine (#2900)
    ## Motivation
    
    In order not to block the network progress rerun is moved to the background goroutine. The majority of the layers are handled in background on timer, and a small number of concurrent layers are applied when HandleIncomingLayer is called by the mesh.
    
    after: https://github.com/spacemeshos/go-spacemesh/pull/2899
    closes: https://github.com/spacemeshos/go-spacemesh/pull/2899
    
    ## Changes
    - move tortoise rerun to a background thread
    
    ## Test Plan
    existing and manual tests
    
    ## TODO
    - [x] Explain motivation or link existing issue(s)
    - [x] Test changes and document test plan
    - [x] Update documentation as needed
    
    ## DevOps Notes
    <!-- Please uncheck these items as applicable to make DevOps aware of changes that may affect releases -->
    - [x] This PR does not require configuration changes (e.g., environment variables, GitHub secrets, VM resources)
    - [x] This PR does not affect public APIs
    - [x] This PR does not rely on a new version of external services (PoET, elasticsearch, etc.)
    - [x] This PR does not make changes to log messages (which monitoring infrastructure may rely on)

---
+++ tortoise/rerun.go
@@ -0,0 +1,148 @@
+package tortoise
+
+import (
+	"context"
+	"fmt"
+	"time"
+
+	"github.com/spacemeshos/go-spacemesh/common/types"
+	"github.com/spacemeshos/go-spacemesh/log"
+	"github.com/spacemeshos/go-spacemesh/mesh"
+)
+
+type rerunResult struct {
+	Consensus *turtle
+	Tracer    *validityTracer
+}
+
+// updateFromRerun must be called while holding appropriate mutex (see struct definition).
+func (t *ThreadSafeVerifyingTortoise) updateFromRerun(ctx context.Context) (bool, types.LayerID) {
+	var (
+		logger   = t.logger.WithContext(ctx).With()
+		reverted bool
+		observed types.LayerID
+	)
+	if t.update != nil {
+		var (
+			completed = t.update
+			current   = t.trtl
+			updated   = completed.Consensus
+			err       error
+		)
+		if current.Last.After(updated.Last) {
+			start := time.Now()
+			logger.Info("tortoise received more layers while rerun was in progress. running a catchup",
+				log.FieldNamed("last", current.Last),
+				log.FieldNamed("rerun-last", updated.Last))
+
+			err = catchupToCurrent(ctx, current, updated)
+			if err != nil {
+				logger.Error("cathup failed", log.Err(err))
+			} else {
+				logger.Info("catchup finished", log.Duration("duration", time.Since(start)))
+			}
+		}
+		if err == nil {
+			reverted = completed.Tracer.Reverted()
+			if reverted {
+				observed = completed.Tracer.FirstLayer()
+			}
+			updated.log = current.log
+			updated.bdp = current.bdp
+			t.trtl = updated
+		}
+		t.update = nil
+	}
+	return reverted, observed
+}
+
+func (t *ThreadSafeVerifyingTortoise) rerunLoop(ctx context.Context, period time.Duration) {
+	timer := time.NewTimer(period)
+	defer timer.Stop()
+	for {
+		select {
+		case <-ctx.Done():
+			return
+		case <-timer.C:
+			_ = t.rerun(ctx) // err is already logged with additional context
+			timer.Reset(period)
+		}
+	}
+}
+
+func (t *ThreadSafeVerifyingTortoise) rerun(ctx context.Context) error {
+	t.mu.RLock()
+	last := t.trtl.Last
+	t.mu.RUnlock()
+
+	logger := t.logger.WithContext(ctx).WithFields(
+		log.Bool("rerun", true),
+		log.FieldNamed("last-layer-id", last),
+	)
+
+	start := time.Now()
+	logger.With().Info("tortoise rerun started")
+
+	consensus := t.trtl.cloneTurtleParams()
+	consensus.logger = logger
+	consensus.init(ctx, mesh.GenesisLayer())
+	tracer := &validityTracer{blockDataProvider: consensus.bdp}
+	consensus.bdp = tracer
+
+	for lid := types.GetEffectiveGenesis(); !lid.After(last); lid = lid.Add(1) {
+		if err := consensus.HandleIncomingLayer(ctx, lid); err != nil {
+			logger.With().Error("tortoise rerun failed", log.Err(err))
+			return err
+		}
+	}
+	if tracer.Reverted() {
+		logger = logger.WithFields(log.FieldNamed("first-reverted-layer", tracer.FirstLayer()))
+	}
+	logger.With().Info("tortoise rerun completed", last, log.Duration("duration", time.Since(start)))
+
+	t.mu.Lock()
+	defer t.mu.Unlock()
+	t.update = &rerunResult{Consensus: consensus, Tracer: tracer}
+	return nil
+}
+
+// validityTracer monitors the tortoise rerun for database changes that would cause us to need to revert state.
+type validityTracer struct {
+	blockDataProvider
+	firstUpdatedLayer *types.LayerID
+}
+
+// SaveContextualValidity overrides the method in the embedded type to check if we've made changes.
+func (vt *validityTracer) SaveContextualValidity(bid types.BlockID, lid types.LayerID, validityNew bool) error {
+	if vt.firstUpdatedLayer == nil {
+		validityCur, err := vt.ContextualValidity(bid)
+		if err != nil {
+			return fmt.Errorf("error reading contextual validity of block %v: %w", bid, err)
+		}
+		if validityCur != validityNew {
+			vt.firstUpdatedLayer = &lid
+		}
+	}
+	if err := vt.blockDataProvider.SaveContextualValidity(bid, lid, validityNew); err != nil {
+		return fmt.Errorf("save contextual validity: %w", err)
+	}
+	return nil
+}
+
+func (vt *validityTracer) Reverted() bool {
+	return vt.firstUpdatedLayer != nil
+}
+
+// FirstLayer should be called only if Reverted returns true.
+func (vt *validityTracer) FirstLayer() types.LayerID {
+	return *vt.firstUpdatedLayer
+}
+
+func catchupToCurrent(ctx context.Context, current, updated *turtle) error {
+	for lid := updated.Last.Add(1); !lid.After(current.Last); lid = lid.Add(1) {
+		if err := updated.HandleIncomingLayer(ctx, lid); err != nil {
+			return err
+		}
+	}
+	return nil
+}
