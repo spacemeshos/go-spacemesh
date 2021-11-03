a6150f9b2bba3310ccd43d8f09d912f54e0fab18
dmitry <yashulyak@gmail.com>
2021-11-03 18:49:11 +0200

    Persis after rerun to avoid leaking persisted state

--- tortoise/rerun.go
+++ tortoise/rerun.go
@@ -29,9 +29,14 @@ func (t *ThreadSafeVerifyingTortoise) updateFromRerun(ctx context.Context) (bool
 			completed = t.update
 			current   = t.trtl
 			updated   = completed.Consensus
-			err       error
 		)
-		if current.Last.After(updated.Last) {
+		// we need to persist after rerun is completed so that eviction will not miss any layers
+		// that were received concurrently.
+		err := completed.Consensus.Persist()
+		if err != nil {
+			logger.Error("failed to persist state after catchup", log.Err(err))
+		}
+		if current.Last.After(updated.Last) && err == nil {
 			start := time.Now()
 			logger.Info("tortoise received more layers while rerun was in progress. running a catchup",
 				log.FieldNamed("last", current.Last),
