dee0205366d52c219f6207209cda7c5102b62f24
dmitry <yashulyak@gmail.com>
2022-01-17 09:51:59 +0200

    definte api for transactions

--- sql/transactions/transactions.go
+++ sql/transactions/transactions.go
@@ -5,6 +5,37 @@ import (
 	"github.com/spacemeshos/go-spacemesh/sql"
 )
 
+// Add unapplied transaction to the database.
 func Add(db sql.Executor, lid types.LayerID, tx *types.Transaction) error {
+	return nil
+}
 
+// Update layer/block when transaction is included into the mesh.
+func Update(db sql.Executor, id types.TransactionID, lid types.LayerID, bid types.BlockID) error {
+	return nil
+}
+
+// Delete transaction from database.
+func Delete(db sql.Executor, id types.TransactionID) error {
+	return nil
+}
+
+// Get transaction from database.
+func Get(db sql.Executor, id types.TransactionID) (*types.MeshTransaction, error) {
+	return nil, nil
+}
+
+// FilterByOrigin filter transaction by origin [from, to] layers.
+func FilterByOrigin(db sql.Executor, from, to types.LayerID, address types.Address) ([]*types.MeshTransaction, error) {
+	return nil, nil
+}
+
+// FilterByDestination filter transaction by destnation [from, to] layers.
+func FilterByDestination(db sql.Executor, from, to types.LayerID, address types.Address) ([]*types.MeshTransaction, error) {
+	return nil, nil
+}
+
+// FilterPending filters all transactions that are not yet applied (have empty block).
+func FilterPending(db sql.Executor, address types.Address) ([]*types.MeshTransaction, error) {
+	return nil, nil
 }
