a058f458827a992f8a080ecb8c1691774232c032
Dmitry Shulyak <yashulyak@gmail.com>
2022-01-20 05:28:00 +0000

    sql, mesh, api: refactor transactions (#3064)
    ## Motivation
    
    part of: https://github.com/spacemeshos/go-spacemesh/issues/2918
    
    ## Changes
    - table for transactions to replace transactions and unappliedTxs databases
    - add API to filter transactions for multiple layers. instead of querying layer by layer
    - simplify logic around pending transactions (transactions now added as pending to the table, and either switch to applied or marked as deleted)

--- mesh/meshdb_test.go
+++ mesh/meshdb_test.go
@@ -211,10 +211,10 @@ func TestMeshDB_GetStateProjection(t *testing.T) {
 	mdb := NewMemMeshDB(logtest.New(t))
 	defer mdb.Close()
 	signer, origin := newSignerAndAddress(t, "123")
-	err := mdb.addToUnappliedTxs([]*types.Transaction{
+	err := mdb.writeTransactions(types.NewLayerID(1), types.BlockID{1},
 		newTx(t, signer, 0, 10),
 		newTx(t, signer, 1, 20),
-	}, types.NewLayerID(1))
+	)
 	require.NoError(t, err)
 
 	nonce, balance, err := mdb.GetProjection(origin, initialNonce, initialBalance)
@@ -228,10 +228,10 @@ func TestMeshDB_GetStateProjection_WrongNonce(t *testing.T) {
 	defer mdb.Close()
 
 	signer, origin := newSignerAndAddress(t, "123")
-	err := mdb.addToUnappliedTxs([]*types.Transaction{
+	err := mdb.writeTransactions(types.NewLayerID(1), types.BlockID{1},
 		newTx(t, signer, 1, 10),
 		newTx(t, signer, 2, 20),
-	}, types.NewLayerID(1))
+	)
 	require.NoError(t, err)
 
 	nonce, balance, err := mdb.GetProjection(origin, initialNonce, initialBalance)
@@ -245,10 +245,10 @@ func TestMeshDB_GetStateProjection_DetectNegativeBalance(t *testing.T) {
 	defer mdb.Close()
 
 	signer, origin := newSignerAndAddress(t, "123")
-	err := mdb.addToUnappliedTxs([]*types.Transaction{
+	err := mdb.writeTransactions(types.NewLayerID(1), types.BlockID{1},
 		newTx(t, signer, 0, 10),
 		newTx(t, signer, 1, 95),
-	}, types.NewLayerID(1))
+	)
 	require.NoError(t, err)
 
 	nonce, balance, err := mdb.GetProjection(origin, initialNonce, initialBalance)
@@ -273,12 +273,12 @@ func TestMeshDB_UnappliedTxs(t *testing.T) {
 
 	signer1, origin1 := newSignerAndAddress(t, "thc")
 	signer2, origin2 := newSignerAndAddress(t, "cbd")
-	err := mdb.addToUnappliedTxs([]*types.Transaction{
+	err := mdb.writeTransactions(types.NewLayerID(1), types.BlockID{1},
 		newTx(t, signer1, 420, 240),
 		newTx(t, signer1, 421, 241),
 		newTx(t, signer2, 0, 100),
 		newTx(t, signer2, 1, 101),
-	}, types.NewLayerID(1))
+	)
 	require.NoError(t, err)
 
 	txns1 := getTxns(t, mdb, origin1)
@@ -295,9 +295,7 @@ func TestMeshDB_UnappliedTxs(t *testing.T) {
 	require.Equal(t, 100, int(txns2[0].TotalAmount))
 	require.Equal(t, 101, int(txns2[1].TotalAmount))
 
-	mdb.removeFromUnappliedTxs([]*types.Transaction{
-		newTx(t, signer2, 0, 100),
-	})
+	require.NoError(t, mdb.markTransactionsDeleted(newTx(t, signer2, 0, 100)))
 
 	txns1 = getTxns(t, mdb, origin1)
 	require.Len(t, txns1, 2)
@@ -326,20 +324,21 @@ func TestMeshDB_testGetTransactions(t *testing.T) {
 		newTxWithDest(t, signer2, addr1, 1, 101),
 	))
 
-	txs, err := mdb.GetTransactionsByOrigin(types.NewLayerID(1), addr1)
+	lid := types.NewLayerID(1)
+	txs, err := mdb.GetTransactionsByOrigin(lid, lid, addr1)
 	require.NoError(t, err)
 	require.Equal(t, 2, len(txs))
 
-	txs, err = mdb.GetTransactionsByDestination(types.NewLayerID(1), addr1)
+	txs, err = mdb.GetTransactionsByDestination(lid, lid, addr1)
 	require.NoError(t, err)
 	require.Equal(t, 2, len(txs))
 
 	// test negative case
-	txs, err = mdb.GetTransactionsByOrigin(types.NewLayerID(1), addr3)
+	txs, err = mdb.GetTransactionsByOrigin(lid, lid, addr3)
 	require.NoError(t, err)
 	require.Equal(t, 0, len(txs))
 
-	txs, err = mdb.GetTransactionsByDestination(types.NewLayerID(1), addr3)
+	txs, err = mdb.GetTransactionsByDestination(lid, lid, addr3)
 	require.NoError(t, err)
 	require.Equal(t, 0, len(txs))
 }
@@ -622,7 +621,8 @@ func TestMesh_FindOnce(t *testing.T) {
 	}
 	t.Run("ByDestination", func(t *testing.T) {
 		for _, layer := range layers {
-			txs, err := mdb.GetTransactionsByDestination(types.NewLayerID(layer), addr1)
+			lid := types.NewLayerID(layer)
+			txs, err := mdb.GetTransactionsByDestination(lid, lid, addr1)
 			require.NoError(t, err)
 			assert.Len(t, txs, 1)
 		}
@@ -630,7 +630,8 @@ func TestMesh_FindOnce(t *testing.T) {
 
 	t.Run("ByOrigin", func(t *testing.T) {
 		for _, layer := range layers {
-			txs, err := mdb.GetTransactionsByOrigin(types.NewLayerID(layer), addr1)
+			lid := types.NewLayerID(layer)
+			txs, err := mdb.GetTransactionsByOrigin(lid, lid, addr1)
 			require.NoError(t, err)
 			assert.Len(t, txs, 1)
 		}
