0f3371d4493ed2b42ba148b1116377075a6cd07f
kimmy lin <30611210+countvonzero@users.noreply.github.com>
2022-03-03 16:21:44 +0800

    remove mesh from proposal db

--- cmd/node/node.go
+++ cmd/node/node.go
@@ -559,7 +559,7 @@ func (app *App) initServices(ctx context.Context,
 	}
 	updater.Mesh = msh
 
-	proposalDB, err := proposals.NewProposalDB(sqlDB, msh, app.addLogger(ProposalDBLogger, lg))
+	proposalDB, err := proposals.NewProposalDB(sqlDB, app.addLogger(ProposalDBLogger, lg))
 	if err != nil {
 		return fmt.Errorf("create proposal DB: %w", err)
 	}
