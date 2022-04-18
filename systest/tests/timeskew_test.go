8c2c28409a82c77b83c83e9cc7a883b01d3e8821
ds <yashulyak@gmail.com>
2022-04-18 09:23:27 +0300

    refactor expectations

--- systest/tests/timeskew_test.go
+++ systest/tests/timeskew_test.go
@@ -3,7 +3,6 @@ package tests
 import (
 	"context"
 	"testing"
-	"time"
 
 	spacemeshv1 "github.com/spacemeshos/api/release/go/spacemesh/v1"
 	"github.com/spacemeshos/go-spacemesh/systest/chaos"
@@ -11,7 +10,6 @@ import (
 	"github.com/spacemeshos/go-spacemesh/systest/testcontext"
 	"golang.org/x/sync/errgroup"
 
-	"github.com/stretchr/testify/assert"
 	"github.com/stretchr/testify/require"
 )
 
@@ -19,14 +17,14 @@ func TestShortTimeskew(t *testing.T) {
 	const (
 		enableSkew = 10
 		stopSkew   = enableSkew + 3
-		stopTest   = stopSkew + 2
+		stopTest   = stopSkew + 3
 		skewOffset = "-3s" // hare round is 2s
 	)
 	tctx := testcontext.New(t, testcontext.Labels("sanity"))
 	cl, err := cluster.Default(tctx, cluster.WithKeys(10))
 	require.NoError(t, err)
 
-	failed := int(0.1 * float64(tctx.ClusterSize))
+	failed := int(0.2 * float64(tctx.ClusterSize))
 	eg, ctx := errgroup.WithContext(tctx)
 	client := cl.Client(0)
 	scheduleChaos(ctx, eg, client, enableSkew, stopSkew, func(ctx context.Context) (chaos.Teardown, error) {
@@ -37,26 +35,26 @@ func TestShortTimeskew(t *testing.T) {
 		return chaos.Timeskew(tctx, "skew", skewOffset, names...)
 	})
 
-	var reference *time.Time
+	// there are two possible failure scenarios:
+	// in 1st 20% of the skewed nodes are not leaders, and they will simply cast abstain
+	// votes for layer 11 and 12. it may delay layer confirmation, by no more than one layer
+	// in 2nd - some nodes might be leaders, and in such case whole cluster will have to
+	// abstain on one or two layers. in such case longer delay might be necessary to confirm that layer
+
+	var confirmed uint32
 	watchLayers(ctx, eg, client, func(layer *spacemeshv1.LayerStreamResponse) (bool, error) {
+		if layer.Layer.Number.Number == stopTest {
+			return false, nil
+		}
 		if layer.Layer.Status == spacemeshv1.Layer_LAYER_STATUS_CONFIRMED {
 			tctx.Log.Debugw("confirmed layer",
 				"layer", layer.Layer.Number.Number,
 				"hash", prettyHex(layer.Layer.Hash),
 			)
-			if layer.Layer.Number.Number == stopTest {
-				return false, nil
-			}
-			now := time.Now()
-			if reference == nil {
-				reference = &now
-			} else {
-				prev := *reference
-				reference = &now
-				return assert.WithinDuration(t, prev, now, 20*time.Second), nil
-			}
+			confirmed = layer.Layer.Number.Number
 		}
 		return true, nil
 	})
 	require.NoError(t, eg.Wait())
+	require.LessOrEqual(t, stopSkew, int(confirmed))
 }
