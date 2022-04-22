baca339f8d2a379cb8753d595f4e2903ef6b65f4
Eugene Toropov <eugene.toropov@gmail.com>
2022-04-22 04:22:19 +0000

    Systest: update README (#3179)
    ## Motivation
    I've noticed a few discrepancies when going through systest installation process. So here are a few changes to make this process cleaner.
    
    ## DevOps Notes
    <!-- Please uncheck these items as applicable to make DevOps aware of changes that may affect releases -->
    - [x] This PR does not require configuration changes (e.g., environment variables, GitHub secrets, VM resources)
    - [x] This PR does not affect public APIs
    - [x] This PR does not rely on a new version of external services (PoET, elasticsearch, etc.)
    - [x] This PR does not make changes to log messages (which monitoring infrastructure may rely on)

--- systest/cluster/nodes.go
+++ systest/cluster/nodes.go
@@ -189,7 +189,7 @@ func deployNodes(ctx *testcontext.Context, name string, replicas int, flags []De
 						WithResources(corev1.ResourceRequirements().WithRequests(
 							v1.ResourceList{
 								v1.ResourceCPU:    resource.MustParse("0.5"),
-								v1.ResourceMemory: resource.MustParse("1Gi"),
+								v1.ResourceMemory: resource.MustParse("200Mi"),
 							},
 						)).
 						WithEnv(
