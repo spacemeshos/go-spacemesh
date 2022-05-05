66a7931e564a43210ee9b95257fc58abf2b81704
Dmitry <yashulyak@gmail.com>
2022-05-05 13:54:48 +0300

    remove json flags from Account

--- common/types/account.go
+++ common/types/account.go
@@ -2,8 +2,8 @@ package types
 
 // Account represents account state at a certain layer.
 type Account struct {
-	Layer   LayerID `json:"-"`
-	Address Address `json:"-"`
-	Nonce   uint64  `jons:"-"`
-	Balance uint64  `json:"balance"`
+	Layer   LayerID
+	Address Address
+	Nonce   uint64
+	Balance uint64
 }
