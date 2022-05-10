2b65df45d1349d2f6cea442e068d13a0da74a721
kimmy lin <30611210+countvonzero@users.noreply.github.com>
2022-05-10 19:16:11 -0400

    decouple layer data sync and ballots processing

---
+++ syncer/interface.go
@@ -0,0 +1,27 @@
+package syncer
+
+import (
+	"context"
+
+	"github.com/spacemeshos/go-spacemesh/common/types"
+	"github.com/spacemeshos/go-spacemesh/layerfetcher"
+)
+
+//go:generate mockgen -package=mocks -destination=./mocks/mocks.go -source=./interface.go
+
+type layerTicker interface {
+	GetCurrentLayer() types.LayerID
+}
+
+type layerFetcher interface {
+	PollLayerContent(context.Context, types.LayerID) chan layerfetcher.LayerPromiseResult
+	GetEpochATXs(context.Context, types.EpochID) error
+}
+
+type layerPatrol interface {
+	IsHareInCharge(types.LayerID) bool
+}
+
+type layerProcessor interface {
+	ProcessLayer(context.Context, types.LayerID) error
+}
