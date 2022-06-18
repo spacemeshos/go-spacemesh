feb91aa8a5766107a3839d828dda5342eeb8c228
kimmy lin <30611210+countvonzero@users.noreply.github.com>
2022-06-17 18:02:32 -0700

    use one db for proposals pkg

--- proposals/interface.go
+++ proposals/interface.go
@@ -4,31 +4,12 @@ import (
 	"context"
 
 	"github.com/spacemeshos/go-spacemesh/common/types"
-	"github.com/spacemeshos/go-spacemesh/signing"
 )
 
 //go:generate mockgen -package=mocks -destination=./mocks/mocks.go -source=./interface.go
 
-type atxDB interface {
-	GetAtxHeader(types.ATXID) (*types.ActivationTxHeader, error)
-}
-
-type ballotDB interface {
-	GetBallot(types.BallotID) (*types.Ballot, error)
-}
-
-type meshDB interface {
+type meshProvider interface {
 	AddTXsFromProposal(context.Context, types.LayerID, types.ProposalID, []types.TransactionID) error
-	HasBallot(types.BallotID) bool
-	AddBallot(*types.Ballot) error
-	GetBallot(types.BallotID) (*types.Ballot, error)
-	GetBlockLayer(types.BlockID) (types.LayerID, error)
-	SetIdentityMalicious(*signing.PublicKey) error
-}
-
-type proposalDB interface {
-	HasProposal(types.ProposalID) bool
-	AddProposal(context.Context, *types.Proposal) error
 }
 
 type eligibilityValidator interface {
