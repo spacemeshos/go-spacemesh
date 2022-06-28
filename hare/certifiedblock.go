b6853dbeed7edf8726c39febe52a3f06d3473fd7
Jon Zlotnik <me@jonzlotnik.info>
2022-06-28 17:45:10 -0400

    more of the same

--- hare/certifiedblock.go
+++ hare/certifiedblock.go
@@ -7,34 +7,37 @@ import (
 	"github.com/spacemeshos/go-spacemesh/common/types"
 )
 
-// CertifiedBlockService is a thread-safe service which participates in the building
-// of certified blocks.
+// CertifiedBlockService allows a node to participate in the building of certified blocks.
+// This includes gossiping block signatures and verifying gossiped block signatures upon
+// hare termination of every layer.
 type BlockCertifiyingService struct {
-}
-
-func (bcs *BlockCertifiyingService) Start(ctx context.Context) error {
-	return fmt.Errorf("not yet implemented")
 }
 
 func NewBlockCertifiyingService() (*BlockCertifiyingService, error) {
 	return nil, fmt.Errorf("not yet implemented")
 }
 
-type CertifiedBlockStore struct {
-}
-
-func (cbs *CertifiedBlockStore) StoreCertifiedBlock() error {
+func (bcs *BlockCertifiyingService) Start(ctx context.Context) error {
 	return fmt.Errorf("not yet implemented")
 }
 
+type CertifiedBlockStore struct {
+}
+
+func (cbs *CertifiedBlockStore) StoreCertifiedBlock(cBlock []byte) error {
+	return fmt.Errorf("not yet implemented")
+}
+
+// CertifiedBlockProvider is an object designed to retrieve one CertifiedBlock
+// from a BlockCertifiyingService.
 type CertifiedBlockProvider struct {
 }
 
-func NewCertifiedBlockProvider(s BlockCertifiyingService) (*CertifiedBlockProvider, error) {
+func NewCertifiedBlockProvider(s BlockCertifiyingService, bID types.BlockID) (*CertifiedBlockProvider, error) {
 	return nil, fmt.Errorf("not yet implemented")
 }
 
-func (cbp *CertifiedBlockProvider) WaitForCertifiedBlock() (<-chan types.BlockID, error) {
+func (cbp *CertifiedBlockProvider) WaitForCertifiedBlock() (<-chan struct{}, error) {
 	return nil, fmt.Errorf("not implemented yet")
 }
 
@@ -56,4 +59,9 @@ type certifiedBlock struct {
 
 func (cbs *CertifiedBlockStore) storeBlockSignature(blockID types.BlockID, sig terminationSignature) error {
 	return fmt.Errorf("not yet implemented")
+}
+
+// BlockSignatureCollector collects gossiped block signatures and groups them by
+// block.
+type blockSignatureCollectionService struct {
 }
