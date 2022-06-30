de23f8c0aab04ca9c202a4be524f151d19b6ec96
Jon Zlotnik <me@jonzlotnik.info>
2022-06-30 12:37:48 -0400

    testing use cases

--- hare/certifiedblock_test.go
+++ hare/certifiedblock_test.go
@@ -1,10 +1,47 @@
 package hare
 
-import "testing"
+import (
+	"context"
+	"sync"
+	"testing"
 
-func Test_BlockCertifiyingService_ProducesCertifiedBlocks(t *testing.T) {
+	"github.com/spacemeshos/go-spacemesh/common/types"
+	"github.com/stretchr/testify/require"
+)
+
+// Integration Tests
+
+func Test_WaitingForCertificate_SignaturesFromGossip(t *testing.T) {
+	service, err := NewBlockCertifyingService()
+	require.NoError(t, err)
+	store, err := NewCertifiedBlockStore()
+	require.NoError(t, err)
+
+	// Wait until a block is certified:
+	bID := types.RandomBlockID()
+	cBlockProvider, _ := NewCertifiedBlockProvider(service, store, bID)
+	wg := sync.WaitGroup{}
+	wg.Add(1)
+	go func() {
+		defer wg.Done()
+		// Block until valid BlockCertificate is successfully built.
+		cert := <-cBlockProvider.GetBlockCertificateChannel()
+		if cert != nil {
+
+		}
+		// Do something with the block certificate from the channel or just block and get
+		// notified once a valid certificate is successfully built.
+	}()
+
+	err = service.Start(context.Background())
+	require.NoError(t, err)
+	wg.Wait()
+}
+
+func Test_WaitingForCertificate_SignaturesAddedToStore(t *testing.T) {
 
 }
+
 func Test_CertifiedBlockStore_StoresBlock(t *testing.T) {
 
 }
