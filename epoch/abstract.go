d5267508cee1ef5cb0cdd46981ac171646905f85
Alejandro <abergasov@gmail.com>
2022-07-04 10:46:59 +0400

    back crud

--- epoch/abstract.go
+++
@@ -1,8 +0,0 @@
-package epoch
-
-import "github.com/spacemeshos/go-spacemesh/common/types"
-
-type Epocher interface {
-	UpdateEpochStatus(epoch types.EpochID, status types.EpochStatus) error
-	GetEpochStatus(epoch []types.EpochID) (map[types.EpochID]types.EpochStatus, error)
-}
--- sql/migrations/0001_initial.sql
+++ sql/migrations/0001_initial.sql
@@ -151,9 +151,3 @@ CREATE TABLE accounts
 );
 
 CREATE INDEX accounts_by_layer_udated ON accounts (layer_updated);
-
-CREATE TABLE epochs
-(
-    epoch_id      INT PRIMARY KEY,
-    epoch_status  INT NOT NULL
-);
