c404fc696a52bc75f9d79fb01f0ec39d03fd882e
Eugene Toropov <eugene.toropov@gmail.com>
2022-08-16 10:45:50 +0400

    make linter happy + go mod tidy

--- hare/hare_test.go
+++ hare/hare_test.go
@@ -7,10 +7,9 @@ import (
 	"testing"
 	"time"
 
+	"github.com/golang/mock/gomock"
 	"github.com/google/go-cmp/cmp"
 	"github.com/google/go-cmp/cmp/cmpopts"
-
-	"github.com/golang/mock/gomock"
 	"github.com/stretchr/testify/assert"
 	"github.com/stretchr/testify/require"
 
