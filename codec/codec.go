8b3aca19a5965e1b28ac2993f2f6a1444e855f2d
Eugene Toropov <eugene.toropov@gmail.com>
2022-08-16 10:20:54 +0000

    Enable scale codec for SQL proposal txIDs (#3448)
    ## Motivation
    <!-- Please mention the issue fixed by this PR or detailed motivation -->
    Part of #3271 
    
    ## Changes
    Introduced EncodeSlice/DecodeSlice codec methods + enabled scale codec for SQL proposal txIDs
    
    ## DevOps Notes
    <!-- Please uncheck these items as applicable to make DevOps aware of changes that may affect releases -->
    - [x] This PR does not require configuration changes (e.g., environment variables, GitHub secrets, VM resources)
    - [x] This PR does not affect public APIs
    - [x] This PR does not rely on a new version of external services (PoET, elasticsearch, etc.)
    - [x] This PR does not make changes to log messages (which monitoring infrastructure may rely on)

--- codec/codec.go
+++ codec/codec.go
@@ -90,3 +90,22 @@ func Decode(buf []byte, value Decodable) error {
 
 	return nil
 }
+
+// EncodeSlice encodes slice to a buffer.
+func EncodeSlice[V any, H scale.EncodablePtr[V]](value []V) ([]byte, error) {
+	var b bytes.Buffer
+	_, err := scale.EncodeStructSlice[V, H](scale.NewEncoder(&b), value)
+	if err != nil {
+		return nil, fmt.Errorf("encode struct slice: %w", err)
+	}
+	return b.Bytes(), nil
+}
+
+// DecodeSlice decodes slice from a buffer.
+func DecodeSlice[V any, H scale.DecodablePtr[V]](buf []byte) ([]V, error) {
+	v, _, err := scale.DecodeStructSlice[V, H](scale.NewDecoder(bytes.NewReader(buf)))
+	if err != nil {
+		return nil, fmt.Errorf("decode struct slice: %w", err)
+	}
+	return v, nil
+}
