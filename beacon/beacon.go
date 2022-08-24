530a6c6da3998d3533062ed9b9fba2f5b8359941
Eugene Toropov <eugene.toropov@gmail.com>
2022-08-24 17:08:03 +0400

    apply scale codec to buildProposal in beacon

--- beacon/beacon.go
+++ beacon/beacon.go
@@ -930,11 +930,15 @@ func signMessage(signer signing.Signer, message interface{}, logger log.Log) []b
 	return signer.Sign(encoded)
 }
 
+//go:generate scalegen -types BuildProposalMessage
+
+type BuildProposalMessage struct {
+	Prefix string
+	Epoch  uint32
+}
+
 func buildProposal(epoch types.EpochID, logger log.Log) []byte {
-	message := &struct {
-		Prefix string
-		Epoch  uint32
-	}{
+	message := &BuildProposalMessage{
 		Prefix: proposalPrefix,
 		Epoch:  uint32(epoch),
 	}
