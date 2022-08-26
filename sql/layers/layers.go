f0de6421da1010858241ff908cfb47397d73ce10
kimmy lin <30611210+countvonzero@users.noreply.github.com>
2022-08-25 22:22:39 -0500

    fix sql bugs for cert

--- sql/layers/layers.go
+++ sql/layers/layers.go
@@ -55,7 +55,7 @@ func GetWeakCoin(db sql.Executor, lid types.LayerID) (bool, error) {
 // SetHareOutput for the layer to a block id.
 func SetHareOutput(db sql.Executor, lid types.LayerID, output types.BlockID) error {
 	if _, err := db.Exec(`insert into layers (id, hare_output) values (?1, ?2) 
-					on conflict(id) do update set hare_output=?2 where hare_output is null;`,
+					on conflict(id) do update set hare_output=?2 where hare_output is null and cert is null;`,
 		func(stmt *sql.Statement) {
 			stmt.BindInt64(1, int64(lid.Value))
 			stmt.BindBytes(2, output[:])
@@ -72,14 +72,14 @@ func SetHareOutputWithCert(db sql.Executor, lid types.LayerID, cert *types.Certi
 	if err != nil {
 		return fmt.Errorf("encode cert %w", err)
 	}
-	if _, err := db.Exec(`insert into layers (id, hare_output, cert) values (?1, ?2, ?3) 
-					on conflict(id) do update set hare_output=?2, cert=?3;`,
+	if _, err = db.Exec(`insert into layers (id, hare_output, cert) values (?1, ?2, ?3)
+					on conflict(id) do update set hare_output=?2, cert=?3 where cert is null;`,
 		func(stmt *sql.Statement) {
 			stmt.BindInt64(1, int64(lid.Value))
 			stmt.BindBytes(2, output[:])
 			stmt.BindBytes(3, data[:])
 		}, nil); err != nil {
-		return fmt.Errorf("set hare output %s: %w", lid, err)
+		return fmt.Errorf("set hare output w cert %s: %w", lid, err)
 	}
 	return nil
 }
@@ -91,7 +91,7 @@ func GetCert(db sql.Executor, lid types.LayerID) (*types.Certificate, error) {
 		err  error
 		rows int
 	)
-	if rows, err = db.Exec("select cert from layers where id = ?1;", func(stmt *sql.Statement) {
+	if rows, err = db.Exec("select cert from layers where id = ?1 and cert is not null;", func(stmt *sql.Statement) {
 		stmt.BindInt64(1, int64(lid.Value))
 	}, func(stmt *sql.Statement) bool {
 		data := make([]byte, stmt.ColumnLen(0))
