b5b085dc3398084d900b2bd57e6943679bd84c46
Dmitry <yashulyak@gmail.com>
2022-08-31 08:45:09 +0300

    reuse test code

--- tortoise/tortoise_test.go
+++ tortoise/tortoise_test.go
@@ -2790,142 +2790,79 @@ func TestFutureHeight(t *testing.T) {
 	})
 }
 
+func testEmptyLayers(t *testing.T, hdist int) {
+	const size = 4
+
+	ctx := context.Background()
+	cfg := defaultTestConfig()
+	cfg.Hdist = uint32(hdist)
+	cfg.Zdist = 3
+	cfg.LayerSize = size
+
+	// TODO(dshulyak) parametrize test with varying skipFrom, skipTo
+	// skiping layers 9, 10, 11, 12, 13
+	skipFrom, skipTo := 1, 6
+
+	s := sim.New(
+		sim.WithLayerSize(cfg.LayerSize),
+	)
+	s.Setup(
+		sim.WithSetupMinerRange(size, size),
+	)
+	tortoise := tortoiseFromSimState(
+		s.GetState(0), WithConfig(cfg), WithLogger(logtest.New(t)),
+	)
+	var (
+		last     = types.GetEffectiveGenesis()
+		verified types.LayerID
+	)
+	for i := 0; i < int(skipTo); i++ {
+		opts := []sim.NextOpt{
+			sim.WithVoteGenerator(tortoiseVotingWithCurrent(tortoise)),
+		}
+		skipped := i >= skipFrom && i < skipTo
+		if skipped {
+			opts = append(opts, sim.WithoutHareOutput(), sim.WithNumBlocks(0))
+		} else {
+			opts = append(opts, sim.WithNumBlocks(1))
+		}
+		last = s.Next(opts...)
+		if skipped {
+			continue
+		}
+		verified = tortoise.HandleIncomingLayer(ctx, last)
+	}
+	require.Equal(t, types.GetEffectiveGenesis(), verified)
+	for i := 0; i <= int(cfg.Zdist); i++ {
+		last = s.Next(
+			sim.WithNumBlocks(1),
+			sim.WithVoteGenerator(tortoiseVotingWithCurrent(tortoise)),
+		)
+		verified = tortoise.HandleIncomingLayer(ctx, last)
+	}
+	expected := types.GetEffectiveGenesis().Add(uint32(skipFrom))
+	require.Equal(t, expected, verified)
+	for i := 0; i < skipTo-skipFrom-1; i++ {
+		last = s.Next(
+			sim.WithNumBlocks(1),
+			sim.WithVoteGenerator(tortoiseVotingWithCurrent(tortoise)),
+		)
+		verified = tortoise.HandleIncomingLayer(ctx, last)
+		require.Equal(t, expected.Add(uint32(i)+1), verified)
+	}
+	last = s.Next(
+		sim.WithNumBlocks(1),
+		sim.WithVoteGenerator(tortoiseVotingWithCurrent(tortoise)),
+	)
+	verified = tortoise.HandleIncomingLayer(ctx, last)
+	require.Equal(t, last.Sub(1), verified)
+}
+
 func TestEmptyLayers(t *testing.T) {
-	t.Run("no hare output recovered within hdist", func(t *testing.T) {
-		const size = 4
-
-		ctx := context.Background()
-		cfg := defaultTestConfig()
-		cfg.Hdist = 10
-		cfg.Zdist = 3
-		cfg.LayerSize = size
-
-		// TODO(dshulyak) parametrize test with varying skipFrom, skipTo
-		// skiping layers 9, 10, 11, 12, 13
-		skipFrom, skipTo := 1, 6
-
-		s := sim.New(
-			sim.WithLayerSize(cfg.LayerSize),
-		)
-		s.Setup(
-			sim.WithSetupMinerRange(size, size),
-		)
-		tortoise := tortoiseFromSimState(
-			s.GetState(0), WithConfig(cfg), WithLogger(logtest.New(t)),
-		)
-		var (
-			last     = types.GetEffectiveGenesis()
-			verified types.LayerID
-		)
-		for i := 0; i < int(skipTo); i++ {
-			opts := []sim.NextOpt{
-				sim.WithVoteGenerator(tortoiseVotingWithCurrent(tortoise)),
-			}
-			skipped := i >= skipFrom && i < skipTo
-			if skipped {
-				opts = append(opts, sim.WithoutHareOutput(), sim.WithNumBlocks(0))
-			} else {
-				opts = append(opts, sim.WithNumBlocks(1))
-			}
-			last = s.Next(opts...)
-			if skipped {
-				continue
-			}
-			verified = tortoise.HandleIncomingLayer(ctx, last)
-		}
-		require.Equal(t, types.GetEffectiveGenesis(), verified,
-			"votes for layer genesis+skipFrom are not counted yet")
-		for i := 0; i <= int(cfg.Zdist); i++ {
-			last = s.Next(
-				sim.WithNumBlocks(1),
-				sim.WithVoteGenerator(tortoiseVotingWithCurrent(tortoise)),
-			)
-			verified = tortoise.HandleIncomingLayer(ctx, last)
-		}
-		expected := types.GetEffectiveGenesis().Add(uint32(skipFrom))
-		require.Equal(t, expected, verified,
-			"votes from all skipped layers are counted because they cross zdist",
-		)
-		for i := 0; i < skipTo-skipFrom-1; i++ {
-			last = s.Next(
-				sim.WithNumBlocks(1),
-				sim.WithVoteGenerator(tortoiseVotingWithCurrent(tortoise)),
-			)
-			verified = tortoise.HandleIncomingLayer(ctx, last)
-			require.Equal(t, expected.Add(uint32(i)+1), verified)
-		}
-		last = s.Next(
-			sim.WithNumBlocks(1),
-			sim.WithVoteGenerator(tortoiseVotingWithCurrent(tortoise)),
-		)
-		verified = tortoise.HandleIncomingLayer(ctx, last)
-		require.Equal(t, last.Sub(1), verified)
+	t.Run("recovered within hdist", func(t *testing.T) {
+		testEmptyLayers(t, 10)
 	})
-	t.Run("no hare output not recovered within hdist", func(t *testing.T) {
-		const size = 4
-
-		ctx := context.Background()
-		cfg := defaultTestConfig()
-		cfg.Hdist = 5
-		cfg.Zdist = 3
-		cfg.LayerSize = size
-
-		// TODO(dshulyak) parametrize test with varying skipFrom, skipTo
-		// skiping layers 9, 10, 11, 12, 13
-		skipFrom, skipTo := 1, 6
-
-		s := sim.New(
-			sim.WithLayerSize(cfg.LayerSize),
-		)
-		s.Setup(
-			sim.WithSetupMinerRange(size, size),
-		)
-		tortoise := tortoiseFromSimState(
-			s.GetState(0), WithConfig(cfg), WithLogger(logtest.New(t)),
-		)
-		var (
-			last     = types.GetEffectiveGenesis()
-			verified types.LayerID
-		)
-		for i := 0; i < int(skipTo); i++ {
-			opts := []sim.NextOpt{
-				sim.WithVoteGenerator(tortoiseVotingWithCurrent(tortoise)),
-			}
-			skipped := i >= skipFrom && i < skipTo
-			if skipped {
-				opts = append(opts, sim.WithoutHareOutput(), sim.WithNumBlocks(0))
-			} else {
-				opts = append(opts, sim.WithNumBlocks(1))
-			}
-			last = s.Next(opts...)
-			verified = tortoise.HandleIncomingLayer(ctx, last)
-		}
-		require.Equal(t, types.GetEffectiveGenesis(), verified,
-			"votes for layer genesis+skipFrom are not counted yet")
-		for i := 0; i <= int(cfg.Zdist); i++ {
-			last = s.Next(
-				sim.WithNumBlocks(1),
-				sim.WithVoteGenerator(tortoiseVotingWithCurrent(tortoise)),
-			)
-			verified = tortoise.HandleIncomingLayer(ctx, last)
-		}
-		expected := types.GetEffectiveGenesis().Add(uint32(skipFrom))
-		require.Equal(t, expected, verified,
-			"votes from all skipped layers are counted because they cross zdist",
-		)
-		for i := 0; i < skipTo-skipFrom-1; i++ {
-			last = s.Next(
-				sim.WithNumBlocks(1),
-				sim.WithVoteGenerator(tortoiseVotingWithCurrent(tortoise)),
-			)
-			verified = tortoise.HandleIncomingLayer(ctx, last)
-			require.Equal(t, expected.Add(uint32(i)+1), verified)
-		}
-		last = s.Next(
-			sim.WithNumBlocks(1),
-			sim.WithVoteGenerator(tortoiseVotingWithCurrent(tortoise)),
-		)
-		verified = tortoise.HandleIncomingLayer(ctx, last)
-		require.Equal(t, last.Sub(1), verified)
+	t.Run("not recovered within hdist", func(t *testing.T) {
+		testEmptyLayers(t, 5)
 	})
 }
