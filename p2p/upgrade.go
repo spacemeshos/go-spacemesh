88dc71764550191f2cc71aff7568c0112cd80604
Eugene Toropov <eugene.toropov@gmail.com>
2022-09-23 15:46:34 +0300

    clean

--- p2p/upgrade.go
+++ p2p/upgrade.go
@@ -47,14 +47,6 @@ func WithNodeReporter(reporter func()) Opt {
 	}
 }
 
-// WithNodeReporter updates reporter that is notified every time when
-// node added or removed a peer.
-func WithGenesisID(reporter func()) Opt {
-	return func(fh *Host) {
-		fh.nodeReporter = reporter
-	}
-}
-
 // Host is a conveniency wrapper for all p2p related functionality required to run
 // a full spacemesh node.
 type Host struct {
@@ -131,7 +123,7 @@ func Upgrade(h host.Host, genesisID types.Hash20, opts ...Opt) (*Host, error) {
 	}, fh, fh.discovery); err != nil {
 		return nil, fmt.Errorf("failed to initiliaze bootstrap: %w", err)
 	}
-	fh.hs = handshake.New(fh, genesisID, handshake.WithLog(fh.logger)) // TODO
+	fh.hs = handshake.New(fh, genesisID, handshake.WithLog(fh.logger))
 	return fh, nil
 }
 
