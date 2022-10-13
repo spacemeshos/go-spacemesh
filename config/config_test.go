1f4ac3929af622470b94aadf386b6c2bc7f5afb7
Matthias <5011972+fasmat@users.noreply.github.com>
2022-10-13 13:28:23 +0000

    Remove obsolete test

--- config/config_test.go
+++ config/config_test.go
@@ -1,8 +1,6 @@
 package config
 
 import (
-	"os"
-	"path/filepath"
 	"testing"
 
 	"github.com/spf13/viper"
@@ -15,17 +13,4 @@ func TestLoadConfig(t *testing.T) {
 	err := LoadConfig(".asdasda", vip)
 	// verify that after attempting to load a non-existent file, an attempt is made to load the default config
 	assert.EqualError(t, err, "failed to read config file open ./config.toml: no such file or directory")
-}
-
-func TestConfig_DataDir(t *testing.T) {
-	sep := string(filepath.Separator)
-
-	config := DefaultTestConfig()
-	config.DataDirParent = "~" + sep + "space-a-mesh"
-	home, err := os.UserHomeDir()
-	assert.NoError(t, err)
-	assert.Equal(t, filepath.Join(home, "space-a-mesh"), config.DataDir())
-
-	config.DataDirParent = "~" + sep + "space-a-mesh" + sep // trailing slash should be ignored
-	assert.Equal(t, filepath.Join(home, "space-a-mesh"), config.DataDir())
 }
