b274e275f4a705e9b31aa048d866aaccce93fbdc
Bartosz Różański <bartek.roza@gmail.com>
2022-11-08 12:43:46 +0000

    Reduce grace period in systests (#3689)
    ## Motivation
    Currently, in system tests, the _cycle gap_ parameter is set to 2x _grace period_. It leaves zero space for setting a deadline to wait for poet proofs.
    
    ## Changes
    Changed grace period to 3s. That should give a 4s window for setting up the poet proof deadline (the cycle gap is 10s).
    
    ## DevOps Notes
    - [x] This PR does not require configuration changes (e.g., environment variables, GitHub secrets, VM resources)
    - [x] This PR does not affect public APIs
    - [x] This PR does not rely on a new version of external services (PoET, elasticsearch, etc.)
    - [x] This PR does not make changes to log messages (which monitoring infrastructure may rely on)

--- systest/cluster/cluster.go
+++ systest/cluster/cluster.go
@@ -101,7 +101,7 @@ func New(cctx *testcontext.Context, opts ...Opt) *Cluster {
 	cluster.addFlag(TargetOutbound(defaultTargetOutbound(cctx.ClusterSize)))
 	cluster.addFlag(CycleGap(10 * time.Second))
 	cluster.addFlag(PhaseShift(20 * time.Second))
-	cluster.addFlag(GracePeriod(5 * time.Second))
+	cluster.addFlag(GracePeriod(3 * time.Second))
 
 	cluster.addPoetFlag(genesis)
 	cluster.addPoetFlag(PoetRestListen(poetPort))
