c95115aa0cd2cb1088cb8d38c4b4db5a82d0e861
Dmitry Shulyak <yashulyak@gmail.com>
2022-11-15 06:56:32 +0000

    prevent double close panic (#3743)
    closes: https://github.com/spacemeshos/go-spacemesh/issues/3735
    
    this closer should be dropped from the codebase, but i want a quick fix to prevent panics

--- common/util/closer.go
+++ common/util/closer.go
@@ -8,14 +8,18 @@ type Closer struct {
 }
 
 // NewCloser creates a new (not closed) closer.
+// DEPRECATED: use context.WithCancel.
 func NewCloser() Closer {
 	return Closer{make(chan struct{})}
 }
 
 // Close signals all listening instances to close.
-// Note: should be called only once.
 func (closer *Closer) Close() {
-	close(closer.channel)
+	select {
+	case <-closer.channel:
+	default:
+		close(closer.channel)
+	}
 }
 
 // CloseChannel returns the channel to wait on for close signal.
