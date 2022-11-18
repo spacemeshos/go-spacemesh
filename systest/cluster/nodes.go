69ac4fc344a9e117b2f4a7df65217a85229c356d
Dmitry <yashulyak@gmail.com>
2022-11-18 07:36:58 +0200

    update limit

--- systest/cluster/nodes.go
+++ systest/cluster/nodes.go
@@ -393,13 +393,13 @@ func deployNode(ctx *testcontext.Context, name string, labels map[string]string,
 							WithRequests(
 								v1.ResourceList{
 									v1.ResourceCPU:    resource.MustParse("0.2"),
-									v1.ResourceMemory: resource.MustParse("400Mi"),
+									v1.ResourceMemory: resource.MustParse("500Mi"),
 								},
 							).
 							WithLimits(
 								v1.ResourceList{
 									v1.ResourceCPU:    resource.MustParse("1"),
-									v1.ResourceMemory: resource.MustParse("400Mi"),
+									v1.ResourceMemory: resource.MustParse("1Gi"),
 								},
 							),
 						).
