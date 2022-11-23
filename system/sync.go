e0ec1f16445aa6372f7a5dbeb6449e23a3a68af5
kimmy <30611210+countvonzero@users.noreply.github.com>
2022-11-23 15:53:16 +0000

    hare: make beacon syncedness criteria for hare participation and msg validation (#3782)
    ## Motivation
    <!-- Please mention the issue fixed by this PR or detailed motivation -->
    Closes #2937
    Closes #3776
    <!-- `Closes #XXXX, closes #XXXX, ...` links mentioned issues to this PR and automatically closes them when this it's merged -->
    
    ## Changes
    <!-- Please describe in detail the changes made -->
    - remove unnecessary turbo hare
    - serialize hare onTick() for each layer
    - remove caching for syncedness for each layer. there is really no need for this cache
    - add beacon syncedness as a criteria for hare participation and message validation

--- system/sync.go
+++ system/sync.go
@@ -1,10 +1,15 @@
 package system
 
-import "context"
+import (
+	"context"
+
+	"github.com/spacemeshos/go-spacemesh/common/types"
+)
 
 //go:generate mockgen -package=mocks -destination=./mocks/sync.go -source=./sync.go
 
 // SyncStateProvider defines the interface that provides the node's sync state.
 type SyncStateProvider interface {
 	IsSynced(context.Context) bool
+	IsBeaconSynced(types.EpochID) bool
 }
