d4841f140c0f47de1e96eed5b46911040ea6a795
Matthias <5011972+fasmat@users.noreply.github.com>
2022-12-19 18:30:12 +0000

    Use VRFPostIndex to create VRF signatures

--- signing/vrfsigner.go
+++ signing/vrfsigner.go
@@ -1,21 +1,47 @@
 package signing
 
 import (
+	"encoding/binary"
+	"fmt"
+
 	"github.com/oasisprotocol/curve25519-voi/primitives/ed25519"
 	"github.com/oasisprotocol/curve25519-voi/primitives/ed25519/extra/ecvrf"
 
 	"github.com/spacemeshos/go-spacemesh/common/types"
+	"github.com/spacemeshos/go-spacemesh/datastore"
+	"github.com/spacemeshos/go-spacemesh/sql/atxs"
 )
 
 // VRFSigner is a signer for VRF purposes.
 type VRFSigner struct {
+	cdb *datastore.CachedDB
+
 	privateKey []byte
 	nodeID     types.NodeID
 }
 
+func (s VRFSigner) getVRFNonce(nodeId types.NodeID) (*types.VRFPostIndex, error) {
+	atxId, err := atxs.GetFirstIDByNodeID(s.cdb, nodeId)
+	if err != nil {
+		return nil, fmt.Errorf("failed to get initial atx for miner %s: %w", nodeId.String(), err)
+	}
+	atx, err := s.cdb.GetAtxHeader(atxId)
+	if err != nil {
+		return nil, fmt.Errorf("failed to get initial atx for miner %s: %w", nodeId.String(), err)
+	}
+	return atx.VRFNonce, nil
+}
+
 // Sign signs a message for VRF purposes.
 func (s VRFSigner) Sign(msg []byte) []byte {
-	return ecvrf.Prove(ed25519.PrivateKey(s.privateKey), msg)
+	nonce, err := s.getVRFNonce(s.nodeID)
+	if err != nil {
+		panic(err)
+	}
+
+	buf := make([]byte, 8)
+	binary.LittleEndian.PutUint64(buf, uint64(*nonce))
+	return ecvrf.Prove(ed25519.PrivateKey(s.privateKey), append(buf, msg...))
 }
 
 // PublicKey of the signer.
