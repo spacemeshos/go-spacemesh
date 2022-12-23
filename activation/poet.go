a54d56feccbb8f0d819655b47fa12ef5cb9e9800
Matthias <5011972+fasmat@users.noreply.github.com>
2022-12-23 02:58:21 +0000

    Undo accidental change

--- activation/poet.go
+++ activation/poet.go
@@ -42,6 +42,7 @@ func NewHTTPPoetHarness(disableBroadcast bool, opts ...HTTPPoetOpt) (*HTTPPoetHa
 		return nil, fmt.Errorf("default integration config: %w", err)
 	}
 
+	cfg.DisableBroadcast = disableBroadcast
 	cfg.Reset = true
 	cfg.Genesis = time.Now().Add(5 * time.Second)
 	cfg.EpochDuration = 4 * time.Second
