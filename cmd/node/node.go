spacemeshos/go-spacemesh

commit 4be28499b6a2787bb2f4551da0d9749e7a144abf

Enable `-race` flag in unit tests (#2930)
## Motivation
Closes #2226
There're some data races left after merging https://github.com/spacemeshos/go-spacemesh/pull/2608. I cannot reproduce them locally but in CI only. Therefore, I'm creating this PR to be able to check what's left to be fixed.

## Changes

- Enable `-race` flag in unit tests

## Test Plan
- Unit tests


## DevOps Notes
- [x] This PR does not require configuration changes (e.g., environment variables, GitHub secrets, VM resources)
- [x] This PR does not affect public APIs
- [x] This PR does not rely on a new version of external services (PoET, elasticsearch, etc.)
- [x] This PR does not make changes to log messages (which monitoring infrastructure may rely on)


Co-authored-by: Dmitry Shulyak <yashulyak@gmail.com>
Co-authored-by: Bartosz Różański <bartek.roza@gmail.com>

cmd/node/node.go
@@ -95,62 +95,74 @@ const (
 	ConStateLogger         = "conState"
 )
 
-// Cmd is the cobra wrapper for the node, that allows adding parameters to it.
-var Cmd = &cobra.Command{
-	Use:   "node",
-	Short: "start node",
-	Run: func(c *cobra.Command, args []string) {
-		conf, err := loadConfig(c)
-		if err != nil {
-			log.With().Fatal("failed to initialize config", log.Err(err))
-		}
-
-		if conf.LOGGING.Encoder == config.JSONLogEncoder {
-			log.JSONLog(true)
-		}
-		app := New(
-			WithConfig(conf),
-			// NOTE(dshulyak) this needs to be max level so that child logger can can be current level or below.
-			// otherwise it will fail later when child logger will try to increase level.
-			WithLog(log.RegisterHooks(
-				log.NewWithLevel("", zap.NewAtomicLevelAt(zapcore.DebugLevel)),
-				events.EventHook())),
-		)
-		starter := func() error {
-			if err := app.Initialize(); err != nil {
-				return err
-			}
-			// This blocks until the context is finished or until an error is produced
-			if err := app.Start(); err != nil {
-				return err
+func GetCommand() *cobra.Command {
+	c := &cobra.Command{
+		Use:   "node",
+		Short: "start node",
+		Run: func(c *cobra.Command, args []string) {
+			conf, err := loadConfig(c)
+			if err != nil {
+				log.With().Fatal("failed to initialize config", log.Err(err))
 			}
 
-			return nil
-		}
-		err = starter()
-		app.Cleanup()
-		if err != nil {
-			log.With().Fatal(err.Error())
-		}
-	},
+			if conf.LOGGING.Encoder == config.JSONLogEncoder {
+				log.JSONLog(true)
+			}
+			app := New(
+				WithConfig(conf),
+				// NOTE(dshulyak) this needs to be max level so that child logger can can be current level or below.
+				// otherwise it will fail later when child logger will try to increase level.
+				WithLog(log.RegisterHooks(
+					log.NewWithLevel("", zap.NewAtomicLevelAt(zapcore.DebugLevel)),
+					events.EventHook())),
+			)
+			starter := func() error {
+				if err := app.Initialize(); err != nil {
+					return err
+				}
+				// This blocks until the context is finished or until an error is produced
+				if err := app.Start(); err != nil {
+					return err
+				}
+
+				return nil
+			}
+			err = starter()
+			app.Cleanup()
+			if err != nil {
+				log.With().Fatal(err.Error())
+			}
+		},
+	}
+
+	cmd.AddCommands(c)
+
+	// versionCmd returns the current version of spacemesh.
+	versionCmd := cobra.Command{
+		Use:   "version",
+		Short: "Show version info",
+		Run: func(c *cobra.Command, args []string) {
+			fmt.Print(cmd.Version)
+			if cmd.Commit != "" {
+				fmt.Printf("+%s", cmd.Commit)
+			}
+			fmt.Println()
+		},
+	}
+	c.AddCommand(&versionCmd)
+
+	return c
 }
 
-// VersionCmd returns the current version of spacemesh.
-var VersionCmd = &cobra.Command{
-	Use:   "version",
-	Short: "Show version info",
-	Run: func(c *cobra.Command, args []string) {
-		fmt.Print(cmd.Version)
-		if cmd.Commit != "" {
-			fmt.Printf("+%s", cmd.Commit)
-		}
-		fmt.Println()
-	},
-}
+var (
+	appLog  log.Log
+	grpcLog *zap.Logger
+)
 
 func init() {
-	cmd.AddCommands(Cmd)
-	Cmd.AddCommand(VersionCmd)
+	appLog = log.NewNop()
+	grpcLog = appLog.WithName(GRPCLogger).WithFields(log.String("module", GRPCLogger)).Zap()
+	grpczap.ReplaceGrpcLoggerV2(grpcLog)
 }
 
 // Service is a general service interface that specifies the basic start/stop functionality.
@@ -238,7 +250,7 @@ func New(opts ...Option) *App {
 	defaultConfig := config.DefaultConfig()
 	app := &App{
 		Config:  &defaultConfig,
-		log:     log.NewNop(),
+		log:     appLog,
 		loggers: make(map[string]*zap.AtomicLevel),
 		term:    make(chan struct{}),
 		started: make(chan struct{}),
@@ -288,6 +300,10 @@ type App struct {
 	loggers map[string]*zap.AtomicLevel
 	term    chan struct{} // this channel is closed when closing services, goroutines should wait on this channel in order to terminate
 	started chan struct{} // this channel is closed once the app has finished starting
+}
+
+func (app *App) Started() chan struct{} {
+	return app.started
 }
 
 func (app *App) introduction() {
@@ -781,15 +797,14 @@ func (app *App) startAPIServices(ctx context.Context) {
 	var services []grpcserver.ServiceAPI
 	registerService := func(svc grpcserver.ServiceAPI) {
 		if app.grpcAPIService == nil {
-			logger := app.addLogger(GRPCLogger, app.log).Zap()
-			grpczap.ReplaceGrpcLoggerV2(logger)
+			app.addLogger(GRPCLogger, app.log)
 			app.grpcAPIService = grpcserver.NewServerWithInterface(apiConf.GrpcServerPort, apiConf.GrpcServerInterface,
 				grpcmw.WithStreamServerChain(
 					grpctags.StreamServerInterceptor(),
-					grpczap.StreamServerInterceptor(logger)),
+					grpczap.StreamServerInterceptor(grpcLog)),
 				grpcmw.WithUnaryServerChain(
 					grpctags.UnaryServerInterceptor(),
-					grpczap.UnaryServerInterceptor(logger)),
+					grpczap.UnaryServerInterceptor(grpcLog)),
 			)
 		}
 		services = append(services, svc)

cmd/node/node_test.go
@@ -168,8 +168,7 @@ func TestSpacemeshApp_AddLogger(t *testing.T) {
 	r.Equal(fmt.Sprintf("INFO\t%-13s\t%s\t{\"module\": \"%s\"}\n", mylogger, teststr, mylogger), buf.String())
 }
 
-func testArgs(ctx context.Context, args ...string) (string, error) {
-	root := Cmd
+func testArgs(ctx context.Context, root *cobra.Command, args ...string) (string, error) {
 	buf := new(bytes.Buffer)
 	root.SetOut(buf)
 	root.SetErr(buf)
@@ -182,6 +181,12 @@ func testArgs(ctx context.Context, args ...string) (string, error) {
 	return buf.String(), err
 }
 
+func cmdWithRun(run func(*cobra.Command, []string)) *cobra.Command {
+	cmd := GetCommand()
+	cmd.Run = run
+	return cmd
+}
+
 func TestSpacemeshApp_Cmd(t *testing.T) {
 	r := require.New(t)
 	app := New(WithLog(logtest.New(t)))
@@ -191,30 +196,26 @@ func TestSpacemeshApp_Cmd(t *testing.T) {
 	r.Equal(config.ConsoleLogEncoder, app.Config.LOGGING.Encoder)
 
 	// Test an illegal flag
-	Cmd.Run = func(*cobra.Command, []string) {
+	c := cmdWithRun(func(*cobra.Command, []string) {
 		// We don't expect this to be called at all
 		r.Fail("Command.Run not expected to run")
-	}
-	str, err := testArgs(context.Background(), "illegal")
+	})
+
+	str, err := testArgs(context.Background(), c, "illegal")
 	r.Error(err)
 	r.Equal(expected, err.Error())
 	r.Equal(expected2, str)
 
 	// Test a legal flag
-	Cmd.Run = func(c *cobra.Command, args []string) {
+	c = cmdWithRun(func(c *cobra.Command, args []string) {
 		r.NoError(cmd.EnsureCLIFlags(c, app.Config))
-	}
-	str, err = testArgs(context.Background(), "--log-encoder", "json")
+	})
+
+	str, err = testArgs(context.Background(), c, "--log-encoder", "json")
 
 	r.NoError(err)
 	r.Empty(str)
 	r.Equal(config.JSONLogEncoder, app.Config.LOGGING.Encoder)
-}
-
-// This must be called in between each test that changes flags.
-func resetFlags() {
-	Cmd.ResetFlags()
-	cmd.AddCommands(Cmd)
 }
 
 func setup() {
@@ -226,7 +227,6 @@ func setup() {
 	cmd.SetCancel(cancel)
 
 	events.CloseEventReporter()
-	resetFlags()
 }
 
 func TestSpacemeshApp_GrpcFlags(t *testing.T) {
@@ -241,96 +241,85 @@ func TestSpacemeshApp_GrpcFlags(t *testing.T) {
 	r.Equal(false, app.Config.API.StartNodeService)
 
 	// Try enabling an illegal service
-	Cmd.Run = func(c *cobra.Command, args []string) {
+	run := func(c *cobra.Command, args []string) {
 		err := cmd.EnsureCLIFlags(c, app.Config)
 		r.Error(err)
 		r.Equal("parse services list: unrecognized GRPC service requested: illegal", err.Error())
 	}
-	str, err := testArgs(context.Background(), "--grpc-port", strconv.Itoa(port), "--grpc", "illegal")
+
+	str, err := testArgs(context.Background(), cmdWithRun(run), "--grpc-port", strconv.Itoa(port), "--grpc", "illegal")
 	r.NoError(err)
 	r.Empty(str)
 	// This should still be set
 	r.Equal(port, app.Config.API.GrpcServerPort)
 	r.Equal(false, app.Config.API.StartNodeService)
 
-	resetFlags()
 	events.CloseEventReporter()
 
 	// Try enabling two services, one with a legal name and one with an illegal name
 	// In this case, the node service will be enabled because it comes first
-	// Uses Cmd.Run as defined above
-	str, err = testArgs(context.Background(), "--grpc-port", strconv.Itoa(port), "--grpc", "node", "--grpc", "illegal")
+	str, err = testArgs(context.Background(), cmdWithRun(run), "--grpc-port", strconv.Itoa(port), "--grpc", "node", "--grpc", "illegal")
 	r.NoError(err)
 	r.Empty(str)
 	r.Equal(true, app.Config.API.StartNodeService)
 
-	resetFlags()
 	events.CloseEventReporter()
 	app.Config.API = apiConfig.DefaultTestConfig()
 
 	// Try the same thing but change the order of the flags
 	// In this case, the node service will not be enabled because it comes second
-	// Uses Cmd.Run as defined above
-	str, err = testArgs(context.Background(), "--grpc", "illegal", "--grpc-port", strconv.Itoa(port), "--grpc", "node")
+	str, err = testArgs(context.Background(), cmdWithRun(run), "--grpc", "illegal", "--grpc-port", strconv.Itoa(port), "--grpc", "node")
 	r.NoError(err)
 	r.Empty(str)
 	r.Equal(false, app.Config.API.StartNodeService)
 
-	resetFlags()
 	events.CloseEventReporter()
 
 	// Use commas instead
 	// In this case, the node service will be enabled because it comes first
 	// Uses Cmd.Run as defined above
-	str, err = testArgs(context.Background(), "--grpc", "node,illegal", "--grpc-port", strconv.Itoa(port))
+	str, err = testArgs(context.Background(), cmdWithRun(run), "--grpc", "node,illegal", "--grpc-port", strconv.Itoa(port))
 	r.NoError(err)
 	r.Empty(str)
 	r.Equal(true, app.Config.API.StartNodeService)
 
-	resetFlags()
 	events.CloseEventReporter()
 
 	// This should work
-	Cmd.Run = func(c *cobra.Command, args []string) {
+	run = func(c *cobra.Command, args []string) {
 		r.NoError(cmd.EnsureCLIFlags(c, app.Config))
 	}
-	str, err = testArgs(context.Background(), "--grpc", "node")
+	str, err = testArgs(context.Background(), cmdWithRun(run), "--grpc", "node")
 	r.Empty(str)
 	r.NoError(err)
 	r.Equal(true, app.Config.API.StartNodeService)
 
-	resetFlags()
 	events.CloseEventReporter()
 
 	// This should work too
-	str, err = testArgs(context.Background(), "--grpc", "node,node")
+	str, err = testArgs(context.Background(), cmdWithRun(run), "--grpc", "node,node")
 	r.Empty(str)
 	r.NoError(err)
 	r.Equal(true, app.Config.API.StartNodeService)
 
 	// Test enabling two services both ways
-
-	// Reset flags and config
-	resetFlags()
 	events.CloseEventReporter()
 	app.Config.API = apiConfig.DefaultTestConfig()
 
 	r.Equal(false, app.Config.API.StartNodeService)
 	r.Equal(false, app.Config.API.StartMeshService)
-	str, err = testArgs(context.Background(), "--grpc", "node,mesh")
+	str, err = testArgs(context.Background(), cmdWithRun(run), "--grpc", "node,mesh")
 	r.Empty(str)
 	r.NoError(err)
 	r.Equal(true, app.Config.API.StartNodeService)
 	r.Equal(true, app.Config.API.StartMeshService)
 
-	// Reset flags and config
-	resetFlags()
 	events.CloseEventReporter()
 	app.Config.API = apiConfig.DefaultTestConfig()
 
 	r.Equal(false, app.Config.API.StartNodeService)
 	r.Equal(false, app.Config.API.StartMeshService)
-	str, err = testArgs(context.Background(), "--grpc", "node", "--grpc", "mesh")
+	str, err = testArgs(context.Background(), cmdWithRun(run), "--grpc", "node", "--grpc", "mesh")
 	r.Empty(str)
 	r.NoError(err)
 	r.Equal(true, app.Config.API.StartNodeService)
@@ -347,37 +336,35 @@ func TestSpacemeshApp_JsonFlags(t *testing.T) {
 	r.Equal(false, app.Config.API.StartNodeService)
 
 	// Try enabling just the JSON service (without the GRPC service)
-	Cmd.Run = func(c *cobra.Command, args []string) {
+	run := func(c *cobra.Command, args []string) {
 		err := cmd.EnsureCLIFlags(c, app.Config)
 		r.Error(err)
 		r.Equal("parse services list: must enable at least one GRPC service along with JSON gateway service", err.Error())
 	}
-	str, err := testArgs(context.Background(), "--json-server")
+	str, err := testArgs(context.Background(), cmdWithRun(run), "--json-server")
 	r.NoError(err)
 	r.Empty(str)
 	r.Equal(true, app.Config.API.StartJSONServer)
 	r.Equal(false, app.Config.API.StartNodeService)
 
-	resetFlags()
 	events.CloseEventReporter()
 
 	// Try enabling both the JSON and the GRPC services
-	Cmd.Run = func(c *cobra.Command, args []string) {
+	run = func(c *cobra.Command, args []string) {
 		r.NoError(cmd.EnsureCLIFlags(c, app.Config))
 	}
-	str, err = testArgs(context.Background(), "--grpc", "node", "--json-server")
+	str, err = testArgs(context.Background(), cmdWithRun(run), "--grpc", "node", "--json-server")
 	r.NoError(err)
 	r.Empty(str)
 	r.Equal(true, app.Config.API.StartNodeService)
 	r.Equal(true, app.Config.API.StartJSONServer)
 
-	resetFlags()
 	events.CloseEventReporter()
 	app.Config.API = apiConfig.DefaultTestConfig()
 
 	// Try changing the port
 	// Uses Cmd.Run as defined above
-	str, err = testArgs(context.Background(), "--json-port", "1234")
+	str, err = testArgs(context.Background(), cmdWithRun(run), "--json-port", "1234")
 	r.NoError(err)
 	r.Empty(str)
 	r.Equal(false, app.Config.API.StartNodeService)
@@ -415,7 +402,7 @@ func TestSpacemeshApp_GrpcService(t *testing.T) {
 
 	path := t.TempDir()
 
-	Cmd.Run = func(c *cobra.Command, args []string) {
+	run := func(c *cobra.Command, args []string) {
 		r.NoError(cmd.EnsureCLIFlags(c, app.Config))
 		app.Config.API.GrpcServerPort = port
 		app.Config.DataDirParent = path
@@ -424,7 +411,7 @@ func TestSpacemeshApp_GrpcService(t *testing.T) {
 	defer app.stopServices()
 
 	// Make sure the service is not running by default
-	str, err := testArgs(context.Background()) // no args
+	str, err := testArgs(context.Background(), cmdWithRun(run)) // no args
 	r.Empty(str)
 	r.NoError(err)
 	r.Equal(false, app.Config.API.StartNodeService)
@@ -437,12 +424,10 @@ func TestSpacemeshApp_GrpcService(t *testing.T) {
 	)
 	r.ErrorContains(err, "context deadline exceeded")
 
-	resetFlags()
 	events.CloseEventReporter()
 
 	// Test starting the server from the command line
-	// uses Cmd.Run from above
-	str, err = testArgs(context.Background(), "--grpc-port", strconv.Itoa(port), "--grpc", "node")
+	str, err = testArgs(context.Background(), cmdWithRun(run), "--grpc-port", strconv.Itoa(port), "--grpc", "node")
 	r.Empty(str)
 	r.NoError(err)
 	r.Equal(port, app.Config.API.GrpcServerPort)
@@ -468,24 +453,22 @@ func TestSpacemeshApp_GrpcService(t *testing.T) {
 	r.Equal(message, response.Msg.Value)
 }
 
-func TestSpacemeshApp_JsonService(t *testing.T) {
+func TestSpacemeshApp_JsonServiceNotRunning(t *testing.T) {
 	setup()
-
 	r := require.New(t)
 	app := New(WithLog(logtest.New(t)))
 
-	path := t.TempDir()
-
 	// Make sure the service is not running by default
-	Cmd.Run = func(c *cobra.Command, args []string) {
+	run := func(c *cobra.Command, args []string) {
 		r.NoError(cmd.EnsureCLIFlags(c, app.Config))
-		app.Config.DataDirParent = path
+		app.Config.DataDirParent = t.TempDir()
 		app.startAPIServices(context.TODO())
 	}
-	defer app.stopServices()
-	str, err := testArgs(context.Background())
+
+	str, err := testArgs(context.Background(), cmdWithRun(run))
 	r.Empty(str)
 	r.NoError(err)
+	defer app.stopServices()
 	r.Equal(false, app.Config.API.StartJSONServer)
 	r.Equal(false, app.Config.API.StartNodeService)
 	r.Equal(false, app.Config.API.StartMeshService)
@@ -504,14 +487,29 @@ func TestSpacemeshApp_JsonService(t *testing.T) {
 	_, err = http.Post(url, "application/json", strings.NewReader(payload))
 	r.Error(err)
 
-	resetFlags()
 	events.CloseEventReporter()
+}
+
+func TestSpacemeshApp_JsonService(t *testing.T) {
+	setup()
+	r := require.New(t)
+	app := New(WithLog(logtest.New(t)))
+	const message = "nihao shijie"
+	payload := marshalProto(t, &pb.EchoRequest{Msg: &pb.SimpleString{Value: message}})
+
+	// Make sure the service is not running by default
+	run := func(c *cobra.Command, args []string) {
+		r.NoError(cmd.EnsureCLIFlags(c, app.Config))
+		app.Config.DataDirParent = t.TempDir()
+		app.startAPIServices(context.TODO())
+	}
 
 	// Test starting the JSON server from the commandline
 	// uses Cmd.Run from above
-	str, err = testArgs(context.Background(), "--json-server", "--grpc", "node", "--json-port", "1234")
+	str, err := testArgs(context.Background(), cmdWithRun(run), "--json-server", "--grpc", "node", "--json-port", "1234")
 	r.Empty(str)
 	r.NoError(err)
+	defer app.stopServices()
 	r.Equal(1234, app.Config.API.JSONServerPort)
 	r.Equal(true, app.Config.API.StartJSONServer)
 	r.Equal(true, app.Config.API.StartNodeService)
@@ -568,7 +566,7 @@ func TestSpacemeshApp_NodeService(t *testing.T) {
 		poetHarness.HTTPPoetClient, clock, h, edSgn)
 	require.NoError(t, err)
 
-	Cmd.Run = func(c *cobra.Command, args []string) {
+	run := func(c *cobra.Command, args []string) {
 		defer app.Cleanup()
 		require.NoError(t, cmd.EnsureCLIFlags(c, app.Config))
 
@@ -598,7 +596,7 @@ func TestSpacemeshApp_NodeService(t *testing.T) {
 	go func() {
 		// This makes sure the test doesn't end until this goroutine closes
 		defer wg.Done()
-		str, err := testArgs(ctx, "--grpc-port", strconv.Itoa(port), "--grpc", "node", "--grpc-interface", "localhost")
+		str, err := testArgs(ctx, cmdWithRun(run), "--grpc-port", strconv.Itoa(port), "--grpc", "node", "--grpc-interface", "localhost")
 		assert.Empty(t, str)
 		assert.NoError(t, err)
 	}()
@@ -694,7 +692,7 @@ func TestSpacemeshApp_TransactionService(t *testing.T) {
 	signer := signing.NewEdSigner()
 	address := wallet.Address(signer.PublicKey().Bytes())
 
-	Cmd.Run = func(c *cobra.Command, args []string) {
+	run := func(c *cobra.Command, args []string) {
 		defer app.Cleanup()
 		r.NoError(app.Initialize())
 
@@ -735,12 +733,13 @@ func TestSpacemeshApp_TransactionService(t *testing.T) {
 	wg := sync.WaitGroup{}
 	wg.Add(1)
 	go func() {
-		str, err := testArgs(ctx)
+		str, err := testArgs(ctx, cmdWithRun(run))
 		r.Empty(str)
 		r.NoError(err)
 		wg.Done()
 	}()
 
+	<-app.Started()
 	conn, err := grpc.Dial(
 		fmt.Sprintf("localhost:%d", port),
 		grpc.WithTransportCredentials(insecure.NewCredentials()),

Makefile
@@ -123,7 +123,7 @@ clear-test-cache:
 .PHONY: clear-test-cache
 
 test: get-libs
-	@$(ULIMIT) CGO_LDFLAGS="$(CGO_TEST_LDFLAGS)" gotestsum -- -timeout 5m -p 1 $(UNIT_TESTS)
+	@$(ULIMIT) CGO_LDFLAGS="$(CGO_TEST_LDFLAGS)" gotestsum -- -race -timeout 5m -p 1 $(UNIT_TESTS)
 .PHONY: test
 
 generate: get-libs

fetch/mesh_data.go
@@ -184,23 +184,23 @@ func (f *Fetch) PeerEpochInfo(ctx context.Context, peer p2p.Peer, epoch types.Ep
 		log.Stringer("epoch", epoch))
 
 	var (
-		done = make(chan struct{}, 1)
+		done = make(chan error, 1)
 		ed   EpochData
 		err  error
 	)
 	okCB := func(data []byte) {
 		defer close(done)
-		err = codec.Decode(data, &ed)
+		done <- codec.Decode(data, &ed)
 	}
 	errCB := func(perr error) {
 		defer close(done)
-		err = perr
+		done <- perr
 	}
 	if err = f.servers[atxProtocol].Request(ctx, peer, epoch.ToBytes(), okCB, errCB); err != nil {
 		return nil, err
 	}
 	select {
-	case <-done:
+	case err := <-done:
 		if err != nil {
 			return nil, err
 		}
@@ -236,7 +236,7 @@ func (f *Fetch) PeerMeshHashes(ctx context.Context, peer p2p.Peer, req *MeshHash
 		log.Object("req", req))
 
 	var (
-		done    = make(chan struct{}, 1)
+		done    = make(chan error, 1)
 		hashes  []types.Hash32
 		reqData []byte
 		err     error
@@ -252,17 +252,19 @@ func (f *Fetch) PeerMeshHashes(ctx context.Context, peer p2p.Peer, req *MeshHash
 
 	okCB := func(data []byte) {
 		defer close(done)
-		hashes, err = codec.DecodeSlice[types.Hash32](data)
+		h, err := codec.DecodeSlice[types.Hash32](data)
+		hashes = h
+		done <- err
 	}
 	errCB := func(perr error) {
 		defer close(done)
-		err = perr
+		done <- perr
 	}
 	if err = f.servers[meshHashProtocol].Request(ctx, peer, reqData, okCB, errCB); err != nil {
 		return nil, err
 	}
 	select {
-	case <-done:
+	case err := <-done:
 		if err != nil {
 			return nil, err
 		}

hare/hare_rounds_test.go
@@ -60,7 +60,7 @@ type (
 	}
 )
 
-func runNodesFor(t *testing.T, nodes, leaders, maxLayers, limitIterations int, createProposal bool, oracle funcOracle, validate funcValidate) *TestHareWrapper {
+func runNodesFor(t *testing.T, ctx context.Context, nodes, leaders, maxLayers, limitIterations int, createProposal bool, oracle funcOracle, validate funcValidate) *TestHareWrapper {
 	r := require.New(t)
 	w := newTestHareWrapper(maxLayers)
 	cfg := config.Config{
@@ -91,7 +91,7 @@ func runNodesFor(t *testing.T, nodes, leaders, maxLayers, limitIterations int, c
 
 	for i := 0; i < nodes; i++ {
 		host := mesh.Hosts()[i]
-		ps, err := pubsub.New(context.TODO(), logtest.New(t), host, pubsub.DefaultConfig())
+		ps, err := pubsub.New(ctx, logtest.New(t), host, pubsub.DefaultConfig())
 		require.NoError(t, err)
 		mp2p := &p2pManipulator{nd: ps, stalledLayer: types.NewLayerID(1), err: errors.New("fake err")}
 
@@ -109,7 +109,7 @@ func runNodesFor(t *testing.T, nodes, leaders, maxLayers, limitIterations int, c
 			}
 		}()
 		w.hare = append(w.hare, th.Hare)
-		e := th.Start(context.TODO())
+		e := th.Start(ctx)
 		r.NoError(e)
 	}
 	require.NoError(t, mesh.ConnectAllButSelf())
@@ -124,7 +124,9 @@ func Test_HarePreRoundEmptySet(t *testing.T) {
 	var mu sync.RWMutex
 	m := [layers][nodes]int{}
 
-	w := runNodesFor(t, nodes, 2, layers, 2, false,
+	ctx, cancel := context.WithCancel(context.Background())
+	defer cancel()
+	w := runNodesFor(t, ctx, nodes, 2, layers, 2, false,
 		func(layer types.LayerID, round uint32, committee int, id types.NodeID, blocks []byte, hare *testHare) (uint16, error) {
 			if round/4 > 1 && round != preRound {
 				t.Fatalf("out of round %d limit", round)
@@ -142,9 +144,7 @@ func Test_HarePreRoundEmptySet(t *testing.T) {
 	w.LayerTicker(100 * time.Millisecond)
 	time.Sleep(time.Second * 6)
 
-	for _, h := range w.hare {
-		close(h.blockGenCh)
-	}
+	cancel()
 
 	mu.RLock()
 	defer mu.RUnlock()
@@ -167,7 +167,10 @@ func Test_HareNotEnoughStatuses(t *testing.T) {
 	const layers = 2
 	m := [layers][nodes]int{}
 
-	w := runNodesFor(t, nodes, 2, layers, 1, false,
+	ctx, cancel := context.WithCancel(context.Background())
+	defer cancel()
+
+	w := runNodesFor(t, ctx, nodes, 2, layers, 1, false,
 		func(layer types.LayerID, round uint32, committee int, id types.NodeID, blocks []byte, hare *testHare) (uint16, error) {
 			if round%4 == statusRound && hare.N >= committee/2-1 {
 				return 0, nil
@@ -199,7 +202,9 @@ func Test_HareNotEnoughLeaders(t *testing.T) {
 	const layers = 2
 	m := [layers][nodes]int{}
 
-	w := runNodesFor(t, nodes, 2, layers, 1, false,
+	ctx, cancel := context.WithCancel(context.Background())
+	defer cancel()
+	w := runNodesFor(t, ctx, nodes, 2, layers, 1, false,
 		func(layer types.LayerID, round uint32, committee int, id types.NodeID, blocks []byte, hare *testHare) (uint16, error) {
 			if round%4 == proposalRound {
 				return 0, nil
@@ -231,7 +236,10 @@ func Test_HareNotEnoughCommits(t *testing.T) {
 	const layers = 2
 	m := [layers][nodes]int{}
 
-	w := runNodesFor(t, nodes, 2, layers, 1, true,
+	ctx, cancel := context.WithCancel(context.Background())
+	defer cancel()
+
+	w := runNodesFor(t, ctx, nodes, 2, layers, 1, true,
 		func(layer types.LayerID, round uint32, committee int, id types.NodeID, blocks []byte, hare *testHare) (uint16, error) {
 			if round%4 == commitRound && hare.N >= committee/2-1 {
 				return 0, nil
@@ -263,7 +271,10 @@ func Test_HareNotEnoughNotifications(t *testing.T) {
 	const layers = 2
 	m := [layers][nodes]int{}
 
-	w := runNodesFor(t, nodes, 2, layers, 1, true,
+	ctx, cancel := context.WithCancel(context.Background())
+	defer cancel()
+
+	w := runNodesFor(t, ctx, nodes, 2, layers, 1, true,
 		func(layer types.LayerID, round uint32, committee int, id types.NodeID, blocks []byte, hare *testHare) (uint16, error) {
 			if round%4 == notifyRound && hare.N >= committee/2-1 {
 				return 0, nil
@@ -295,7 +306,10 @@ func Test_HareComplete(t *testing.T) {
 	const layers = 2
 	m := [layers][nodes]int{}
 
-	w := runNodesFor(t, nodes, 2, layers, 1, true,
+	ctx, cancel := context.WithCancel(context.Background())
+	defer cancel()
+
+	w := runNodesFor(t, ctx, nodes, 2, layers, 1, true,
 		func(layer types.LayerID, round uint32, committee int, id types.NodeID, blocks []byte, hare *testHare) (uint16, error) {
 			return 1, nil
 		},

main.go
@@ -21,7 +21,7 @@ func main() { // run the app
 	cmd.Version = version
 	cmd.Commit = commit
 	cmd.Branch = branch
-	if err := node.Cmd.Execute(); err != nil {
+	if err := node.GetCommand().Execute(); err != nil {
 		fmt.Fprintln(os.Stderr, err)
 		os.Exit(1)
 	}

commit dbdb8cfa3caec96c7620a048587eda882f247ecd

Fix grpc logger (#3926)
## Motivation
GRPC logs are missing. It's a regression introduced by #2930.

## Changes
Fixed configuring logging middleware for GRPC.

Additionally, removed the artificial extension of a logger name prefix to 13 characters. It resulted in awkward logger names like:
```
"5678c.app          .grpc         "
```

cmd/node/node.go
@@ -15,6 +15,7 @@ import (
 
 	"github.com/gofrs/flock"
 	grpcmw "github.com/grpc-ecosystem/go-grpc-middleware"
+	grpc_logsettable "github.com/grpc-ecosystem/go-grpc-middleware/logging/settable"
 	grpczap "github.com/grpc-ecosystem/go-grpc-middleware/logging/zap"
 	grpctags "github.com/grpc-ecosystem/go-grpc-middleware/tags"
 	"github.com/mitchellh/mapstructure"
@@ -169,13 +170,12 @@ func GetCommand() *cobra.Command {
 
 var (
 	appLog  log.Log
-	grpcLog *zap.Logger
+	grpclog grpc_logsettable.SettableLoggerV2
 )
 
 func init() {
 	appLog = log.NewNop()
-	grpcLog = appLog.WithName(GRPCLogger).WithFields(log.String("module", GRPCLogger)).Zap()
-	grpczap.ReplaceGrpcLoggerV2(grpcLog)
+	grpclog = grpc_logsettable.ReplaceGrpcLoggerV2()
 }
 
 // Service is a general service interface that specifies the basic start/stop functionality.
@@ -797,14 +797,15 @@ func (app *App) startAPIServices(ctx context.Context) {
 	var services []grpcserver.ServiceAPI
 	registerService := func(svc grpcserver.ServiceAPI) {
 		if app.grpcAPIService == nil {
-			app.addLogger(GRPCLogger, app.log)
+			logger := app.addLogger(GRPCLogger, app.log).Zap()
+			grpczap.SetGrpcLoggerV2(grpclog, logger)
 			app.grpcAPIService = grpcserver.NewServerWithInterface(apiConf.GrpcServerPort, apiConf.GrpcServerInterface,
 				grpcmw.WithStreamServerChain(
 					grpctags.StreamServerInterceptor(),
-					grpczap.StreamServerInterceptor(grpcLog)),
+					grpczap.StreamServerInterceptor(logger)),
 				grpcmw.WithUnaryServerChain(
 					grpctags.UnaryServerInterceptor(),
-					grpczap.UnaryServerInterceptor(grpcLog)),
+					grpczap.UnaryServerInterceptor(logger)),
 			)
 		}
 		services = append(services, svc)

log/zap.go
@@ -209,7 +209,7 @@ func (l Log) Core() zapcore.Core {
 
 // WithName appends a name to a current name.
 func (l Log) WithName(prefix string) Log {
-	lgr := l.logger.Named(fmt.Sprintf("%-13s", prefix))
+	lgr := l.logger.Named(prefix)
 	var name string
 	if l.name == "" {
 		name = prefix

cmd/node/node_test.go
@@ -116,7 +116,7 @@ func TestSpacemeshApp_SetLoggers(t *testing.T) {
 	app.log = app.addLogger(mylogger, myLog)
 	msg1 := "hi there"
 	app.log.Info(msg1)
-	r.Equal(fmt.Sprintf("INFO\t%-13s\t%s\t{\"module\": \"%s\"}\n", mylogger, msg1, mylogger), buf1.String())
+	r.Equal(fmt.Sprintf("INFO\t%s\t%s\t{\"module\": \"%s\"}\n", mylogger, msg1, mylogger), buf1.String())
 	r.NoError(app.SetLogLevel(mylogger, "warn"))
 	r.Equal("warn", app.loggers[mylogger].String())
 	buf1.Reset()
@@ -129,7 +129,7 @@ func TestSpacemeshApp_SetLoggers(t *testing.T) {
 	app.log.Info(msg2)
 	// This one should be printed
 	app.log.Warning(msg3)
-	r.Equal(fmt.Sprintf("WARN\t%-13s\t%s\t{\"module\": \"%s\"}\n", mylogger, msg3, mylogger), buf1.String())
+	r.Equal(fmt.Sprintf("WARN\t%s\t%s\t{\"module\": \"%s\"}\n", mylogger, msg3, mylogger), buf1.String())
 	r.Equal(fmt.Sprintf("INFO\t%s\n", msg1), buf2.String())
 	buf1.Reset()
 
@@ -138,7 +138,7 @@ func TestSpacemeshApp_SetLoggers(t *testing.T) {
 	msg4 := "nihao"
 	app.log.Info(msg4)
 	r.Equal("info", app.loggers[mylogger].String())
-	r.Equal(fmt.Sprintf("INFO\t%-13s\t%s\t{\"module\": \"%s\"}\n", mylogger, msg4, mylogger), buf1.String())
+	r.Equal(fmt.Sprintf("INFO\t%s\t%s\t{\"module\": \"%s\"}\n", mylogger, msg4, mylogger), buf1.String())
 
 	// test bad logger name
 	r.Error(app.SetLogLevel("anton3", "warn"))
@@ -160,7 +160,7 @@ func TestSpacemeshApp_AddLogger(t *testing.T) {
 	subLogger.Debug("should not get printed")
 	teststr := "should get printed"
 	subLogger.Info(teststr)
-	r.Equal(fmt.Sprintf("INFO\t%-13s\t%s\t{\"module\": \"%s\"}\n", mylogger, teststr, mylogger), buf.String())
+	r.Equal(fmt.Sprintf("INFO\t%s\t%s\t{\"module\": \"%s\"}\n", mylogger, teststr, mylogger), buf.String())
 }
 
 func testArgs(ctx context.Context, root *cobra.Command, args ...string) (string, error) {

log/log_test.go
@@ -52,7 +52,7 @@ func TestLogLevel(t *testing.T) {
 	r.NoError(lvl.UnmarshalText([]byte("INFO")))
 	svcName := "mysvc"
 	subLogger := logger.SetLevel(&lvl).WithName(svcName)
-	prefix := fmt.Sprintf("%s.%-13s", loggerName, svcName)
+	prefix := fmt.Sprintf("%s.%s", loggerName, svcName)
 
 	// Test the default app logger
 	// This is NOT hooked