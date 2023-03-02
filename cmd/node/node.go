8bdb7d39575a0046c5f33ecc995c5af3127e74f8
kimmy lin <30611210+countvonzero@users.noreply.github.com>
2023-03-02 23:37:46 +0000

    make clock's log level configurable

--- cmd/node/node.go
+++ cmd/node/node.go
@@ -70,6 +70,7 @@ const (
 // Logger names.
 const (
 	AppLogger              = "app"
+	ClockLogger            = "clock"
 	P2PLogger              = "p2p"
 	PostLogger             = "post"
 	StateDbLogger          = "stateDbStore"
@@ -1091,7 +1092,7 @@ func (app *App) Start(ctx context.Context) error {
 		timesync.WithLayerDuration(app.Config.LayerDuration),
 		timesync.WithTickInterval(1*time.Second),
 		timesync.WithGenesisTime(gTime),
-		timesync.WithLogger(lg.WithName("clock")),
+		timesync.WithLogger(app.addLogger(ClockLogger, lg)),
 	)
 	if err != nil {
 		return fmt.Errorf("cannot create clock: %w", err)
