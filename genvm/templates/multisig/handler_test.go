e8951dc1476a37e90a3b89e589c7b66537a142e5
Dmitry Shulyak <yashulyak@gmail.com>
2023-03-08 11:28:37 +0000

    add max size for public keys in multisig spawn tx according to storage limits (#4129)
    related: https://github.com/spacemeshos/go-spacemesh/issues/4095
    
    the only place in vm that requires dyn sized collection, more than 10 keys are not going to be supported.

--- genvm/templates/multisig/handler_test.go
+++ genvm/templates/multisig/handler_test.go
@@ -1,9 +1,11 @@
 package multisig
 
 import (
+	"bytes"
 	"strconv"
 	"testing"
 
+	"github.com/spacemeshos/go-scale"
 	"github.com/stretchr/testify/require"
 
 	"github.com/spacemeshos/go-spacemesh/genvm/core"
@@ -31,4 +33,17 @@ func TestKeysLimits(t *testing.T) {
 			}
 		})
 	}
+}
+
+func TestDecodeMaxKeys(t *testing.T) {
+	keys := make([]core.PublicKey, 11)
+	buf := bytes.NewBuffer(nil)
+	enc := scale.NewEncoder(buf)
+	_, err := scale.EncodeStructSlice(enc, keys)
+	require.NoError(t, err)
+
+	args := SpawnArguments{}
+	dec := scale.NewDecoder(buf)
+	_, err = args.DecodeScale(dec)
+	require.ErrorContains(t, err, "11 > 10")
 }
