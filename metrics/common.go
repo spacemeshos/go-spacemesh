5ffb48ea0694e1bbf5bb1cabaa54ea05fc9bc1a2
Piers Powlesland <pierspowlesland@gmail.com>
2023-03-23 12:20:35 +0000

    Add common message latency histogram
    For use across multiple protocols.

--- metrics/common.go
+++ metrics/common.go
@@ -1,6 +1,8 @@
 package metrics
 
 import (
+	"time"
+
 	"github.com/prometheus/client_golang/prometheus"
 	"github.com/prometheus/client_golang/prometheus/promauto"
 )
@@ -28,4 +30,29 @@ func NewHistogram(name, subsystem, help string, labels []string) *prometheus.His
 // NewHistogramWithBuckets creates a Histogram metrics with custom buckets.
 func NewHistogramWithBuckets(name, subsystem, help string, labels []string, buckets []float64) *prometheus.HistogramVec {
 	return promauto.NewHistogramVec(prometheus.HistogramOpts{Namespace: Namespace, Subsystem: subsystem, Name: name, Help: help, Buckets: buckets}, labels)
+}
+
+// receivedMessagesLatency measures the time a message was received relative to
+// the time it should be sent in the protocol. Metrics are labeled by protocol
+// and sign. Sign is either "pos" or "neg" and is chosen depending on the sign
+// of the observed latency. Negative latencies occur when a message is received
+// before the time it should be sent, this could happen in a network where
+// nodes' clocks are not in sync.
+var receivedMessagesLatency = NewHistogramWithBuckets(
+	"message_latency_seconds",
+	"",
+	"Observed latency for message",
+	[]string{"protocol", "sign"},
+	prometheus.ExponentialBuckets(0.1, 2, 12),
+)
+
+func ReportMessageLatency(protocol string, latency time.Duration) {
+	seconds := latency.Seconds()
+	sign := "pos"
+	if seconds < 0 {
+		sign = "neg"
+		// If the observation is negative make it positive.
+		seconds = -seconds
+	}
+	receivedMessagesLatency.WithLabelValues(protocol, sign).Observe(seconds)
 }
