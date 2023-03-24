4745248a4081044a1cf99039e13fe64600296893
Jedrzej Nowak <pigmej@gmail.com>
2023-03-24 10:16:07 +0100

    Increased max send size to 50MB

--- cmd/node/node.go
+++ cmd/node/node.go
@@ -806,6 +806,7 @@ func (app *App) startAPIServices(ctx context.Context) {
 			app.grpcAPIService = grpcserver.NewServerWithInterface(apiConf.GrpcServerPort, apiConf.GrpcServerInterface,
 				grpc.ChainStreamInterceptor(grpctags.StreamServerInterceptor(), grpczap.StreamServerInterceptor(logger)),
 				grpc.ChainUnaryInterceptor(grpctags.UnaryServerInterceptor(), grpczap.UnaryServerInterceptor(logger)),
+				grpc.MaxSendMsgSize(50*1024*1024), // 50MB
 			)
 		}
 		services = append(services, svc)
