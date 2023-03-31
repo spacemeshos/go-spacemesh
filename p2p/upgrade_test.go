4771c6c9c273032652e3255aa5efe53ab27a74e2
Dmitry <yashulyak@gmail.com>
2023-03-31 11:07:45 +0300

    imports

--- p2p/upgrade_test.go
+++ p2p/upgrade_test.go
@@ -9,8 +9,9 @@ import (
 	"github.com/libp2p/go-libp2p"
 	"github.com/libp2p/go-libp2p/p2p/net/connmgr"
 	mocknet "github.com/libp2p/go-libp2p/p2p/net/mock"
-	"github.com/spacemeshos/go-spacemesh/common/types"
 	"github.com/stretchr/testify/require"
+
+	"github.com/spacemeshos/go-spacemesh/common/types"
 )
 
 func TestIsBootnode(t *testing.T) {
