9025d1905da9de3cef5c285c9afbe0333e0f6e60
Matthias <5011972+fasmat@users.noreply.github.com>
2023-03-31 09:18:47 +0000

    Cleanup

--- common/types/activation.go
+++ common/types/activation.go
@@ -66,6 +66,87 @@ func (t *ATXID) DecodeScale(d *scale.Decoder) (int, error) {
 // EmptyATXID is a canonical empty ATXID.
 var EmptyATXID = ATXID{}
 
+// NIPostChallenge is the set of fields that's serialized, hashed and submitted to the PoET service to be included in the
+// PoET membership proof. It includes ATX sequence number, the previous ATX's ID (for all but the first in the sequence),
+// the intended publication layer ID, the PoET's start and end ticks, the positioning ATX's ID and for
+// the first ATX in the sequence also the commitment Merkle root.
+type NIPostChallenge struct {
+	PubLayerID LayerID
+	// Sequence number counts the number of ancestors of the ATX. It sequentially increases for each ATX in the chain.
+	// Two ATXs with the same sequence number from the same miner can be used as the proof of malfeasance against that miner.
+	Sequence       uint64
+	PrevATXID      ATXID
+	PositioningATX ATXID
+
+	// CommitmentATX is the ATX used in the commitment for initializing the PoST of the node.
+	CommitmentATX      *ATXID
+	InitialPostIndices []byte `scale:"max=8000"` // needs to hold K2*8 bytes at most
+}
+
+func (c *NIPostChallenge) MarshalLogObject(encoder log.ObjectEncoder) error {
+	if c == nil {
+		return nil
+	}
+	encoder.AddUint32("PubLayerID", c.PubLayerID.Uint32())
+	encoder.AddUint64("Sequence", c.Sequence)
+	encoder.AddString("PrevATXID", c.PrevATXID.String())
+	encoder.AddString("PositioningATX", c.PositioningATX.String())
+	if c.CommitmentATX != nil {
+		encoder.AddString("CommitmentATX", c.CommitmentATX.String())
+	}
+	encoder.AddBinary("InitialPostIndices", c.InitialPostIndices)
+	return nil
+}
+
+// Hash serializes the NIPostChallenge and returns its hash.
+func (challenge *NIPostChallenge) Hash() Hash32 {
+	ncBytes, err := codec.Encode(challenge)
+	if err != nil {
+		log.With().Fatal("failed to encode NIPostChallenge", log.Err(err))
+	}
+	return CalcHash32(ncBytes)
+}
+
+// String returns a string representation of the NIPostChallenge, for logging purposes.
+// It implements the Stringer interface.
+func (challenge *NIPostChallenge) String() string {
+	return fmt.Sprintf("<seq: %v, prevATX: %v, PubLayer: %v, posATX: %s>",
+		challenge.Sequence,
+		challenge.PrevATXID.ShortString(),
+		challenge.PubLayerID,
+		challenge.PositioningATX.ShortString())
+}
+
+// TargetEpoch returns the target epoch of the NIPostChallenge. This is the epoch in which the miner is eligible
+// to participate thanks to the ATX.
+func (challenge *NIPostChallenge) TargetEpoch() EpochID {
+	return challenge.PubLayerID.GetEpoch() + 1
+}
+
+// PublishEpoch returns the publishing epoch of the NIPostChallenge.
+func (challenge *NIPostChallenge) PublishEpoch() EpochID {
+	return challenge.PubLayerID.GetEpoch()
+}
+
+// InnerActivationTx is a set of all of an ATX's fields, except the signature. To generate the ATX signature, this
+// structure is serialized and signed. It includes the header fields, as well as the larger fields that are only used
+// for validation: the NIPost and the initial Post.
+type InnerActivationTx struct {
+	NIPostChallenge
+	Coinbase Address
+	NumUnits uint32
+
+	NIPost      *NIPost
+	InitialPost *Post
+	VRFNonce    *VRFPostIndex
+
+	// the following fields are kept private and from being serialized
+	id                ATXID     // non-exported cache of the ATXID
+	nodeID            NodeID    // the id of the Node that created the ATX (public key)
+	effectiveNumUnits uint32    // the number of effective units in the ATX (minimum of this ATX and the previous ATX)
+	received          time.Time // time received by node, gossiped or synced
+}
+
 // ATXMetadata is the signed data of ActivationTx.
 type ATXMetadata struct {
 	Target EpochID
@@ -263,87 +344,6 @@ func (atx *ActivationTx) Verify(baseTickHeight, tickCount uint64) (*VerifiedActi
 		tickCount:      tickCount,
 	}
 	return vAtx, nil
-}
-
-// InnerActivationTx is a set of all of an ATX's fields, except the signature. To generate the ATX signature, this
-// structure is serialized and signed. It includes the header fields, as well as the larger fields that are only used
-// for validation: the NIPost and the initial Post.
-type InnerActivationTx struct {
-	NIPostChallenge
-	Coinbase Address
-	NumUnits uint32
-
-	NIPost      *NIPost
-	InitialPost *Post
-	VRFNonce    *VRFPostIndex
-
-	// the following fields are kept private and from being serialized
-	id                ATXID     // non-exported cache of the ATXID
-	nodeID            NodeID    // the id of the Node that created the ATX (public key)
-	effectiveNumUnits uint32    // the number of effective units in the ATX (minimum of this ATX and the previous ATX)
-	received          time.Time // time received by node, gossiped or synced
-}
-
-// NIPostChallenge is the set of fields that's serialized, hashed and submitted to the PoET service to be included in the
-// PoET membership proof. It includes ATX sequence number, the previous ATX's ID (for all but the first in the sequence),
-// the intended publication layer ID, the PoET's start and end ticks, the positioning ATX's ID and for
-// the first ATX in the sequence also the commitment Merkle root.
-type NIPostChallenge struct {
-	PubLayerID LayerID
-	// Sequence number counts the number of ancestors of the ATX. It sequentially increases for each ATX in the chain.
-	// Two ATXs with the same sequence number from the same miner can be used as the proof of malfeasance against that miner.
-	Sequence       uint64
-	PrevATXID      ATXID
-	PositioningATX ATXID
-
-	// CommitmentATX is the ATX used in the commitment for initializing the PoST of the node.
-	CommitmentATX      *ATXID
-	InitialPostIndices []byte `scale:"max=8000"` // needs to hold K2*8 bytes at most
-}
-
-func (c *NIPostChallenge) MarshalLogObject(encoder log.ObjectEncoder) error {
-	if c == nil {
-		return nil
-	}
-	encoder.AddUint32("PubLayerID", c.PubLayerID.Uint32())
-	encoder.AddUint64("Sequence", c.Sequence)
-	encoder.AddString("PrevATXID", c.PrevATXID.String())
-	encoder.AddString("PositioningATX", c.PositioningATX.String())
-	if c.CommitmentATX != nil {
-		encoder.AddString("CommitmentATX", c.CommitmentATX.String())
-	}
-	encoder.AddBinary("InitialPostIndices", c.InitialPostIndices)
-	return nil
-}
-
-// Hash serializes the NIPostChallenge and returns its hash.
-func (challenge *NIPostChallenge) Hash() Hash32 {
-	ncBytes, err := codec.Encode(challenge)
-	if err != nil {
-		log.With().Fatal("failed to encode NIPostChallenge", log.Err(err))
-	}
-	return CalcHash32(ncBytes)
-}
-
-// String returns a string representation of the NIPostChallenge, for logging purposes.
-// It implements the Stringer interface.
-func (challenge *NIPostChallenge) String() string {
-	return fmt.Sprintf("<seq: %v, prevATX: %v, PubLayer: %v, posATX: %s>",
-		challenge.Sequence,
-		challenge.PrevATXID.ShortString(),
-		challenge.PubLayerID,
-		challenge.PositioningATX.ShortString())
-}
-
-// TargetEpoch returns the target epoch of the NIPostChallenge. This is the epoch in which the miner is eligible
-// to participate thanks to the ATX.
-func (challenge *NIPostChallenge) TargetEpoch() EpochID {
-	return challenge.PubLayerID.GetEpoch() + 1
-}
-
-// PublishEpoch returns the publishing epoch of the NIPostChallenge.
-func (challenge *NIPostChallenge) PublishEpoch() EpochID {
-	return challenge.PubLayerID.GetEpoch()
 }
 
 // NIPost is Non-Interactive Proof of Space-Time.
