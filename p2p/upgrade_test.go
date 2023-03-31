c87e64a67ea02419f7fde64a5f4909f14c8615ee
Dmitry <yashulyak@gmail.com>
2023-03-31 11:01:16 +0300

    fix connections reporter

--- p2p/upgrade_test.go
+++ p2p/upgrade_test.go
@@ -2,11 +2,14 @@ package p2p
 
 import (
 	"fmt"
+	"sync/atomic"
 	"testing"
 	"time"
 
 	"github.com/libp2p/go-libp2p"
 	"github.com/libp2p/go-libp2p/p2p/net/connmgr"
+	mocknet "github.com/libp2p/go-libp2p/p2p/net/mock"
+	"github.com/spacemeshos/go-spacemesh/common/types"
 	"github.com/stretchr/testify/require"
 )
 
@@ -45,4 +48,33 @@ func TestIsBootnode(t *testing.T) {
 			require.Equal(t, tc.isBoot, got)
 		})
 	}
+}
+
+func TestConnectionsNotifier(t *testing.T) {
+	const n = 3
+	mesh, err := mocknet.FullMeshLinked(n)
+	require.NoError(t, err)
+	counter := [n]atomic.Uint32{}
+	// we count events - not peers
+	reporter := func(i int) func() {
+		return func() {
+			counter[i].Add(1)
+		}
+	}
+
+	for i, host := range mesh.Hosts() {
+		_, err := Upgrade(host, types.Hash20{}, WithNodeReporter(reporter(i)))
+		require.NoError(t, err)
+	}
+
+	mesh.ConnectPeers(mesh.Hosts()[0].ID(), mesh.Hosts()[1].ID())
+	mesh.ConnectPeers(mesh.Hosts()[2].ID(), mesh.Hosts()[0].ID())
+	require.Eventually(t, func() bool {
+		return counter[0].Load() == 2 && counter[1].Load() == 1 && counter[2].Load() == 1
+	}, 100*time.Millisecond, 10*time.Millisecond)
+
+	mesh.DisconnectPeers(mesh.Hosts()[0].ID(), mesh.Hosts()[1].ID())
+	require.Eventually(t, func() bool {
+		return counter[0].Load() == 3 && counter[1].Load() == 2
+	}, 100*time.Millisecond, 10*time.Millisecond)
 }
