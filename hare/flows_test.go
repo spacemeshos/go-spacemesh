123e1a8c426a4cdb7046f87bc069879aabbe1b49
Piers Powlesland <pierspowlesland@gmail.com>
2023-04-05 01:52:57 +0100

    Fix issue with discarded future messages
    Nodes that progressed to layers more than one beyond other layers would
    have their messages discarded by the trailing nodes because the messages
    would be too early. The broker only accounts for messages one layer
    early, so this change limits layer initiation such that the most
    recently started layer can at most be one layer beyond the layer that
    all nodes are executing.

--- hare/flows_test.go
+++ hare/flows_test.go
@@ -137,9 +137,20 @@ func Test_multipleCPs(t *testing.T) {
 		return true
 	}, 5*time.Second, 10*time.Millisecond)
 
+	ahead := 0
 	for j := types.GetEffectiveGenesis().Add(1); !j.After(finalLyr); j = j.Add(1) {
+		if ahead > 1 {
+			// We can't allow layers to progress more than 1 ahead of the latest layer
+			// that all nodes have started, otherwise we can end up losing messages
+			// when a node from a future layer sends messages to a node that has not
+			// started that layer. E.g A node that has started layer 8 receives a
+			// message for layer 10, its broker will reject it because the broker only
+			// allows messages one layer ahead not two.
+			<-roundClocks.roundClock(j.Sub(1)).AwaitEndOfRound(preRound)
+			ahead--
+		}
 		test.clock.advanceLayer()
-		time.Sleep(10 * time.Millisecond)
+		ahead++
 	}
 
 	test.WaitForTimedTermination(t, time.Minute)
@@ -321,13 +332,21 @@ func Test_multipleCPsAndIterations(t *testing.T) {
 		}
 		return true
 	}, 5*time.Second, 10*time.Millisecond)
-	layerDuration := 250 * time.Millisecond
-	go func() {
-		for j := types.GetEffectiveGenesis().Add(1); !j.After(finalLyr); j = j.Add(1) {
-			test.clock.advanceLayer()
-			time.Sleep(layerDuration)
+	ahead := 0
+	for j := types.GetEffectiveGenesis().Add(1); !j.After(finalLyr); j = j.Add(1) {
+		if ahead > 1 {
+			// We can't allow layers to progress more than 1 ahead of the latest layer
+			// that all nodes have started, otherwise we can end up losing messages
+			// when a node from a future layer sends messages to a node that has not
+			// started that layer. E.g A node that has started layer 8 receives a
+			// message for layer 10, its broker will reject it because the broker only
+			// allows messages one layer ahead not two.
+			<-roundClocks.roundClock(j.Sub(1)).AwaitEndOfRound(preRound)
+			ahead--
 		}
-	}()
+		test.clock.advanceLayer()
+		ahead++
+	}
 
 	// There are 5 rounds per layer and totalCPs layers and we double to allow
 	// for the for good measure. Also one layer in this test will run 2
