4a5bf4d78636f0d74dbe10bd0b78b6e6c4e5af1b
Dmitry <yashulyak@gmail.com>
2023-04-07 12:16:48 +0300

    fix tests

--- config/genesis_test.go
+++ config/genesis_test.go
@@ -2,22 +2,31 @@ package config
 
 import (
 	"testing"
+	"time"
+
+	"github.com/spacemeshos/go-spacemesh/hash"
 
 	"github.com/stretchr/testify/require"
 )
 
 func TestGenesisID(t *testing.T) {
 	t.Run("changes based on cfg vars", func(t *testing.T) {
-		cfg1 := GenesisConfig{ExtraData: "one", GenesisTime: "two"}
-		cfg2 := GenesisConfig{ExtraData: "one", GenesisTime: "one"}
+		cfg1 := GenesisConfig{ExtraData: "one", GenesisTime: "2023-03-15T18:00:00Z"}
+		cfg2 := GenesisConfig{ExtraData: "one", GenesisTime: "1989-03-15T18:00:00Z"}
+		require.NoError(t, cfg1.Validate())
 		require.NotEqual(t, cfg1.GenesisID(), cfg2.GenesisID())
 	})
 	t.Run("consistent", func(t *testing.T) {
-		cfg := GenesisConfig{ExtraData: "one", GenesisTime: "two"}
+		cfg := GenesisConfig{ExtraData: "one", GenesisTime: "2023-03-15T18:00:00Z"}
+		require.NoError(t, cfg.Validate())
 		require.Equal(t, cfg.GenesisID(), cfg.GenesisID())
 	})
 	t.Run("stable", func(t *testing.T) {
-		cfg := GenesisConfig{ExtraData: "one", GenesisTime: "two"}
-		require.Equal(t, "0x374ea0818fe293007e99bc08185cac7a12aea8dd", cfg.GenesisID().Hex())
+		unixtime := time.Unix(10101, 0)
+		cfg := GenesisConfig{ExtraData: "one", GenesisTime: unixtime.Format(time.RFC3339)}
+		require.NoError(t, cfg.Validate())
+
+		expected := hash.Sum([]byte("10101"), []byte("one"))
+		require.Equal(t, expected[:20], cfg.GenesisID().Bytes())
 	})
 }
