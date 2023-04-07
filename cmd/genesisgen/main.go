67f8a32455b5051831497225eb713cee6591a30a
Dmitry <yashulyak@gmail.com>
2023-04-07 07:06:15 +0300

    add an option to generate key

--- cmd/genesisgen/main.go
+++ cmd/genesisgen/main.go
@@ -11,7 +11,7 @@ import (
 )
 
 var (
-	key   = flag.String("key", "key.bin", "ed25519 keyfile")
+	key   = flag.String("key", "", "ed25519 keyfile")
 	extra = flag.String("extra", "", "genesis extra data")
 	time  = flag.String("time", "", "genesis time")
 )
@@ -22,9 +22,16 @@ func main() {
 	conf := config.GenesisConfig{GenesisTime: *time, ExtraData: *extra}
 	key, err := ioutil.ReadFile(*key)
 	if err != nil {
-		panic(err)
+		pkey, err := signing.NewEdSigner()
+		if err != nil {
+			panic(err)
+		}
+		key = pkey.PrivateKey()
+		fmt.Printf("key is not found. i generated one for you:\n%x\n", key)
 	}
 
 	id := types.BytesToNodeID(signing.Public(signing.PrivateKey(key)))
+	fmt.Println("---")
+	fmt.Println("values below can be passed to post")
 	fmt.Printf("id %v\ngolden %x\n", id.String(), conf.GenesisID().ToHash32())
 }
