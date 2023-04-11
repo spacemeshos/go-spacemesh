7eecd61237363c1ef1a8ff94ee8185f5da799573
Dmitry <yashulyak@gmail.com>
2023-04-11 09:41:27 +0300

    wip

--- genvm/core/gas.go
+++ genvm/core/gas.go
@@ -9,3 +9,15 @@ func ComputeIntrinsicGasCost(baseGas uint64, tx []byte, storageFactor uint64) ui
 func ComputeGasCost(baseGas, fixedGas uint64, tx []byte, storageFactor uint64) uint64 {
 	return ComputeIntrinsicGasCost(baseGas, tx, storageFactor) + fixedGas
 }
+
+const (
+	// TXDATA is a cost for storing transaction data included into the block. Charged per 8 byte.
+	TXDATA = 128
+	TX     = 20000
+	SPAWN  = 30000
+	// STORE is a cost for
+	STORE    = 5000
+	UPDATE   = 725
+	LOAD     = 182
+	EDVERIFY = 3000
+)
