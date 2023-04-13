2d6a2f8284a002d68c73b1600a0379207bc9a883
Matthias <5011972+fasmat@users.noreply.github.com>
2023-04-13 19:45:29 +0000

    Fix more failing tests

--- txs/conservative_state_test.go
+++ txs/conservative_state_test.go
@@ -9,6 +9,8 @@ import (
 	"time"
 
 	"github.com/golang/mock/gomock"
+	"github.com/libp2p/go-libp2p/core/crypto"
+	"github.com/libp2p/go-libp2p/core/peer"
 	"github.com/stretchr/testify/require"
 
 	"github.com/spacemeshos/go-spacemesh/common/types"
@@ -38,7 +40,9 @@ const (
 
 func newTx(tb testing.TB, nonce uint64, amount, fee uint64, signer *signing.EdSigner) *types.Transaction {
 	tb.Helper()
-	dest := types.Address{byte(rand.Int()), byte(rand.Int()), byte(rand.Int()), byte(rand.Int())}
+	var dest types.Address
+	_, err := rand.Read(dest[:])
+	require.NoError(tb, err)
 	return newTxWthRecipient(tb, dest, nonce, amount, fee, signer)
 }
 
@@ -65,10 +69,13 @@ type testConState struct {
 	logger log.Log
 	db     *sql.Database
 	mvm    *MockvmState
+
+	key crypto.PrivKey
+	id  peer.ID
 }
 
 func (t *testConState) handler() *TxHandler {
-	return NewTxHandler(t, "", t.logger)
+	return NewTxHandler(t, t.id, t.logger)
 }
 
 func createTestState(t *testing.T, gasLimit uint64) *testConState {
@@ -80,6 +87,11 @@ func createTestState(t *testing.T, gasLimit uint64) *testConState {
 		NumTXsPerProposal: numTXsInProposal,
 	}
 	logger := logtest.New(t)
+	priv, pub, err := crypto.GenerateEd25519Key(nil)
+	require.NoError(t, err)
+	id, err := peer.IDFromPublicKey(pub)
+	require.NoError(t, err)
+
 	return &testConState{
 		ConservativeState: NewConservativeState(mvm, db,
 			WithCSConfig(cfg),
@@ -87,6 +99,8 @@ func createTestState(t *testing.T, gasLimit uint64) *testConState {
 		logger: logger,
 		db:     db,
 		mvm:    mvm,
+		key:    priv,
+		id:     id,
 	}
 }
 
@@ -544,7 +558,7 @@ func TestConsistentHandling(t *testing.T) {
 			failed.EXPECT().Parse().Times(1).Return(nil, errors.New("test"))
 			instances[1].mvm.EXPECT().Validation(txs[i].RawTx).Times(1).Return(failed)
 
-			require.Equal(t, pubsub.ValidationAccept, instances[0].handler().HandleGossipTransaction(context.Background(), "", txs[i].Raw))
+			require.Equal(t, pubsub.ValidationAccept, instances[0].handler().HandleGossipTransaction(context.Background(), p2p.NoPeer, txs[i].Raw))
 			require.NoError(t, instances[1].handler().HandleBlockTransaction(context.Background(), p2p.NoPeer, txs[i].Raw))
 		}
 		block := types.NewExistingBlock(types.BlockID{byte(lid)},
