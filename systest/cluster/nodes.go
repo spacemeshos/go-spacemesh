639231bb3a284fe944952e1be359a81a575d007b
Dmitry <yashulyak@gmail.com>
2023-05-18 10:29:43 +0200

    increase attempts

--- systest/cluster/nodes.go
+++ systest/cluster/nodes.go
@@ -488,7 +488,8 @@ func deployNode(ctx *testcontext.Context, id string, labels map[string]string, f
 								WithSizeLimit(resource.MustParse(ctx.Storage.Size))),
 					).
 					WithDNSConfig(corev1.PodDNSConfig().WithOptions(
-						corev1.PodDNSConfigOption().WithName("timeout").WithValue("2"),
+						corev1.PodDNSConfigOption().WithName("timeout").WithValue("1"),
+						corev1.PodDNSConfigOption().WithName("attempts").WithValue("5"),
 					)).
 					WithContainers(corev1.Container().
 						WithName("smesher").
