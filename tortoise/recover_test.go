7c227c7721aa541a7361f5315193f685fcc4e579
Dmitry <yashulyak@gmail.com>
2023-05-27 07:57:59 +0200

    tortoise: generalize loading atxs from epochs without ballots

--- tortoise/recover_test.go
+++ tortoise/recover_test.go
@@ -8,7 +8,9 @@ import (
 
 	"github.com/spacemeshos/go-spacemesh/common/types"
 	"github.com/spacemeshos/go-spacemesh/datastore"
+	"github.com/spacemeshos/go-spacemesh/log/logtest"
 	"github.com/spacemeshos/go-spacemesh/system"
+	"github.com/spacemeshos/go-spacemesh/tortoise/sim"
 )
 
 type recoveryAdapter struct {
@@ -29,4 +31,43 @@ func (a *recoveryAdapter) TallyVotes(ctx context.Context, current types.LayerID)
 		require.NoError(a, RecoverLayer(ctx, a.Tortoise, a.db, a.beacon, lid))
 		a.prev = lid
 	}
+}
+
+func TestRecoverState(t *testing.T) {
+	ctx := context.Background()
+	const size = 10
+	s := sim.New(sim.WithLayerSize(size))
+	s.Setup()
+
+	cfg := defaultTestConfig()
+	cfg.LayerSize = size
+	tortoise := tortoiseFromSimState(t, s.GetState(0), WithLogger(logtest.New(t)), WithConfig(cfg))
+	var last, verified types.LayerID
+	for i := 0; i < 50; i++ {
+		last = s.Next()
+		tortoise.TallyVotes(ctx, last)
+		verified = tortoise.LatestComplete()
+	}
+	require.Equal(t, last.Sub(1), verified)
+
+	tortoise2, err := Recover(s.GetState(0).DB, s.GetState(0).Beacons, WithLogger(logtest.New(t)), WithConfig(cfg))
+	require.NoError(t, err)
+	verified = tortoise2.LatestComplete()
+	require.Equal(t, last.Sub(1), verified)
+	tortoiseFromSimState(t, s.GetState(0), WithLogger(logtest.New(t)), WithConfig(cfg))
+	tortoise2.TallyVotes(ctx, last)
+	verified = tortoise2.LatestComplete()
+	require.Equal(t, last.Sub(1), verified)
+}
+
+func TestRecoverEmpty(t *testing.T) {
+	const size = 10
+	s := sim.New(sim.WithLayerSize(size))
+	s.Setup()
+
+	cfg := defaultTestConfig()
+	cfg.LayerSize = size
+	tortoise, err := Recover(s.GetState(0).DB, s.GetState(0).Beacons, WithLogger(logtest.New(t)), WithConfig(cfg))
+	require.NoError(t, err)
+	require.NotNil(t, tortoise)
 }
