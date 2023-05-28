9978ed287bbdb857fa5659354d197af12fbcb4a8
Dmitry Shulyak <yashulyak@gmail.com>
2023-05-28 14:16:13 +0000

    tortoise: don't evict until updates are consumed (#4444)

--- tortoise/tortoise_test.go
+++ tortoise/tortoise_test.go
@@ -1039,6 +1039,7 @@ func TestBaseBallotEvictedBlock(t *testing.T) {
 		sim.WithSequence(2),
 	) {
 		last = lid
+		tortoise.Updates() // drain pending
 		tortoise.TallyVotes(ctx, lid)
 		verified = tortoise.LatestComplete()
 	}
@@ -1906,6 +1907,12 @@ func TestStateManagement(t *testing.T) {
 		verified = tortoise.LatestComplete()
 	}
 	require.Equal(t, last.Sub(1), verified)
+	require.Equal(t, types.GetEffectiveGenesis()-1, tortoise.trtl.evicted,
+		"should not be evicted unless pending is drained",
+	)
+
+	tortoise.Updates()
+	tortoise.TallyVotes(ctx, last)
 
 	evicted := tortoise.trtl.evicted
 	require.Equal(t, verified.Sub(window).Sub(1), evicted)
