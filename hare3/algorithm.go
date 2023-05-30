bcb03091f94e869076c46a320c021fa490ca9170
Piers Powlesland <pierspowlesland@gmail.com>
2023-05-30 11:46:35 +0100

    wip thinking on hare3

--- hare3/algorithm.go
+++ hare3/algorithm.go
@@ -1,5 +1,7 @@
 package hare3
 
+// Hare 2 below --------------------------------------------------
+
 // Don't really want this to be part of the impl because it introduces errors
 // that we don't want or really need.
 //
@@ -57,3 +59,35 @@ package hare3
 
 // I'm going to remove signature verification from these protocols to keep them
 // simple, we assume signature verification is done up front.
+
+// Hare 3 below --------------------------------------------------
+
+// Hare 3 is pretty similare just with an underlying PKI grading protocol.
+
+// type GradedGossiper interface {
+// 	// Gossip takes the given sid (session id) value and verification key (key)
+// 	Gossip(sid, value, key, sig []byte) (s, v, k []byte, grade uint8)
+// }
+
+// But we want to take signatures out so that we don't deal with errors
+
+type KeyGrader any
+
+// not sure exactly what to put here, but the output should be the grade of key
+
+type GradedGossiper interface {
+	// GradedGossip takes the given sid (session id) value, key (verification
+	// key) and grade and processes them. If a non nil v is returned the
+	// originally received input message should be forwarded to all neighbors
+	// and (sid, value, key, grade) is considered to have been output.
+	GradedGossip(sid, value, key []byte, grade uint8) (v []byte)
+}
+
+type TrhesholdGradedGossiper interface {
+	// Threshold graded gossip takes outputs from graded gossip, which are in
+	// fact sets of values not single values, and returns sets of values that
+	// have reached the required threshold, along with the grade for that set.
+	ThresholdGradedGossip(sid, round uint32, value [][]byte, key []byte, grade uint8) (v [][]byte, g uint8) // (sid, r, v, d + 1 − s)
+	// Increment round increments the current round
+	IncRound()
+}
