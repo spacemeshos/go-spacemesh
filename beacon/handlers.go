4bc1f1594c8f0de532de16ef02f39ab3b1b6631d
Dmitry <yashulyak@gmail.com>
2023-06-05 08:37:47 +0200

    beacon: fix mistake in votes tracker

--- beacon/handlers.go
+++ beacon/handlers.go
@@ -6,7 +6,6 @@ import (
 	"errors"
 	"fmt"
 	"math/big"
-	"math/bits"
 	"time"
 
 	"github.com/spacemeshos/go-spacemesh/codec"
@@ -475,39 +474,20 @@ func (pd *ProtocolDriver) registerVoted(epoch types.EpochID, nodeID types.NodeID
 	return pd.states[epoch].registerVoted(nodeID, round)
 }
 
-const wordSize = 64
-
-func newVotesTracker(limit uint32) *votesTracker {
-	b := bits.Len32(limit)
-	words := b / wordSize
-	if b%wordSize != 0 {
-		words += 1
-	}
-	return &votesTracker{words: make([]uint64, words)}
+func newVotesTracker() *votesTracker {
+	return &votesTracker{votes: new(big.Int)}
 }
 
 type votesTracker struct {
-	words []uint64
+	votes *big.Int
 }
 
 func (v *votesTracker) register(round types.RoundID) bool {
-	word := uint64(round) / wordSize
-	if word >= uint64(len(v.words)) {
-		return false
-	}
-	position := uint64(1) << (uint64(round) % wordSize)
-	if v.words[word]&position > 0 {
-		return false
-	}
-	v.words[word] |= position
-	return true
+	rst := !v.voted(round)
+	v.votes.SetBit(v.votes, int(round), 1)
+	return rst
 }
 
 func (v *votesTracker) voted(round types.RoundID) bool {
-	word := uint64(round) / wordSize
-	if word >= uint64(len(v.words)) {
-		return false
-	}
-	position := uint64(1) << (uint64(round) % wordSize)
-	return v.words[word]&position > 0
+	return v.votes.Bit(int(round)) > 0
 }
