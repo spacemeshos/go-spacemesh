d9f3e864eaecc53d303fe898bdc9e2810674e4c6
Dmitry <yashulyak@gmail.com>
2023-06-07 09:16:09 +0200

    notify vm that layer crossed back

--- tortoise/tortoise.go
+++ tortoise/tortoise.go
@@ -412,6 +412,8 @@ func (t *turtle) verifyLayers() {
 				t.pending = types.MinLayer(t.pending, target)
 			}
 			break
+		} else if target > t.verified {
+			t.pending = types.MinLayer(t.pending, target)
 		}
 
 		verified = target
@@ -443,7 +445,6 @@ func (t *turtle) verifyLayers() {
 				log.Stringer("emitted", block.emitted),
 			)
 			block.emitted = block.validity
-			t.pending = types.MinLayer(t.pending, target)
 		}
 	}
 	t.verified = verified
