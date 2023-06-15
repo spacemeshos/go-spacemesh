d5ea6e960a6eaf0f51b6a79e9a3d81d006ecb244
kimmy lin <30611210+countvonzero@users.noreply.github.com>
2023-06-15 05:34:07 -0700

    fix preround checker as well

--- hare/preroundtracker_test.go
+++ hare/preroundtracker_test.go
@@ -87,6 +87,7 @@ func TestPreRoundTracker_OnPreRound(t *testing.T) {
 								Round:   m1.Round,
 								MsgHash: types.BytesToHash(m1.HashBytes()),
 							},
+							SmesherID: m1.SmesherID,
 							Signature: m1.Signature,
 						},
 						{
@@ -95,6 +96,7 @@ func TestPreRoundTracker_OnPreRound(t *testing.T) {
 								Round:   m2.Round,
 								MsgHash: types.BytesToHash(m2.HashBytes()),
 							},
+							SmesherID: m2.SmesherID,
 							Signature: m2.Signature,
 						},
 					},
