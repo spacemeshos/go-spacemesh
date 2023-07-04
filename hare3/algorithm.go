da1e45e1d79937451691d1f995e86cd5fa8077c7
Piers Powlesland <pierspowlesland@gmail.com>
2023-07-04 16:36:50 +0100

    Extend documentation for GradedGossiper

--- hare3/algorithm.go
+++ hare3/algorithm.go
@@ -87,6 +87,35 @@ type GradedGossiper interface {
 	// the message round and a grade. It returns a value indicating what action
 	// to take with the message and the hash of an equivocating message if
 	// equivocation was detected.
+	//
+	// There are 3 different results that graded gossip can output:
+	//
+	// * SendValue indicates that the message be forwarded to downstream sub
+	// protocols (Gradecaster & TrhesholdGradedGossiper) and ultimately that
+	// the message should be relayed to the network. Note that the first
+	// message in a round from a known malicious identity needs to be
+	// forwarded, since hare3 includes the number of known malicious entities
+	// in its threshold calculations. Also it is necessary to forward it so
+	// that the rest of the network can also include the malicious identity in
+	// their threshold calculations. Messages from known malicious entities are
+	// signified to GradedGossiper by having a nil valueHash.
+	//
+	// * SendEquivocationProof is returned in the case that a new equivocation
+	// has been detected, the equivocationHash will be the hash of the prior
+	// message from the same identity. In this case again the equivocating
+	// message still needs to be forwarded, for the same reasons as above.
+	//
+	// * DropMessage is returned in the case of any further messages from
+	// entities for which we have already forwarded a malicious message this
+	// round. In this case the message should be ignored and not relayed to the
+	// network.
+	//
+	// Note that there can be at most 2 messages forwarded per identity per
+	// round, either:
+	//
+	// * One good message.
+	// * One malicious message from a known eqiuvotator.
+	// * One good message followed by a malicious message indicating equivocation.
 	ReceiveMsg(hash, id types.Hash20, valueHash *types.Hash20, round AbsRound, grade uint8) (result GradedGossipResult, equivocationHash *types.Hash20)
 }
 
