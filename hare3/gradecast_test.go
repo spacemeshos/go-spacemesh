3071d3e40167bc4f225e0e1078634e745f6090ad
Piers Powlesland <pierspowlesland@gmail.com>
2023-07-06 10:34:46 +0100

    Separate tests into subtests

--- hare3/gradecast_test.go
+++ hare3/gradecast_test.go
@@ -7,138 +7,182 @@ import (
 )
 
 func TestGradecast(t *testing.T) {
-	// Grade 2, received same round as message with max grade.
-	gc := NewGradecaster()
-	gc.ReceiveMsg(id1, values3, r0, r0, 3)
-	assert.ElementsMatch(t, []GradecastedSet{{id1, values3, 2}}, gc.RetrieveGradecastedMessages(r0))
+	t.Run("", func(t *testing.T) {
+		// Grade 2, received same round as message with max grade.
+		gc := NewGradecaster()
+		gc.ReceiveMsg(id1, values3, r0, r0, 3)
+		assert.ElementsMatch(t, []GradecastedSet{{id1, values3, 2}}, gc.RetrieveGradecastedMessages(r0))
+	})
 
-	// Grade 2, received 1 round after message round with max grade.
-	gc = NewGradecaster()
-	gc.ReceiveMsg(id1, values3, r0, r1, 3)
-	assert.ElementsMatch(t, []GradecastedSet{{id1, values3, 2}}, gc.RetrieveGradecastedMessages(r0))
+	t.Run("", func(t *testing.T) {
+		// Grade 2, received 1 round after message round with max grade.
+		gc := NewGradecaster()
+		gc.ReceiveMsg(id1, values3, r0, r1, 3)
+		assert.ElementsMatch(t, []GradecastedSet{{id1, values3, 2}}, gc.RetrieveGradecastedMessages(r0))
+	})
 
-	// Grade 1, received 2 rounds after message round with max grade.
-	gc = NewGradecaster()
-	gc.ReceiveMsg(id1, values3, r0, r2, 3)
-	assert.ElementsMatch(t, []GradecastedSet{{id1, values3, 1}}, gc.RetrieveGradecastedMessages(r0))
+	t.Run("", func(t *testing.T) {
+		// Grade 1, received 2 rounds after message round with max grade.
+		gc := NewGradecaster()
+		gc.ReceiveMsg(id1, values3, r0, r2, 3)
+		assert.ElementsMatch(t, []GradecastedSet{{id1, values3, 1}}, gc.RetrieveGradecastedMessages(r0))
+	})
 
-	// No result, received 3 rounds after message round with max grade.
-	gc = NewGradecaster()
-	gc.ReceiveMsg(id1, values3, r0, r3, 3)
-	assert.Nil(t, gc.RetrieveGradecastedMessages(r0))
+	t.Run("", func(t *testing.T) {
+		// No result, received 3 rounds after message round with max grade.
+		gc := NewGradecaster()
+		gc.ReceiveMsg(id1, values3, r0, r3, 3)
+		assert.Nil(t, gc.RetrieveGradecastedMessages(r0))
+	})
 
-	// No result, received 4 rounds after message round with max grade.
-	gc = NewGradecaster()
-	gc.ReceiveMsg(id1, values3, r0, r4, 3)
-	assert.Nil(t, gc.RetrieveGradecastedMessages(r0))
+	t.Run("", func(t *testing.T) {
+		// No result, received 4 rounds after message round with max grade.
+		gc := NewGradecaster()
+		gc.ReceiveMsg(id1, values3, r0, r4, 3)
+		assert.Nil(t, gc.RetrieveGradecastedMessages(r0))
+	})
 
-	// Grade 1, received same round as message with grade 2.
-	gc = NewGradecaster()
-	gc.ReceiveMsg(id1, values3, r0, r0, 2)
-	assert.ElementsMatch(t, []GradecastedSet{{id1, values3, 1}}, gc.RetrieveGradecastedMessages(r0))
+	t.Run("", func(t *testing.T) {
+		// Grade 1, received same round as message with grade 2.
+		gc := NewGradecaster()
+		gc.ReceiveMsg(id1, values3, r0, r0, 2)
+		assert.ElementsMatch(t, []GradecastedSet{{id1, values3, 1}}, gc.RetrieveGradecastedMessages(r0))
+	})
 
-	// Grade 1, received 1 round after message round with grade 2.
-	gc = NewGradecaster()
-	gc.ReceiveMsg(id1, values3, r0, r1, 2)
-	assert.ElementsMatch(t, []GradecastedSet{{id1, values3, 1}}, gc.RetrieveGradecastedMessages(r0))
+	t.Run("", func(t *testing.T) {
+		// Grade 1, received 1 round after message round with grade 2.
+		gc := NewGradecaster()
+		gc.ReceiveMsg(id1, values3, r0, r1, 2)
+		assert.ElementsMatch(t, []GradecastedSet{{id1, values3, 1}}, gc.RetrieveGradecastedMessages(r0))
+	})
 
-	// Grade 1, received 2 rounds after message round with grade 2.
-	gc = NewGradecaster()
-	gc.ReceiveMsg(id1, values3, r0, r2, 2)
-	assert.ElementsMatch(t, []GradecastedSet{{id1, values3, 1}}, gc.RetrieveGradecastedMessages(r0))
+	t.Run("", func(t *testing.T) {
+		// Grade 1, received 2 rounds after message round with grade 2.
+		gc := NewGradecaster()
+		gc.ReceiveMsg(id1, values3, r0, r2, 2)
+		assert.ElementsMatch(t, []GradecastedSet{{id1, values3, 1}}, gc.RetrieveGradecastedMessages(r0))
+	})
 
-	// No result, received 3 rounds after message round with grade 2.
-	gc = NewGradecaster()
-	gc.ReceiveMsg(id1, values3, r0, r3, 2)
-	assert.Nil(t, gc.RetrieveGradecastedMessages(r0))
+	t.Run("", func(t *testing.T) {
+		// No result, received 3 rounds after message round with grade 2.
+		gc := NewGradecaster()
+		gc.ReceiveMsg(id1, values3, r0, r3, 2)
+		assert.Nil(t, gc.RetrieveGradecastedMessages(r0))
+	})
 
-	// No result, received 4 rounds after message round with grade 2.
-	gc = NewGradecaster()
-	gc.ReceiveMsg(id1, values3, r0, r4, 2)
-	assert.Nil(t, gc.RetrieveGradecastedMessages(r0))
+	t.Run("", func(t *testing.T) {
+		// No result, received 4 rounds after message round with grade 2.
+		gc := NewGradecaster()
+		gc.ReceiveMsg(id1, values3, r0, r4, 2)
+		assert.Nil(t, gc.RetrieveGradecastedMessages(r0))
+	})
 
-	// No result, received 1 round after message round with grade 1.
-	gc = NewGradecaster()
-	gc.ReceiveMsg(id1, values3, r0, r1, 1)
-	assert.Nil(t, gc.RetrieveGradecastedMessages(r0))
+	t.Run("", func(t *testing.T) {
+		// No result, received 1 round after message round with grade 1.
+		gc := NewGradecaster()
+		gc.ReceiveMsg(id1, values3, r0, r1, 1)
+		assert.Nil(t, gc.RetrieveGradecastedMessages(r0))
+	})
 
-	// No result, received 2 rounds after message round with grade 1.
-	gc = NewGradecaster()
-	gc.ReceiveMsg(id1, values3, r0, r2, 1)
-	assert.Nil(t, gc.RetrieveGradecastedMessages(r0))
+	t.Run("", func(t *testing.T) {
+		// No result, received 2 rounds after message round with grade 1.
+		gc := NewGradecaster()
+		gc.ReceiveMsg(id1, values3, r0, r2, 1)
+		assert.Nil(t, gc.RetrieveGradecastedMessages(r0))
+	})
 
-	// No result, received 3 rounds after message round with grade 1.
-	gc = NewGradecaster()
-	gc.ReceiveMsg(id1, values3, r0, r3, 1)
-	assert.Nil(t, gc.RetrieveGradecastedMessages(r0))
+	t.Run("", func(t *testing.T) {
+		// No result, received 3 rounds after message round with grade 1.
+		gc := NewGradecaster()
+		gc.ReceiveMsg(id1, values3, r0, r3, 1)
+		assert.Nil(t, gc.RetrieveGradecastedMessages(r0))
+	})
 
-	// No result, received 1 round after message round with max grade with
-	// subsequent malicious message 1 round after message round.
-	gc = NewGradecaster()
-	gc.ReceiveMsg(id1, values3, r0, r1, 3)
-	gc.ReceiveMsg(id1, nil, r0, r1, 3)
-	assert.Nil(t, gc.RetrieveGradecastedMessages(r0))
+	t.Run("", func(t *testing.T) {
+		// No result, received 1 round after message round with max grade with
+		// subsequent malicious message 1 round after message round.
+		gc := NewGradecaster()
+		gc.ReceiveMsg(id1, values3, r0, r1, 3)
+		gc.ReceiveMsg(id1, nil, r0, r1, 3)
+		assert.Nil(t, gc.RetrieveGradecastedMessages(r0))
+	})
 
-	// No result, received 1 round after message round with max grade with
-	// subsequent malicious message 2 rounds after message round.
-	gc = NewGradecaster()
-	gc.ReceiveMsg(id1, values3, r0, r1, 3)
-	gc.ReceiveMsg(id1, nil, r0, r2, 3)
-	assert.Nil(t, gc.RetrieveGradecastedMessages(r0))
+	t.Run("", func(t *testing.T) {
+		// No result, received 1 round after message round with max grade with
+		// subsequent malicious message 2 rounds after message round.
+		gc := NewGradecaster()
+		gc.ReceiveMsg(id1, values3, r0, r1, 3)
+		gc.ReceiveMsg(id1, nil, r0, r2, 3)
+		assert.Nil(t, gc.RetrieveGradecastedMessages(r0))
+	})
 
-	// No result, received 1 round after message round with max grade with
-	// subsequent malicious message 3 rounds after message round.
-	gc = NewGradecaster()
-	gc.ReceiveMsg(id1, values3, r0, r1, 3)
-	gc.ReceiveMsg(id1, nil, r0, r3, 3)
-	assert.Nil(t, gc.RetrieveGradecastedMessages(r0))
+	t.Run("", func(t *testing.T) {
+		// No result, received 1 round after message round with max grade with
+		// subsequent malicious message 3 rounds after message round.
+		gc := NewGradecaster()
+		gc.ReceiveMsg(id1, values3, r0, r1, 3)
+		gc.ReceiveMsg(id1, nil, r0, r3, 3)
+		assert.Nil(t, gc.RetrieveGradecastedMessages(r0))
+	})
 
-	// Grade 2, received 1 round after message round with max grade with
-	// subsequent malicious message 4 rounds after message round.
-	gc = NewGradecaster()
-	gc.ReceiveMsg(id1, values3, r0, r1, 3)
-	gc.ReceiveMsg(id1, nil, r0, r4, 3)
-	assert.ElementsMatch(t, []GradecastedSet{{id1, values3, 2}}, gc.RetrieveGradecastedMessages(r0))
+	t.Run("", func(t *testing.T) {
+		// Grade 2, received 1 round after message round with max grade with
+		// subsequent malicious message 4 rounds after message round.
+		gc := NewGradecaster()
+		gc.ReceiveMsg(id1, values3, r0, r1, 3)
+		gc.ReceiveMsg(id1, nil, r0, r4, 3)
+		assert.ElementsMatch(t, []GradecastedSet{{id1, values3, 2}}, gc.RetrieveGradecastedMessages(r0))
+	})
 
-	// No result, received 2 rounds after message round with max grade with
-	// subsequent malicious message 2 rounds after message round.
-	gc = NewGradecaster()
-	gc.ReceiveMsg(id1, values3, r0, r2, 3)
-	gc.ReceiveMsg(id1, nil, r0, r2, 3)
-	assert.Nil(t, gc.RetrieveGradecastedMessages(r0))
+	t.Run("", func(t *testing.T) {
+		// No result, received 2 rounds after message round with max grade with
+		// subsequent malicious message 2 rounds after message round.
+		gc := NewGradecaster()
+		gc.ReceiveMsg(id1, values3, r0, r2, 3)
+		gc.ReceiveMsg(id1, nil, r0, r2, 3)
+		assert.Nil(t, gc.RetrieveGradecastedMessages(r0))
+	})
 
-	// Grade 1, received 2 rounds after message round with max grade with
-	// subsequent malicious message 3 rounds after message round.
-	gc = NewGradecaster()
-	gc.ReceiveMsg(id1, values3, r0, r2, 3)
-	gc.ReceiveMsg(id1, nil, r0, r3, 3)
-	assert.ElementsMatch(t, []GradecastedSet{{id1, values3, 1}}, gc.RetrieveGradecastedMessages(r0))
+	t.Run("", func(t *testing.T) {
+		// Grade 1, received 2 rounds after message round with max grade with
+		// subsequent malicious message 3 rounds after message round.
+		gc := NewGradecaster()
+		gc.ReceiveMsg(id1, values3, r0, r2, 3)
+		gc.ReceiveMsg(id1, nil, r0, r3, 3)
+		assert.ElementsMatch(t, []GradecastedSet{{id1, values3, 1}}, gc.RetrieveGradecastedMessages(r0))
+	})
 
-	// Multiple messages from multiple identities.
-	gc = NewGradecaster()
-	gc.ReceiveMsg(id1, values3, r0, r1, 3)
-	gc.ReceiveMsg(id2, values2, r0, r1, 3)
-	gc.ReceiveMsg(id3, values1, r0, r1, 3)
-	expect := []GradecastedSet{{id1, values3, 2}, {id2, values2, 2}, {id3, values1, 2}}
-	assert.ElementsMatch(t, expect, gc.RetrieveGradecastedMessages(r0))
+	t.Run("", func(t *testing.T) {
+		// Multiple messages from multiple identities.
+		gc := NewGradecaster()
+		gc.ReceiveMsg(id1, values3, r0, r1, 3)
+		gc.ReceiveMsg(id2, values2, r0, r1, 3)
+		gc.ReceiveMsg(id3, values1, r0, r1, 3)
+		expect := []GradecastedSet{{id1, values3, 2}, {id2, values2, 2}, {id3, values1, 2}}
+		assert.ElementsMatch(t, expect, gc.RetrieveGradecastedMessages(r0))
+	})
 
-	// Multiple messages from multiple identities with multiple malicious messages.
-	gc = NewGradecaster()
-	gc.ReceiveMsg(id1, values3, r0, r1, 3)
-	gc.ReceiveMsg(id2, values2, r0, r2, 3)
-	gc.ReceiveMsg(id3, values1, r0, r2, 2)
-	gc.ReceiveMsg(id1, nil, r0, r3, 2) // should negate message from id1
-	gc.ReceiveMsg(id2, nil, r0, r2, 3) // should negate message from id2
-	gc.ReceiveMsg(id2, nil, r0, r3, 2) // should not negate message from id3
-	expect = []GradecastedSet{{id3, values1, 1}}
-	assert.ElementsMatch(t, expect, gc.RetrieveGradecastedMessages(r0))
+	t.Run("", func(t *testing.T) {
+		// Multiple messages from multiple identities with multiple malicious messages.
+		gc := NewGradecaster()
+		gc.ReceiveMsg(id1, values3, r0, r1, 3)
+		gc.ReceiveMsg(id2, values2, r0, r2, 3)
+		gc.ReceiveMsg(id3, values1, r0, r2, 2)
+		gc.ReceiveMsg(id1, nil, r0, r3, 2) // should negate message from id1
+		gc.ReceiveMsg(id2, nil, r0, r2, 3) // should negate message from id2
+		gc.ReceiveMsg(id2, nil, r0, r3, 2) // should not negate message from id3
+		expect := []GradecastedSet{{id3, values1, 1}}
+		assert.ElementsMatch(t, expect, gc.RetrieveGradecastedMessages(r0))
+	})
 
-	// Messages from different rounds are segregated.
-	gc = NewGradecaster()
-	gc.ReceiveMsg(id1, values3, r0, r1, 3)
-	gc.ReceiveMsg(id1, values2, r1, r2, 3)
-	gc.ReceiveMsg(id1, values1, r2, r3, 3)
-	assert.ElementsMatch(t, []GradecastedSet{{id1, values3, 2}}, gc.RetrieveGradecastedMessages(r0))
-	assert.ElementsMatch(t, []GradecastedSet{{id1, values2, 2}}, gc.RetrieveGradecastedMessages(r1))
-	assert.ElementsMatch(t, []GradecastedSet{{id1, values1, 2}}, gc.RetrieveGradecastedMessages(r2))
+	t.Run("", func(t *testing.T) {
+		// Messages from different rounds are segregated.
+		gc := NewGradecaster()
+		gc.ReceiveMsg(id1, values3, r0, r1, 3)
+		gc.ReceiveMsg(id1, values2, r1, r2, 3)
+		gc.ReceiveMsg(id1, values1, r2, r3, 3)
+		assert.ElementsMatch(t, []GradecastedSet{{id1, values3, 2}}, gc.RetrieveGradecastedMessages(r0))
+		assert.ElementsMatch(t, []GradecastedSet{{id1, values2, 2}}, gc.RetrieveGradecastedMessages(r1))
+		assert.ElementsMatch(t, []GradecastedSet{{id1, values1, 2}}, gc.RetrieveGradecastedMessages(r2))
+	})
 }
--- hare3/threshold_graded_gossiper_test.go
+++ hare3/threshold_graded_gossiper_test.go
@@ -8,72 +8,89 @@ import (
 
 func TestDefaultThresholdGradedGossiper(t *testing.T) {
 	var threshold uint16 = 2
+	t.Run("", func(t *testing.T) {
+		// Check that with not enough votes we do not reach the threshold.
+		tgg := NewDefaultThresholdGradedGossiper(threshold)
+		tgg.ReceiveMsg(id1, values3, rPre, r0, d)
+		assert.Empty(t, tgg.RetrieveThresholdMessages(rPre, 1))
+		tgg.ReceiveMsg(id2, values3, rPre, r0, d)
+		assert.Empty(t, tgg.RetrieveThresholdMessages(rPre, 1))
+	})
 
-	// Check that with not enough votes we do not reach the threshold.
-	tgg := NewDefaultThresholdGradedGossiper(threshold)
-	tgg.ReceiveMsg(id1, values3, rPre, r0, d)
-	assert.Empty(t, tgg.RetrieveThresholdMessages(rPre, 1))
-	tgg.ReceiveMsg(id2, values3, rPre, r0, d)
-	assert.Empty(t, tgg.RetrieveThresholdMessages(rPre, 1))
+	t.Run("", func(t *testing.T) {
+		// Check that with enough good votes we reach the threshold for all values in values.
+		tgg := NewDefaultThresholdGradedGossiper(threshold)
+		tgg.ReceiveMsg(id1, values3, rPre, r0, d)
+		tgg.ReceiveMsg(id2, values3, rPre, r0, d)
+		tgg.ReceiveMsg(id3, values3, rPre, r0, d)
+		assert.ElementsMatch(t, values3, tgg.RetrieveThresholdMessages(rPre, 1))
+	})
 
-	// Check that with enough good votes we reach the threshold for all values in values.
-	tgg = NewDefaultThresholdGradedGossiper(threshold)
-	tgg.ReceiveMsg(id1, values3, rPre, r0, d)
-	tgg.ReceiveMsg(id2, values3, rPre, r0, d)
-	tgg.ReceiveMsg(id3, values3, rPre, r0, d)
-	assert.ElementsMatch(t, values3, tgg.RetrieveThresholdMessages(rPre, 1))
+	t.Run("", func(t *testing.T) {
+		// Check that with enough good votes we reach the threshold for just val1
+		tgg := NewDefaultThresholdGradedGossiper(threshold)
+		tgg.ReceiveMsg(id1, values3, rPre, r0, d)
+		tgg.ReceiveMsg(id2, values3, rPre, r0, d)
+		tgg.ReceiveMsg(id3, values1, rPre, r0, d)
+		assert.ElementsMatch(t, values1, tgg.RetrieveThresholdMessages(rPre, 1))
+	})
 
-	// Check that with enough good votes we reach the threshold for just val1
-	tgg = NewDefaultThresholdGradedGossiper(threshold)
-	tgg.ReceiveMsg(id1, values3, rPre, r0, d)
-	tgg.ReceiveMsg(id2, values3, rPre, r0, d)
-	tgg.ReceiveMsg(id3, values1, rPre, r0, d)
-	assert.ElementsMatch(t, values1, tgg.RetrieveThresholdMessages(rPre, 1))
+	t.Run("", func(t *testing.T) {
+		// Check that we reach the threshold with just one good vote and remaining malicious votes
+		tgg := NewDefaultThresholdGradedGossiper(threshold)
+		tgg.ReceiveMsg(id1, values3, rPre, r0, d)
+		tgg.ReceiveMsg(id2, nil, rPre, r0, d)
+		tgg.ReceiveMsg(id3, nil, rPre, r0, d)
+		assert.ElementsMatch(t, values3, tgg.RetrieveThresholdMessages(rPre, 1))
+	})
 
-	// Check that we reach the threshold with just one good vote and remaining malicious votes
-	tgg = NewDefaultThresholdGradedGossiper(threshold)
-	tgg.ReceiveMsg(id1, values3, rPre, r0, d)
-	tgg.ReceiveMsg(id2, nil, rPre, r0, d)
-	tgg.ReceiveMsg(id3, nil, rPre, r0, d)
-	assert.ElementsMatch(t, values3, tgg.RetrieveThresholdMessages(rPre, 1))
+	t.Run("", func(t *testing.T) {
+		// Check that no threshold is reached with just malicious votes
+		tgg := NewDefaultThresholdGradedGossiper(threshold)
+		tgg.ReceiveMsg(id1, nil, rPre, r0, d)
+		tgg.ReceiveMsg(id2, nil, rPre, r0, d)
+		tgg.ReceiveMsg(id3, nil, rPre, r0, d)
+		assert.Empty(t, tgg.RetrieveThresholdMessages(rPre, 1))
+	})
 
-	// Check that no threshold is reached with just malicious votes
-	tgg = NewDefaultThresholdGradedGossiper(threshold)
-	tgg.ReceiveMsg(id1, nil, rPre, r0, d)
-	tgg.ReceiveMsg(id2, nil, rPre, r0, d)
-	tgg.ReceiveMsg(id3, nil, rPre, r0, d)
-	assert.Empty(t, tgg.RetrieveThresholdMessages(rPre, 1))
+	t.Run("", func(t *testing.T) {
+		// Check that a node that equivocates has it's good votes removed.
+		tgg := NewDefaultThresholdGradedGossiper(threshold)
+		// id1 will vote for values but then equivocate, which should remove their vote for values, but they will still have a malicious vote.
+		tgg.ReceiveMsg(id1, values3, rPre, r0, d)
+		tgg.ReceiveMsg(id1, nil, rPre, r0, d)
+		tgg.ReceiveMsg(id2, values3, rPre, r0, d)
+		// At this point we should have one vote for values and one malicious not enough to reach any threshold.
+		assert.Empty(t, tgg.RetrieveThresholdMessages(rPre, 1))
+		// With a vote for justVal1 we should see justVal1 pass the threshold.
+		tgg.ReceiveMsg(id3, values1, rPre, r0, d)
+		assert.ElementsMatch(t, values1, tgg.RetrieveThresholdMessages(rPre, 1))
+	})
 
-	// Check that a node that equivocates has it's good votes removed.
-	tgg = NewDefaultThresholdGradedGossiper(threshold)
-	// id1 will vote for values but then equivocate, which should remove their vote for values, but they will still have a malicious vote.
-	tgg.ReceiveMsg(id1, values3, rPre, r0, d)
-	tgg.ReceiveMsg(id1, nil, rPre, r0, d)
-	tgg.ReceiveMsg(id2, values3, rPre, r0, d)
-	// At this point we should have one vote for values and one malicious not enough to reach any threshold.
-	assert.Empty(t, tgg.RetrieveThresholdMessages(rPre, 1))
-	// With a vote for justVal1 we should see justVal1 pass the threshold.
-	tgg.ReceiveMsg(id3, values1, rPre, r0, d)
-	assert.ElementsMatch(t, values1, tgg.RetrieveThresholdMessages(rPre, 1))
+	t.Run("", func(t *testing.T) {
+		// Check that no threshold is reached with just malicious votes
+		tgg := NewDefaultThresholdGradedGossiper(threshold)
+		tgg.ReceiveMsg(id1, nil, rPre, r0, d)
+		tgg.ReceiveMsg(id2, nil, rPre, r0, d)
+		tgg.ReceiveMsg(id3, nil, rPre, r0, d)
+		assert.Empty(t, tgg.RetrieveThresholdMessages(rPre, 1))
+	})
 
-	// Check that no threshold is reached with just malicious votes
-	tgg = NewDefaultThresholdGradedGossiper(threshold)
-	tgg.ReceiveMsg(id1, nil, rPre, r0, d)
-	tgg.ReceiveMsg(id2, nil, rPre, r0, d)
-	tgg.ReceiveMsg(id3, nil, rPre, r0, d)
-	assert.Empty(t, tgg.RetrieveThresholdMessages(rPre, 1))
+	t.Run("", func(t *testing.T) {
+		// Check that good messages for different rounds are segregated.
+		tgg := NewDefaultThresholdGradedGossiper(threshold)
+		tgg.ReceiveMsg(id1, values3, rPre, r2, d)
+		tgg.ReceiveMsg(id2, values3, r0, r2, d)
+		tgg.ReceiveMsg(id2, values3, r0, r2, d)
+		assert.Empty(t, tgg.RetrieveThresholdMessages(rPre, 1))
+	})
 
-	// Check that good messages for different rounds are segregated.
-	tgg = NewDefaultThresholdGradedGossiper(threshold)
-	tgg.ReceiveMsg(id1, values3, rPre, r2, d)
-	tgg.ReceiveMsg(id2, values3, r0, r2, d)
-	tgg.ReceiveMsg(id2, values3, r0, r2, d)
-	assert.Empty(t, tgg.RetrieveThresholdMessages(rPre, 1))
-
-	// Check that malicious messages for different rounds are segregated.
-	tgg = NewDefaultThresholdGradedGossiper(threshold)
-	tgg.ReceiveMsg(id1, values3, rPre, r0, d)
-	tgg.ReceiveMsg(id2, nil, r0, r1, d)
-	tgg.ReceiveMsg(id2, nil, r0, r1, d)
-	assert.Empty(t, tgg.RetrieveThresholdMessages(rPre, 1))
+	t.Run("", func(t *testing.T) {
+		// Check that malicious messages for different rounds are segregated.
+		tgg := NewDefaultThresholdGradedGossiper(threshold)
+		tgg.ReceiveMsg(id1, values3, rPre, r0, d)
+		tgg.ReceiveMsg(id2, nil, r0, r1, d)
+		tgg.ReceiveMsg(id2, nil, r0, r1, d)
+		assert.Empty(t, tgg.RetrieveThresholdMessages(rPre, 1))
+	})
 }
