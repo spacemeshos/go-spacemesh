618d8cc369166c027bb06682309616491fb0c0f9
0xjac <git@0xjac.com>
2023-07-10 14:26:52 +0200

    feat: persist nipost state efficiently
    - write: compute CRC as the file is written. (requires two writes:
      content & CRC)
    - read: compute CRC as the file is read.
    - use CRC Size constant from CRC pkg.
    
    Closes #4506

--- activation/nipost_state_test.go
+++ activation/nipost_state_test.go
@@ -10,6 +10,75 @@ import (
 	"github.com/stretchr/testify/require"
 )
 
+func TestWriteCRC(t *testing.T) {
+	tests := []struct {
+		name     string
+		data     []byte
+		crc      []byte
+		validCRC bool
+	}{
+		{
+			name:     "validCRC",
+			data:     []byte("spacemesh"),
+			crc:      []byte{0xC6, 0x10, 0x72, 0xDF, 0x52, 0xD7, 0x74, 0x0E},
+			validCRC: true,
+		}, {
+			name:     "badCRC",
+			data:     []byte("spacemesh"),
+			crc:      []byte{0xC7, 0x11, 0x73, 0xE0, 0x53, 0xD8, 0x7, 0x0F},
+			validCRC: false,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			path := filepath.Join(t.TempDir(), tt.name)
+			require.NoError(t, write(path, tt.data))
+
+			raw, err := os.ReadFile(path)
+			require.NoError(t, err)
+			require.Equal(t, tt.validCRC, bytes.Equal(raw, append(tt.data, tt.crc...)))
+		})
+	}
+}
+
+func TestReadCRC(t *testing.T) {
+	tests := []struct {
+		name   string
+		data   []byte
+		crc    []byte
+		errMsg string
+	}{
+		{
+			name:   "validCRC",
+			data:   []byte("spacemesh"),
+			crc:    []byte{0xC6, 0x10, 0x72, 0xDF, 0x52, 0xD7, 0x74, 0x0E},
+			errMsg: "",
+		}, {
+			name:   "badCRC",
+			data:   []byte("spacemesh"),
+			crc:    []byte{0xC7, 0x11, 0x73, 0xE0, 0x53, 0xD8, 0x75, 0x0F},
+			errMsg: "wrong checksum 0xC71173E053D8750F, computed 0xC61072DF52D7740E",
+		},
+	}
+
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			path := filepath.Join(t.TempDir(), tt.name)
+			err := atomic.WriteFile(path, bytes.NewReader(append(tt.data, tt.crc...)))
+			require.NoError(t, err)
+
+			data, err := read(path)
+			if tt.errMsg == "" {
+				require.NoError(t, err)
+				require.True(t, bytes.Equal(data, tt.data))
+			} else {
+				require.ErrorContains(t, err, tt.errMsg)
+				require.Nil(t, data)
+			}
+		})
+	}
+}
+
 func TestWriteRecover(t *testing.T) {
 	path := filepath.Join(t.TempDir(), "test")
 	data := []byte("secret")
