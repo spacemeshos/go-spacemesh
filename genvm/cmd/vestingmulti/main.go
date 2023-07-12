921357fcec862692347c581d9cd34eb517ad033d
Dmitry <yashulyak@gmail.com>
2023-07-12 09:34:09 +0200

    add go output and csv parser

--- genvm/cmd/vestingmulti/main.go
+++ genvm/cmd/vestingmulti/main.go
@@ -1,12 +1,15 @@
 package main
 
 import (
+	"bufio"
 	"encoding/json"
 	"errors"
 	"flag"
 	"fmt"
 	"io"
 	"os"
+	"strconv"
+	"strings"
 
 	"github.com/spacemeshos/go-spacemesh/common/types"
 	"github.com/spacemeshos/go-spacemesh/genvm/cmd/gen"
@@ -14,6 +17,7 @@ import (
 
 var (
 	hrp    = flag.String("hrp", "sm", "network human readable prefix")
+	out    = flag.String("out", "json", "json|go")
 	config = flag.String("c", "", "see example json")
 )
 
@@ -24,6 +28,59 @@ func must(err error) {
 	}
 }
 
+func parseJson(f io.Reader) []gen.Input {
+	dec := json.NewDecoder(f)
+	inputs := []gen.Input{}
+	for {
+		var input gen.Input
+		if err := dec.Decode(&input); errors.Is(err, io.EOF) {
+			break
+		} else {
+			must(err)
+		}
+		inputs = append(inputs, input)
+	}
+	return inputs
+}
+
+func parseCsv(f io.Reader) []gen.Input {
+	scan := bufio.NewScanner(f)
+	var inputs []gen.Input
+	for scan.Scan() {
+		var input gen.Input
+		columns := strings.Split(scan.Text(), ",")
+		total, err := strconv.ParseFloat(columns[0], 64)
+		must(err)
+		k, err := strconv.ParseUint(columns[1], 10, 8)
+		must(err)
+		n, err := strconv.ParseInt(columns[2], 10, 8)
+		must(err)
+		input.Required = int(k)
+		input.Total = total
+		for i := int64(3); i < 3+n; i++ {
+			key := columns[i]
+			input.Keys = append(input.Keys, key)
+		}
+		inputs = append(inputs, input)
+	}
+	return inputs
+}
+
+func parseInputs() []gen.Input {
+	f, err := os.Open(*config)
+	must(err)
+	defer f.Close()
+
+	if strings.HasSuffix(f.Name(), "json") {
+		return parseJson(f)
+	} else if strings.HasSuffix(f.Name(), "csv") {
+		return parseCsv(f)
+	}
+	fmt.Printf("unknown file extension for file %v. expect json or csv", f.Name())
+	os.Exit(1)
+	return nil
+}
+
 func main() {
 	flag.Parse()
 	types.SetNetworkHRP(*hrp)
@@ -32,22 +89,25 @@ func main() {
 		os.Exit(1)
 	}
 
-	f, err := os.Open(*config)
-	must(err)
-	defer f.Close()
-
-	dec := json.NewDecoder(f)
-	for {
-		var input gen.Input
-		if err := dec.Decode(&input); errors.Is(err, io.EOF) {
-			break
-		} else {
-			must(err)
-		}
-		output := gen.Generate(input)
-		enc := json.NewEncoder(os.Stdout)
-		if err := enc.Encode(output); err != nil {
-			must(err)
-		}
+	inputs := parseInputs()
+	var outputs []gen.Output
+	for _, input := range inputs {
+		outputs = append(outputs, gen.Generate(input))
 	}
+	switch *out {
+	case "json":
+		for _, output := range outputs {
+			enc := json.NewEncoder(os.Stdout)
+			if err := enc.Encode(output); err != nil {
+				must(err)
+			}
+		}
+	case "go":
+		fmt.Println("func MainnetAccounts() map[string]uint64 {")
+		for _, output := range outputs {
+			fmt.Printf("    \"%s\": %d,\n", output.Address, output.Balance)
+		}
+		fmt.Println("}")
+	}
+
 }
