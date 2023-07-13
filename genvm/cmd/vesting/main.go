92db40a9d93968d98bf6e97a99dcdc318367b1e8
Dmitry Shulyak <yashulyak@gmail.com>
2023-07-13 07:44:15 +0000

    vestingen: make it easier to use and support bulk execution with config (#4690)
    examples:
    
    ./vestingmulti/ -c vestingmulti/example.csv -out=go
    ./vestingmulti/ -c vestingmulti/example.json 
    ./vestingmulti/ -c vestingmulti/example.json -out=go
    
    csv decodes columns in this order:
    1. total amount
    2. required signatures
    3. total keys
    4. next as many columns as specified in prev column (one key - one column)
    
    output is either in json or as a golang map, so that it can be copied as is to config 
    
    ```json
    {"address":"sm1qqqqqqr3cj4agq0g6evj5cxma5r7arlmqzjvr4cqv9h6l","balance":10000000000}
    {"address":"sm1qqqqqqzev03u56u7u4228d23d7p43ujluzp6yxs6uc0ej","balance":100000000000}
    ```
    
    ```go
    func MainnetAccounts() map[string]uint64 {
        "sm1qqqqqqr3cj4agq0g6evj5cxma5r7arlmqzjvr4cqv9h6l": 10000000000,
        "sm1qqqqqqzev03u56u7u4228d23d7p43ujluzp6yxs6uc0ej": 100000000000,
    }
    ```

--- genvm/cmd/vesting/main.go
+++ genvm/cmd/vesting/main.go
@@ -1,34 +1,30 @@
 package main
 
 import (
-	"bytes"
-	"encoding/hex"
-	"encoding/pem"
+	"encoding/json"
 	"flag"
 	"fmt"
-	"io"
 	"os"
-	"path/filepath"
-
-	"github.com/oasisprotocol/curve25519-voi/primitives/ed25519"
-	"github.com/spacemeshos/economics/constants"
 
 	"github.com/spacemeshos/go-spacemesh/common/types"
-	"github.com/spacemeshos/go-spacemesh/genvm/core"
-	"github.com/spacemeshos/go-spacemesh/genvm/templates/multisig"
-	"github.com/spacemeshos/go-spacemesh/genvm/templates/vault"
-	"github.com/spacemeshos/go-spacemesh/genvm/templates/vesting"
+	"github.com/spacemeshos/go-spacemesh/genvm/cmd/gen"
 )
 
 var (
-	dir     = flag.String("d", ".", "directory with public keys (.pem or .hex)")
-	k       = flag.Int("k", 3, "number of required signatures")
-	start   = flag.Uint("start", constants.VestStart, "start of the vesting")
-	end     = flag.Uint("end", constants.VestEnd, "end of the vesting")
-	initial = flag.Uint("initial", 1_000_000, "amount unlocked at the start")
-	total   = flag.Uint("total", 10_000_000, "amount unlocked incrementally over end-start period")
-	hrp     = flag.String("hrp", "sm", "network human readable prefix")
+	k     = flag.Int("k", 1, "number of required signatures")
+	total = flag.Float64("total", 10e9, "total vaulted amount. 1/4 of it is vested at the VestStart (~1 year)")
+	hrp   = flag.String("hrp", "sm", "network human readable prefix")
+	debug = flag.Bool("debug", false, "output all data. otherwise output only vault and balance in json format")
+
+	hexkeys []string
 )
+
+func init() {
+	flag.Func("key", "add a public key to multisig encoded in hex (64 characters, without 0x)", func(key string) error {
+		hexkeys = append(hexkeys, key)
+		return nil
+	})
+}
 
 func must(err error) {
 	if err != nil {
@@ -37,68 +33,22 @@ func must(err error) {
 	}
 }
 
-const pemext = ".pem"
-
-func decodeKeys(dir string) []core.PublicKey {
-	files, err := os.ReadDir(dir)
-	must(err)
-	var keys []core.PublicKey
-	for _, file := range files {
-		fname := filepath.Join(dir, file.Name())
-		f, err := os.Open(fname)
-		must(err)
-		defer f.Close()
-		data, err := io.ReadAll(f)
-		must(err)
-		if filepath.Ext(fname) == pemext {
-			block, _ := pem.Decode(data)
-			key := [ed25519.PublicKeySize]byte{}
-			n := copy(key[:], block.Bytes[len(block.Bytes)-len(key):])
-			if n != len(key) {
-				must(fmt.Errorf("key in pem file %s is not of the expected format", fname))
-			}
-			keys = append(keys, key)
-		} else {
-			data = bytes.Trim(data, "\n")
-			key := [ed25519.PublicKeySize]byte{}
-			n, err := hex.Decode(key[:], data)
-			must(err)
-			if n != len(key) {
-				must(fmt.Errorf("key in file %s can't be decoded from hex into %d bytes", fname, len(key)))
-			}
-			keys = append(keys, key)
-		}
-	}
-	return keys
-}
-
 func main() {
 	flag.Parse()
-
-	vestingArgs := &multisig.SpawnArguments{
-		Required:   uint8(*k),
-		PublicKeys: decodeKeys(*dir),
-	}
-	if int(vestingArgs.Required) > len(vestingArgs.PublicKeys) {
-		fmt.Printf("requires more signatures (%d) then public keys (%d) in the wallet\n",
-			vestingArgs.Required,
-			len(vestingArgs.PublicKeys),
-		)
-		os.Exit(1)
-	}
-	vestingAddress := core.ComputePrincipal(vesting.TemplateAddress, vestingArgs)
-	vaultArgs := &vault.SpawnArguments{
-		Owner:               vestingAddress,
-		TotalAmount:         uint64(*total),
-		InitialUnlockAmount: uint64(*initial),
-		VestingStart:        types.LayerID(uint32(*start)),
-		VestingEnd:          types.LayerID(uint32(*end)),
-	}
-	vaultAddress := core.ComputePrincipal(vault.TemplateAddress, vaultArgs)
 	types.SetNetworkHRP(*hrp)
-	fmt.Printf("vesting: %s\nvault: %s\n", vestingAddress.String(), vaultAddress.String())
-	fmt.Println("public keys:")
-	for i, key := range vestingArgs.PublicKeys {
-		fmt.Printf("%d: 0x%x\n", i, key[:])
+	output := gen.Generate(gen.Input{
+		Keys:     hexkeys,
+		Required: *k,
+		Total:    *total,
+	})
+	if *debug {
+		fmt.Printf("vesting address: %s.\nparameters: %s", output.Debug.VestingAddress, output.Debug.VestingArgs.String())
+		fmt.Println("---")
+		fmt.Printf("vault address: %s.\nparameters: %s\n", output.Debug.VaultAddress, output.Debug.VaultArgs.String())
+	} else {
+		enc := json.NewEncoder(os.Stdout)
+		if err := enc.Encode(output); err != nil {
+			must(err)
+		}
 	}
 }
