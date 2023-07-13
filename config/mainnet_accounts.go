063ecbad06958b7e3dca51e65d1b838cf001594d
Dmitry <yashulyak@gmail.com>
2023-07-13 07:22:18 +0200

    genesis: add final genesis ledger

---
+++ config/mainnet_accounts.go
@@ -0,0 +1,35 @@
+package config
+
+func MainnetAccounts() map[string]uint64 {
+	return map[string]uint64{
+		"sm1qqqqqq96ajwllyxr9mypa7yls9mtgyn556ekdjslp28h2": 1943200000000000,
+		"sm1qqqqqqyp8ueuuh2dgrc2g6ps4xvueyjpky6rfaqnxdy97": 5867100000000000,
+		"sm1qqqqqqzgmt5vv4jgucas8vvrlu4daa4r29cunwqpv0trt": 1022800000000000,
+		"sm1qqqqqq80we5pmwztmqgpxu6xasapgn65r4xjczqxu39a2": 409000000000000,
+		"sm1qqqqqqy6anfdew2sdtvuuaffjy0l7ssu9r8vjsss5c442": 2045400000000000,
+		"sm1qqqqqqyw9lvmmayckrxlnf8u7850tsjdg8zz6dg956gxg": 270600000000000,
+		"sm1qqqqqq9a8g5act6ewmmmmmux8l570kr6l68htzsq94wg4": 4090900000000000,
+		"sm1qqqqqqrgqc65x5q6exujgjs970fvcakd790na3gsr3uu7": 333300000000000,
+		"sm1qqqqqqpc4ppx8s4gmdaa5tzg35s6l3v6ujg6hmqz3s4lc": 859100000000000,
+		"sm1qqqqqq8za0geafhj4avegdwhtaw9fmgjh07s55cufk695": 293300000000000,
+		"sm1qqqqqqpf6djx3axy7aag8zhyf84ljsulhfypfxgpw5y0u": 1990600000000000,
+		"sm1qqqqqq827v998nt99vupxlrfucdk0tapp2hjyygmn3kyd": 409100000000000,
+		"sm1qqqqqqpc55ghjq6sxf5k77yc8n82fkwhlj0jedcgw2zck": 4909100000000000,
+		"sm1qqqqqqxq54zvz484hhcnrghnqrjlw26twwld32slz3lxa": 191800000000000,
+		"sm1qqqqqqyf5uc2n8mutm3tuateu5efcm9awvrclmcm5mhdf": 2933540000000000,
+		"sm1qqqqqq99klpy92mwlfcft5lmz8q5sef2v2qvtucd9y55v": 2933540000000000,
+		"sm1qqqqqqyjpjgup8fz32cufcv2nlqrr3nyvge7akqt0daea": 2933540000000000,
+		"sm1qqqqqq8zukfwtggnfq4jaqpv6m8xgtg5ay2ezaqpr2w6y": 2933540000000000,
+		"sm1qqqqqqrhftrq9knsetema7dt0qfzgd5a20m9rcczk0gk5": 2933540000000000,
+		"sm1qqqqqqyfq5f522mmrzs4lczhaf30jh4pmqyfrzcg8vrpc": 3303792000000000,
+		"sm1qqqqqq9qymyz8d7qhnt3prkuppu899vxmmxwc5cv8wcc8": 417350000000000,
+		"sm1qqqqqqyvet26gqsxjt6w50nnp80jvajr3n25xzsdpxn65": 831250000000000,
+		"sm1qqqqqq8k5g63tqjafgv7jsnwh4cdsn4tjksc6rgd96d6s": 800000000000000,
+		"sm1qqqqqqzgqpjxdw77aw74f8mz540rykda4x2jgjgaca7z5": 184375000000000,
+		"sm1qqqqqq9s5l9tc87wspycr68dfagmzxplzdn7zlcymnkup": 15000000000000,
+		"sm1qqqqqqptx3mdg4gm67arv4ykau6nfy6w9v03x9s49wmru": 100000000000000,
+		"sm1qqqqqq9fwfymdr7qv0tfc3ppa4q8ara6qm7kwugw9gdme": 500000000000000,
+		"sm1qqqqqqy3fc8nvdetan6qjz5cju7h4c60mjyvdlqnlqpxu": 15688500000000000,
+		"sm1qqqqqq8pmkx0kgg6r5u0jjtp7v4ww8amktj7ywqm23n0s": 88856733000000000,
+	}
+}
