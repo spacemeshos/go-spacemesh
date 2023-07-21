8e67e71513615d04c95c63539f27758665308aae
Piers Powlesland <pierspowlesland@gmail.com>
2023-07-21 14:13:12 +0100

    Final part to reach consistent hare3 conversion
    The leader selection was relying on the eligibility code which was
    causing us sometimes to not have any leaders. For now hardcoded to make
    everyone a leader.

--- hare3/leader/leader.go
+++ hare3/leader/leader.go
@@ -1,8 +1,6 @@
 package leader
 
 import (
-	"context"
-
 	"github.com/spacemeshos/go-spacemesh/common/types"
 	"github.com/spacemeshos/go-spacemesh/hare/eligibility"
 	"github.com/spacemeshos/go-spacemesh/hare3"
@@ -29,20 +27,21 @@ func NewDefaultLeaderChecker(oracle *eligibility.Oracle,
 }
 
 func (c *DefaultLeaderChecker) IsLeader(id types.NodeID, round hare3.AbsRound) bool {
-	if _, ok := c.acviteSet[id]; !ok {
-		return false
-	}
-	proof, err := c.oracle.Proof(context.Background(), c.layer, uint32(round))
-	if err != nil {
-		// TODO figure out, error can be that we don't have the beacon
-		return false
-	}
+	return true
+	// if _, ok := c.acviteSet[id]; !ok {
+	// 	return false
+	// }
+	// proof, err := c.oracle.Proof(context.Background(), c.layer, uint32(round))
+	// if err != nil {
+	// 	// TODO figure out, error can be that we don't have the beacon
+	// 	return false
+	// }
 
-	size := c.expectedLeaders
+	// size := c.expectedLeaders
 
-	eligibilityCount, err := c.oracle.CalcEligibility(context.Background(), c.layer, uint32(round), size, id, proof)
-	if err != nil {
-		return false
-	}
-	return eligibilityCount > 0
+	// eligibilityCount, err := c.oracle.CalcEligibility(context.Background(), c.layer, uint32(round), size, id, proof)
+	// if err != nil {
+	// 	return false
+	// }
+	// return eligibilityCount > 0
 }
