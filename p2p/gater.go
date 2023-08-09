a8f86a19fffde6141110b21d398b65bf017d2c57
Dmitry <yashulyak@gmail.com>
2023-08-09 11:25:20 +0200

    p2p: add ip4/ip6 blocklists

--- p2p/gater.go
+++ p2p/gater.go
@@ -1,20 +1,52 @@
 package p2p
 
 import (
+	"fmt"
+	"net"
+
+	"github.com/multiformats/go-multiaddr"
+
 	"github.com/libp2p/go-libp2p/core/connmgr"
 	"github.com/libp2p/go-libp2p/core/control"
 	"github.com/libp2p/go-libp2p/core/host"
 	"github.com/libp2p/go-libp2p/core/network"
 	"github.com/libp2p/go-libp2p/core/peer"
-	"github.com/multiformats/go-multiaddr"
 )
 
 var _ connmgr.ConnectionGater = (*gater)(nil)
+
+func newGater(cfg Config) (*gater, error) {
+	// leaves a small room for outbound connections in order to
+	// reduce risk of network isolation
+	g := &gater{
+		inbound:  int(float64(cfg.HighPeers) * cfg.InboundFraction),
+		outbound: int(float64(cfg.HighPeers) * cfg.OutboundFraction),
+		direct:   map[peer.ID]struct{}{},
+	}
+	direct, err := parseIntoAddr(cfg.Direct)
+	if err != nil {
+		return nil, err
+	}
+	g.ip4blocklist, err = parseCIDR(cfg.IP4Blocklist)
+	if err != nil {
+		return nil, err
+	}
+	g.ip6blocklist, err = parseCIDR(cfg.IP6Blocklist)
+	if err != nil {
+		return nil, err
+	}
+	for _, pid := range direct {
+		g.direct[pid.ID] = struct{}{}
+	}
+	return g, nil
+}
 
 type gater struct {
 	h                 host.Host
 	inbound, outbound int
 	direct            map[peer.ID]struct{}
+	ip4blocklist      []*net.IPNet
+	ip6blocklist      []*net.IPNet
 }
 
 func (g *gater) updateHost(h host.Host) {
@@ -28,8 +60,11 @@ func (g *gater) InterceptPeerDial(pid peer.ID) bool {
 	return len(g.h.Network().Peers()) <= g.outbound
 }
 
-func (*gater) InterceptAddrDial(pid peer.ID, m multiaddr.Multiaddr) bool {
-	return true
+func (g *gater) InterceptAddrDial(pid peer.ID, m multiaddr.Multiaddr) bool {
+	if !g.InterceptPeerDial(pid) {
+		return false
+	}
+	return !g.isBlocked(m)
 }
 
 func (g *gater) InterceptAccept(n network.ConnMultiaddrs) bool {
@@ -42,4 +77,39 @@ func (*gater) InterceptSecured(_ network.Direction, _ peer.ID, _ network.ConnMul
 
 func (*gater) InterceptUpgraded(_ network.Conn) (allow bool, reason control.DisconnectReason) {
 	return true, 0
+}
+
+func (g *gater) isBlocked(m multiaddr.Multiaddr) bool {
+	blocked := false
+	multiaddr.ForEach(m, func(c multiaddr.Component) bool {
+		switch c.Protocol().Code {
+		case multiaddr.P_IP4:
+			blocked = inAddrRange(net.IP(c.RawValue()), g.ip4blocklist)
+		case multiaddr.P_IP6:
+			blocked = inAddrRange(net.IP(c.RawValue()), g.ip6blocklist)
+		}
+		return false
+	})
+	return blocked
+}
+
+func parseCIDR(cidrs []string) ([]*net.IPNet, error) {
+	ipnets := make([]*net.IPNet, len(cidrs))
+	for i, cidr := range cidrs {
+		_, ipnet, err := net.ParseCIDR(cidr)
+		if err != nil {
+			return nil, fmt.Errorf("can't parse %s as valid cidr: %w", cidr, err)
+		}
+		ipnets[i] = ipnet
+	}
+	return ipnets, nil
+}
+
+func inAddrRange(ip net.IP, ipnets []*net.IPNet) bool {
+	for _, ipnet := range ipnets {
+		if ipnet.Contains(ip) {
+			return true
+		}
+	}
+	return false
 }
--- p2p/host.go
+++ p2p/host.go
@@ -45,6 +45,25 @@ func DefaultConfig() Config {
 		InboundFraction:    0.8,
 		OutboundFraction:   1.1,
 		RelayServer:        RelayServer{TTL: 20 * time.Minute, Reservations: 512},
+		IP4Blocklist: []string{
+			// localhost
+			"127.0.0.0/8",
+			// private networks
+			"10.0.0.0/8",
+			"100.64.0.0/10",
+			"172.16.0.0/12",
+			"192.168.0.0/16",
+			// link local
+			"169.254.0.0/16",
+		},
+		IP6Blocklist: []string{
+			// localhost
+			"::1/128",
+			// ULA reserved
+			"fc00::/7",
+			// link local
+			"fe80::/10",
+		},
 	}
 }
 
@@ -85,6 +104,8 @@ type Config struct {
 	DisableLegacyDiscovery   bool        `mapstructure:"p2p-disable-legacy-discovery"`
 	PrivateNetwork           bool        `mapstructure:"p2p-private-network"`
 	RelayServer              RelayServer `mapstructure:"relay-server"`
+	IP4Blocklist             []string    `mapstructure:"ip4-blocklist"`
+	IP6Blocklist             []string    `mapstructure:"ip6-blocklist"`
 }
 
 type RelayServer struct {
@@ -132,19 +153,9 @@ func New(_ context.Context, logger log.Log, cfg Config, prologue []byte, opts ..
 	if err != nil {
 		return nil, fmt.Errorf("can't create peer store: %w", err)
 	}
-	// leaves a small room for outbound connections in order to
-	// reduce risk of network isolation
-	g := &gater{
-		inbound:  int(float64(cfg.HighPeers) * cfg.InboundFraction),
-		outbound: int(float64(cfg.HighPeers) * cfg.OutboundFraction),
-		direct:   map[peer.ID]struct{}{},
-	}
-	direct, err := parseIntoAddr(cfg.Direct)
+	g, err := newGater(cfg)
 	if err != nil {
 		return nil, err
-	}
-	for _, pid := range direct {
-		g.direct[pid.ID] = struct{}{}
 	}
 	lopts := []libp2p.Option{
 		libp2p.Identity(key),
