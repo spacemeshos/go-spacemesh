1d7c2aac7de61edee31986e520757779f8ee262a
Piers Powlesland <pierspowlesland@gmail.com>
2023-08-15 09:56:00 +0100

    Remove unnecessary config option

--- node/nodeservice_api_test.go
+++ node/nodeservice_api_test.go
@@ -17,7 +17,6 @@ import (
 
 func TestPeerInfoApi(t *testing.T) {
 	cfg := config.DefaultTestConfig()
-	cfg.Genesis = config.DefaultGenesisConfig()
 	cfg.P2P.DisableNatPort = true
 	cfg.P2P.Listen = "/ip4/127.0.0.1/tcp/0"
 
