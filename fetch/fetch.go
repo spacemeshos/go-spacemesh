c2444b35829c68d973896c605696cc9cfbae6323
kimmy lin <30611210+countvonzero@users.noreply.github.com>
2023-08-15 22:10:31 +0200

    error out instead of waiting

--- fetch/fetch.go
+++ fetch/fetch.go
@@ -437,26 +437,6 @@ func (f *Fetch) send(requests []RequestMessage) {
 	}
 
 	peer2batches := f.organizeRequests(requests)
-	if len(peer2batches) == 0 {
-		f.mu.Lock()
-		defer f.mu.Unlock()
-		for _, msg := range requests {
-			if req, ok := f.ongoing[msg.Hash]; ok {
-				f.logger.WithContext(req.ctx).With().Info("delaying hash request",
-					log.Stringer("hash", msg.Hash),
-					log.String("hint", string(msg.Hint)),
-				)
-				f.unprocessed[msg.Hash] = req
-			} else {
-				f.logger.With().Error("ongoing request missing",
-					log.Stringer("hash", msg.Hash),
-					log.String("hint", string(msg.Hint)),
-				)
-			}
-		}
-		return
-	}
-
 	for peer, peerBatches := range peer2batches {
 		for _, reqs := range peerBatches {
 			batch := &batchInfo{
@@ -477,7 +457,21 @@ func (f *Fetch) organizeRequests(requests []RequestMessage) map[p2p.Peer][][]Req
 	peers := f.host.GetPeers()
 	if len(peers) == 0 {
 		f.logger.Info("cannot send batch: no peers found")
-		// in loop() we will try again after the batchTimeout
+		f.mu.Lock()
+		defer f.mu.Unlock()
+		errNoPeer := errors.New("no peers")
+		for _, msg := range requests {
+			if req, ok := f.ongoing[msg.Hash]; ok {
+				req.promise.err = errNoPeer
+				close(req.promise.completed)
+				delete(f.ongoing, req.hash)
+			} else {
+				f.logger.With().Error("ongoing request missing",
+					log.Stringer("hash", msg.Hash),
+					log.String("hint", string(msg.Hint)),
+				)
+			}
+		}
 		return nil
 	}
 
