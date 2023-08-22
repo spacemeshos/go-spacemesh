abc8d0a7bc499c991bf9c45e65f4d9b4c16263cd
Dmitry <yashulyak@gmail.com>
2023-08-22 06:39:31 +0200

    remove config option

--- p2p/host.go
+++ p2p/host.go
@@ -81,7 +81,6 @@ type Config struct {
 	Bootnode                 bool        `mapstructure:"p2p-bootnode"`
 	ForceReachability        string      `mapstructure:"p2p-reachability"`
 	EnableHolepunching       bool        `mapstructure:"p2p-holepunching"`
-	DisableLegacyDiscovery   bool        `mapstructure:"p2p-disable-legacy-discovery"`
 	PrivateNetwork           bool        `mapstructure:"p2p-private-network"`
 	RelayServer              RelayServer `mapstructure:"relay-server"`
 }
