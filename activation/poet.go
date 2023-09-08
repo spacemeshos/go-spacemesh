ae739f22012bad94c41000419801260570181753
Bartosz Różański <bartek.roza@gmail.com>
2023-09-08 16:25:08 +0200

    Avoid formatting log args when not needed

--- activation/poet.go
+++ activation/poet.go
@@ -61,19 +61,19 @@ type retryableHttpLogger struct {
 }
 
 func (r retryableHttpLogger) Error(format string, args ...any) {
-	r.inner.Sugar().With(args...).Error(format)
+	r.inner.Sugar().Errorw(format, args...)
 }
 
 func (r retryableHttpLogger) Info(format string, args ...any) {
-	r.inner.Sugar().With(args...).Info(format)
+	r.inner.Sugar().Infow(format, args...)
 }
 
 func (r retryableHttpLogger) Warn(format string, args ...any) {
-	r.inner.Sugar().With(args...).Warn(format)
+	r.inner.Sugar().Warnw(format, args...)
 }
 
 func (r retryableHttpLogger) Debug(format string, args ...any) {
-	r.inner.Sugar().With(args...).Debug(format)
+	r.inner.Sugar().Debugw(format, args...)
 }
 
 type PoetClientOpts func(*HTTPPoetClient)
