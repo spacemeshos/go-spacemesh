2c38a2b7470e92a1f75a83bfbf4c1b12347a2314
Dmitry <yashulyak@gmail.com>
2023-09-25 10:06:23 +0200

    change default p2p log level to warn

--- config/logging.go
+++ config/logging.go
@@ -52,7 +52,7 @@ func DefaultLoggingConfig() LoggerConfig {
 		Encoder:                   ConsoleLogEncoder,
 		AppLoggerLevel:            defaultLoggingLevel.String(),
 		GrpcLoggerLevel:           defaultLoggingLevel.String(),
-		P2PLoggerLevel:            defaultLoggingLevel.String(),
+		P2PLoggerLevel:            zapcore.WarnLevel.String(),
 		PostLoggerLevel:           defaultLoggingLevel.String(),
 		StateDbLoggerLevel:        defaultLoggingLevel.String(),
 		StateLoggerLevel:          defaultLoggingLevel.String(),
