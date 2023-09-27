b7e8b1c84270f2645b1dc9e0f93e6c0f2c529e07
Dmitry <yashulyak@gmail.com>
2023-09-27 14:55:53 +0200

    generator doesn't need to recompute eligibility count and loop over all atxs

--- proposals/util/util.go
+++ proposals/util/util.go
@@ -3,13 +3,8 @@ package util
 import (
 	"encoding/binary"
 	"errors"
-	"fmt"
-	"math/big"
 
 	"github.com/spacemeshos/go-spacemesh/common/types"
-	"github.com/spacemeshos/go-spacemesh/datastore"
-	"github.com/spacemeshos/go-spacemesh/sql/activesets"
-	"github.com/spacemeshos/go-spacemesh/sql/ballots"
 )
 
 var (
@@ -40,56 +35,4 @@ func GetNumEligibleSlots(weight, minWeight, totalWeight uint64, committeeSize, l
 		numEligible = 1
 	}
 	return uint32(numEligible), nil
-}
-
-// ComputeWeightPerEligibility computes the ballot weight per eligibility w.r.t the active set recorded in its reference ballot.
-func ComputeWeightPerEligibility(
-	cdb *datastore.CachedDB,
-	ballot *types.Ballot,
-) (*big.Rat, error) {
-	var (
-		refBallot = ballot
-		hdr       *types.ActivationTxHeader
-		err       error
-		atxWeight uint64
-	)
-	if ballot.EpochData == nil {
-		if ballot.RefBallot == types.EmptyBallotID {
-			return nil, fmt.Errorf("%w: empty ref ballot but no epoch data %s", ErrBadBallotData, ballot.ID())
-		}
-		refBallot, err = ballots.Get(cdb, ballot.RefBallot)
-		if err != nil {
-			return nil, fmt.Errorf("%w: missing ref ballot %s (for %s)", err, ballot.RefBallot, ballot.ID())
-		}
-	}
-	if refBallot.EpochData == nil {
-		return nil, fmt.Errorf("epoch data is nil on ballot %d/%s", refBallot.Layer, refBallot.ID())
-	}
-	if refBallot.EpochData.EligibilityCount == 0 {
-		return nil, fmt.Errorf("eligibility count is 0 on ballot %d/%s", refBallot.Layer, refBallot.ID())
-	}
-	actives, err := activesets.Get(cdb, refBallot.EpochData.ActiveSetHash)
-	if err != nil {
-		return nil, fmt.Errorf("get active set %s (%s)", refBallot.EpochData.ActiveSetHash.ShortString(), refBallot.ID())
-	}
-	if len(actives.Set) == 0 {
-		return nil, fmt.Errorf("empty active set %s (%s)", refBallot.EpochData.ActiveSetHash.ShortString(), refBallot.ID())
-	}
-	for _, atxID := range actives.Set {
-		hdr, err = cdb.GetAtxHeader(atxID)
-		if err != nil {
-			return nil, fmt.Errorf("%w: missing atx %s in active set of %s (for %s)", err, atxID, refBallot.ID(), ballot.ID())
-		}
-		if atxID == ballot.AtxID {
-			atxWeight = hdr.GetWeight()
-			break
-		}
-	}
-	if atxWeight == 0 {
-		return nil, fmt.Errorf("atx id %v is not found in the active set of the reference ballot %v with atxid %v", ballot.AtxID, refBallot.ID(), refBallot.AtxID)
-	}
-	return new(big.Rat).SetFrac(
-		new(big.Int).SetUint64(atxWeight),
-		new(big.Int).SetUint64(uint64(refBallot.EpochData.EligibilityCount)),
-	), nil
 }
