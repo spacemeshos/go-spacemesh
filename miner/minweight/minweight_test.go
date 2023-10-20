04832b5fa7ce408314b67817eac3a8eb87cea35e
Dmitry <yashulyak@gmail.com>
2023-10-20 08:29:10 +0200

    add a couple more test cases

--- miner/minweight/minweight_test.go
+++ miner/minweight/minweight_test.go
@@ -19,6 +19,22 @@ func TestSelect(t *testing.T) {
 			{Epoch: 7, Weight: 11},
 		}))
 	})
+	t.Run("in-between epochs", func(t *testing.T) {
+		require.EqualValues(t, 10, Select(6, []types.EpochMinimalActiveWeight{
+			{Epoch: 0, Weight: 1},
+			{Epoch: 4, Weight: 5},
+			{Epoch: 5, Weight: 10},
+			{Epoch: 7, Weight: 11},
+		}))
+	})
+	t.Run("after all", func(t *testing.T) {
+		require.EqualValues(t, 11, Select(10, []types.EpochMinimalActiveWeight{
+			{Epoch: 0, Weight: 1},
+			{Epoch: 4, Weight: 5},
+			{Epoch: 5, Weight: 10},
+			{Epoch: 7, Weight: 11},
+		}))
+	})
 	t.Run("not sorted panic", func(t *testing.T) {
 		require.Panics(t, func() {
 			Select(5, []types.EpochMinimalActiveWeight{
