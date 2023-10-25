4357342c32c17b305928aaff9fda41c586a88584
Matthias <5011972+fasmat@users.noreply.github.com>
2023-10-25 15:02:42 +0000

    Add clarifying comment

--- activation/post_supervisor_nix.go
+++ activation/post_supervisor_nix.go
@@ -26,6 +26,10 @@ func (g ProcessExitGroup) Dispose() error {
 
 // StartCommand starts the given command and adds it to the ProcessExitGroup.
 func (g ProcessExitGroup) StartCommand(cmd *exec.Cmd) error {
+	// Note: this doesn't actually ensure that the child process will be killed when the
+	// parent process is killed, it only assigns it the same process group ID.
+	//
+	// Getting the same behavior on Linux and Mac requires the child process to listen
 	cmd.SysProcAttr = &syscall.SysProcAttr{Setpgid: true, Pgid: os.Getpid()}
 	if err := cmd.Start(); err != nil {
 		return err
