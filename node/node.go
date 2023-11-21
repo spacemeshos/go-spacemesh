32d87fd5f7ba79a369100791d871dc8b7533d79b
Ivan Shvedunov <ivan4th@gmail.com>
2023-11-21 13:16:39 +0400

    Don't allow builds with "...nomain" git tag to run on mainnet
    The check is done by Makefile. In case if the current version string
    contains `nomain` substring, -X main.noMainNet=true ldflag is used.

--- node/node.go
+++ node/node.go
@@ -131,6 +131,11 @@ func GetCommand() *cobra.Command {
 			if conf.LOGGING.Encoder == config.JSONLogEncoder {
 				log.JSONLog(true)
 			}
+
+			if cmd.NoMainNet && onMainNet(conf) {
+				log.With().Fatal("this is a testnet-only build not intended for mainnet")
+			}
+
 			app := New(
 				WithConfig(conf),
 				// NOTE(dshulyak) this needs to be max level so that child logger can can be current level or below.
@@ -1834,4 +1839,8 @@ func (w tortoiseWeakCoin) Set(lid types.LayerID, value bool) error {
 	}
 	w.tortoise.OnWeakCoin(lid, value)
 	return nil
+}
+
+func onMainNet(conf *config.Config) bool {
+	return conf.Genesis.GenesisTime == config.MainnetConfig().Genesis.GenesisTime
 }
