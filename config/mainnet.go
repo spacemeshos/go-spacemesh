22733d671cb7ba73c97d64046280b5abaf0b02da
Jędrzej Nowak <pigmej@gmail.com>
2024-03-04 10:28:20 +0000

    Removed mainnet-poet-2 from code (#5640)
    This poet is EOL.

--- config/mainnet.go
+++ config/mainnet.go
@@ -93,10 +93,6 @@ func MainnetConfig() Config {
 					Pubkey:  types.MustBase64FromString("Qh1efxY4YhoYBEXKPTiHJ/a7n1GsllRSyweQKO3j7m0="),
 				},
 				{
-					Address: "https://mainnet-poet-2.spacemesh.network",
-					Pubkey:  types.MustBase64FromString("8RXEI0MwO3uJUINFFlOm/uTjJCneV9FidMpXmn55G8Y="),
-				},
-				{
 					Address: "https://poet-110.spacemesh.network",
 					Pubkey:  types.MustBase64FromString("8Qqgid+37eyY7ik+EA47Nd5TrQjXolbv2Mdgir243No="),
 				},
