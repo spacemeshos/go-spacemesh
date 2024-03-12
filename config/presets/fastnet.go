e7cbeef082d3ef032e473960abd2e891491d86ae
Dmitry <yashulyak@gmail.com>
2024-03-12 07:34:18 +0100

    metrics were enabled

--- config/presets/fastnet.go
+++ config/presets/fastnet.go
@@ -54,7 +54,6 @@ func fastnet() config.Config {
 	conf.Sync.AtxSync.EpochInfoInterval = 20 * time.Second
 	conf.LayersPerEpoch = 4
 	conf.RegossipAtxInterval = 30 * time.Second
-	conf.FETCH.EnableServerMetrics = true
 
 	conf.Tortoise.Hdist = 4
 	conf.Tortoise.Zdist = 2
