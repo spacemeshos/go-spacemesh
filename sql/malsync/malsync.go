1a4f6a7550ecd23d52ab5931576ac53b94f8907f
Ivan Shvedunov <ivan4th@gmail.com>
2024-03-14 09:55:11 +0400

    sql: add malfeasance sync state

---
+++ sql/malsync/malsync.go
@@ -0,0 +1,37 @@
+package malsync
+
+import (
+	"fmt"
+	"time"
+
+	"github.com/spacemeshos/go-spacemesh/sql"
+)
+
+func GetSyncState(db sql.Executor) (time.Time, error) {
+	var timestamp time.Time
+	rows, err := db.Exec("select timestamp from malfeasance_sync_state",
+		nil, func(stmt *sql.Statement) bool {
+			v := stmt.ColumnInt64(0)
+			if v > 0 {
+				timestamp = time.Unix(v, 0)
+			}
+			return true
+		})
+	if err != nil {
+		return time.Time{}, fmt.Errorf("error getting malfeasance sync state: %w", err)
+	} else if rows != 1 {
+		return time.Time{}, fmt.Errorf("expected malfeasance_sync_state to have 1 row but got %d rows", rows)
+	}
+	return timestamp, nil
+}
+
+func UpdateSyncState(db sql.Executor, timestamp time.Time) error {
+	_, err := db.Exec("update malfeasance_sync_state set timestamp = ?1",
+		func(stmt *sql.Statement) {
+			stmt.BindInt64(1, timestamp.Unix())
+		}, nil)
+	if err != nil {
+		return fmt.Errorf("error updating malfeasance sync state: %w", err)
+	}
+	return nil
+}
---
+++ sql/migrations/local/0006_malfeasance_sync.sql
@@ -0,0 +1,6 @@
+CREATE TABLE malfeasance_sync_state
+(
+  timestamp INT NOT NULL
+);
+
+INSERT INTO malfeasance_sync_state (timestamp) VALUES (0);
