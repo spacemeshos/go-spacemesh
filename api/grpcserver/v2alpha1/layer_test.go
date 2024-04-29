0cec7c89da56185a59c58f8a53be665f48c23ea1
Kacper Sawicki <kacper@ksawicki.pl>
2024-04-29 13:49:10 +0200

    Fix lint

--- api/grpcserver/v2alpha1/layer_test.go
+++ api/grpcserver/v2alpha1/layer_test.go
@@ -3,13 +3,14 @@ package v2alpha1
 import (
 	"context"
 	"errors"
+	"io"
+	"testing"
+
 	spacemeshv2alpha1 "github.com/spacemeshos/api/release/go/spacemesh/v2alpha1"
 	"github.com/stretchr/testify/assert"
 	"github.com/stretchr/testify/require"
 	"google.golang.org/grpc/codes"
 	"google.golang.org/grpc/status"
-	"io"
-	"testing"
 
 	"github.com/spacemeshos/go-spacemesh/common/types"
 	"github.com/spacemeshos/go-spacemesh/events"
