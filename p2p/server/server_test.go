72f33ea91dfd14ac2ad01e86349a515300102d57
Ivan Shvedunov <ivan4th@gmail.com>
2024-05-24 13:43:39 +0400

    p2p: server: fix import

--- p2p/server/server_test.go
+++ p2p/server/server_test.go
@@ -11,6 +11,7 @@ import (
 	"github.com/spacemeshos/go-scale/tester"
 	"github.com/stretchr/testify/assert"
 	"github.com/stretchr/testify/require"
+	"go.uber.org/zap"
 	"go.uber.org/zap/zaptest"
 	"golang.org/x/sync/errgroup"
 )
