625717453764806ff9dc5c87079c53aefbe3727c
Ivan Shvedunov <ivan4th@gmail.com>
2024-06-05 09:05:41 +0400

    config: fix build errors

--- config/presets/testnet.go
+++ config/presets/testnet.go
@@ -65,7 +65,7 @@ func testnet() config.Config {
 			DatabaseConnections:          16,
 			DatabaseSizeMeteringInterval: 10 * time.Minute,
 			DatabasePruneInterval:        30 * time.Minute,
-			DatabaseIgnoreTableRx:        "^_litestream",
+			DatabaseSchemaIgnoreRx:       "^_litestream",
 			NetworkHRP:                   "stest",
 
 			LayerDuration:  5 * time.Minute,
