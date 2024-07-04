ec6e1d5ed6a50b106d42cf7131099b497c98571a
Bartosz Różański <bartek.roza@gmail.com>
2024-07-04 09:36:11 +0200

    don't modify released migration

--- sql/migrations/state/0019_marriages.sql
+++ sql/migrations/state/0019_marriages.sql
@@ -1,2 +1,1 @@
 ALTER TABLE identities ADD COLUMN marriage_atx CHAR(32);
-ALTER TABLE identities ADD COLUMN marriage_idx INTEGER;
--- sql/migrations/state/0020_atx_merge.sql
+++ sql/migrations/state/0020_atx_merge.sql
@@ -1,2 +1,6 @@
+-- Changes required to handle merged ATXs
+
 ALTER TABLE atxs ADD COLUMN weight INTEGER;
 UPDATE atxs SET weight = effective_num_units * tick_count;
+
+ALTER TABLE identities ADD COLUMN marriage_idx INTEGER;
