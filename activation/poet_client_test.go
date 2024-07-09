ee2e676e0e7f967cabe340818e8c7503b1c247c9
Bartosz Różański <bartek.roza@gmail.com>
2024-07-09 16:19:56 +0000

    cache poet certifier info (#6107)
    ## Motivation
    
    To avoid querying the same poet `/v1/info` endpoint many times in the 1:N setups.

--- activation/poet_client_test.go
+++ activation/poet_client_test.go
@@ -20,6 +20,7 @@ import (
 
 	"github.com/spacemeshos/go-spacemesh/common/types"
 	"github.com/spacemeshos/go-spacemesh/signing"
+	"github.com/spacemeshos/go-spacemesh/sql"
 	"github.com/spacemeshos/go-spacemesh/sql/localsql/certifier"
 )
 
@@ -386,4 +387,37 @@ func TestPoetClient_RecertifiesOnAuthFailure(t *testing.T) {
 	_, err = poet.Submit(context.Background(), time.Time{}, nil, nil, types.RandomEdSignature(), sig.NodeID())
 	require.NoError(t, err)
 	require.Equal(t, 2, submitCount)
+}
+
+func TestPoetService_CachesCertifierInfo(t *testing.T) {
+	t.Parallel()
+	type test struct {
+		name string
+		ttl  time.Duration
+	}
+	for _, tc := range []test{
+		{name: "cache enabled", ttl: time.Hour},
+		{name: "cache disabled"},
+	} {
+		t.Run(tc.name, func(t *testing.T) {
+			t.Parallel()
+			cfg := DefaultPoetConfig()
+			cfg.CertifierInfoCacheTTL = tc.ttl
+			client := NewMockPoetClient(gomock.NewController(t))
+			db := NewPoetDb(sql.InMemory(), zaptest.NewLogger(t))
+			poet := NewPoetServiceWithClient(db, client, cfg, zaptest.NewLogger(t))
+			url := &url.URL{Host: "certifier.hello"}
+			pubkey := []byte("pubkey")
+			exp := client.EXPECT().CertifierInfo(gomock.Any()).Return(url, pubkey, nil)
+			if tc.ttl == 0 {
+				exp.Times(5)
+			}
+			for range 5 {
+				gotUrl, gotPubkey, err := poet.getCertifierInfo(context.Background())
+				require.NoError(t, err)
+				require.Equal(t, url, gotUrl)
+				require.Equal(t, pubkey, gotPubkey)
+			}
+		})
+	}
 }
