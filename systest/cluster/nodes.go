b3420ac8c9414cd048dbbab71b9074c4cd0b02c6
Ivan Shvedunov <ivan4th@gmail.com>
2024-07-10 01:17:25 +0400

    testing: adjust resources

--- systest/cluster/nodes.go
+++ systest/cluster/nodes.go
@@ -62,8 +62,8 @@ var (
 		"requests and limits for smesher container",
 		&apiv1.ResourceRequirements{
 			Requests: apiv1.ResourceList{
-				apiv1.ResourceCPU:    resource.MustParse("0.4"),
-				apiv1.ResourceMemory: resource.MustParse("400Mi"),
+				apiv1.ResourceCPU:    resource.MustParse("2"),
+				apiv1.ResourceMemory: resource.MustParse("1Gi"),
 			},
 			Limits: apiv1.ResourceList{
 				apiv1.ResourceCPU:    resource.MustParse("2"),
