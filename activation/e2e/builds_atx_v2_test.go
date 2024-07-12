57f40e259296e1437777879fbf463fa31daceae1
Bartosz Różański <bartek.roza@gmail.com>
2024-07-12 11:17:53 +0200

    fix waiting for ATXs published in test

--- activation/e2e/builds_atx_v2_test.go
+++ activation/e2e/builds_atx_v2_test.go
@@ -2,6 +2,7 @@ package activation_test
 
 import (
 	"context"
+	"sync"
 	"testing"
 	"time"
 
@@ -135,6 +136,8 @@ func TestBuilder_SwitchesToBuildV2(t *testing.T) {
 	)
 
 	var previous *types.ActivationTx
+	var expectedAtxs sync.WaitGroup
+	expectedAtxs.Add(3)
 	gomock.InOrder(
 		mpub.EXPECT().Publish(gomock.Any(), pubsub.AtxProtocol, gomock.Any()).DoAndReturn(
 			func(ctx context.Context, _ string, msg []byte) error {
@@ -157,6 +160,7 @@ func TestBuilder_SwitchesToBuildV2(t *testing.T) {
 				atx, err := atxs.Get(db, watx.ID())
 				require.NoError(t, err)
 				previous = atx
+				expectedAtxs.Done()
 				return nil
 			},
 		),
@@ -189,6 +193,7 @@ func TestBuilder_SwitchesToBuildV2(t *testing.T) {
 				require.NotZero(t, atx.TickHeight())
 				require.Equal(t, opts.NumUnits, atx.NumUnits)
 				previous = atx
+				expectedAtxs.Done()
 				return nil
 			},
 		).Times(2),
@@ -211,6 +216,6 @@ func TestBuilder_SwitchesToBuildV2(t *testing.T) {
 	tab.Register(sig)
 
 	require.NoError(t, tab.StartSmeshing(coinbase))
-	require.Eventually(t, ctrl.Satisfied, epoch*4, time.Millisecond*100)
+	require.Eventually(t, func() bool { expectedAtxs.Wait(); return true }, epoch*4, time.Millisecond*100)
 	require.NoError(t, tab.StopSmeshing(false))
 }
