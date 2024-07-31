7293438038f149f1870992f11e81064db341a58a
acud <12988138+acud@users.noreply.github.com>
2024-07-31 09:01:03 -0600

    change atx sync settings

--- config/presets/fastnet.go
+++ config/presets/fastnet.go
@@ -51,7 +51,8 @@ func fastnet() config.Config {
 	conf.LayerDuration = 15 * time.Second
 	conf.Sync.Interval = 5 * time.Second
 	conf.Sync.GossipDuration = 10 * time.Second
-	conf.Sync.AtxSync.EpochInfoInterval = 20 * time.Second
+	conf.Sync.AtxSync.EpochInfoInterval = 10 * time.Second
+	conf.Sync.AtxSync.EpochInfoPeers = 5
 	conf.Sync.MalSync.IDRequestInterval = 20 * time.Second
 	conf.LayersPerEpoch = 4
 	conf.RegossipAtxInterval = 30 * time.Second
