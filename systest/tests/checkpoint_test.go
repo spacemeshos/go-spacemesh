7ee142b7376d8f974758d7e8f39b7f4a96b43595
acud <12988138+acud@users.noreply.github.com>
2024-08-01 11:01:08 -0600

    t p

--- systest/tests/checkpoint_test.go
+++ systest/tests/checkpoint_test.go
@@ -32,20 +32,20 @@ func reuseCluster(tctx *testcontext.Context, restoreLayer uint32) (*cluster.Clus
 	)
 }
 
-//func TestCheckpoint1(t *testing.T) {
-//// t.Parallel()
-//TestCheckpoint(t)
-//}
+func TestCheckpoint1(t *testing.T) {
+	t.Parallel()
+	TestCheckpoint(t)
+}
 
-//func TestCheckpoint2(t *testing.T) {
-//// t.Parallel()
-//TestCheckpoint(t)
-//}
+func TestCheckpoint2(t *testing.T) {
+	t.Parallel()
+	TestCheckpoint(t)
+}
 
-//func TestCheckpoint3(t *testing.T) {
-//// t.Parallel()
-//TestCheckpoint(t)
-//}
+func TestCheckpoint3(t *testing.T) {
+	t.Parallel()
+	TestCheckpoint(t)
+}
 
 func TestCheckpoint(t *testing.T) {
 	// TODO(mafa): add new test with multi-smeshing nodes
