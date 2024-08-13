6703062ed1eac388c44df5a9a215bd20c9dad27d
Matthias <5011972+fasmat@users.noreply.github.com>
2024-08-13 15:38:53 +0000

    Remove obsolete log package functions

--- bootstrap/types.go
+++ bootstrap/types.go
@@ -2,7 +2,7 @@ package bootstrap
 
 import (
 	"github.com/spacemeshos/go-spacemesh/common/types"
-	"github.com/spacemeshos/go-spacemesh/log"
+	"go.uber.org/zap/zapcore"
 )
 
 type Update struct {
@@ -31,7 +31,7 @@ type EpochOverride struct {
 	ActiveSet []types.ATXID
 }
 
-func (vd *VerifiedUpdate) MarshalLogObject(encoder log.ObjectEncoder) error {
+func (vd *VerifiedUpdate) MarshalLogObject(encoder zapcore.ObjectEncoder) error {
 	encoder.AddString("persisted", vd.Persisted)
 	encoder.AddString("epoch", vd.Data.Epoch.String())
 	encoder.AddString("beacon", vd.Data.Beacon.String())
