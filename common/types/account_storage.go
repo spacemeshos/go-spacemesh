e1fff7afbaec905827ebe238539e44b07d3a48bd
Lane Rettig <lanerettig@gmail.com>
2024-10-08 11:42:43 -0700

    Account-related changes

---
+++ common/types/account_storage.go
@@ -0,0 +1,10 @@
+package types
+
+//go:generate scalegen
+
+// StorageItem represents a single item of account storage.
+type StorageItem struct {
+	// not actually a hash, just 32 bytes
+	Key   Hash32
+	Value Hash32
+}
