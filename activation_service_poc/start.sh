855ee42cece9ff41cb33e801e153ba1cfb778540
acud <12988138+acud@users.noreply.github.com>
2024-10-31 10:28:27 -0600

    jq

--- activation_service_poc/start.sh
+++ activation_service_poc/start.sh
@@ -7,8 +7,8 @@ cd activation_service_poc
 export IMAGE=$(docker images | head -n 2 | tail -n 1 | awk '{print $3}')
 
 TIME=$(date -u -d '2 minutes' "+%Y-%m-%dT%H:%M:%S%:z")
-sed -i "s/\"genesis-time\".*/\"genesis-time\"\:\"$TIME\"/g" config.standalone.client.json
-sed -i "s/\"genesis-time\".*/\"genesis-time\"\:\"$TIME\"/g" config.standalone.node-service.json
+jq ".genesis.\"genesis-time\" |= \"$TIME\"" config.standalone.client.json
+jq ".genesis.\"genesis-time\" |= \"$TIME\"" config.standalone.node-service.json
 
 rm -rf /tmp/spacemesh*
 docker compose up
