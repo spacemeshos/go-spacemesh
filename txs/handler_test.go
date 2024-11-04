d48c6a431d8909cdda4a67e3115d82e669059cbd
Lane Rettig <lanerettig@gmail.com>
2024-11-04 13:02:18 -0800

    txs, api tests passing

--- txs/handler_test.go
+++ txs/handler_test.go
@@ -37,8 +37,9 @@ func Test_WrongHash(t *testing.T) {
 	require.ErrorIs(t, err, errWrongHash)
 	require.ErrorIs(t, err, pubsub.ErrValidationReject)
 	cstate.EXPECT().GetMeshTransaction(tx.ID).Return(nil, nil)
-	req := smocks.NewMockValidationRequest(ctrl)
-	req.EXPECT().Parse().Times(1).Return(tx.TxHeader, nil)
+	req := smocks.NewMockValidationRequestNew(ctrl)
+	req.EXPECT().Cache().Times(1).Return(nil)
+	req.EXPECT().Parse(gomock.Any()).Times(1).Return(tx.TxHeader, nil)
 	cstate.EXPECT().Validation(tx.RawTx).Times(1).Return(req)
 	err = th.HandleProposalTransaction(context.Background(), types.RandomHash(), p2p.NoPeer, tx.Raw)
 	require.ErrorIs(t, err, errWrongHash)
@@ -97,8 +98,9 @@ func Test_HandleBlock(t *testing.T) {
 			tx := newTx(t, 3, 10, tc.fee, signer)
 			cstate.EXPECT().HasTx(tx.ID).Return(tc.has, tc.hasErr).Times(1)
 			if tc.hasErr == nil && !tc.has {
-				req := smocks.NewMockValidationRequest(ctrl)
-				req.EXPECT().Parse().Times(1).Return(tx.TxHeader, tc.parseErr)
+				req := smocks.NewMockValidationRequestNew(ctrl)
+				req.EXPECT().Cache().Times(1).Return(nil)
+				req.EXPECT().Parse(gomock.Any()).Times(1).Return(tx.TxHeader, tc.parseErr)
 				cstate.EXPECT().Validation(tx.RawTx).Times(1).Return(req)
 				if tc.parseErr == nil {
 					req.EXPECT().Verify().Times(1).Return(true)
@@ -145,8 +147,9 @@ func gossipExpectations(
 	}
 	cstate.EXPECT().GetMeshTransaction(tx.ID).Return(rst, hasErr).Times(1)
 	if hasErr == nil && !has {
-		req := smocks.NewMockValidationRequest(ctrl)
-		req.EXPECT().Parse().Times(1).Return(tx.TxHeader, parseErr)
+		req := smocks.NewMockValidationRequestNew(ctrl)
+		req.EXPECT().Parse(gomock.Any()).Times(1).Return(tx.TxHeader, parseErr)
+		req.EXPECT().Cache().Times(1).Return(nil)
 		cstate.EXPECT().Validation(tx.RawTx).Times(1).Return(req)
 		if parseErr == nil && fee != 0 {
 			req.EXPECT().Verify().Times(1).Return(verify)
