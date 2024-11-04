61f294ce739861955a109cc7c9565a30620e6026
Ivan Shvedunov <ivan4th@gmail.com>
2024-11-04 16:29:27 +0400

    node: fix db-allow-schema-drift handling for API db connection pool

--- node/node.go
+++ node/node.go
@@ -1998,6 +1998,7 @@ func (app *App) setupDBs(ctx context.Context, lg log.Log) error {
 		sql.WithReadOnly(),
 		sql.WithLogger(apiDBLog),
 		sql.WithConnections(app.Config.API.DatabaseConnections),
+		sql.WithAllowSchemaDrift(app.Config.DatabaseSchemaAllowDrift),
 		sql.WithMigrationsDisabled(),
 	)
 	if err != nil {
