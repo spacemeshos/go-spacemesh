b6e8d4cc4b234bc9aa9bc177c456af5177fbbd81
acud <12988138+acud@users.noreply.github.com>
2024-11-05 11:32:33 -0600

    start

--- activation_service_poc/start.sh
+++ activation_service_poc/start.sh
@@ -8,7 +8,6 @@ export IMAGE=$(docker images | head -n 2 | tail -n 1 | awk '{print $3}')
 
 TIME=$(date -u -d '2 minutes' "+%Y-%m-%dT%H:%M:%S%:z")
 for file in config.standalone.client.json config.standalone.node-service.json;do
-  # Run your command on each file
   jq ".genesis.\"genesis-time\" |= \"$TIME\"" "$file" > temp.json && mv temp.json "$file"
 done
 
