7882b98173a73b18626220baadf8296cb777002a
acud <12988138+acud@users.noreply.github.com>
2024-11-05 09:44:35 -0600

    compose-setup

--- activation_service_poc/start.sh
+++ activation_service_poc/start.sh
@@ -7,8 +7,8 @@ cd activation_service_poc
 export IMAGE=$(docker images | head -n 2 | tail -n 1 | awk '{print $3}')
 
 TIME=$(date -u -d '2 minutes' "+%Y-%m-%dT%H:%M:%S%:z")
-jq ".genesis.\"genesis-time\" |= \"$TIME\"" config.standalone.client.json
-jq ".genesis.\"genesis-time\" |= \"$TIME\"" config.standalone.node-service.json
+jq ".genesis.\"genesis-time\" |= \"$TIME\"" config.standalone.client.json > config.standalone.client.json
+jq ".genesis.\"genesis-time\" |= \"$TIME\"" config.standalone.node-service.json > config.standalone.node-service.json
 
 rm -rf /tmp/space*
 docker compose up
