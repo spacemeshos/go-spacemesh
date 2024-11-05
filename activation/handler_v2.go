2e1619981b1cb38570a2e107f6d2500e7b42250e
Matthias <5011972+fasmat@users.noreply.github.com>
2024-11-05 18:00:21 +0000

    Simplify check

--- activation/handler_v2.go
+++ activation/handler_v2.go
@@ -679,10 +679,8 @@ func (h *HandlerV2) publishInvalidPostProof(
 		zap.Stringer("id", atx.ID()),
 		zap.Uint32("index", invalidPostIndex),
 	)
-	var initialAtx *wire.ActivationTxV2
-	if atx.Initial != nil {
-		initialAtx = atx
-	} else {
+	initialAtx := atx
+	if initialAtx.Initial == nil {
 		initialID, err := atxs.GetFirstIDByNodeID(h.cdb, nodeID)
 		if err != nil {
 			return fmt.Errorf("fetch initial ATX for ID %s: %w", nodeID.ShortString(), err)
