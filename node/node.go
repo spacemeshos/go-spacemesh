be6c4b1d426dce64bb3facb6e62eb2ff3e988c8e
Kacper Sawicki <kacper@ksawicki.pl>
2024-11-18 13:17:20 +0100

    Remove commented code

--- node/node.go
+++ node/node.go
@@ -1674,16 +1674,6 @@ func (app *App) grpcService(svc grpcserver.Service, lg log.Log) (grpcserver.Serv
 		app.grpcServices[svc] = service
 		return service, nil
 	case v2alpha1.SmeshingIdentities:
-		//nodeIds := make(map[types.NodeID]struct{})
-		//for _, signer := range app.signers {
-		//	nodeIds[signer.NodeID()] = struct{}{}
-		//}
-		//
-		//configuredPoets := make(map[string]struct{})
-		//for _, server := range app.Config.PoetServers {
-		//	configuredPoets[server.Address] = struct{}{}
-		//}
-
 		service := v2alpha1.NewSmeshingIdentitiesService(app.idStates)
 		app.grpcServices[svc] = service
 		return service, nil
