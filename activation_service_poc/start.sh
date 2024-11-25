fb9ddcaa153f0064cf96e7adb2fdf8f7f7d72259
Piotr Skamruk <piotr.skamruk@gmail.com>
2024-11-25 15:42:50 +0100

    Tune poc startup script

--- activation_service_poc/start.sh
+++ activation_service_poc/start.sh
@@ -1,10 +1,8 @@
 #!/bin/bash
-#
-cd ..
-make dockerbuild-go
-cd activation_service_poc
 
-export IMAGE=$(docker images | head -n 2 | tail -n 1 | awk '{print $3}')
+make -C .. dockerbuild-go
+
+export IMAGE=$(docker images | awk 'FNR == 2 { print $3 }')
 
 TIME=$(date -u -d '2 minutes' "+%Y-%m-%dT%H:%M:%S%:z")
 for file in config.standalone.client.json config.standalone.node-service.json;do
