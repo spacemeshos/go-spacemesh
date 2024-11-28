992d3870d787e8dedbe95d3e4ef8ba8a08d6cf00
Matthias <5011972+fasmat@users.noreply.github.com>
2024-11-28 09:51:04 +0000

    Increase number of remote smeshers

--- systest/testcontext/context.go
+++ systest/testcontext/context.go
@@ -362,9 +362,9 @@ func New(t *testing.T, opts ...Opt) *Context {
 		Keep:              keep.Get(p),
 		ClusterSize:       clSize,
 		BootnodeSize:      max(2, (clSize/1000)*2),
-		RemoteSize:        0,
+		RemoteSize:        clSize / 2, // 50% of smeshers are remote
 		PoetSize:          poetSize.Get(p),
-		OldSize:           0,
+		OldSize:           clSize / 4, // 25% of smeshers are old (use previous version of go-spacemesh)
 		BootstrapperSize:  bsSize.Get(p),
 		Image:             imageFlag.Get(p),
 		OldImage:          oldImageFlag.Get(p),
