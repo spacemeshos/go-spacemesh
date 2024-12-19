a30a55c8066633788accc4e79027a4d220de11ce
Matthias <5011972+fasmat@users.noreply.github.com>
2024-12-19 11:58:07 +0000

    Review feedback

--- api/grpcserver/v2alpha1/malfeasance.go
+++ api/grpcserver/v2alpha1/malfeasance.go
@@ -195,11 +195,15 @@ func (s *MalfeasanceStreamService) Stream(
 		select {
 		// process pending events first
 		case rst := <-eventsOut:
-			proof := toProof(stream.Context(), s.infoLegacy, rst.Smesher)
+			proof := fetchMetaData(stream.Context(), s.infoLegacy, rst.Smesher)
 			if proof == nil {
 				// try again with the new handler
-				proof = toProof(stream.Context(), s.info, rst.Smesher)
+				proof = fetchMetaData(stream.Context(), s.info, rst.Smesher)
 				if proof == nil {
+					ctxzap.Debug(stream.Context(), "failed to get malfeasance info",
+						zap.String("smesher", rst.Smesher.String()),
+						zap.Error(err),
+					)
 					continue
 				}
 			}
@@ -213,11 +217,15 @@ func (s *MalfeasanceStreamService) Stream(
 		default:
 			select {
 			case rst := <-eventsOut:
-				proof := toProof(stream.Context(), s.infoLegacy, rst.Smesher)
+				proof := fetchMetaData(stream.Context(), s.infoLegacy, rst.Smesher)
 				if proof == nil {
 					// try again with the new handler
-					proof = toProof(stream.Context(), s.info, rst.Smesher)
+					proof = fetchMetaData(stream.Context(), s.info, rst.Smesher)
 					if proof == nil {
+						ctxzap.Debug(stream.Context(), "failed to get malfeasance info",
+							zap.String("smesher", rst.Smesher.String()),
+							zap.Error(err),
+						)
 						continue
 					}
 				}
@@ -237,17 +245,13 @@ func (s *MalfeasanceStreamService) Stream(
 	}
 }
 
-func toProof(
+func fetchMetaData(
 	ctx context.Context,
 	info malfeasanceInfo,
 	id types.NodeID,
 ) *spacemeshv2alpha1.MalfeasanceProof {
 	properties, err := info.Info(ctx, id)
 	if err != nil {
-		ctxzap.Debug(ctx, "failed to get malfeasance info",
-			zap.String("smesher", id.String()),
-			zap.Error(err),
-		)
 		return nil
 	}
 	domain, err := strconv.ParseUint(properties["domain"], 10, 64)
@@ -297,7 +301,7 @@ func fetchFromDB(
 	}
 	proofs := make([]*spacemeshv2alpha1.MalfeasanceProof, 0, len(ids))
 	for _, id := range ids {
-		rst := toProof(ctx, info, id)
+		rst := fetchMetaData(ctx, info, id)
 		if rst == nil {
 			continue
 		}
@@ -325,7 +329,7 @@ func fetchLegacyFromDB(
 	}
 	proofs := make([]*spacemeshv2alpha1.MalfeasanceProof, 0, len(ids))
 	for _, id := range ids {
-		rst := toProof(ctx, info, id)
+		rst := fetchMetaData(ctx, info, id)
 		if rst == nil {
 			continue
 		}
--- api/grpcserver/v2alpha1/v2alpha1_test.go
+++ api/grpcserver/v2alpha1/v2alpha1_test.go
@@ -1,15 +1,13 @@
 package v2alpha1
 
 import (
-	"context"
 	"testing"
 	"time"
 
 	"github.com/stretchr/testify/assert"
 	"github.com/stretchr/testify/require"
-	"go.uber.org/zap"
+	"go.uber.org/zap/zaptest"
 	"google.golang.org/grpc"
-	"google.golang.org/grpc/connectivity"
 	"google.golang.org/grpc/credentials/insecure"
 
 	"github.com/spacemeshos/go-spacemesh/api/grpcserver"
@@ -22,9 +20,7 @@ const (
 
 func launchServer(tb testing.TB, services ...grpcserver.ServiceAPI) (grpcserver.Config, func()) {
 	cfg := grpcserver.DefaultTestConfig()
-	// logger := zaptest.NewLogger(tb).Named("grpc")
-	logger := zap.NewNop()
-	grpc, err := grpcserver.NewWithServices(cfg.PublicListener, logger, cfg, services)
+	grpc, err := grpcserver.NewWithServices(cfg.PublicListener, zaptest.NewLogger(tb).Named("grpc"), cfg, services)
 	require.NoError(tb, err)
 
 	// start gRPC server
@@ -43,24 +39,6 @@ func dialGrpc(tb testing.TB, cfg grpcserver.Config) *grpc.ClientConn {
 		grpc.WithTransportCredentials(insecure.NewCredentials()),
 	)
 	require.NoError(tb, err)
-
-	// block until the clientConn is ready.
-	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
-	defer cancel()
-	for {
-		s := conn.GetState()
-		if s == connectivity.Ready {
-			break
-		}
-		if s == connectivity.Idle {
-			conn.Connect()
-		}
-		if !conn.WaitForStateChange(ctx, s) {
-			tb.Fatalf("timeout waiting for connection to %s", conn.Target())
-			return nil
-		}
-	}
-
 	tb.Cleanup(func() { require.NoError(tb, conn.Close()) })
 	return conn
 }
