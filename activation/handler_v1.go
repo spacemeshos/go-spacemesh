cbc29a3b8bf4874c9cce10237832ffcb3e29bae6
Matthias <5011972+fasmat@users.noreply.github.com>
2024-12-20 19:36:15 +0000

    Fix failing tests

--- activation/handler_v1.go
+++ activation/handler_v1.go
@@ -504,22 +504,14 @@ func (h *HandlerV1) storeAtx(ctx context.Context, atx *types.ActivationTx, watx 
 	}
 
 	atxs.AtxAdded(h.cdb, atx)
-	if proof != nil {
-		if err := h.malPublisher.PublishProof(ctx, atx.SmesherID, proof); err != nil {
-			return fmt.Errorf("publishing malfeasance proof: %w", err)
-		}
-	}
-
 	h.beacon.OnAtx(atx)
 	if added := h.cacheAtx(ctx, atx, malicious || proof != nil); added != nil {
 		h.tortoise.OnAtx(atx.TargetEpoch(), atx.ID(), added)
 	}
-
 	h.logger.Debug("finished storing atx in epoch",
 		zap.Stringer("atx_id", atx.ID()),
 		zap.Uint32("epoch_id", atx.PublishEpoch.Uint32()),
 	)
-
 	if proof != nil {
 		if err := h.malPublisher.PublishProof(ctx, atx.SmesherID, proof); err != nil {
 			return fmt.Errorf("publishing malfeasance proof: %w", err)
