c2c3e3fec9ebc97d6f95cd255d11c695599b190f
Matthias <5011972+fasmat@users.noreply.github.com>
2025-01-02 17:37:56 +0000

    Fix typo

--- activation/handler.go
+++ activation/handler.go
@@ -126,7 +126,7 @@ func NewHandler(
 			beacon:          beacon,
 			tortoise:        tortoise,
 			malPublisher:    legacyMalPublisher,
-			malPublisher2:   &MalfeasancePublisher{}, // TODO(mafa): pass real publisher when available
+			malPublisher2:   &MalfeasanceHandlerV2{}, // TODO(mafa): pass real publisher when available
 			signers:         make(map[types.NodeID]*signing.EdSigner),
 		},
 
