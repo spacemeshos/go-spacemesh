ffbe1de8f554871bb8bb703cbc7aa162c4480c04
Ivan Shvedunov <ivan4th@gmail.com>
2025-01-03 18:04:48 +0400

    sql: fix goroutine leak

--- sql/database.go
+++ sql/database.go
@@ -817,14 +817,12 @@ func (db *sqliteDatabase) WithConnection(ctx context.Context, exec func(Executor
 		return ErrClosed
 	}
 	conCtx, cancel := context.WithCancel(ctx)
+	defer cancel()
 	conn := db.getConn(conCtx)
 	if conn == nil {
 		return ErrNoConnection
 	}
-	defer func() {
-		cancel()
-		db.pool.Put(conn)
-	}()
+	defer db.pool.Put(conn)
 	return exec(&sqliteConn{queryCache: db.queryCache, db: db, conn: conn})
 }
 
