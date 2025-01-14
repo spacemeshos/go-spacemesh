b467637c3922d86f5084aa1ceed9330c3d4aec25
Kacper Sawicki <kacper@ksawicki.pl>
2025-01-14 16:22:44 +0100

    Fix builder

--- sql/builder/builder.go
+++ sql/builder/builder.go
@@ -120,14 +120,14 @@ func FilterFrom(operations Operations) string {
 					queryBuilder.WriteString(fmt.Sprintf(" %s", op.GroupOperator))
 				}
 				if groupOp.Token == In {
-					values, ok := groupOp.Value.([][]byte)
-					if !ok {
-						panic("value for 'In' token must be a slice of []byte")
-					}
-					params := make([]string, len(values))
-					for j := range values {
-						params[j] = fmt.Sprintf("?%d", bindIndex)
-						bindIndex++
+					var params []string
+					switch values := op.Value.(type) {
+					case [][]byte:
+						params = buildInParams(len(values), &bindIndex)
+					case []int32:
+						params = buildInParams(len(values), &bindIndex)
+					default:
+						panic("value for 'In' token must be a slice of []byte or []int32")
 					}
 					fmt.Fprintf(&queryBuilder, " %s%s %s (%s)", groupOp.Prefix, groupOp.Field, groupOp.Token,
 						strings.Join(params, ", "))
@@ -142,19 +142,17 @@ func FilterFrom(operations Operations) string {
 
 		switch op.Token {
 		case In:
+			var params []string
 			switch values := op.Value.(type) {
 			case [][]byte:
+				params = buildInParams(len(values), &bindIndex)
 			case []int32:
-				params := make([]string, len(values))
-				for j := range values {
-					params[j] = fmt.Sprintf("?%d", bindIndex)
-					bindIndex++
-				}
-				fmt.Fprintf(&queryBuilder, " %s%s %s (%s)", op.Prefix, op.Field,
-					op.Token, strings.Join(params, ", "))
+				params = buildInParams(len(values), &bindIndex)
 			default:
 				panic("value for 'In' token must be a slice of []byte or []int32")
 			}
+			fmt.Fprintf(&queryBuilder, " %s%s %s (%s)", op.Prefix, op.Field,
+				op.Token, strings.Join(params, ", "))
 		case IsNotNull:
 			fmt.Fprintf(&queryBuilder, " %s%s %s", op.Prefix, op.Field, op.Token)
 		default:
@@ -212,4 +210,13 @@ func bindValue(stmt *sql.Statement, bindIndex int, value any) int {
 	}
 
 	return bindIndex
+}
+
+func buildInParams(valuesLen int, bindIndex *int) []string {
+	params := make([]string, valuesLen)
+	for j := 0; j < valuesLen; j++ {
+		params[j] = fmt.Sprintf("?%d", *bindIndex)
+		*bindIndex++
+	}
+	return params
 }
