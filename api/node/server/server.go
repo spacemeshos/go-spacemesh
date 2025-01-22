eb062962f7aad192f3254b4357b8668199f9049e
Bartosz Różański <bartek.roza@gmail.com>
2025-01-22 15:32:54 +0100

    replace switch with if

--- api/node/server/server.go
+++ api/node/server/server.go
@@ -189,8 +189,7 @@ func (s *Server) PostPublishProtocol(
 	}
 
 	protocol := string(request.Protocol)
-	switch protocol {
-	case "hare3":
+	if protocol == "hare3" {
 		// Revert protocol change (avoiding slashes) done on the client side.
 		// TODO: hare3 takes that from configuration what also should be done
 		// there instead of using the default value
