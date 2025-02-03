00e4d8533c0117c39e72341203f7f8d04f4f2ede
Jedrzej Nowak <pigmej@gmail.com>
2025-02-03 13:02:03 +0100

    added test

---
+++ api/grpcserver/v2beta1/smeshing_test.go
@@ -0,0 +1,31 @@
+package v2beta1
+
+import (
+	"context"
+	"testing"
+
+	spacemeshv2beta1 "github.com/spacemeshos/api/release/go/spacemesh/v2beta1"
+	"github.com/stretchr/testify/require"
+)
+
+func TestSmeshingService(t *testing.T) {
+	ctx := context.Background()
+	svc := NewSmeshingService(testVersion, testCommit)
+	cfg, cleanup := launchServer(t, svc)
+	t.Cleanup(cleanup)
+
+	conn := dialGrpc(t, cfg)
+	client := spacemeshv2beta1.NewSmeshingServiceClient(conn)
+
+	t.Run("smeshing version", func(t *testing.T) {
+		version, err := client.Version(ctx, &spacemeshv2beta1.SmeshingVersionRequest{})
+		require.NoError(t, err)
+		require.Equal(t, testVersion, version.Version)
+	})
+
+	t.Run("smeshing build", func(t *testing.T) {
+		build, err := client.Build(ctx, &spacemeshv2beta1.SmeshingBuildRequest{})
+		require.NoError(t, err)
+		require.Equal(t, testCommit, build.Build)
+	})
+}
