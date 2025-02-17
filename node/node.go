5f06fb7403713166c8af0303e16877e7f017b36c
Bartosz Różański <bartek.roza@gmail.com>
2025-02-17 17:27:44 +0000

    remove unused TimeConfigValues global (#6731)
    ## Motivation
    
    `TimeConfigValues` global is unused.

--- node/node.go
+++ node/node.go
@@ -90,7 +90,6 @@ import (
 	"github.com/spacemeshos/go-spacemesh/syncer/malsync"
 	"github.com/spacemeshos/go-spacemesh/system"
 	"github.com/spacemeshos/go-spacemesh/timesync"
-	timeCfg "github.com/spacemeshos/go-spacemesh/timesync/config"
 	"github.com/spacemeshos/go-spacemesh/timesync/peersync"
 	"github.com/spacemeshos/go-spacemesh/tortoise"
 	"github.com/spacemeshos/go-spacemesh/txs"
@@ -520,9 +519,6 @@ func (app *App) Initialize() error {
 			return errors.New("genesis config updated after node initialization")
 		}
 	}
-
-	// override default config in timesync since timesync is using TimeConfigValues
-	timeCfg.TimeConfigValues = app.Config.TIME
 
 	app.setupLogging()
 	app.log.Info("Welcome to Spacemesh. Spacemesh full node is starting...")
