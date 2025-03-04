e415af71d432b9dce2f3e8e03527c9f319263808
Matthias <5011972+fasmat@users.noreply.github.com>
2025-03-04 21:24:24 +0000

    Fix failing test

--- genvm/core/staged_cache_test.go
+++ genvm/core/staged_cache_test.go
@@ -20,7 +20,7 @@ func TestCacheGetCopies(t *testing.T) {
 	accountCopy, err := ss.Get(address)
 	require.NoError(t, err)
 	require.Empty(t, accountCopy.Balance)
-	require.Equal(t, 100, account.Balance)
+	require.Equal(t, uint64(100), account.Balance)
 }
 
 func TestCacheUpdatePreserveOrder(t *testing.T) {
